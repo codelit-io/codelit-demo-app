c62203f28df886a0a250abe441473304918e7ed1
Mo Sharif <mo@mosh-media.com>
2019-10-27 16:46:48 -0700

    Moved Auth logic to another component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Navigation from "../Navigation";
@@ -11,47 +11,22 @@ import Account from "../Account";
 import Admin from "../Admin";
 
 import * as ROUTES from "../../constants/routes";
-import { withFirebase } from "../Firebase";
+import { withAuthentication } from "../Session";
 
-class App extends Component {
-	constructor(props) {
-		super(props);
+const App = () => (
+	<Router>
+		<div>
+			<Navigation />
+			<hr />
+			<Route exact path={ROUTES.LANDING} component={LandingPage} />
+			<Route path={ROUTES.SIGN_UP} component={SignUp} />
+			<Route path={ROUTES.SIGN_IN} component={SignIn} />
+			<Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgot} />
+			<Route path={ROUTES.HOME} component={Home} />
+			<Route path={ROUTES.ACCOUNT} component={Account} />
+			<Route path={ROUTES.ADMIN} component={Admin} />
+		</div>
+	</Router>
+);
 
-		this.state = {
-			authUser: null
-		};
-	}
-
-	componentDidMount() {
-		this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
-			authUser
-				? this.setState({ authUser })
-				: this.setState({ authUser: null });
-		});
-	}
-
-	componentWillUnmount() {
-		this.listener();
-	}
-
-	render() {
-		return (
-			<Router>
-				<div>
-					<Navigation authUser={this.state.authUser} />
-					<hr />
-
-					<Route exact path={ROUTES.LANDING} component={LandingPage} />
-					<Route path={ROUTES.SIGN_UP} component={SignUp} />
-					<Route path={ROUTES.SIGN_IN} component={SignIn} />
-					<Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgot} />
-					<Route path={ROUTES.HOME} component={Home} />
-					<Route path={ROUTES.ACCOUNT} component={Account} />
-					<Route path={ROUTES.ADMIN} component={Admin} />
-				</div>
-			</Router>
-		);
-	}
-}
-
-export default withFirebase(App);
+export default withAuthentication(App);
