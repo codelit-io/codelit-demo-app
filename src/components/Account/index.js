23e38a100f0018a938e0f7cae797f744a9011cca
Mo Sharif <mo@mosh-media.com>
2019-10-27 21:59:17 -0700

    Update index.js

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const Account = () => (
-    <div>
-        <h1>Account</h1>
-    </div>
+import { PasswordForgetForm } from "../PasswordForgot";
+import PasswordChangeForm from "../PasswordChange";
+import { withAuthorization, AuthUserContext } from "../Session";
+
+const AccountPage = () => (
+	<AuthUserContext.Consumer>
+		{authUser => (
+			<div>
+				<h1>Account: {authUser.email}</h1>
+				<PasswordForgetForm />
+				<PasswordChangeForm />
+			</div>
+		)}
+	</AuthUserContext.Consumer>
 );
 
-export default Account;
+const condition = authUser => !!authUser;
+
+export default withAuthorization(condition)(AccountPage);
