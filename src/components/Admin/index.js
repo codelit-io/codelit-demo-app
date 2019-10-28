2808c7ade7fadc8604ddcd2e398ce6aed2439b51
Mo Sharif <mo@mosh-media.com>
2019-10-27 21:59:35 -0700

    Update index.js

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,9 +1,58 @@
-import React from 'react';
+/* Refactor using hooks */
 
-const Admin = () => (
-    <div>
-        <h1>Admin</h1>
-    </div>
-);
+import React, { useState, useEffect } from "react";
 
-export default Admin;
+import { withFirebase } from "../Firebase";
+
+const AdminPage = props => {
+	const [loading, setLoading] = useState(false);
+	const [users, setUsers] = useState([]);
+	const UserList = ({ users }) => (
+		<ul>
+			{users.map(user => (
+				<li key={user.uid}>
+					<p>
+						<strong>ID:</strong> {user.uid}
+					</p>
+					<p>
+						<strong>Email:</strong> {user.email}
+					</p>
+					<p>
+						<strong>Username:</strong> {user.username}
+					</p>
+				</li>
+			))}
+		</ul>
+	);
+	useEffect(() => {
+		setLoading(true);
+		props.firebase.users().on("value", snapshot => {
+			const usersObject = snapshot.val();
+
+			const usersList =
+				usersObject &&
+				Object.keys(usersObject).map(key => ({
+					...usersObject[key],
+					uid: key
+				}));
+
+			setUsers(usersList);
+			setLoading(false);
+		});
+
+		return () => {
+			props.firebase.users().off();
+		};
+	}, [props]);
+	return (
+		<div>
+			<h1>Users</h1>
+
+			{loading && <div>Loading ...</div>}
+
+			<UserList users={users} />
+		</div>
+	);
+};
+
+export default withFirebase(AdminPage);
