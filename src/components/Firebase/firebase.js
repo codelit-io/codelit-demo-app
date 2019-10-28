9f7387c892181d975b4e968846567a2850a3f689
Mo Sharif <mo@mosh-media.com>
2019-10-27 17:59:05 -0700

    Added firebase db instances

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,6 +1,6 @@
-import app from "firebase/app";
-
+import app from "firebase/app";\
 import "firebase/auth";
+import "firebase/database";
 
 const config = {
 	apiKey: process.env.REACT_APP_API_KEY,
@@ -16,6 +16,7 @@ class Firebase {
 		app.initializeApp(config);
 
 		this.auth = app.auth();
+		this.db = app.database();
 	}
 
 	/* Auth API */
