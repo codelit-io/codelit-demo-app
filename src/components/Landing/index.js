7ee12aa99987763968998e64613d21915dbcb789
Mo Sharif <mo@mosh-media.com>
2019-10-27 21:59:32 -0700

    Update index.js

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const LandingPage = () => (
-    <div>
-        <h1>Landing</h1>
-    </div>
+	<div>
+		<h1>
+			Hello React + Firebase{" "}
+			<span role="img" aria-label="heart">
+				{" "}
+				💙
+			</span>
+		</h1>
+	</div>
 );
 
 export default LandingPage;
