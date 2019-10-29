59cb50d9937953ca82342f1c2c5ce07bd883273e
Mo Sharif <mo@mosh-media.com>
2019-10-28 23:09:37 -0700

    Removed hr tag

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -17,7 +17,6 @@ const App = () => (
 	<Router>
 		<div>
 			<Navigation />
-			<hr />
 			<Route exact path={ROUTES.LANDING} component={LandingPage} />
 			<Route path={ROUTES.SIGN_UP} component={SignUp} />
 			<Route path={ROUTES.SIGN_IN} component={SignIn} />
