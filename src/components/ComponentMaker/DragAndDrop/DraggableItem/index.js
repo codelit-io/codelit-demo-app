ad538c7fc24e70c04783eda71ee0d0037f8d950f
Mo Sharif <mo@mosh-media.com>
2019-11-16 22:26:19 -0800

    Updated Draggable item component

---
+++ src/components/ComponentMaker/DragAndDrop/DraggableItem/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Draggable } from "react-beautiful-dnd";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import { Card } from "@material-ui/core";
+import CustomElement from "../../CustomElement";
+
+const useStyles = makeStyles(theme =>
+	createStyles({
+		card: {
+			width: 275,
+			marginBottom: theme.spacing(2),
+			padding: theme.spacing(2)
+		}
+	})
+);
+const DraggableItem = ({ draggableItem, index }) => {
+	const classes = useStyles();
+	return (
+		<Draggable draggableId={draggableItem.id} index={index}>
+			{provided => (
+				<Card
+					className={classes.card}
+					ref={provided.innerRef}
+					{...provided.draggableProps}
+					{...provided.dragHandleProps}
+				>
+					<CustomElement { ...draggableItem }></CustomElement>
+				</Card>
+			)}
+		</Draggable>
+	);
+};
+
+export default DraggableItem;
