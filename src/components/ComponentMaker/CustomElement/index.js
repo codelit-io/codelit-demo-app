0b233e87de8c8d963c58e7c948b4ef0c625efed6
Mo Sharif <mo@mosh-media.com>
2019-11-17 13:54:41 -0800

    Update index.js

--- src/components/ComponentMaker/CustomElement/index.js
+++ src/components/ComponentMaker/CustomElement/index.js
@@ -1,20 +1,22 @@
-import React from "react";
-import { Button, TextField, Input, Switch, Checkbox, Slider, Link } from "@material-ui/core";
+import React, { Suspense } from "react";
 
 const Components = {
-	button: Button,
-	textField: TextField,
-	input: Input,
-	switch: Switch,
-	checkbox: Checkbox,
-	slider: Slider,
-	link: Link,
-
+	button: React.lazy(() => import("@material-ui/core/Button")),
+	textField: React.lazy(() => import("@material-ui/core/TextField")),
+	input: React.lazy(() => import("@material-ui/core/Input")),
+	switch: React.lazy(() => import("@material-ui/core/Switch")),
+	checkbox: React.lazy(() => import("@material-ui/core/Checkbox")),
+	slider: React.lazy(() => import("@material-ui/core/Slider")),
+	link: React.lazy(() => import("@material-ui/core/Link")),
 };
 
 const CustomElement = props => {
 	const CustomElement = Components[props.content];
-	return <CustomElement>{props.content}</CustomElement>;
+	return (
+		<Suspense fallback={<div>Loading ...</div>}>
+			<CustomElement>{props.content}</CustomElement>
+		</Suspense>
+	);
 };
 
 export default CustomElement;
