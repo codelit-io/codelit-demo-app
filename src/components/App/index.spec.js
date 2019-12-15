62a64e8b355958f8b2e1ddb5fbc2ca819029a8e5
Mo Sharif <mo@mosh-media.com>
2019-12-14 23:51:35 -0800

    Remove withAuth

--- src/components/App/index.spec.js
+++ src/components/App/index.spec.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './index.js';
-import { withAuthentication } from "../Session";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
