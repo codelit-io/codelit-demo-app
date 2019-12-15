4f2848035c0594476c9ec95f8a03361f9908f9db
Mo Sharif <mo@mosh-media.com>
2019-12-14 23:51:47 -0800

    Update index.js

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 import { withFirebase } from "../Firebase";
-import { Button } from "@material-ui/core";
+
+import { Button } from '@material-ui/core';
 
 const SingOutButton = ({ firebase }) => (
 	<Button type="button" onClick={firebase.signOut}>
