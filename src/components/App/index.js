7fbeba477167aefc7bb1f2529762116af40f3230
Mo Sharif <mo@mosh-media.com>
2019-12-14 17:59:05 -0800

    Updated App routing with Switch

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Navigation from "../Navigation";
 import LandingPage from "../Landing";
@@ -14,19 +14,23 @@ import * as ROUTES from "../../constants/routes";
 import { withAuthentication } from "../Session";
 import Container from "@material-ui/core/Container";
 import { Project } from "../Project";
+import { BuildProject } from "../Project/BuildProject";
 
 const App = () => (
 	<Router>
 		<Navigation />
 		<Container maxWidth="xl" className="container">
-			<Route exact path={ROUTES.LANDING} component={LandingPage} />
-			<Route path={ROUTES.SIGN_UP} component={SignUp} />
-			<Route path={ROUTES.SIGN_IN} component={SignIn} />
-			<Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgot} />
-			<Route path={ROUTES.HOME} component={Home} />
-			<Route path={ROUTES.ACCOUNT} component={Account} />
-			<Route path={ROUTES.ADMIN} component={Admin} />
-			<Route path={ROUTES.NEW_PROJECT} component={Project} />
+			<Switch>
+				<Route exact path={ROUTES.LANDING} component={LandingPage} />
+				<Route path={ROUTES.SIGN_UP} component={SignUp} />
+				<Route path={ROUTES.SIGN_IN} component={SignIn} />
+				<Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgot} />
+				<Route path={ROUTES.HOME} component={Home} />
+				<Route path={ROUTES.ACCOUNT} component={Account} />
+				<Route path={ROUTES.ADMIN} component={Admin} />
+				<Route exact path={ROUTES.NEW_PROJECT} component={Project}/>
+				<Route path={ROUTES.NEW_PROJECT + "/:type"} component={BuildProject} />
+			</Switch>
 			{/* <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
 		</Container>
 	</Router>
