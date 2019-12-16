93cabedf9274bad667530ec56aabf0ebc0ef5c6c
Mo Sharif <mo@mosh-media.com>
2019-12-15 21:49:45 -0800

    Update index.js

--- src/components/Project/BuildProject/Component/index.js
+++ src/components/Project/BuildProject/Component/index.js
@@ -2,8 +2,8 @@ import React, { Suspense } from "react";
 
 export const Component = props => {
     const Component = React.lazy(() => import(`../../../../library/components/${props.type}`))
-    debugger
-	return (
+
+    return (
 		<Suspense fallback={<div>Loading...</div>}>
 			<Component {...props}></Component>
 		</Suspense>
