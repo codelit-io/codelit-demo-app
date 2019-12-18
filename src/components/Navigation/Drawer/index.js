e40a2f28f11e188c992e82de2608e3b8b1080390
Mo Sharif <mo@mosh-media.com>
2019-12-17 22:23:54 -0800

    Create index.js

---
+++ src/components/Navigation/Drawer/index.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { AuthUserContext } from "../../Session";
+import { IconButton, SwipeableDrawer, makeStyles } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import NavigationAuth from "../NavigationAuth";
+import NavigationNonAuth from "../NavigationNonAuth";
+
+const useStyles = makeStyles(theme => ({
+	menuButton: {
+		marginRight: theme.spacing(2)
+	}
+}));
+
+const Drawer = () => {
+	const [state, setState] = useState({
+		isDrawerOpen: false
+	});
+
+	const toggleDrawer = isDrawerOpen => {
+		setState({ ...state, isDrawerOpen });
+	};
+
+	const classes = useStyles();
+
+	return (
+		<div>
+			<IconButton
+				edge="start"
+				className={classes.menuButton}
+				color="inherit"
+				aria-label="open drawer"
+				onClick={() => toggleDrawer(true)}
+			>
+				<MenuIcon />
+			</IconButton>
+			<SwipeableDrawer
+				anchor="left"
+				open={state.isDrawerOpen}
+				onClose={() => toggleDrawer(false)}
+				onOpen={() => toggleDrawer(true)}
+			>
+				<AuthUserContext.Consumer>
+					{authUser =>
+						authUser ? (
+							<div
+								onClick={() => toggleDrawer(false)}
+								onKeyDown={() => toggleDrawer(false)}
+							>
+								<NavigationAuth />
+							</div>
+						) : (
+							<div
+								onClick={() => toggleDrawer(false)}
+								onKeyDown={() => toggleDrawer(false)}
+							>
+								<NavigationNonAuth />
+							</div>
+						)
+					}
+				</AuthUserContext.Consumer>
+			</SwipeableDrawer>
+		</div>
+	);
+};
+
+export default Drawer;
