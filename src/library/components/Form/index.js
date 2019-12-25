221bfdf270ca098535f0a71dde39fea9a40c341e
Mo Sharif <mo@mosh-media.com>
2019-12-25 08:55:14 -0700

    Update index.js

--- src/library/components/Form/index.js
+++ src/library/components/Form/index.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Button, FormControl, InputLabel, Input, FormHelperText } from "@material-ui/core";
 
-const Form = ({ type }) => {
-	return <h1>{type}</h1>;
+const Form = ({onClick, type}) => {
+	return (
+		<form>
+			<h1>{type}</h1>
+			<FormControl>
+				<InputLabel htmlFor="my-input">Email</InputLabel>
+				<Input id="my-input" aria-describedby="my-helper-text" />
+				<FormHelperText id="my-helper-text">
+					Your personal email address
+				</FormHelperText>
+			</FormControl>
+			<Button onClick={() => onClick()}>Save</Button>
+		</form>
+	);
 };
 
 export default Form;
