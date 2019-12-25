5bef01c056e396ae3877b47caaccaccc31c7c9cc
Mo Sharif <mo@mosh-media.com>
2019-12-25 08:55:13 -0700

    Update index.js

--- src/containers/Project/BuildProject/Component/index.js
+++ src/containers/Project/BuildProject/Component/index.js
@@ -1,11 +1,12 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useState } from "react";
 
 export const Component = props => {
     const Component = React.lazy(() => import(`../../../../library/components/${props.type}`))
-
+	const [state, setState] = useState({status: ""});
     return (
 		<Suspense fallback={<div>Loading...</div>}>
-			<Component {...props}></Component>
+			<Component onClick={() => setState({status: "Saved!"})} {...props}></Component>
+			<p>{state.status}</p>
 		</Suspense>
 	);
 };
