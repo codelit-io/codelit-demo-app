3bd06fd12f581aff7a34078a7dcda463408e39ee
Mo Sharif <mo@mosh-media.com>
2019-12-29 20:07:06 -0800

    Delete index.js

--- src/containers/Project/BuildProject/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Component } from "./Component";
-
-export const BuildProject = ({ match }) => {
-    
-	return (
-        <Component {...match.params}></Component>
-    );
-};
