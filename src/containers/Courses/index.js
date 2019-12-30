67f8fff5b255f5e9e26cddec1b6a1f07be378612
Mo Sharif <mo@mosh-media.com>
2019-12-29 20:06:55 -0800

    Create index.js

---
+++ src/containers/Courses/index.js
@@ -0,0 +1,55 @@
+import React from "react";
+import MoshCard from "../../components/shared/MoshCard";
+import { Grid, Typography } from "@material-ui/core";
+import * as ROUTES from "../../constants/routes";
+
+const Courses = ({ match }) => {
+	const icons = [
+		{
+			label: "Html",
+			desc: "Elements on web pages",
+			img: "",
+			url: ROUTES.HTML
+		},
+		{
+			label: "Css",
+			desc: "Styling to elements",
+			img: "",
+			url: ROUTES.CSS
+		},
+		{
+			label: "JavaScript",
+			desc: "Functionality to elements",
+			img: "",
+			url: ROUTES.JAVASCRIPT
+		},
+		{
+			label: "React",
+			desc: "Web Apps JS framework",
+			img: "",
+			url: ROUTES.REACT
+		},
+		{
+			label: "Angular",
+			desc: "Web Apps JS framework",
+			img: "",
+			url: ROUTES.ANGULAR
+		}
+	];
+	return (
+		<>
+			<Typography variant="h4" gutterBottom>
+				{match.params.course}
+			</Typography>
+			<Grid container spacing={3}>
+				{icons.map((icon, index) => (
+					<Grid key={index} item sm={6} md={3} xs={12}>
+						<MoshCard icon={icon}></MoshCard>
+					</Grid>
+				))}
+			</Grid>
+		</>
+	);
+};
+
+export default Courses;
