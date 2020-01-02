b17da047ff13246ba4ced0c027f8993ddead0bd2
Mo Sharif <mo@mosh-media.com>
2020-01-01 21:13:36 -0800

    Update index.js

--- src/library/components/Html/index.js
+++ src/library/components/Html/index.js
@@ -6,29 +6,42 @@ import {
 	Input,
 	FormHelperText
 } from "@material-ui/core";
+import Iframe from "react-iframe";
+import useStyles from "./styles";
 
 const Html = ({ onClick, topic }) => {
+	const classes = useStyles();
 	return (
-		<form>
-			<h1>{topic}</h1>
-			<pre>
-				<code>{"<p> I am a paragraph </p>"}</code>
-			</pre>
-			<pre>
-				<code>{"<h1> I am a top level heading </h1>"}</code>
-			</pre>
-			<pre>
-				<code>{"<h2> I am a second level heading </h2>"}</code>
-			</pre>
-			<FormControl>
-				<InputLabel htmlFor="my-input">Email</InputLabel>
-				<Input id="my-input" aria-describedby="my-helper-text" />
-				<FormHelperText id="my-helper-text">
-					Your personal email address
-				</FormHelperText>
-			</FormControl>
-			<Button onClick={() => onClick()}>Save</Button>
-		</form>
+		<>
+			<form>
+				<h1>{topic}</h1>
+				<pre>
+					<code>{"<p> I am a paragraph </p>"}</code>
+				</pre>
+				<pre>
+					<code>{"<h1> I am a top level heading </h1>"}</code>
+				</pre>
+				<pre>
+					<code>{"<h2> I am a second level heading </h2>"}</code>
+				</pre>
+				<FormControl>
+					<InputLabel htmlFor="my-input">Email</InputLabel>
+					<Input id="my-input" aria-describedby="my-helper-text" />
+					<FormHelperText id="my-helper-text">
+						Your personal email address
+					</FormHelperText>
+				</FormControl>
+				<Button onClick={() => onClick()}>Save</Button>
+			</form>
+			<div className={classes.card}>
+				<Iframe
+					url="https://codesandbox.io/embed/brave-galois-5b0rb?fontsize=12&hidenavigation=1"
+					width="100%"
+					height="100%"
+					className={classes.iframe}
+				/>
+			</div>
+		</>
 	);
 };
 
