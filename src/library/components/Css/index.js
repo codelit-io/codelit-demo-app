be409cb4e0d54ed4969ec696dc5b6d692167188c
Mo Sharif <mo@mosh-media.com>
2020-01-02 21:35:15 -0800

    Update index.js

--- src/library/components/Css/index.js
+++ src/library/components/Css/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import PageHeader from "../../../components/shared/PageHeader";
 
-export const Css = ({ topic }) => {
+export const Css = ({ course, topic }) => {
 	return (
 		<>
-			<PageHeader title={topic}></PageHeader>
+			<PageHeader title={topic} course={course}></PageHeader>
 			<pre>
 				<code>{"<p class='paragraph'> I am a paragraph </p>"}</code>
 			</pre>
