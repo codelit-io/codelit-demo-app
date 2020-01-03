5e4b35b1b8be43af4562155ae0a8f773dbf7072f
Mo Sharif <mo@mosh-media.com>
2020-01-03 11:44:22 -0800

    Add fade in animation to Mo Card

--- src/components/shared/MoCard/index.js
+++ src/components/shared/MoCard/index.js
@@ -6,35 +6,27 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import { Link } from "react-router-dom";
 import useStyles from "./styles";
+import Fade from "@material-ui/core/Fade";
 
 const MoCard = ({ icon }) => {
 	const classes = useStyles();
 	return (
 		<Link to={(icon && icon.url) || ""} className={classes.link}>
-			<Card className={classes.card}>
-				<CardActionArea className={classes.content}>
-					{icon && icon.img && (
-						<CardMedia
-							className={classes.img}
-							image={icon.img}
-							title={icon.label}
-						/>
-					)}
-					<CardContent>
-						<Typography gutterBottom variant="h5" component="h2">
-							{icon && icon.label ? icon.label : "No Name"}
-						</Typography>
-						<Typography
-							className="desc"
-							variant="body2"
-							color="textSecondary"
-							component="p"
-						>
-							{icon && icon.desc}
-						</Typography>
-					</CardContent>
-				</CardActionArea>
-			</Card>
+			<Fade timeout={{ enter: 1000 }} in={true}>
+				<Card className={classes.card}>
+					<CardActionArea className={classes.content}>
+						{icon && icon.img && <CardMedia className={classes.img} image={icon.img} title={icon.label} />}
+						<CardContent>
+							<Typography gutterBottom variant="h5" component="h2">
+								{icon && icon.label ? icon.label : "No Name"}
+							</Typography>
+							<Typography className="desc" variant="body2" color="textSecondary" component="p">
+								{icon && icon.desc}
+							</Typography>
+						</CardContent>
+					</CardActionArea>
+				</Card>
+			</Fade>
 		</Link>
 	);
 };
