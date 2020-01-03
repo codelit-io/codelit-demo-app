a7216e6edd93020347cb30b5ada5ffdd61d6d774
Mo Sharif <mo@mosh-media.com>
2020-01-02 21:35:27 -0800

    Create styles.js

---
+++ src/components/shared/PageHeader/styles.js
@@ -0,0 +1,14 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+	linkText: {
+		color: "#383c40",
+		textTransform: "capitalize",
+		textDecoration: "none"
+    },
+    header: {
+        marginBottom: "40px"
+    }
+}));
+
+export default useStyles;
