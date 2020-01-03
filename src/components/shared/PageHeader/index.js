0531bcb3e565981b249d3089a156215d9bfff55a
Mo Sharif <mo@mosh-media.com>
2020-01-03 11:44:38 -0800

    Add fade animation to PageHeader

--- src/components/shared/PageHeader/index.js
+++ src/components/shared/PageHeader/index.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Typography, Box } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Fade from "@material-ui/core/Fade";
 import { Link } from "react-router-dom";
 import * as ROUTES from "../../../constants/routes";
 import useStyles from "./styles";
@@ -7,21 +9,16 @@ import useStyles from "./styles";
 const PageHeader = ({ title, course }) => {
 	const classes = useStyles();
 	return (
-		<Typography
-			variant="h4"
-			gutterBottom
-			className={classes.header}
-		>
-			<Box fontWeight="fontWeightLight" className={classes.linkText}>
-				<Link
-					to={`${ROUTES.LEARN.path}/${course}`}
-					className={classes.linkText}
-				>
-					{course && <span>{course} / </span>}
-				</Link>{" "}
-				{title}
-			</Box>
-		</Typography>
+		<Fade in={true} timeout={{ enter: 1000 }}>
+			<Typography variant="h4" gutterBottom className={classes.header}>
+				<Box fontWeight="fontWeightLight" className={classes.linkText}>
+					<Link to={`${ROUTES.LEARN.path}/${course}`} className={classes.linkText}>
+						{course && <span>{course} / </span>}
+					</Link>{" "}
+					{title}
+				</Box>
+			</Typography>
+		</Fade>
 	);
 };
 
