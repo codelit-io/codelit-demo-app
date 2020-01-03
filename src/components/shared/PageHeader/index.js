f2abd535d0d5972d8d03a6dcf3e7b247110595e8
Mo Sharif <mo@mosh-media.com>
2020-01-02 21:35:08 -0800

    Update index.js

--- src/components/shared/PageHeader/index.js
+++ src/components/shared/PageHeader/index.js
@@ -1,10 +1,26 @@
 import React from "react";
 import { Typography, Box } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import * as ROUTES from "../../../constants/routes";
+import useStyles from "./styles";
 
-const PageHeader = ({ title }) => {
+const PageHeader = ({ title, course }) => {
+	const classes = useStyles();
 	return (
-		<Typography variant="h4" gutterBottom style={{ marginBottom: "40px", color: "#383c40" }}>
-			<Box fontWeight="fontWeightLight">{title}</Box>
+		<Typography
+			variant="h4"
+			gutterBottom
+			className={classes.header}
+		>
+			<Box fontWeight="fontWeightLight" className={classes.linkText}>
+				<Link
+					to={`${ROUTES.LEARN.path}/${course}`}
+					className={classes.linkText}
+				>
+					{course && <span>{course} / </span>}
+				</Link>{" "}
+				{title}
+			</Box>
 		</Typography>
 	);
 };
