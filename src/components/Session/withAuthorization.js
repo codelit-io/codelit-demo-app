d49b7b0c148759a427214df470dfbbf97a640581
Mo Sharif <mo@mosh-media.com>
2020-01-02 19:59:15 -0800

    Update withAuthorization.js

--- src/components/Session/withAuthorization.js
+++ src/components/Session/withAuthorization.js
@@ -11,7 +11,7 @@ const withAuthorization = condition => Component => {
 		componentDidMount() {
 			this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
 				if (!condition(authUser)) {
-					this.props.history.push(ROUTES.SIGN_IN);
+					this.props.history.push(ROUTES.SIGN_IN.path);
 				}
 			});
 		}
