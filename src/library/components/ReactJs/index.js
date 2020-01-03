717dddc28bf5075837abe17a1048519331734746
Mo Sharif <mo@mosh-media.com>
2020-01-02 21:35:14 -0800

    Update index.js

--- src/library/components/ReactJs/index.js
+++ src/library/components/ReactJs/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import PageHeader from "../../../components/shared/PageHeader";
 
-const ReactJs = ({ topic }) => {
-	return <PageHeader title={topic}></PageHeader>;
+const ReactJs = ({ topic, course }) => {
+	return <PageHeader title={topic} course={course}></PageHeader>
 };
 
 export default ReactJs;
