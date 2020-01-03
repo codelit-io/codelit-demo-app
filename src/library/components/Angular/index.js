ad5fc9b13147e50609437fa8381cf206efc60958
Mo Sharif <mo@mosh-media.com>
2020-01-02 21:35:18 -0800

    Update index.js

--- src/library/components/Angular/index.js
+++ src/library/components/Angular/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import PageHeader from "../../../components/shared/PageHeader";
 
-const Angular = ({ topic }) => {
-	return <PageHeader title={topic}></PageHeader>;
+const Angular = ({ topic , course}) => {
+	return <PageHeader title={topic} course={course}></PageHeader>;
 };
 
 export default Angular;
