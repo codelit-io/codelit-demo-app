6b25bfd8cda7c8027e5eeb3b1be596b70155554a
Mo Sharif <mo@mosh-media.com>
2020-01-04 12:42:11 -0800

    Update styles.js

--- src/library/components/Html/styles.js
+++ src/library/components/Html/styles.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles(theme => ({
 	iframe: {
 		borderRadius: theme.shape.borderRadius,
-		border: "1px solid black"
+		border: "none"
 	},
 	card: {
 		boxShadow: "0 0 51px 0 rgba(0,0,0,.08), 0 6px 18px 0 rgba(0,0,0,.05)",
