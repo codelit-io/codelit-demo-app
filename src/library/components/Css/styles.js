a471234c8551e6bb532c1073c28616066a04b8fb
Mo Sharif <mo@mosh-media.com>
2020-01-04 12:42:08 -0800

    Create styles.js

---
+++ src/library/components/Css/styles.js
@@ -0,0 +1,18 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+	iframe: {
+		borderRadius: theme.shape.borderRadius,
+		border: "none"
+	},
+	card: {
+		boxShadow: "0 0 51px 0 rgba(0,0,0,.08), 0 6px 18px 0 rgba(0,0,0,.05)",
+		height: "550px",
+		"&:hover": {
+			boxShadow: "0 0 114px 0 rgba(0,0,0,.08), 0 30px 25px 0 rgba(0,0,0,.05)",
+			background: "white"
+		}
+	}
+}));
+
+export default useStyles;
