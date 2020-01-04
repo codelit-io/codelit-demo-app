f94d2c6eedfe1f1d556677d4a700a5b91686ea4c
Mo Sharif <mo@mosh-media.com>
2020-01-04 12:42:06 -0800

    Update index.js

--- src/library/components/Css/index.js
+++ src/library/components/Css/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 import PageHeader from "../../../components/shared/PageHeader";
+import Typography from "@material-ui/core/Typography"
+import Iframe from "react-iframe";
+import useStyles from "./styles";
 
-export const Css = ({ course, topic }) => {
+const Css = ({ course, topic }) => {
+	const classes = useStyles();
 	return (
 		<>
 			<PageHeader title={topic} course={course}></PageHeader>
-			<pre>
-				<code>{"<p class='paragraph'> I am a paragraph </p>"}</code>
-			</pre>
-			<pre>
-				<code>{".paragraph {}"}</code>
-			</pre>
+			<Typography gutterBottom variant="h5" component="h2"> Video Coming soon! </Typography>
+			<div className={classes.card}>
+				<Iframe url="https://stackblitz.com/edit/basic-css-moskool?embed=1&file=index.html&hideExplorer=1&hideNavigation=1" width="100%" height="100%" className={classes.iframe} />
+			</div>
 		</>
 	);
 };
