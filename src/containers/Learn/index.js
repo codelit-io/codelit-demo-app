b8f9dd996d3c7fef21906ea5c87188f3b3eb1e58
Mo Sharif <mo@mosh-media.com>
2020-01-03 16:54:37 -0800

    Update index.js

--- src/containers/Learn/index.js
+++ src/containers/Learn/index.js
@@ -3,25 +3,26 @@ import MoCard from "../../components/shared/MoCard";
 import { Grid } from "@material-ui/core";
 import PageHeader from "../../components/shared/PageHeader";
 import * as ROUTES from "../../constants/routes";
+import Typography from "@material-ui/core/Typography";
 
 const Learn = ({ match }) => {
 	const icons = [
 		{
 			label: "Front-end",
 			desc: "Learn HTML, CSS, JS and JS Frameworks",
-			img: "",
+			img: "https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/icons%2F012-startup.png?alt=media&token=15a19307-46ea-4995-87d6-2cf060098b3d",
 			url: ROUTES.FRONTEND.path
 		},
 		{
 			label: "Back-end",
 			desc: "Learn nodeJs, mongoDB and Firebase cloud",
-			img: "",
+			img: "https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/icons%2F010-creativity.png?alt=media&token=db80e5d6-710d-4005-a4a1-e42c756a6387",
 			url: ROUTES.BACKEND.path
 		},
 		{
 			label: "DevOps",
 			desc: "Learn Docker and other fun stuff",
-			img: "",
+			img: "https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/icons%2F029-wearable%20technogy.png?alt=media&token=bc20e7d4-e9b7-4674-8b5b-36bb761e693a",
 			url: ROUTES.DEVOPS.path
 		}
 	];
@@ -35,6 +36,14 @@ const Learn = ({ match }) => {
 					</Grid>
 				))}
 			</Grid>
+			{
+				<Typography variant="subtitle1">
+					Icons made by{" "}
+					<a href="https://www.flaticon.com/authors/wanicon" title="wanicon">
+						wanicon
+					</a>
+				</Typography>
+			}
 		</>
 	);
 };
