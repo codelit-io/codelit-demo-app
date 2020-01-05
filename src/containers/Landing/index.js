9054c1e408fa981a1ac54e939ebb90979c72272f
Mo Sharif <mo@mosh-media.com>
2020-01-04 22:03:08 -0800

    Update index.js

--- src/containers/Landing/index.js
+++ src/containers/Landing/index.js
@@ -1,45 +1,45 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
 import MoCard from "../../components/shared/MoCard";
 import PageHeader from "../../components/shared/PageHeader";
 import * as ROUTES from "../../constants/routes";
 import Typography from "@material-ui/core/Typography";
 
 const LandingPage = () => {
-	const icons = [
+	const topics = [
 		{
 			label: "Start Learning",
 			desc: "Learn anything coding",
 			img: "https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/icons%2F006-science.png?alt=media&token=9942d7dd-19c9-497e-8597-4cb59df6cdc7",
-			url: ROUTES.LEARN.path
+			url: ROUTES.LEARN.path,
 		},
 		{
 			label: "My Lessons",
 			desc: "All in one place",
 			img: "https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/icons%2F026-education.png?alt=media&token=59b529d9-14ae-4f05-a5eb-c758ce55b2a1",
-			url: ROUTES.LEARN.path
+			disable: true,
 		},
 		{
 			label: "My Team",
 			desc: "Say Hi!",
 			img: "https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/icons%2F009-collaboration.png?alt=media&token=a7e1e9e8-8b2b-4a32-86ad-0302de87407e",
-			url: ROUTES.LEARN.path
+			disable: true
 		}
 	];
-	// const list = ["input", "button", "switch", "checkbox", "slider", "link"];
+
 	return (
 		<>
 			<PageHeader title={ROUTES.LANDING.title}></PageHeader>
 			<Grid container spacing={3}>
-				{icons.map((icon, index) => (
+				{topics.map((topic, index) => (
 					<Grid key={index} item sm={6} md={3} xs={12}>
-						<MoCard icon={icon}></MoCard>
+						<MoCard topic={topic}></MoCard>
 					</Grid>
 				))}
 			</Grid>
 			<Typography variant="subtitle1">
 				Icons made by{" "}
-				<a href="https://www.flaticon.com/authors/wanicon" title="wanicon">
+				<a href="https://www.flaticon.com/authors/wanicon" title="wanicon" style={{color: "#3a9fff"}}>
 					wanicon
 				</a>
 			</Typography>
