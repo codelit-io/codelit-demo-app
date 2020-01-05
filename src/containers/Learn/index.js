43d6618d327362e74d47ca8fc7e24863705521f6
Mo Sharif <mo@mosh-media.com>
2020-01-04 22:03:01 -0800

    Update index.js

--- src/containers/Learn/index.js
+++ src/containers/Learn/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import MoCard from "../../components/shared/MoCard";
-import { Grid } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
 import PageHeader from "../../components/shared/PageHeader";
 import * as ROUTES from "../../constants/routes";
 import Typography from "@material-ui/core/Typography";
 
 const Learn = ({ match }) => {
-	const icons = [
+	const topics = [
 		{
 			label: "Front-end",
 			desc: "Learn HTML, CSS, JS and JS Frameworks",
@@ -17,29 +17,29 @@ const Learn = ({ match }) => {
 			label: "Back-end",
 			desc: "Learn nodeJs, mongoDB and Firebase cloud",
 			img: "https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/icons%2F010-creativity.png?alt=media&token=db80e5d6-710d-4005-a4a1-e42c756a6387",
-			url: ROUTES.BACKEND.path
+			disable: true,
 		},
 		{
 			label: "DevOps",
 			desc: "Learn Docker and other fun stuff",
 			img: "https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/icons%2F029-wearable%20technogy.png?alt=media&token=bc20e7d4-e9b7-4674-8b5b-36bb761e693a",
-			url: ROUTES.DEVOPS.path
+			disable: true,
 		}
 	];
 	return (
 		<>
 			<PageHeader title={ROUTES.LEARN.title}></PageHeader>
 			<Grid container spacing={3}>
-				{icons.map((icon, index) => (
+				{topics.map((topic, index) => (
 					<Grid key={index} item sm={6} md={3} xs={12}>
-						<MoCard icon={icon}></MoCard>
+						<MoCard topic={topic}></MoCard>
 					</Grid>
 				))}
 			</Grid>
 			{
 				<Typography variant="subtitle1">
 					Icons made by{" "}
-					<a href="https://www.flaticon.com/authors/wanicon" title="wanicon">
+					<a href="https://www.flaticon.com/authors/wanicon" title="wanicon" style={{color: "#3a9fff"}}>
 						wanicon
 					</a>
 				</Typography>
