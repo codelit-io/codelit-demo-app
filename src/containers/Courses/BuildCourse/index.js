7f531182fd33a311f07c63d729ff477d30100bbd
Mo Sharif <mo@mosh-media.com>
2020-01-04 22:03:15 -0800

    Update index.js

--- src/containers/Courses/BuildCourse/index.js
+++ src/containers/Courses/BuildCourse/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import * as ROUTES from "../../../constants/routes";
 import { Component } from "./Component";
-import { Grid } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
 import MoCard from "../../../components/shared/MoCard";
 
 const BuildCourse = ({ match }) => {
-	const icons = [
+	const topics = [
 		{
 			label: "Html",
 			desc: "Elements on web pages",
@@ -46,9 +46,9 @@ const BuildCourse = ({ match }) => {
 			</Grid>
 			<Grid container spacing={3}>
 				<Grid item xs={12}>
-					{icons.map((icon, index) => (
+					{topics.map((topic, index) => (
 						<Grid key={index} item sm={12} md={12} xs={12}>
-							<MoCard icon={icon}></MoCard>
+							<MoCard topic={topic}></MoCard>
 						</Grid>
 					))}
 				</Grid>
