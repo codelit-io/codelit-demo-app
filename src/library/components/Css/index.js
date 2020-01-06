82c360ee921e19360d081624f78fbc2e887ac9ef
Mo Sharif <mo@mosh-media.com>
2020-01-05 17:46:34 -0800

    Update index.js

--- src/library/components/Css/index.js
+++ src/library/components/Css/index.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { lazy } from "react";
 import PageHeader from "../../../components/shared/PageHeader";
-import Typography from "@material-ui/core/Typography"
-import Iframe from "react-iframe";
 import useStyles from "./styles";
+
+const ReactPlayer = lazy(() => import("react-player"));
 
 const Css = ({ course, topic }) => {
 	const classes = useStyles();
 	return (
 		<>
 			<PageHeader title={topic} course={course}></PageHeader>
-			<Typography gutterBottom variant="h5" component="h2"> Podcast Coming soon! </Typography>
-			<div className={classes.card}>
-				<Iframe url="https://stackblitz.com/edit/basic-css-moskool?embed=1&file=index.html&hideExplorer=1&hideNavigation=1" width="100%" height="100%" className={classes.iframe} />
+			<div className={`${classes.card} ${classes.audioCard}`}>
+				<ReactPlayer
+					url="https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/podcasts%2FtestAudio.mp3?alt=media&token=780760ee-5214-4973-b83a-f951bbb90d1a"
+					playing
+					controls={true}
+					light={false}
+					width="100%"
+					height="50px"
+				/>
+			</div>
+			<div className={`${classes.card} ${classes.playgroundCard}`}>
+				<iframe
+					src="https://stackblitz.com/edit/basic-css-moskool?embed=1&file=index.html&hideExplorer=1&hideNavigation=1"
+					title="basic-css-moskool"
+					width="100%"
+					height="100%"
+					className={classes.iframe}
+				/>
 			</div>
 		</>
 	);
