f995a251e1065cdc73ea77f38c1db96ad7971fa5
Mo Sharif <mo@mosh-media.com>
2020-01-05 23:46:50 -0800

    Create index.js

---
+++ src/library/components/React/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import PageHeader from "../../../components/shared/PageHeader";
+
+const ReactJs = ({ topic, course }) => {
+	return <PageHeader title={topic} course={course}></PageHeader>
+};
+
+export default ReactJs;
