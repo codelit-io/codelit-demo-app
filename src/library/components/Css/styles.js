d5523358012bb1182efba0d61161f288a0f9e0c5
Mo Sharif <mo@mosh-media.com>
2020-01-05 17:46:43 -0800

    Update styles.js

--- src/library/components/Css/styles.js
+++ src/library/components/Css/styles.js
@@ -6,12 +6,21 @@ const useStyles = makeStyles(theme => ({
 		border: "none"
 	},
 	card: {
+		borderRadius: theme.shape.borderRadius,
 		boxShadow: "0 0 51px 0 rgba(0,0,0,.08), 0 6px 18px 0 rgba(0,0,0,.05)",
-		height: "550px",
 		"&:hover": {
-			boxShadow: "0 0 114px 0 rgba(0,0,0,.08), 0 30px 25px 0 rgba(0,0,0,.05)",
-			background: "white"
-		}
+			boxShadow: "0 0 114px 0 rgba(0,0,0,.08), 0 30px 25px 0 rgba(0,0,0,.05)"
+		},
+		backgroundColor: "none",
+		marginBottom: "20px"
+	},
+	audioCard: {
+		backgroundColor: "#f2f3f4",
+		padding: "10px",
+		height: "50px"
+	},
+	playgroundCard: {
+		height: "550px"
 	}
 }));
 
