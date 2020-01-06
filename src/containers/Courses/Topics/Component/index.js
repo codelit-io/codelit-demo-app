60bb9c768e558c043f7cb28d671628b024b577c2
Mo Sharif <mo@mosh-media.com>
2020-01-05 23:13:23 -0800

    Create index.js

---
+++ src/containers/Courses/Topics/Component/index.js
@@ -0,0 +1,14 @@
+import React, { Suspense, useState } from "react";
+import Spinner from "../../../../components/shared/Spinner";
+
+export const Component = props => {
+	/* Replace with better method! */
+    const Component = React.lazy(() => import(`../../../../library/components/${props.topic}`))
+	const [state, setState] = useState({status: ""});
+    return (
+		<Suspense fallback={<Spinner loading={true} color="primary" />}>
+			<Component onClick={() => setState({status: "Saved!"})} {...props}></Component>
+			<p>{state.status}</p>
+		</Suspense>
+	);
+};
