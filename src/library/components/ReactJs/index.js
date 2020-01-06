e7b7ec97075c4f1fa7c10d79f14c954f7e63aa8a
Mo Sharif <mo@mosh-media.com>
2020-01-05 23:46:48 -0800

    Delete index.js

--- src/library/components/ReactJs/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import PageHeader from "../../../components/shared/PageHeader";
-
-const ReactJs = ({ topic, course }) => {
-	return <PageHeader title={topic} course={course}></PageHeader>
-};
-
-export default ReactJs;
