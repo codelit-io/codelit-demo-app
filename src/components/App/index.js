2a631d9639e60d2c5bad9a8dccc42d554721a3ae
Mo Sharif <mo@mosh-media.com>
2020-01-05 17:46:56 -0800

    Update index.js

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,40 +1,57 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-import Navigation from "../Navigation";
-import LandingPage from "../../containers/Landing";
-import SignUp from "../../containers/SignUp";
-import SignIn from "../../containers/SignIn";
-import PasswordForgot from "../PasswordForgot";
-import Home from "../../containers/Home";
-import Account from "../../containers/Account";
-import Admin from "../../containers/Admin";
-
 import * as ROUTES from "../../constants/routes";
-import BuildCourse from "../../containers/Courses/BuildCourse";
-import Container from "@material-ui/core/Container";
-import Courses from "../../containers/Courses";
-import Learn from "../../containers/Learn";
 import { withAuthentication } from "../Session";
+import Spinner from "../../components/shared/Spinner";
+
+const Navigation = lazy(() => import("../Navigation"));
+const LandingPage = lazy(() => import("../../containers/Landing"));
+const SignUp = lazy(() => import("../../containers/SignUp"));
+const SignIn = lazy(() => import("../../containers/SignIn"));
+const PasswordForgot = lazy(() => import("../PasswordForgot"));
+const Home = lazy(() => import("../../containers/Home"));
+const Account = lazy(() => import("../../containers/Account"));
+const Admin = lazy(() => import("../../containers/Admin"));
+const BuildCourse = lazy(() => import("../../containers/Courses/BuildCourse"));
+const Container = lazy(() => import("@material-ui/core/Container"));
+const Courses = lazy(() => import("../../containers/Courses"));
+const Learn = lazy(() => import("../../containers/Learn"));
 
 const App = () => (
 	<Router>
-		<Navigation />
-		<Container maxWidth="xl" className="container">
-			<Switch>
-				<Route exact path={ROUTES.LANDING.path} component={LandingPage} />
-				<Route path={ROUTES.SIGN_UP.path} component={SignUp} />
-				<Route path={ROUTES.SIGN_IN.path} component={SignIn} />
-				<Route path={ROUTES.PASSWORD_FORGET.path} component={PasswordForgot} />
-				<Route path={ROUTES.HOME.path} component={Home} />
-				<Route path={ROUTES.ACCOUNT.path} component={Account} />
-				<Route path={ROUTES.ADMIN.path} component={Admin} />
-				<Route exact path={ROUTES.LEARN.path} component={Learn}/>
-				<Route exact path={ROUTES.LEARN.path + "/:course"} component={Courses}/>
-				<Route exact path={ROUTES.LEARN.path + "/:course/:topic"} component={BuildCourse} />
-				<Route exact path={ROUTES.LEARN.path + "/:course/:topic/:subTopic"} component={BuildCourse} />
-			</Switch>
-		</Container>
+		<Suspense fallback={<Spinner loading={true} color="primary" />}>
+			<Navigation />
+			<Container maxWidth="xl" className="container">
+				<Switch>
+					<Route exact path={ROUTES.LANDING.path} component={LandingPage} />
+					<Route path={ROUTES.SIGN_UP.path} component={SignUp} />
+					<Route path={ROUTES.SIGN_IN.path} component={SignIn} />
+					<Route
+						path={ROUTES.PASSWORD_FORGET.path}
+						component={PasswordForgot}
+					/>
+					<Route path={ROUTES.HOME.path} component={Home} />
+					<Route path={ROUTES.ACCOUNT.path} component={Account} />
+					<Route path={ROUTES.ADMIN.path} component={Admin} />
+					<Route exact path={ROUTES.LEARN.path} component={Learn} />
+					<Route
+						exact
+						path={ROUTES.LEARN.path + "/:course"}
+						component={Courses}
+					/>
+					<Route
+						exact
+						path={ROUTES.LEARN.path + "/:course/:topic"}
+						component={BuildCourse}
+					/>
+					<Route
+						exact
+						path={ROUTES.LEARN.path + "/:course/:topic/:subTopic"}
+						component={BuildCourse}
+					/>
+				</Switch>
+			</Container>
+		</Suspense>
 	</Router>
 );
 
