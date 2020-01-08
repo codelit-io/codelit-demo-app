f0f5b6db39e60ca69f2120598ec5ca86607d404e
Mo Sharif <mo@mosh-media.com>
2020-01-07 20:58:02 -0800

    Update index.js

--- src/containers/Learn/index.js
+++ src/containers/Learn/index.js
@@ -31,7 +31,7 @@ const Learn = ({ match }) => {
 			<PageHeader title={ROUTES.LEARN.title}></PageHeader>
 			<Grid container spacing={3}>
 				{topics.map((topic, index) => (
-					<Grid key={index} item sm={6} md={3} xs={12}>
+					<Grid key={index} item sm={12} md={6} xs={12}>
 						<MoCard topic={topic}></MoCard>
 					</Grid>
 				))}
