892825b90e6b3a91111af1f15c2985a6680adfac
Mo Sharif <mo@mosh-media.com>
2020-01-07 20:58:21 -0800

    Update styles.js

--- src/components/Navigation/styles.js
+++ src/components/Navigation/styles.js
@@ -1,41 +1,18 @@
-import { fade, makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
 	root: {
 		flexGrow: 1
 	},
+	container: {
+		alignItems: "center",
+	},
 	title: {
-		display: "none",
+		textAlign: "center",
+		alignSelf: "center",
 		[theme.breakpoints.up("sm")]: {
 			display: "block"
 		}
-	},
-	search: {
-		flexGrow: 1,
-		position: "relative",
-		borderRadius: theme.shape.borderRadius,
-		backgroundColor: fade(theme.palette.common.white, 0.15),
-		"&:hover": {
-			backgroundColor: fade(theme.palette.common.white, 0.25)
-		},
-		marginLeft: 0,
-		width: "100%",
-		[theme.breakpoints.up("sm")]: {
-			marginLeft: theme.spacing(1),
-			width: "auto"
-		}
-	},
-	searchIcon: {
-		width: theme.spacing(7),
-		height: "100%",
-		position: "absolute",
-		pointerEvents: "none",
-		display: "flex",
-		alignItems: "center",
-		justifyContent: "center"
-	},
-	toolbar: {
-		backgroundColor: "#fff"
 	},
 	inputRoot: {
 		color: "inherit"
@@ -50,6 +27,9 @@ const useStyles = makeStyles(theme => ({
 				width: 200
 			}
 		}
+	},
+	avatar: {
+		float: "right"
 	},
 	sectionDesktop: {
 		display: "none",
