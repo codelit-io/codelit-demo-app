cf7a36deaadffeb908f84601c40a955cebacf3c0
Mo Sharif <mo@mosh-media.com>
2020-01-07 23:48:37 -0800

    Update index.js

--- src/components/shared/PageHeader/index.js
+++ src/components/shared/PageHeader/index.js
@@ -5,13 +5,12 @@ import Fade from "@material-ui/core/Fade";
 import { Link } from "react-router-dom";
 import * as ROUTES from "../../../constants/routes";
 import useStyles from "./styles";
-import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 
 const PageHeader = ({ title, course, topic, subTopic }) => {
 	const classes = useStyles();
 	return (
 		<Fade in={true} timeout={{ enter: 1000 }}>
-			<Typography variant="h5" gutterBottom className={classes.header}>
+			<Typography variant="h4" gutterBottom className={classes.header}>
 				<Box fontWeight="fontWeightLight" className={classes.linkText}>
 					<Link
 						to={`${ROUTES.LEARN.path}/${course}`}
@@ -23,16 +22,17 @@ const PageHeader = ({ title, course, topic, subTopic }) => {
 						to={`${ROUTES.LEARN.path}/${course}/${topic}`}
 						className={classes.linkText}
 					>
-						{topic && (
-							<span>&nbsp;/&nbsp;{topic}</span>
-						)}
+						{topic && <span>&nbsp;/&nbsp;{topic}</span>}
 					</Link>
-					<Link className={classes.linkText}> 
+					<Link
+						className={classes.linkText}
+						to={`${ROUTES.LEARN.path}/${course}/${topic}/${subTopic}`}
+					>
 						{subTopic && (
 							<span>&nbsp;/&nbsp;{subTopic.replace(/-/g, " ")}</span>
 						)}
-						{title}
 					</Link>
+					{title}
 				</Box>
 			</Typography>
 		</Fade>
