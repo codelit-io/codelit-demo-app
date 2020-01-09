29f2fee9cdfe42d226bed568ed91f5df9feaacdb
Mo Sharif <mo@mosh-media.com>
2020-01-08 22:55:18 -0800

    Update index.js

--- src/components/Navigation/NavigationAuth/index.js
+++ src/components/Navigation/NavigationAuth/index.js
@@ -3,8 +3,9 @@ import * as ROUTES from "../../../constants/routes";
 import { Link } from "react-router-dom";
 import SignOutButton from "../../SignOut";
 import { List, ListItem, Button } from "@material-ui/core";
+import * as ROLES from '../../../constants/roles';
 
-const NavigationAuth = () => (
+const NavigationAuth = ({ authUser }) => (
 	<List>
 		<ListItem>
 			<Button>
@@ -16,16 +17,18 @@ const NavigationAuth = () => (
 				</Link>
 			</Button>
 		</ListItem>
-		<ListItem>
-			<Button>
-				<Link
-					to={ROUTES.ADMIN.path}
-					style={{ color: "black", textDecoration: "none" }}
-				>
-					Admin
-				</Link>
-			</Button>
-		</ListItem>
+		{authUser.roles && !!authUser.roles[ROLES.ADMIN] && (
+			<ListItem>
+				<Button>
+					<Link
+						to={ROUTES.ADMIN.path}
+						style={{ color: "black", textDecoration: "none" }}
+					>
+						Admin
+					</Link>
+				</Button>
+			</ListItem>
+		)}
 		<ListItem>
 			<SignOutButton />
 		</ListItem>
