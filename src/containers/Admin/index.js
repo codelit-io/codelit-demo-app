3be76589cf3187c03c1678232ed2aaec990c550b
Mo Sharif <mo@mosh-media.com>
2020-01-10 00:20:28 -0800

    Update index.js

--- src/containers/Admin/index.js
+++ src/containers/Admin/index.js
@@ -1,83 +1,28 @@
-/* Refactor using hooks */
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { compose } from 'recompose';
 
-import React, { useState, useEffect } from "react";
-import * as ROLES from "../../constants/roles";
-import { compose } from "recompose";
-import { makeStyles } from "@material-ui/core/styles";
-import { Grid } from "@material-ui/core";
-import MoshCard from "../../components/shared/MoCard";
-import { withAuthorization } from "../../components/Session";
-import { withFirebase } from "../../components/Firebase";
-import Spinner from "../../components/shared/Spinner";
+import { withAuthorization, withEmailVerification } from '../../components/Session';
+import { UserList, UserItem } from '../../components/Users';
+import * as ROLES from '../../constants/roles';
+import * as ROUTES from '../../constants/routes';
 
-const useStyles = makeStyles({
-	card: {
-		minWidth: 275
-	},
-	bullet: {
-		display: "inline-block",
-		margin: "0 2px",
-		transform: "scale(0.8)"
-	},
-	title: {
-		fontSize: 14
-	},
-	pos: {
-		marginBottom: 12
-	}
-});
+const AdminPage = () => (
+  <div>
+    <h1>Admin</h1>
+    <p>The Admin Page is accessible by every signed in admin user.</p>
 
-const AdminPage = props => {
-	const classes = useStyles();
-	const [loading, setLoading] = useState(false);
-	const [users, setUsers] = useState([]);
-	const UserList = ({ users }) => (
-		<Grid container spacing={3}>
-			{users.map(user => (
-				<Grid item sm={6} md={3} xs={12} key={user.uid}>
-					<MoshCard
-						className={classes.card}
-						topic={{
-							label: user.username,
-							desc: user.email
-						}}
-					></MoshCard>
-				</Grid>
-			))}
-		</Grid>
-	);
+    <Switch>
+      <Route exact path={ROUTES.ADMIN_DETAILS.path} component={UserItem} />
+      <Route exact path={ROUTES.ADMIN.path} component={UserList} />
+    </Switch>
+  </div>
+);
 
-	useEffect(() => {
-		setLoading(true);
-		props.firebase.users().on("value", snapshot => {
-			const usersObject = snapshot.val();
+const condition = authUser =>
+  authUser && !!authUser.roles[ROLES.ADMIN];
 
-			const usersList =
-				usersObject &&
-				Object.keys(usersObject).map(key => ({
-					...usersObject[key],
-					uid: key
-				}));
-
-			setUsers(usersList);
-			setLoading(false);
-		});
-
-		return () => {
-			props.firebase.users().off();
-		};
-	}, [props]);
-
-	return (
-		<div>
-			<h1>Users</h1>
-			<p>The Admin Page is accessible by every signed in admin user.</p>
-			{<Spinner loading={loading} color="primary" />}
-
-			<UserList users={users} />
-		</div>
-	);
-};
-
-const condition = authUser => authUser && !!authUser.roles[ROLES.ADMIN];
-export default compose(withAuthorization(condition), withFirebase)(AdminPage);
+export default compose(
+	withEmailVerification,
+  withAuthorization(condition),
+)(AdminPage);
