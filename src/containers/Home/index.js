3ef4d962874be69d13183e93bea1c4e465b23ca0
Mo Sharif <mo@mosh-media.com>
2020-01-10 00:20:15 -0800

    Update index.js

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,14 +1,22 @@
 import React from "react";
+import { compose } from "recompose";
 
-import { withAuthorization } from "../../components/Session";
+import {
+	withAuthorization,
+	withEmailVerification
+} from "../../components/Session";
+import Messages from "../../components/Messages";
 
 const HomePage = () => (
 	<div>
-		<h1>HomePage</h1>
-		<p>Welcome to Home Page</p>
+		<h1>Welcome to Mo Skool</h1>
+		<Messages />
 	</div>
 );
 
 const condition = authUser => !!authUser;
 
-export default withAuthorization(condition)(HomePage);
+export default compose(
+	withEmailVerification,
+	withAuthorization(condition)
+)(HomePage);
