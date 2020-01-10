e9dc3b8836eccf465d215edeefcd59d209a4841a
Mo Sharif <mo@mosh-media.com>
2020-01-10 00:20:13 -0800

    Update index.js

--- src/containers/Landing/index.js
+++ src/containers/Landing/index.js
@@ -4,25 +4,29 @@ import MoCard from "../../components/shared/MoCard";
 import PageHeader from "../../components/shared/PageHeader";
 import * as ROUTES from "../../constants/routes";
 import Typography from "@material-ui/core/Typography";
+import GooCads from "../../components/shared/GooCards";
 
 const LandingPage = () => {
 	const topics = [
 		{
 			label: "Start Learning",
 			desc: "Learn anything coding",
-			img: "https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/icons%2F006-science.png?alt=media&token=9942d7dd-19c9-497e-8597-4cb59df6cdc7",
-			url: ROUTES.LEARN.path,
+			img:
+				"https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/icons%2F006-science.png?alt=media&token=9942d7dd-19c9-497e-8597-4cb59df6cdc7",
+			url: ROUTES.LEARN.path
 		},
 		{
 			label: "My Lessons",
 			desc: "All in one place",
-			img: "https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/icons%2F026-education.png?alt=media&token=59b529d9-14ae-4f05-a5eb-c758ce55b2a1",
-			disable: true,
+			img:
+				"https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/icons%2F026-education.png?alt=media&token=59b529d9-14ae-4f05-a5eb-c758ce55b2a1",
+			disable: true
 		},
 		{
 			label: "My Team",
 			desc: "Say Hi!",
-			img: "https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/icons%2F009-collaboration.png?alt=media&token=a7e1e9e8-8b2b-4a32-86ad-0302de87407e",
+			img:
+				"https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/icons%2F009-collaboration.png?alt=media&token=a7e1e9e8-8b2b-4a32-86ad-0302de87407e",
 			disable: true
 		}
 	];
@@ -30,16 +34,21 @@ const LandingPage = () => {
 	return (
 		<>
 			<PageHeader title={ROUTES.LANDING.title}></PageHeader>
-			<Grid container spacing={3}>
+			{/* <Grid container spacing={3}>
 				{topics.map((topic, index) => (
 					<Grid key={index} item sm={12} md={6} xs={12}>
 						<MoCard topic={topic}></MoCard>
 					</Grid>
 				))}
-			</Grid>
+			</Grid> */}
+			<GooCads topics={topics} />
 			<Typography variant="subtitle1">
 				Icons made by{" "}
-				<a href="https://www.flaticon.com/authors/wanicon" title="wanicon" style={{color: "#3a9fff"}}>
+				<a
+					href="https://www.flaticon.com/authors/wanicon"
+					title="wanicon"
+					style={{ color: "#3a9fff" }}
+				>
 					wanicon
 				</a>
 			</Typography>
