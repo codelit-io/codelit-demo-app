91f6687b30f5a0b8da69b1a20fd39806270187bf
Mo Sharif <mo@mosh-media.com>
2020-01-11 01:24:47 -0800

    Create index.test.js

---
+++ src/containers/Home/index.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Home from "./index.js";
+import renderer from "react-test-renderer";
+
+it("Home Page", () => {
+	const tree = renderer.create(<Home />).toJSON();
+	expect(tree).toMatchSnapshot();
+});
