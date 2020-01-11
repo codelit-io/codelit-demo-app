61a840503bacdb38af6292dd063c11b2833e1dbf
Mo Sharif <mo@mosh-media.com>
2020-01-11 01:25:32 -0800

    Create index.test.js

---
+++ src/containers/Admin/index.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Admin from "./index.js";
+import renderer from "react-test-renderer";
+
+it("Admin Page", () => {
+	const tree = renderer.create(<Admin />).toJSON();
+	expect(tree).toMatchSnapshot();
+});
