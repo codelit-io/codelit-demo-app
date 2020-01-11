f552db1c09c13f510ebb7d417d13f53d31b1683b
Mo Sharif <mo@mosh-media.com>
2020-01-11 01:24:39 -0800

    Create index.test.js

---
+++ src/containers/SignIn/index.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import SignInPage from "./index.js";
+import renderer from "react-test-renderer";
+
+it("Sign In Page", () => {
+	const tree = renderer.create(<SignInPage />).toJSON();
+	expect(tree).toMatchSnapshot();
+});
