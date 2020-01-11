0be062e815e55ec14c5eedbd49d36af048760c30
Mo Sharif <mo@mosh-media.com>
2020-01-11 01:25:50 -0800

    Update styles.js

--- src/components/shared/PageCard/styles.js
+++ src/components/shared/PageCard/styles.js
@@ -3,7 +3,6 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles(theme => ({
 	card: {
 		boxShadow: "0 0 51px 0 rgba(0,0,0,.08), 0 6px 18px 0 rgba(0,0,0,.05)",
-		maxWidth: 345,
 		"&:hover": {
 			// boxShadow: "0 0 51px 0 rgba(0,0,0,.08), 0 6px 18px 0 rgba(0,0,0,.05)",
 			transform: "translateY(-5px)",
