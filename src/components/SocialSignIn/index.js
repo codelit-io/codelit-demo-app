851ad2e792cfe6967fef5eb804ad610725d59fdd
Mo Sharif <mo@mosh-media.com>
2020-01-12 15:43:27 -0800

    Update index.js

--- src/components/SocialSignIn/index.js
+++ src/components/SocialSignIn/index.js
@@ -2,13 +2,15 @@ import React from "react";
 
 import Avatar from "@material-ui/core/Avatar";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import LockOpenIcon from '@material-ui/icons/LockOpen';
+import { compose } from "recompose";
+import LockOpenIcon from "@material-ui/icons/LockOpen";
 import Typography from "@material-ui/core/Typography";
 import withStyles from "@material-ui/core/styles/withStyles";
 import PropTypes from "prop-types";
 import styles from "./styles";
 import SignInWithFacebook from "./SignInWithFacebook";
 import SignInWithGoogle from "./SignInWithGoogle";
+import withLayout from "../shared/Layout";
 
 const SocialSignIn = ({ classes }) => {
 	return (
@@ -21,7 +23,7 @@ const SocialSignIn = ({ classes }) => {
 				<Typography component="h1" variant="h5">
 					Social Media Sign in
 				</Typography>
-				<SignInWithFacebook/>
+				<SignInWithFacebook />
 				<SignInWithGoogle />
 			</div>
 		</main>
@@ -32,4 +34,4 @@ SocialSignIn.propTypes = {
 	classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(SocialSignIn);
+export default compose(withLayout,withStyles(styles))(SocialSignIn);
