34e6680f21850665d1edbb7efc83bc2f89b6f708
Mo Sharif <mo@mosh-media.com>
2020-01-12 15:43:30 -0800

    Update index.js

--- src/components/shared/Layout/index.js
+++ src/components/shared/Layout/index.js
@@ -1,5 +1,12 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
 
-const Layout = () => {
-    return <h1>Layout</h1>
-}
+const withLayout = Component => ({ rows, cols }) => {
+	return (
+		<Grid item sm={12} md={6}>
+			<Component />
+		</Grid>
+	);
+};
+
+export default withLayout;
