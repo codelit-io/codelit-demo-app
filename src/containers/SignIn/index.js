8674ece4d0b12b1f85a54cacb7392949a925f9c6
Mo Sharif <mo@mosh-media.com>
2020-01-12 01:56:13 -0800

    Update index.js

--- src/containers/SignIn/index.js
+++ src/containers/SignIn/index.js
@@ -1,15 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 
-import * as ROUTES from "../../constants/routes";
-import { compose } from "recompose";
-import EmailSignInForm from "./EmailSignInForm";
 import Grid from "@material-ui/core/Grid";
-
 import PageCard from "../../components/shared/PageCard";
-import { withFirebase } from "../../components/Firebase";
-import { withRouter } from "react-router-dom";
-
 import SocialSignIn from "../../components/SocialSignIn/";
+import SignInForm from "./SignInForm";
 
 const SignInPage = () => (
 	<PageCard img="" title="Welcome Back!">
@@ -25,49 +19,4 @@ const SignInPage = () => (
 	</PageCard>
 );
 
-const INITIAL_STATE = {
-	email: "",
-	password: "",
-	error: null
-};
-
-const SignInFormBase = props => {
-	const [state, setState] = useState({ ...INITIAL_STATE });
-	const { email, password, error } = state;
-
-	const handleSubmit = event => {
-		props.firebase
-			.signInWithEmailAndPassword(email, password)
-			.then(() => {
-				setState({ ...INITIAL_STATE });
-				props.history.push(ROUTES.HOME.path);
-			})
-			.catch(error => {
-				setState({ ...state, error });
-			});
-
-		event.preventDefault();
-	};
-
-	const onChange = event => {
-		setState({ ...state, [event.target.name]: event.target.value });
-	};
-
-	return (
-		<EmailSignInForm
-			onSubmit={handleSubmit}
-			isInvalid={password === "" || email === ""}
-			onChange={onChange}
-			error={error}
-		/>
-	);
-};
-
-
-
-const SignInForm = compose(withRouter, withFirebase)(SignInFormBase);
-
-
 export default SignInPage;
-
-export { SignInForm };
