bbbbaea5cb0e9176a1a8a852a5e3630968729fbf
Mo Sharif <mo@mosh-media.com>
2020-01-11 17:30:47 -0800

    Update index.js

--- src/components/shared/Spinner/index.js
+++ src/components/shared/Spinner/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import CircularProgress from "@material-ui/core/CircularProgress";
-import useStyles from "./styles";
+import styles from "./styles";
+import withStyles from "@material-ui/core/styles/withStyles";
 
-const Spinner = ({ loading, color }) => {
-    const classes = useStyles();
+const Spinner = ({ loading, color, classes }) => {
 
 	return loading && <CircularProgress className={classes.spinner} color={color} />;
 };
 
-export default Spinner;
+export default withStyles(styles)(Spinner);
