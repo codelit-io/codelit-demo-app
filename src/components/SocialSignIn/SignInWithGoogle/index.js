2f0685a2f26b06cb68ed4cfc6cd9582490bcf62f
Mo Sharif <mo@mosh-media.com>
2020-01-11 18:14:09 -0800

    Create index.js

---
+++ src/components/SocialSignIn/SignInWithGoogle/index.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+
+import * as ROUTES from "../../../constants/routes";
+import Button from "@material-ui/core/Button";
+import { compose } from "recompose";
+import PropTypes from "prop-types";
+import { withFirebase } from "../../Firebase";
+import { withRouter } from "react-router-dom";
+import withStyles from "@material-ui/core/styles/withStyles";
+
+const styles = theme => ({
+	button: {
+		margin: theme.spacing(1)
+	},
+	input: {
+		margin: theme.spacing(1)
+	}
+});
+
+const SignInWithGoogleBase = ({ firebase, classes, history }) => {
+	const [error, setError] = useState({ error: null });
+	const onSubmit = event => {
+		firebase
+			.signInWithGoogle()
+			.then(socialAuthUser => {
+				// Create a user in Firebase Realtime Database
+				return firebase.user(socialAuthUser.user.uid).set(
+					{
+						username: socialAuthUser.user.displayName,
+						email: socialAuthUser.user.email,
+						roles: {}
+					},
+					{ merge: true }
+				);
+			})
+			.then(() => {
+				setError(null);
+				// Investigate if this this the correct approach
+				history.push(ROUTES.HOME.path);
+			})
+			.catch(error => setError(error));
+		event.preventDefault();
+	};
+
+	return (
+		<form onSubmit={onSubmit}>
+			<Button color="primary" className={classes.button} type="submit">
+				{" "}
+				SIgn In with Google
+			</Button>
+
+			{error && <p>{error.message}</p>}
+		</form>
+	);
+};
+
+SignInWithGoogleBase.propTypes = {
+	classes: PropTypes.object.isRequired
+};
+
+const SignInWithGoogle = compose(
+	withStyles(styles),
+	withRouter,
+	withFirebase
+)(SignInWithGoogleBase);
+
+export default SignInWithGoogle;
