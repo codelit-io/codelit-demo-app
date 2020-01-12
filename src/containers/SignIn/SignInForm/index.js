c9772b5ec837c7fe05594f0d0a4138c152c69282
Mo Sharif <mo@mosh-media.com>
2020-01-12 15:43:22 -0800

    Update index.js

--- src/containers/SignIn/SignInForm/index.js
+++ src/containers/SignIn/SignInForm/index.js
@@ -6,6 +6,7 @@ import EmailSignInForm from "../../../components/EmailSignInForm";
 
 import { withFirebase } from "../../../components/Firebase";
 import { withRouter } from "react-router-dom";
+import Layout from "../../../components/shared/Layout";
 
 const INITIAL_STATE = {
 	email: "",
@@ -47,4 +48,4 @@ const SignInFormBase = ({ firebase, history}) => {
 
 const SignInForm = compose(withRouter, withFirebase)(SignInFormBase);
 
-export default SignInForm;
+export default Layout(SignInForm);
