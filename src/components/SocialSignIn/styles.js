ab0758808855c9ef284e0040f2b822042b71b332
Mo Sharif <mo@mosh-media.com>
2020-01-11 18:14:07 -0800

    Create styles.js

---
+++ src/components/SocialSignIn/styles.js
@@ -0,0 +1,30 @@
+const styles = theme => ({
+	main: {
+		width: "auto",
+		display: "block", // Fix IE 11 issue.
+		marginLeft: theme.spacing(3),
+		marginRight: theme.spacing(3),
+		[theme.breakpoints.up(400 + theme.spacing(6))]: {
+			width: 400,
+			marginLeft: "auto",
+			marginRight: "auto"
+		}
+	},
+	paper: {
+		marginTop: theme.spacing(8),
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+		padding: `${theme.spacing(2)}px ${theme.spacing(3)}px ${theme.spacing(3)}px`
+	},
+	avatar: {
+		margin: theme.spacing(),
+		backgroundColor: theme.palette.primary.main
+	},
+	form: {
+		width: "100%", // Fix IE 11 issue.
+		marginTop: theme.spacing(3)
+	},
+});
+
+export default styles;
