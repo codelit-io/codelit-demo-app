c19a4752661f01038b03489bbe450bd4912e9e52
Mo Sharif <mo@mosh-media.com>
2020-01-11 18:14:06 -0800

    Update index.js

--- src/containers/SignIn/EmailSignInForm/index.js
+++ src/containers/SignIn/EmailSignInForm/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -9,51 +8,19 @@ import Checkbox from "@material-ui/core/Checkbox";
 import Input from "@material-ui/core/Input";
 import InputLabel from "@material-ui/core/InputLabel";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import withStyles from "@material-ui/core/styles/withStyles";
 import FormHelperText from "@material-ui/core/FormHelperText";
-
-const styles = theme => ({
-	main: {
-		width: "auto",
-		display: "block", // Fix IE 11 issue.
-		marginLeft: theme.spacing(3),
-		marginRight: theme.spacing(3),
-		[theme.breakpoints.up(400 + theme.spacing(6))]: {
-			width: 400,
-			marginLeft: "auto",
-			marginRight: "auto"
-		}
-	},
-	paper: {
-		marginTop: theme.spacing(8),
-		display: "flex",
-		flexDirection: "column",
-		alignItems: "center",
-		padding: `${theme.spacing(2)}px ${theme.spacing(3)}px ${theme.spacing(3)}px`
-	},
-	avatar: {
-		margin: theme.spacing(),
-		backgroundColor: theme.palette.secondary.main
-	},
-	form: {
-		width: "100%", // Fix IE 11 issue.
-		marginTop: theme.spacing()
-	},
-	submit: {
-		marginTop: theme.spacing(3)
-	},
-	error: {
-		marginTop: theme.spacing(3)
-	}
-});
+import styles from "./styles";
+import { SignUpLink } from "../../SignUp";
+import PropTypes from "prop-types";
+import { PasswordForgetLink } from "../../../components/PasswordForgot";
 
 const EmailSignInForm = ({ classes, onSubmit, isInvalid, onChange, error }) => {
 	return (
 		<main className={classes.main}>
 			<CssBaseline />
-			<Paper className={classes.paper}>
+			<div className={classes.paper}>
 				<Avatar className={classes.avatar}>
 					<LockOutlinedIcon />
 				</Avatar>
@@ -108,7 +75,9 @@ const EmailSignInForm = ({ classes, onSubmit, isInvalid, onChange, error }) => {
 						</FormHelperText>
 					)}
 				</form>
-			</Paper>
+				<PasswordForgetLink />
+				<SignUpLink />
+			</div>
 		</main>
 	);
 };
