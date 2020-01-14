512fd35a555d38be670c18389861ac144570dc6e
Mo Sharif <mo@mosh-media.com>
2020-01-13 23:39:50 -0800

    Delete Component.test.js

--- src/containers/Courses/Topics/Component/__tests__/Component.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Component } from "../../Component";
-import renderer from "react-test-renderer";
-
-it("Project Component", () => {
-	const tree = renderer.create(<Component />).toJSON();
-	expect(tree).toMatchSnapshot();
-});
