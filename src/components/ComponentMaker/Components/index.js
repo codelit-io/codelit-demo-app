c6866e9a0dff426f2cd7663812a30890be17ed5d
Mo Sharif <mo@mosh-media.com>
2020-01-15 08:22:56 -0800

    Update index.js

--- src/components/ComponentMaker/Components/index.js
+++ src/components/ComponentMaker/Components/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 
+const inputHtml = () => {
+	return <input type="text" />;
+};
+
+const buttonHtml = () => {
+	return <button type="submit" >I am a boring button :)</button>;
+};
+
 const Components = {
+	inputHtml: inputHtml,
+	buttonHtml: buttonHtml,
 	button: React.lazy(() => import("@material-ui/core/Button")),
 	textField: React.lazy(() => import("@material-ui/core/TextField")),
 	input: React.lazy(() => import("@material-ui/core/Input")),
@@ -8,7 +18,7 @@ const Components = {
 	checkbox: React.lazy(() => import("@material-ui/core/Checkbox")),
 	slider: React.lazy(() => import("@material-ui/core/Slider")),
 	link: React.lazy(() => import("@material-ui/core/Link")),
-	typography: React.lazy(() => import("@material-ui/core/Typography")),
+	typography: React.lazy(() => import("@material-ui/core/Typography"))
 };
 
 export default Components;
