ab90900cb21d7a0441aed1e4e67635d0d0a96320
Mo Sharif <mo@mosh-media.com>
2020-01-15 23:44:44 -0800

    Update index.js

--- src/components/ComponentMaker/DragAndDrop/DraggableContainer/index.js
+++ src/components/ComponentMaker/DragAndDrop/DraggableContainer/index.js
@@ -1,51 +1,74 @@
 import React, { useState } from "react";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import DraggableItemList from "../DraggableItemList";
-import { reorder } from "../reorder.js";
+import { reorder } from "../reorder";
 import { Grid } from "@material-ui/core";
+import CheckIcon from "@material-ui/icons/Check";
+import { move } from "../move";
+/* A semi-generic way to handle multiple lists. Matches
+ * the IDs of the droppable container to the names of the
+ * source arrays stored in the state.
+ */
 
-const DraggableContainer = ({ list }) => {
-	const initial = list.map((item, index) => {
-		const custom = {
-			id: `${item}${index}`,
-			element: item
-		};
-		return custom;
-	});
+const DraggableContainer = ({ data }) => {
+	const [lists, setLists] = useState(data);
 
-	const [state, setState] = useState({ draggableItems: initial });
+	const getList = id => {
+		return lists[id];
+	};
 
 	const onDragEnd = result => {
-		if (!result.destination) {
+		const { source, destination } = result;
+		// dropped outside the list
+		if (!destination) {
 			return;
 		}
+		if (source.droppableId === destination.droppableId) {
+			const items = reorder(
+				getList(source.droppableId),
+				source.index,
+				destination.index
+			);
+			
+			debugger;
+			let newLists = { items };
 
-		if (result.destination.index === result.source.index) {
-			return;
+			if (source.droppableId === "selected") {
+				newLists = { selected: items };
+			}
+
+			setLists({ ...lists, ...newLists });
+		} else {
+			const result = move(
+				getList(source.droppableId),
+				getList(destination.droppableId),
+				source,
+				destination
+			);
+			setLists({ ...lists, ...result });
 		}
-
-		const draggableItems = reorder(
-			state.draggableItems,
-			result.source.index,
-			result.destination.index
-		);
-
-		setState({ draggableItems });
 	};
+
+	const setNewLists = (result) => {
+		console.log(result)
+		// setLists({ ...lists, ...result });
+	}
 
 	return (
 		<DragDropContext onDragEnd={onDragEnd}>
 			<Grid container spacing={3}>
-				<Grid item xs={12} sm={6} md={4} lg={3}>
-					<Droppable droppableId="list">
-						{provided => (
-							<span ref={provided.innerRef} {...provided.droppableProps}>
-								<DraggableItemList draggableItems={state.draggableItems} />
-								{provided.placeholder}
-							</span>
-						)}
-					</Droppable>
-				</Grid>
+				{Object.keys(lists).map((list, index) => (
+					<Grid key={index} item xs={12} sm={6} md={6} lg={4}>
+						<Droppable droppableId={list}>
+							{provided => (
+								<span ref={provided.innerRef} {...provided.droppableProps}>
+									<DraggableItemList draggableItems={lists[list]} listId={list} setNewLists={setNewLists} />
+									{provided.placeholder}
+								</span>
+							)}
+						</Droppable>
+					</Grid>
+				))}
 			</Grid>
 		</DragDropContext>
 	);
