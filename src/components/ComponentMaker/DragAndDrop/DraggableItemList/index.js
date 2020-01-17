9861ab276988a34acf2b4a864083db98af30b60e
Mo Sharif <mo@mosh-media.com>
2020-01-16 23:30:02 -0800

    Update index.js

--- src/components/ComponentMaker/DragAndDrop/DraggableItemList/index.js
+++ src/components/ComponentMaker/DragAndDrop/DraggableItemList/index.js
@@ -2,17 +2,16 @@ import React from "react";
 import DraggableItem from "../DraggableItem";
 
 const DraggableItemList = React.memo(function DraggableItemList({
-	draggableItems, listId, setNewLists
+	draggableItems, listId, setNewLists, classes
 }) {
-
-	const setNewListsTest = () => setNewLists
 	return draggableItems.map((draggableItem, index) => (
 		<DraggableItem
 			listId={listId}
 			draggableItem={draggableItem}
 			index={index}
 			key={draggableItem.id}
-			setNewLists={setNewListsTest}
+			setNewLists={setNewLists}
+			classes={classes}
 		/>
 	));
 });
