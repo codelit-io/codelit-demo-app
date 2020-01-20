3515d21c5e9d5d9667a5ab69ed71c2dcabb10f2b
Mo Sharif <mo@mosh-media.com>
2020-01-19 22:59:32 -0800

    Create index.js

---
+++ src/components/shared/Title/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import Box from "@material-ui/core/Box";
+import Fade from "@material-ui/core/Fade";
+import Typography from "@material-ui/core/Typography";
+
+const classes = {
+	text: {
+		color: "#383c40",
+		textTransform: "capitalize",
+		verticalAlign: "middle",
+		textDecoration: "none",
+		textAlign: "center",
+		marginBottom: "40px",
+	}
+};
+
+const Title = ({ text, fade }) => {
+	return (
+		<Fade in={fade} timeout={{ enter: 1000 }}>
+			<Typography variant="h4" gutterBottom>
+				<Box fontWeight="fontWeightLight" style={classes.text}>
+					{text}
+				</Box>
+			</Typography>
+		</Fade>
+	);
+};
+
+export default Title;
