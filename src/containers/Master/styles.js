1a94a9573a18dc85d1320bc6e3cfe13a6773dcef
Mo Sharif <mo@mosh-media.com>
2020-01-19 22:59:29 -0800

    Update styles.js

--- src/containers/Master/styles.js
+++ src/containers/Master/styles.js
@@ -3,7 +3,15 @@ const styles = theme => ({
 		background: "#fff",
 		borderRadius: "8px",
 		boxShadow:
-			"0 24px 24px -18px rgba(69,104,129,.33), 0 9px 45px 0 rgba(114,119,160,.12)"
+			"0 24px 24px -18px rgba(69,104,129,.33), 0 9px 45px 0 rgba(114,119,160,.12)",
+		width: "100%"
+	},
+	preview: {
+		// background: "#fff",
+		borderRadius: "8px",
+		// padding: "10px",
+		// boxShadow:"0 24px 24px -18px rgba(69,104,129,.33), 0 9px 45px 0 rgba(114,119,160,.12)",
+		// width: "100%"
 	}
 });
 
