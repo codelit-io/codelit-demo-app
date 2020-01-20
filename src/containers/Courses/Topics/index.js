70ab4844f39562363c69c3ebdd31340c2aa87e5b
Mo Sharif <mo@mosh-media.com>
2020-01-19 21:31:16 -0800

    Update index.js

--- src/containers/Courses/Topics/index.js
+++ src/containers/Courses/Topics/index.js
@@ -5,6 +5,7 @@ import { withAuthentication } from "../../../components/Session";
 import Spinner from "../../../components/shared/Spinner";
 import PageHeader from "../../../components/shared/PageHeader";
 import DragAndDrop from "../../../components/ComponentMaker/DragAndDrop/DraggableContainer";
+import { data } from "../../../assets/dummyData";
 
 const Topics = ({ firebase, match, history }) => {
 	const [topics, setTopics] = useState([]);
@@ -21,92 +22,6 @@ const Topics = ({ firebase, match, history }) => {
 				))}
 		</Grid>
 	);
-	const data = {
-		leftList: [
-			{
-				id: "0",
-				label: "Button",
-				element: "button",
-				question: "<button> I am a Button <button>",
-				answer: "<button> I am a Button </button>",
-				status: "😴",
-				isCorrect: false
-			},
-			{
-				id: "1",
-				label: "Paragraph",
-				element: "typography",
-				question: "I am a Paragraph </p>",
-				answer: "<p> I am a Paragraph </p>",
-				status: "🤕",
-				isCorrect: false
-			},
-			{
-				id: "2",
-				label: "Input",
-				element: "input",
-				question: "<input value=\"I am a text Input\" type=\"NULL\" />",
-				answer: "<input value=\"I am a text Input\" type=\"text\" />",
-				status: "😷",
-				isCorrect: false
-			},
-			{
-				id: "3",
-				label: "Switch",
-				element: "switch",
-				question: "<switch> I am a Material switch",
-				answer: "<switch> I am a Material switch </switch>",
-				status: "🤒",
-				isCorrect: false
-			},
-			{
-				id: "4",
-				label: "Checkbox",
-				element: "checkbox",
-				question: "<input type=\"NULL\" /> I am a checkbox",
-				answer: "<input type=\"checkbox\" /> I am a checkbox",
-				status: "🤧",
-				isCorrect: false
-			},
-			{
-				id: "5",
-				label: "Slider",
-				element: "slider",
-				question: "<Slider > I am a Material Slider",
-				answer: "<Slider > I am a Material Slider </Slider>",
-				status: "🥵",
-				isCorrect: false
-			},
-			{
-				id: "6",
-				label: "Html Link",
-				element: "link",
-				question: "<a src=\"moskool.com\"> I am an html link </a>",
-				answer: "<a href=\"moskool.com\"> I am an html link </a>",
-				status: "😵",
-				isCorrect: false
-			},
-			{
-				id: "7",
-				label: "Html text Input",
-				element: "inputHtml",
-				question: '< type="text" />',
-				answer: '<input type="text" />',
-				status: "🥶",
-				isCorrect: false
-			},
-			{
-				id: "8",
-				label: "Html submit button",
-				element: "buttonHtml",
-				question: '<button type="NULL">I am a submit button :)</button>',
-				answer: '<button type="submit">I am a submit button :)</button>',
-				status: "🤒",
-				isCorrect: false
-			}
-		],
-		rightList: []
-	};
 
 	useEffect(() => {
 		setLoading(true);
