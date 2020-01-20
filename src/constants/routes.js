2924c6cda0f408573d7a0a59c07efb48995a13c9
Mo Sharif <mo@mosh-media.com>
2020-01-19 21:31:18 -0800

    Update routes.js

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,5 +1,5 @@
 /*  Pages */
-export const LANDING = { path: '/', title: 'New way to Mastering software engineering'};
+export const LANDING = { path: '/', title: 'New way to Mastering Front-end Development'};
 export const SIGN_UP = { path: '/signup', title: 'Sign up'};
 export const SIGN_IN = { path: '/signin', title: 'Sign in'};
 export const HOME = { path: '/home', title: 'Home Page'};
@@ -8,6 +8,7 @@ export const ADMIN = { path: '/admin', title: 'Admin Page'};
 export const ADMIN_DETAILS = { path: '/admin/:id', title: 'Admin Details'};
 export const PASSWORD_FORGET = { path: '/pw-forget', title: 'Forgot Password'};
 export const LEARN = { path: '/learn', title: 'Pick your career path'};
+export const MASTER = { path: '/master', title: ''}
 export const BUILD_PROJECT = { path: '/build_project', title: 'Build your project'};
 export const NOT_FOUND = {path: '/not_found', title: "Page Not Found"}
 
