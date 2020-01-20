8f47aa9bd32578be13f73b1c94bff24635c76989
Mo Sharif <mo@mosh-media.com>
2020-01-19 21:31:25 -0800

    Update styles.js

--- src/components/ComponentMaker/DragAndDrop/DraggableContainer/styles.js
+++ src/components/ComponentMaker/DragAndDrop/DraggableContainer/styles.js
@@ -4,7 +4,7 @@ const styles = theme => ({
 		padding: theme.spacing(4),
 		userSelect: "none",
 		borderRadius: 5,
-		boxShadow: "0 0 114px 0 rgba(0,0,0,.08), 0 30px 25px 0 rgba(0,0,0,.05)",
+		boxShadow: "0 24px 24px -18px rgba(69,104,129,.33), 0 9px 45px 0 rgba(114,119,160,.12)",
 		// change background colour if dragging
 		background: "white"
 	},
@@ -15,7 +15,7 @@ const styles = theme => ({
 	wavyErrorBorder: {
 		display: "inline-block",
 		position: "relative",
-		"&::before": {
+		"	&::before": {
 			content: "~~~~~~~~~~~~",
 			fontSize: "0.6em",
 			fontWeight: 700,
@@ -27,6 +27,11 @@ const styles = theme => ({
 			left: "-1px",
 			overflow: "hidden"
 		}
+	},
+	dropZone: {
+		color: "#617aa2",
+		margin: "20px",
+		textAlign: "center",
 	}
 });
 
