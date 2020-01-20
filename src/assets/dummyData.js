8c0414026a1f312523fad0c3b89a8b10ca3d9716
Mo Sharif <mo@mosh-media.com>
2020-01-19 21:31:30 -0800

    Create dummyData.js

---
+++ src/assets/dummyData.js
@@ -0,0 +1,93 @@
+export const data = {
+	leftList: [
+		{
+			id: "0",
+			label: "Fix Html button tag syntax",
+			element: "button",
+			question: "<button> I am a Button <button>",
+			answer: "<button> I am a Button </button>",
+			status: "😴",
+			isCorrect: false
+		},
+		{
+			id: "1",
+			label: "Fix Html paragraph tag syntax",
+			element: "typography",
+			question: "I am a Paragraph </p>",
+			answer: "<p> I am a Paragraph </p>",
+			status: "🤕",
+			isCorrect: false
+		},
+		{
+			id: "2",
+			label: "Update missing Html input type",
+			element: "input",
+			question: '<input value="I am a text Input" type="NULL" />',
+			answer: '<input value="I am a text Input" type="text" />',
+			status: "😷",
+			isCorrect: false
+		},
+		{
+			id: "3",
+			label: "Fix missing React material component",
+			element: "switch",
+			question: "<switch> I am a Material switch",
+			answer: "<switch> I am a Material switch </switch>",
+			status: "🤒",
+			isCorrect: false
+		},
+		{
+			id: "4",
+			label: "Update missing Html checkbox type",
+			element: "checkbox",
+			question: '<input type="NULL" /> I am a checkbox',
+			answer: '<input type="checkbox" /> I am a checkbox',
+			status: "🤧",
+			isCorrect: false
+		},
+		{
+			id: "5",
+			label: "Fix missing React material component",
+			element: "slider",
+			question: "<Slider > I am a Material Slider",
+			answer: "<Slider > I am a Material Slider </Slider>",
+			status: "🥵",
+			isCorrect: false
+		},
+		{
+			id: "6",
+			label: "Fix broken Html a tag",
+			element: "link",
+			question: '<a src="moskool.com"> I am an html link </a>',
+			answer: '<a href="moskool.com"> I am an html link </a>',
+			status: "😵",
+			isCorrect: false
+		},
+		{
+			id: "7",
+			label: "Fix broken Html input tag",
+			element: "inputHtml",
+			question: '< type="text" />',
+			answer: '<input type="text" />',
+			status: "🥶",
+			isCorrect: false
+		},
+		{
+			id: "8",
+			label: "Update submit button type",
+			element: "buttonHtml",
+			question: '<button type="NULL">I am a submit button :)</button>',
+			answer: '<button type="submit">I am a submit button :)</button>',
+			status: "🤒",
+			isCorrect: false
+		}
+	],
+	rightList: []
+};
+
+export const questions = [{
+	id: 0,
+	label: "",
+},{
+
+}]
