9060c21d642e3e1951c89f70e3bd5203d5747c9c
Mo Sharif <mo@mosh-media.com>
2020-01-22 22:43:17 -0800

    Update index.js

--- src/components/CodeEditor/index.js
+++ src/components/CodeEditor/index.js
@@ -1,59 +1,71 @@
 import React, { useEffect, useState } from "react";
-import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
-import AceEditor from "react-ace";
-import "ace-builds/src-min-noconflict/mode-html";
-import "ace-builds/src-min-noconflict/theme-tomorrow";
 
+import { LiveEditor, LiveProvider, LivePreview } from "react-live";
 import { Grid } from "@material-ui/core";
+import Headline from "../shared/Headline";
 import styles from "./styles";
+import Slide from "@material-ui/core/Slide";
 import Spinner from "../../components/shared/Spinner";
 import Title from "../../components/shared/Title";
 import withStyles from "@material-ui/core/styles/withStyles";
+import { reactLiveTheme } from "../../utils/reactLiveTheme";
 
 const CodeEditor = ({ classes, subTopic }) => {
 	const [state, setState] = useState([]);
 	const [loading, setLoading] = useState(true);
 
 	const handleOnChange = userAnswer => {
-        console.log(userAnswer)
-		setState({...state, question: userAnswer});
+		if (userAnswer === "{}" || userAnswer === "") {
+			return;
+		}
+
+		if (userAnswer.replace(/\s/g, "") === state.answer.replace(/\s/g, "")) {
+			setState({ ...state, isCorrect: true, question: userAnswer });
+		} else {
+			setState({ ...state, isCorrect: false, question: userAnswer });
+		}
 	};
 
 	useEffect(() => {
-        setState(subTopic);
-        setLoading(false);
+		setState(subTopic);
+		setLoading(false);
 	}, [subTopic]);
-
 	return (
 		<Grid container spacing={6}>
 			<Spinner loading={loading} color="primary" />
 
-			<LiveProvider code={state.question}>
+			<LiveProvider code={state.question} language="html">
 				<Grid item md={6} sm={12} xs={12}>
 					<Title text={state.label} fade={true} />
-					<AceEditor
-						className={classes.editor}
-						editorProps={{ $blockScrolling: true }}
-						enableBasicAutocompletion={true}
-						fontSize={14}
-						mode="html"
-						name="livePreview"
-						onChange={handleOnChange}
-						theme="tomorrow"
-						style={{ width: "100%", height: "300px" }}
-						value={state.question}
-					/>
+					<Slide
+						direction="right"
+						in={state.answer && true}
+						mountOnEnter
+						unmountOnExit
+					>
+						<div
+							className={`${classes.editor} ${state.isCorrect &&
+								classes.correct}`}
+						>
+							<LiveEditor onChange={handleOnChange} theme={reactLiveTheme} noInline={true}/>
+						</div>
+					</Slide>
 				</Grid>
 				<Grid item md={6} sm={12} xs={12}>
 					<Title text="Code Preview" fade={true} />
-					<LivePreview className={classes.preview} />
+					<Slide
+						direction="left"
+						in={state.question && true}
+						mountOnEnter
+						unmountOnExit
+					>
+						<div>
+							<LivePreview className={classes.preview} />
+						</div>
+					</Slide>
 				</Grid>
 				<Grid item md={12} sm={12} xs={12}>
-					<Title text="Debug" fade={true}></Title>
-					<LiveError className={classes.error} />
-				</Grid>
-				<Grid item md={12} sm={12} xs={12}>
-					<LiveEditor />
+					<Headline isCorrect={state && state.isCorrect} />
 				</Grid>
 			</LiveProvider>
 		</Grid>
