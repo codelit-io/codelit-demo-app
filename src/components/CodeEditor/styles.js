5f975a544c121a6e8befba85c43f8399a4939918
Mo Sharif <mo@mosh-media.com>
2020-01-22 22:43:19 -0800

    Update styles.js

--- src/components/CodeEditor/styles.js
+++ src/components/CodeEditor/styles.js
@@ -5,15 +5,23 @@ const styles = theme => ({
 		boxShadow:
 			"0 24px 24px -18px rgba(69,104,129,.33), 0 9px 45px 0 rgba(114,119,160,.12)",
 		width: "100%",
-        height: "300px",
+        padding: "1.2em",
+        transition: "all 500ms",
+        font: "16px/normal 'Monaco', 'Menlo', 'Ubuntu Mono', 'Consolas', 'source-code-pro', monospace",
         [theme.breakpoints.up(500)]: {
 			width: "380",
 			marginLeft: "auto",
 			marginRight: "auto"
-		}
+        },
+        "& textarea": {
+            fontSize: "14px",
+            outline: "none"
+        }
 	},
 	preview: {
-		// background: "#fff",
+        // background: "#fff",
+        display: "flex",
+        justifyContent: "center",
 		borderRadius: "8px"
 		// padding: "10px",
 		// boxShadow:"0 24px 24px -18px rgba(69,104,129,.33), 0 9px 45px 0 rgba(114,119,160,.12)",
@@ -26,6 +34,10 @@ const styles = theme => ({
 		borderRadius: "8px",
 		boxShadow:
 			"0 24px 24px -18px rgba(69,104,129,.33), 0 9px 45px 0 rgba(114,119,160,.12)"
+    },
+    correct: {
+		boxShadow:
+			"0 0 114px 0 rgba(0,0,0,.08), 0 30px 25px 0 rgba(196, 245, 46, 0.49)"
 	}
 });
 
