b4c469e2d8c771f5f02be185830fa59cd215404f
Mo Sharif <mo@mosh-media.com>
2020-01-25 11:20:32 -0800

    Create index.js

---
+++ src/containers/Questions/Question/index.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+
+import BottomNav from "../../../components/BottomNav";
+import CodeEditor from "../../../components/CodeEditor";
+import PageHeader from "../../../components/shared/PageHeader";
+import { withAuthentication } from "../../../components/Session";
+import Spinner from "../../../components/shared/Spinner";
+
+const Question = ({ firebase, history, match }) => {
+	const [loading, setLoading] = useState(false);
+	const [question, setQuestion] = useState({});
+
+	// const CodeEditorBase = ({})
+	useEffect(() => {
+		const questionRoute = match.params.question;
+		setLoading(true);
+		firebase.getQuestions().on("value", snapshot => {
+			const filteredQuestion = snapshot
+				.val()
+				.filter(
+					question => question.label === questionRoute.replace(/-/g, " ")
+				);
+
+			/* Future Update */
+			/* Fetch data based on post id rather than label */
+			/* Fetch individual posts instead of the whole thing */
+			/* Use FB collections to help with this */
+
+			setQuestion(filteredQuestion[0]);
+			setLoading(false);
+		});
+
+		return () => {
+			firebase.getQuestions(questionRoute).off();
+		};
+	}, [firebase, match]);
+
+	return (
+		<>
+			<PageHeader img="" title="Code Playground" history={history} />
+			<Spinner loading={loading} color="primary" />
+			<CodeEditor question={question} />
+			<BottomNav question={question} history={history} firebase={firebase} />
+		</>
+	);
+};
+export default withAuthentication(Question);
