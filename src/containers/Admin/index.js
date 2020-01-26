d8ea02f6be4b790e5a36dab395d79b1528e95b50
Mo Sharif <mo@mosh-media.com>
2020-01-26 14:45:58 -0800

    Update index.js

--- src/containers/Admin/index.js
+++ src/containers/Admin/index.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { compose } from 'recompose';
+import React from "react";
 
-import { withAuthorization, withEmailVerification } from '../../components/Session';
-import { UserList, UserItem } from '../../components/Users';
-import * as ROLES from '../../constants/roles';
-import * as ROUTES from '../../constants/routes';
-import PageCard from '../../components/shared/PageCard';
+import * as ROLES from "../../constants/roles";
+import * as ROUTES from "../../constants/routes";
+import AdminNav from "./AdminNav";
+import { compose } from "recompose";
+import { Switch, Route } from "react-router-dom";
+import PageCard from "../../components/shared/PageCard";
+import { UserList, UserItem } from "../../components/Users";
+import {
+	withAuthorization,
+	withEmailVerification
+} from "../../components/Session";
 
-const AdminPage = () => (
-  <PageCard title="Admin">
-    <Switch>
-      <Route exact path={ROUTES.ADMIN_DETAILS.path} component={UserItem} />
-      <Route exact path={ROUTES.ADMIN.path} component={UserList} />
-    </Switch>
-  </PageCard>
+const AdminPage = ({ history }) => (
+	<PageCard title="Admin">
+    <AdminNav history={history} />
+		<Switch>
+			<Route exact path={ROUTES.ADMIN_DETAILS.path} component={UserItem} />
+			<Route exact path={ROUTES.ADMIN.path} component={UserList} />
+			<Route exact path={ROUTES.ADMIN_ADD_QUESTION.path} component={UserList} />
+		</Switch>
+	</PageCard>
 );
 
-const condition = authUser =>
-  authUser && !!authUser.roles[ROLES.ADMIN];
+const condition = authUser => authUser && !!authUser.roles[ROLES.ADMIN];
 
 export default compose(
 	withEmailVerification,
-  withAuthorization(condition),
+	withAuthorization(condition)
 )(AdminPage);
