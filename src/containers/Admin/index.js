e0febe16892c7704098f649e7a350d4269455769
Mo Sharif <mo@mosh-media.com>
2020-01-28 23:20:42 -0800

    Update index.js

--- src/containers/Admin/index.js
+++ src/containers/Admin/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 
 import * as ROLES from "../../constants/roles";
 import * as ROUTES from "../../constants/routes";
@@ -6,19 +6,28 @@ import AdminNav from "./AdminNav";
 import { compose } from "recompose";
 import { Switch, Route } from "react-router-dom";
 import PageCard from "../../components/shared/PageCard";
+import Spinner from "../../components/shared/Spinner";
 import { UserList, UserItem } from "../../components/Users";
 import {
 	withAuthorization,
 	withEmailVerification
 } from "../../components/Session";
 
+const Questions = lazy(() => import("./Questions"));
+
 const AdminPage = ({ history }) => (
 	<PageCard title="Admin">
-    <AdminNav history={history} />
+		<AdminNav history={history} />
+		<Suspense fallback={<Spinner loading={true} color="primary" />}></Suspense>
 		<Switch>
-			<Route exact path={ROUTES.ADMIN_DETAILS.path} component={UserItem} />
-			<Route exact path={ROUTES.ADMIN.path} component={UserList} />
-			<Route exact path={ROUTES.ADMIN_ADD_QUESTION.path} component={UserList} />
+			<Route exact path={ROUTES.ADMIN_QUESTIONS.path} component={Questions} />
+			<Route exact path={ROUTES.ADMIN_DETAILS.path} component={UserList} />
+			<Route exact path={ROUTES.ADMIN_USERS.path} component={UserList} />
+			<Route
+				exact
+				path={ROUTES.ADMIN_USERS.path + "/:id"}
+				component={UserItem}
+			/>
 		</Switch>
 	</PageCard>
 );
