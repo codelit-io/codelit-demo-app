31ea29392924fde8d748945e931c99e38ecebd5a
Mo Sharif <mo@mosh-media.com>
2020-01-29 22:20:16 -0800

    Update Questions.js

--- src/containers/Admin/Questions/Questions.js
+++ src/containers/Admin/Questions/Questions.js
@@ -2,132 +2,93 @@ import React, { useState, useEffect } from "react";
 import Button from "@material-ui/core/Button";
 import Input from "@material-ui/core/Input";
 import { AuthUserContext } from "../../../components/Session";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import PostAddIcon from "@material-ui/icons/PostAdd";
 import Spinner from "../../../components/shared/Spinner";
 import { withFirebase } from "../../../components/Firebase";
-import QuestionList from "./QuestionList";
+import QuestionsTable from "./QuestionsTable";
 
 const Questions = ({ firebase }) => {
-	const [state, setState] = useState({
-		text: "",
-		loading: false,
-		questions: [],
-		limit: 15
+	const [loading, setLoading] = useState(false);
+	const [question] = useState({
+		answer: "<button> I am a Button </button>",
+		element: "button",
+		id: "0",
+		isCorrect: false,
+		label: "Fix Html button tag syntax",
+		url: "Fix-Html-button-tag-syntax",
+		question: "<button> I am a Button <button>",
+		status: "😴"
 	});
-
-	// const onListenForQuestions = () => {
-	// 	setState({ loading: false });
-	// 	firebase
-	// 		._getQuestions()
-	// 		.orderBy("createdAt", "desc")
-	// 		.limit(state.limit)
-	// 		.onSnapshot(snapshot => {
-	// 			if (snapshot.size) {
-	// 				let questions = [];
-	// 				snapshot.forEach(doc =>
-	// 					questions.push({ ...doc.data(), uid: doc.id })
-	// 				);
-	// 				setState({
-	// 					questions: questions.reverse(),
-	// 					loading: false
-	// 				});
-	// 			} else {
-	// 				setState({ questions: null, loading: false });
-	// 			}
-	// 		});
-	// };
+	const [questions, setQuestions] = useState(null);
 
 	useEffect(() => {
-		setState({ loading: false });
-		const unsubscribe = firebase
-			._getQuestions()
-			.orderBy("createdAt", "desc")
-			.limit(15)
-			.onSnapshot(snapshot => {
-				if (snapshot.size) {
-					let questions = [];
-					snapshot.forEach(doc =>
-						questions.push({ ...doc.data(), uid: doc.id })
-					);
-					setState({
-						questions: questions.reverse(),
-						loading: false
-					});
-				} else {
-					setState({ questions: null, loading: false });
-				}
-			});
+		setLoading(true);
+		const unsubscribe = firebase.questions().onSnapshot(snapshot => {
+			if (snapshot.size) {
+				let questions = [];
+				snapshot.forEach(doc => questions.push({ ...doc.data(), uid: doc.id }));
+				setQuestions(questions);
+			} else {
+				setQuestions(null);
+			}
+			setLoading(false);
+		});
 
 		return () => unsubscribe();
 	}, [firebase]);
 
 	const onChangeText = event => {
-		setState({ text: event.target.value });
+		setQuestions({ label: event.target.value });
 	};
 
 	const onCreateQuestion = (event, authUser) => {
-		if (state.text) {
+		console.log(event)
+		if (question.label) {
 			firebase.questions().add({
-				text: state.text,
+				...question,
 				userId: authUser.uid,
 				createdAt: firebase.fieldValue.serverTimestamp()
 			});
-
-			setState({ text: "" });
 		}
-		event.preventDefault();
+		// event.preventDefault();
 	};
 
-	const onEditQuestion = (question, text) => {
+	const onEditQuestion = (question, label) => {
 		const { uid, ...questionSnapshot } = question;
 
 		firebase.question(question.uid).update({
 			...questionSnapshot,
-			text,
+			label,
 			editedAt: firebase.fieldValue.serverTimestamp()
 		});
 	};
 
 	const onRemoveQuestion = uid => {
-		firebase._getQuestion(uid).delete();
+		firebase.question(uid).delete();
 	};
-
-	const onNextPage = () => {
-		setState(state => ({ limit: state.limit + 15 }));
-	};
-
-	const { text, questions, loading } = state;
 
 	return (
 		<AuthUserContext.Consumer>
 			{authUser => (
-				<div>
+				<>
 					{loading && <Spinner loading={loading} />}
 
-					{questions && (
-						<QuestionList
-							authUser={authUser}
-							questions={questions}
-							onEditQuestion={onEditQuestion}
-							onRemoveQuestion={onRemoveQuestion}
-						/>
-					)}
-
 					{!questions && <div>There are no questions ...</div>}
-					{!loading && questions && (
-						<Button variant="contained" onClick={onNextPage}>
-							<ExpandMoreIcon />
-							More
-						</Button>
-					)}
 					<form onSubmit={event => onCreateQuestion(event, authUser)}>
-						<Input type="text" value={text} onChange={onChangeText} />
+						<Input type="text" value={question.label} onChange={onChangeText} />
 						<Button variant="contained" color="primary" type="submit">
 							Post <PostAddIcon />
 						</Button>
 					</form>
-				</div>
+					{questions && (
+						<QuestionsTable
+							questions={questions}
+							onEditQuestion={onEditQuestion}
+							onRemoveQuestion={onRemoveQuestion}
+							onCreateQuestion={event => onCreateQuestion(event, authUser)}
+						/>
+					)}
+				</>
 			)}
 		</AuthUserContext.Consumer>
 	);
