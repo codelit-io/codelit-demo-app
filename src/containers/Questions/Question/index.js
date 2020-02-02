d13655608a52d8df53bbebbcd8e6fd035ad00f4d
Mo Sharif <mo@mosh-media.com>
2020-02-02 00:28:23 -0800

    Update index.js

--- src/containers/Questions/Question/index.js
+++ src/containers/Questions/Question/index.js
@@ -1,19 +1,37 @@
 import React, { useEffect, useState } from "react";
 
+import * as ROUTES from "../../../constants/routes";
+
 import { AuthUserContext } from "../../../components/Session";
-import BottomNav from "../../../components/BottomNav";
 import CodeEditor from "../../../components/CodeEditor";
+import CongratsCard from "../CongratsCard";
 import PageHeader from "../../../components/shared/PageHeader";
-import { withAuthentication } from "../../../components/Session";
 import Spinner from "../../../components/shared/Spinner";
+import { withAuthentication } from "../../../components/Session";
 
 const Question = ({ firebase, history, match }) => {
 	const [loading, setLoading] = useState(true);
 	const [question, setQuestion] = useState({});
+	const [isCorrect, setIsCorrect] = useState(false);
+
+	const getQuestionById = id => {
+		firebase.getQuestionById(id).onSnapshot(snapshot => {
+			if (snapshot.size) {
+				let question = [];
+				snapshot.forEach(doc => question.push({ ...doc.data(), uid: doc.id }));
+				history.push(ROUTES.QUESTIONS.path + "/" + question[0].slug);
+				setQuestion(question[0]);
+			} else {
+				setQuestion({});
+				setIsCorrect(false);
+			}
+		});
+	};
 
 	useEffect(() => {
 		const slug = match.params.question;
 		setLoading(true);
+		setIsCorrect(false);
 		const unsubscribe = firebase.question(slug).onSnapshot(snapshot => {
 			setQuestion(snapshot.data());
 			setLoading(false);
@@ -24,17 +42,20 @@ const Question = ({ firebase, history, match }) => {
 
 	return (
 		<>
-			<PageHeader img="" title="Code Playground" history={history} />
+			<PageHeader img="" title="Questions" history={history} />
 			<Spinner loading={loading} color="primary" />
-			<CodeEditor question={question} />
+
+			<CodeEditor
+				question={question}
+				setIsCorrect={isCorrect => setIsCorrect(isCorrect)}
+			/>
+
 			<AuthUserContext.Consumer>
 				{authUser => (
-					<BottomNav
+					<CongratsCard
+						isActive={isCorrect}
 						authUser={authUser}
-						firebase={firebase}
-						history={history}
-						match={match}
-						question={question}
+						triggerNextQuestion={() => getQuestionById(Number(question.id) + 1)}
 					/>
 				)}
 			</AuthUserContext.Consumer>
