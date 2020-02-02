abe95e113f26928a5549eba801b1ee30d08911b5
Mo Sharif <mo@mosh-media.com>
2020-02-02 00:28:43 -0800

    Update styles.js

--- src/components/CodeEditor/styles.js
+++ src/components/CodeEditor/styles.js
@@ -1,6 +1,6 @@
 const styles = theme => ({
 	editor: {
-		background: "#fff",
+		// background: "#fff",
 		borderRadius: "8px",
 		boxShadow:
 			"0 24px 24px -18px rgba(69,104,129,.33), 0 9px 45px 0 rgba(114,119,160,.12)",
