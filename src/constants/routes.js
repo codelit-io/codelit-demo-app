eb289b851ad6a7cf23074a462f866d90a6112359
Mo Sharif <mo@mosh-media.com>
2020-02-02 00:28:36 -0800

    Update routes.js

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,27 +1,49 @@
 /*  Pages */
-export const ACCOUNT = { path: '/account', title: 'My account'};
-export const ADMIN = { path: '/admin', title: 'Admin Page'};
-export const ADMIN_DETAILS = { path: '/admin/:id', title: 'Admin Details'};
-export const ADMIN_QUESTIONS = { path: '/admin/questions', title: 'Questions'};
-export const ADMIN_USERS = { path: '/admin/users', title: 'Users'};
+export const ACCOUNT = { path: "/account", title: "My account" };
+export const ADMIN = { path: "/admin", title: "Admin Page" };
+export const ADMIN_DETAILS = { path: "/admin/:id", title: "Admin Details" };
+export const ADMIN_QUESTIONS = { path: "/admin/questions", title: "Questions" };
+export const ADMIN_USERS = { path: "/admin/users", title: "Users" };
 
-export const HOME = { path: '/home', title: 'Home Page'};
-export const LANDING = { path: '/', title: 'New way to Mastering Front-end Development'};
-export const LEARN = { path: '/learn', title: 'Pick your career path'};
-export const QUESTIONS = { path: '/questions', title: 'Question'}
-export const SIGN_IN = { path: '/signin', title: 'Sign in'};
-export const SIGN_UP = { path: '/signup', title: 'Sign up'};
-export const PASSWORD_FORGET = { path: '/pw-forget', title: 'Forgot Password'};
-export const NOT_FOUND = {path: '/not_found', title: "Page Not Found"}
+export const HOME = { path: "/home", title: "Home Page" };
+export const LANDING = {
+	path: "/",
+	title: "New way to Mastering Front-end Development"
+};
+export const LEARN = { path: "/learn", title: "Pick your career path" };
+export const QUESTIONS = { path: "/questions", title: "Question" };
+export const SIGN_IN = { path: "/signin", title: "Sign in" };
+export const SIGN_UP = { path: "/signup", title: "Sign up" };
+export const PASSWORD_FORGET = { path: "/pw-forget", title: "Forgot Password" };
+export const PLAYGROUND = { path: "/playgound", title: "React Playground" };
+export const NOT_FOUND = { path: "/not_found", title: "Page Not Found" };
 
 /*  Course */
-export const FRONTEND = { path: '/learn/frontend', title: 'Get started with front end tech'};
-export const BACKEND = { path: '/learn/backend', title: 'Get started with backend end tech'};
-export const DEVOPS = { path: '/learn/devops', title: 'Get started with DevOps tech'};
+export const FRONTEND = {
+	path: "/learn/frontend",
+	title: "Get started with front end tech"
+};
+export const BACKEND = {
+	path: "/learn/backend",
+	title: "Get started with backend end tech"
+};
+export const DEVOPS = {
+	path: "/learn/devops",
+	title: "Get started with DevOps tech"
+};
 
 /* Topics */
-export const HTML = { path: '/learn/frontend/Html', title: 'Basics of HTML'};
-export const CSS = { path: '/learn/frontend/Css', title: 'Basics of CSS'};
-export const JAVASCRIPT = { path: '/learn/frontend/JavaScript', title: 'Basics of JavaScript'};
-export const REACT = { path: '/learn/frontend/ReactJs', title: 'Basics of React JS Library'};
-export const ANGULAR = { path: '/learn/frontend/Angular', title: 'Basics of Angular JS Framework'};
+export const HTML = { path: "/learn/frontend/Html", title: "Basics of HTML" };
+export const CSS = { path: "/learn/frontend/Css", title: "Basics of CSS" };
+export const JAVASCRIPT = {
+	path: "/learn/frontend/JavaScript",
+	title: "Basics of JavaScript"
+};
+export const REACT = {
+	path: "/learn/frontend/ReactJs",
+	title: "Basics of React JS Library"
+};
+export const ANGULAR = {
+	path: "/learn/frontend/Angular",
+	title: "Basics of Angular JS Framework"
+};
