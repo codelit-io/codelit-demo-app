e0c95b58b0881d0ac8bcdf56a3c75491b6236543
Mo Sharif <mo@mosh-media.com>
2020-02-02 00:28:33 -0800

    Update index.js

--- src/containers/Landing/index.js
+++ src/containers/Landing/index.js
@@ -10,25 +10,29 @@ const LandingPage = () => {
 		{
 			label: "Get Started",
 			desc: "Your first step to success",
-			img: "https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/icons%2F016-elearning.png?alt=media&token=cae6ea58-363c-4e6f-a7fe-563bb30b34bc",
+			img:
+				"https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/icons%2F016-elearning.png?alt=media&token=cae6ea58-363c-4e6f-a7fe-563bb30b34bc",
 			url: ROUTES.QUESTIONS.path
+		},
+		{
+			label: "Playground",
+			desc: "Live React JSX environment",
+			img:
+				"https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/icons%2F026-education.png?alt=media&token=59b529d9-14ae-4f05-a5eb-c758ce55b2a1",
+			url: ROUTES.PLAYGROUND.path
 		},
 		{
 			label: "Start Learning",
 			desc: "Explorer Front-end topics",
-			img: "https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/icons%2F023-teaching.png?alt=media&token=9d079583-6608-4c1f-8762-c52c15179bcc",
+			img:
+				"https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/icons%2F023-teaching.png?alt=media&token=9d079583-6608-4c1f-8762-c52c15179bcc",
 			url: ROUTES.LEARN.path
-		},
-		{
-			label: "My Lessons",
-			desc: "All in one place",
-			img: "https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/icons%2F026-education.png?alt=media&token=59b529d9-14ae-4f05-a5eb-c758ce55b2a1",
-			disable: true
 		},
 		{
 			label: "My Team",
 			desc: "Say Hi!",
-			img: "https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/icons%2F009-collaboration.png?alt=media&token=a7e1e9e8-8b2b-4a32-86ad-0302de87407e",
+			img:
+				"https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/icons%2F009-collaboration.png?alt=media&token=a7e1e9e8-8b2b-4a32-86ad-0302de87407e",
 			disable: true
 		}
 	];
@@ -45,7 +49,11 @@ const LandingPage = () => {
 			</Grid>
 			<Typography variant="subtitle1">
 				Icons made by{" "}
-				<a href="https://www.flaticon.com/authors/wanicon" title="wanicon" style={{ color: "#3a9fff" }}>
+				<a
+					href="https://www.flaticon.com/authors/wanicon"
+					title="wanicon"
+					style={{ color: "#3a9fff" }}
+				>
 					wanicon
 				</a>
 			</Typography>
