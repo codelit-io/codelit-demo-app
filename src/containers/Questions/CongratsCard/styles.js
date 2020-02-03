3b137f53b5d968d7580f2541e3de84436d1a971f
Mo Sharif <mo@mosh-media.com>
2020-02-02 18:09:42 -0800

    Update styles.js

--- src/containers/Questions/CongratsCard/styles.js
+++ src/containers/Questions/CongratsCard/styles.js
@@ -1,30 +1,14 @@
 const styles = theme => ({
 	container: {
-		position: "fixed",
-		bottom: "0px",
-		width: "95%",
 		textAlign: "center",
-		zIndex: "-1"
-	},
-	card: {
-		boxShadow:
-			"0 24px 24px -18px rgba(69,104,129,.33), 0 9px 45px 0 rgba(114,119,160,.12)",
-		"&:hover": {
-			transform: "translateY(-5px)",
-			backgroundColor: "#FFF"
-		},
-		"&:hover .desc": {
-			color: "#1890ff"
-		},
-		transition:
-			"transform .35s cubic-bezier(.4,0,.2,1),box-shadow .35s cubic-bezier(.4,0,.2,1)",
-		transform: "translateY(0)"
 	},
 	img: {
-		width: "40%",
-		maxWidth:"40%",
+		position:"fixed",
+    	zIndex: "-3",
+		bottom: "0",
+		maxWidth: "48%",
 		[theme.breakpoints.down(500)]: {
-			width: "65%"
+			display: "none"
 		}
 	},
 	button: {
@@ -35,7 +19,7 @@ const styles = theme => ({
 		fontSize: "16px",
 		padding: "0 30px",
 		background: "linear-gradient(45deg, #dbf2a1 30%, #04baab 90%)",
-		radius: "3px"
+		radius: "3px",
 	}
 });
 
