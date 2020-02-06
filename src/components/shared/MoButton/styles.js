813b75254a0efef66ae52aef873696d1152c88ab
Mo Sharif <mo@mosh-media.com>
2020-02-05 22:52:33 -0800

    Create styles.js

---
+++ src/components/shared/MoButton/styles.js
@@ -0,0 +1,14 @@
+const styles = theme => ({
+	button: {
+		marginBottom: theme.spacing(6),
+		color: "#252828",
+		border: "0",
+		height: "48px",
+		fontSize: "16px",
+		padding: "0 30px",
+		background: "linear-gradient(45deg, #dbf2a1 30%, #04baab 90%)",
+		radius: "3px"
+	}
+});
+
+export default styles;
