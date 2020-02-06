a69c9655173949d7112c0920c270d5f9141e9a6c
Mo Sharif <mo@mosh-media.com>
2020-02-05 22:52:36 -0800

    Update index.js

--- src/components/ComponentMaker/MoComponent/index.js
+++ src/components/ComponentMaker/MoComponent/index.js
@@ -2,6 +2,12 @@ import React, { Suspense } from "react";
 import Components from "../Components"
 import Spinner from "../../shared/Spinner";
 
+/*  MoComponent is awesome because
+*	Just feed it props.element with
+*	a component name like "button" or
+*	"input". These are defined in Components
+ */
+
 const MoComponent = props => {
 	const Component = Components[props.element];
 	return (
