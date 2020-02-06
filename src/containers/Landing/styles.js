1bae6a5d73231d068a65fcef910a50543644aadd
Mo Sharif <mo@mosh-media.com>
2020-02-05 22:52:26 -0800

    Create styles.js

---
+++ src/containers/Landing/styles.js
@@ -0,0 +1,23 @@
+const styles = theme => ({
+    container: {
+        paddingTop: "90px",  
+        paddingBottom: "90px",  
+    },
+    heroText: {
+        color: "rgb(43, 43, 43)",
+        fontSize: "40px",
+        fontWeight: "500",
+        lineHeight: "1.2",
+        letterSpacing:"-0.01em",
+        marginBottom: "30px",
+    },
+    heroSubtitle: {
+        color: "rgb(94, 94, 94)",
+        fontSize: "24px",
+        lineHeight: "1.5",
+        letterSpacing: "-0.01em",
+        marginBottom: "30px",
+    }
+})
+
+export default styles;
