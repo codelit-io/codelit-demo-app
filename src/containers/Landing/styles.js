4825e6fea1affdb9683e9404582ef61f2a308371
Mo Sharif <mo@mosh-media.com>
2020-02-07 23:47:55 -0800

    Update styles.js

--- src/containers/Landing/styles.js
+++ src/containers/Landing/styles.js
@@ -1,23 +1,31 @@
 const styles = theme => ({
-    container: {
-        paddingTop: "90px",  
-        paddingBottom: "90px",  
-    },
-    heroText: {
-        color: "rgb(43, 43, 43)",
-        fontSize: "40px",
-        fontWeight: "500",
-        lineHeight: "1.2",
-        letterSpacing:"-0.01em",
-        marginBottom: "30px",
-    },
-    heroSubtitle: {
-        color: "rgb(94, 94, 94)",
-        fontSize: "24px",
-        lineHeight: "1.5",
-        letterSpacing: "-0.01em",
-        marginBottom: "30px",
-    }
-})
+	container: {
+		alignItems: "center"
+	},
+	heroText: {
+		color: "rgb(43, 43, 43)",
+		fontSize: "40px",
+		fontWeight: "500",
+		lineHeight: "1.2",
+		letterSpacing: "-0.01em",
+		marginBottom: "30px"
+	},
+	heroSubtitle: {
+		color: "rgb(94, 94, 94)",
+		fontSize: "24px",
+		lineHeight: "1.5",
+		letterSpacing: "-0.01em",
+		marginBottom: "30px"
+	},
+	img: {
+		width: "100%",
+		[theme.breakpoints.down("sm")]: {
+			display: "none"
+		}
+	},
+	centeredContainer: {
+		textAlign: "center"
+	}
+});
 
 export default styles;
