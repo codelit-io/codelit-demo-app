29891f745c01d59ba57a787ff7ba10d5b090732c
Mo Sharif <mo@mosh-media.com>
2020-02-08 17:01:24 -0800

    Delete index.test.js

--- src/containers/Learn/index.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Learn from "./index.js";
-import renderer from "react-test-renderer";
-
-it("Learn Page", () => {
-	const tree = renderer.create(<Learn />).toJSON();
-	expect(tree).toMatchSnapshot();
-});
