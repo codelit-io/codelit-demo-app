da730ab7f34390d6b25901b4fae08d6bb10c1108
Mo Sharif <mo@mosh-media.com>
2020-02-08 17:01:15 -0800

    Update routes.js

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -10,40 +10,9 @@ export const LANDING = {
 	path: "/",
 	title: "New way to Mastering Front-end Development"
 };
-export const LEARN = { path: "/learn", title: "Pick your career path" };
 export const QUESTIONS = { path: "/questions", title: "Question" };
 export const SIGN_IN = { path: "/signin", title: "Sign in" };
 export const SIGN_UP = { path: "/signup", title: "Sign up" };
 export const PASSWORD_FORGET = { path: "/pw-forget", title: "Forgot Password" };
 export const PLAYGROUND = { path: "/playgound", title: "React Playground" };
 export const NOT_FOUND = { path: "/not_found", title: "Page Not Found" };
-
-/*  Course */
-export const FRONTEND = {
-	path: "/learn/frontend",
-	title: "Get started with front end tech"
-};
-export const BACKEND = {
-	path: "/learn/backend",
-	title: "Get started with backend end tech"
-};
-export const DEVOPS = {
-	path: "/learn/devops",
-	title: "Get started with DevOps tech"
-};
-
-/* Topics */
-export const HTML = { path: "/learn/frontend/Html", title: "Basics of HTML" };
-export const CSS = { path: "/learn/frontend/Css", title: "Basics of CSS" };
-export const JAVASCRIPT = {
-	path: "/learn/frontend/JavaScript",
-	title: "Basics of JavaScript"
-};
-export const REACT = {
-	path: "/learn/frontend/ReactJs",
-	title: "Basics of React JS Library"
-};
-export const ANGULAR = {
-	path: "/learn/frontend/Angular",
-	title: "Basics of Angular JS Framework"
-};
