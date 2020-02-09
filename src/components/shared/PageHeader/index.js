52384cb5a641bdb29b42f198dfe73b0b28eecddf
Mo Sharif <mo@mosh-media.com>
2020-02-08 17:01:16 -0800

    Update index.js

--- src/components/shared/PageHeader/index.js
+++ src/components/shared/PageHeader/index.js
@@ -1,24 +1,15 @@
 import React from "react";
 
-import * as ROUTES from "../../../constants/routes";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import Box from "@material-ui/core/Box";
 import Fade from "@material-ui/core/Fade";
 import IconButton from "@material-ui/core/IconButton";
 import Grid from "@material-ui/core/Grid";
-import { Link } from "react-router-dom";
 import styles from "./styles";
 import Typography from "@material-ui/core/Typography";
 import withStyles from "@material-ui/core/styles/withStyles";
 
-const PageHeader = ({
-	classes,
-	course,
-	history,
-	subTopic,
-	title,
-	topic
-}) => {
+const PageHeader = ({ classes, history, title }) => {
 	return (
 		<Fade in={true} timeout={{ enter: 1000 }}>
 			<Grid container spacing={4}>
@@ -32,26 +23,6 @@ const PageHeader = ({
 				<Grid item xs={12} sm={12} md={4}>
 					<Typography variant="h4" gutterBottom className={classes.header}>
 						<Box fontWeight="fontWeightLight" className={classes.linkText}>
-							<Link
-								to={`${ROUTES.LEARN.path}/${course}`}
-								className={classes.linkText}
-							>
-								{course && <span>{course}</span>}
-							</Link>
-							<Link
-								to={`${ROUTES.LEARN.path}/${course}/${topic}`}
-								className={classes.linkText}
-							>
-								{topic && <span>&nbsp;/&nbsp;{topic}</span>}
-							</Link>
-							<Link
-								className={classes.linkText}
-								to={`${ROUTES.LEARN.path}/${course}/${topic}/${subTopic}`}
-							>
-								{subTopic && (
-									<span>&nbsp;/&nbsp;{subTopic.replace(/-/g, " ")}</span>
-								)}
-							</Link>
 							{title}
 						</Box>
 					</Typography>
