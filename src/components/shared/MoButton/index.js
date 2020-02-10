0df01199fa175fe2b0fa5fce09da4a910f77f811
Mo Sharif <mo@mosh-media.com>
2020-02-09 22:38:58 -0800

    Update index.js

--- src/components/shared/MoButton/index.js
+++ src/components/shared/MoButton/index.js
@@ -2,20 +2,23 @@ import React from "react";
 
 import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
 import styles from "./styles";
 import withStyles from "@material-ui/core/styles/withStyles";
 
 const MoButton = ({ classes, handleClick, href, text }) => {
 	return (
-		<Button
-			color="primary"
-			className={classes.button}
-			href={href}
-			onClick={() => (href ? "" : handleClick())}
-			variant="contained"
-		>
-			{text} <ArrowForwardIcon />
-		</Button>
+		<Link to={href ? href : "#"} style={{textDecoration:"none"}}>
+			<Button
+				color="primary"
+				className={classes.button}
+				onClick={() => (href ? "" : handleClick())}
+				variant="contained"
+			>
+				{text}
+				<ArrowForwardIcon />
+			</Button>
+		</Link>
 	);
 };
 
