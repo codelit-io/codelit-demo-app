e9d4e93516f0c2cf5b4f2faf203c28adf91fe8d2
Mo Sharif <mo@mosh-media.com>
2020-02-10 22:34:03 -0800

    Update index.js

--- src/components/CodeEditor/index.js
+++ src/components/CodeEditor/index.js
@@ -1,86 +1,60 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import { LiveEditor, LiveProvider, LivePreview, LiveError } from "react-live";
 import { Grid } from "@material-ui/core";
 import Headline from "../shared/Headline";
 import styles from "./styles";
 import Slide from "@material-ui/core/Slide";
-import Spinner from "../../components/shared/Spinner";
 import Title from "../../components/shared/Title";
 import withStyles from "@material-ui/core/styles/withStyles";
 import { reactLiveTheme } from "../../utils/reactLiveTheme";
 
-const CodeEditor = ({ classes, question, setIsCorrect }) => {
-	const [state, setState] = useState([]);
-	const [loading, setLoading] = useState(true);
-
-	const handleOnChange = userAnswer => {
-		if (userAnswer === "{}" || userAnswer === "") {
-			return;
-		}
-		if (userAnswer.replace(/\s/g, "") === state.answer.replace(/\s/g, "")) {
-			setState({ ...state, isCorrect: true, question: userAnswer });
-			setIsCorrect(true);
-		} else {
-			setState({ ...state, question: userAnswer });
-		}
-	};
-
-	useEffect(() => {
-		setState(question);
-		setLoading(false);
-		return () => setState([]);
-	}, [question]);
-
+const CodeEditor = ({ classes, question, handleOnChange }) => {
 	return (
-		question && (
-			<Grid container spacing={4}>
-				<Spinner loading={loading} color="primary" />
-
-				<LiveProvider code={state.question} language="jsx" noInline={false}>
-					<Grid item md={6} sm={12} xs={12} style={{ width: "100%" }}>
-						<Title text={state.label} fade={true} />
-						<Slide
-							direction="right"
-							in={state.answer && true}
-							mountOnEnter
-							unmountOnExit
+		<Grid container spacing={4}>
+			<LiveProvider code={question.question} language="jsx" noInline={false}>
+				<Grid item md={6} sm={12} xs={12} style={{ width: "100%" }}>
+					<Title text={question.label} fade={question.label && true} />
+					<Slide
+						direction="right"
+						in={question.label && true}
+						mountOnEnter
+						unmountOnExit
+					>
+						<div
+							className={`${classes.editor} ${
+								classes.card
+							} ${question.isCorrect && classes.correct}`}
 						>
-							<div
-								className={`${classes.editor} ${
-									classes.card
-								} ${state.isCorrect && classes.correct}`}
-							>
-								<LiveEditor onChange={handleOnChange} theme={reactLiveTheme} />
-							</div>
-						</Slide>
-					</Grid>
-					<Grid item md={6} sm={12} xs={12}>
-						<Title text="Code Preview" fade={true} />
-						<Slide
-							direction="left"
-							in={question.isPlayground || state.isCorrect}
-							mountOnEnter
-							unmountOnExit
-						>
-							<div className={classes.browserMockup}>
-								<div className={classes.browserButtons}></div>
-								<div className={classes.browserButtons2}></div>
-								<LivePreview className={classes.preview} />
-							</div>
-						</Slide>
-					</Grid>
-					{question.isPlayground && (
-						<Grid item md={12} sm={12} xs={12}>
-							<LiveError />
-						</Grid>
-					)}
+							<LiveEditor onChange={handleOnChange} theme={reactLiveTheme} />
+						</div>
+					</Slide>
+				</Grid>
+				<Grid item md={6} sm={12} xs={12}>
+					<Title text="Code Preview" fade={true} />
+					<Slide
+						direction="left"
+						in={question.isPlayground || question.isCorrect}
+						mountOnEnter
+						unmountOnExit
+					>
+						<div className={classes.browserMockup}>
+							<div className={classes.browserButtons}></div>
+							<div className={classes.browserButtons2}></div>
+							<LivePreview className={classes.preview} />
+						</div>
+					</Slide>
+				</Grid>
+				{question.isPlayground && (
 					<Grid item md={12} sm={12} xs={12}>
-						<Headline isCorrect={state && state.isCorrect} />
+						<LiveError />
 					</Grid>
-				</LiveProvider>
-			</Grid>
-		)
+				)}
+				<Grid item md={12} sm={12} xs={12}>
+					<Headline isCorrect={question && question.isCorrect} />
+				</Grid>
+			</LiveProvider>
+		</Grid>
 	);
 };
 
