f6952d0badc98f846d0b89d071b68fd909f72bf9
Mo Sharif <mo@moskool.com>
2020-02-28 19:08:46 -0800

    Delete index.test.js

--- src/containers/SignUp/index.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import SignUpPage from "./index.js";
-import renderer from "react-test-renderer";
-
-it("Sign Up Page", () => {
-	const tree = renderer.create(<SignUpPage />).toJSON();
-	expect(tree).toMatchSnapshot();
-});
