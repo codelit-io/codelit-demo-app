94d44eb8aa315a5aab3b119f08c43f7627c16914
Mo Sharif <mo@moskool.com>
2020-02-28 19:09:06 -0800

    Update index.js

--- src/components/shared/MoButton/index.js
+++ src/components/shared/MoButton/index.js
@@ -2,24 +2,21 @@ import React from "react";
 
 import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 import Button from "@material-ui/core/Button";
-import { Link } from "react-router-dom";
 import styles from "./styles";
 import withStyles from "@material-ui/core/styles/withStyles";
 
-const MoButton = ({ classes, handleClick, href, text }) => {
-	return (
-		<Link to={href ? href : "#"} style={{textDecoration:"none"}}>
-			<Button
-				color="primary"
-				className={classes.button}
-				onClick={() => (href ? "" : handleClick())}
-				variant="contained"
-			>
-				{text}
-				<ArrowForwardIcon />
-			</Button>
-		</Link>
-	);
+const MoButton = ({ classes, handleClick, text }) => {
+  return (
+    <Button
+      color="primary"
+      className={classes.button}
+      onClick={() => handleClick()}
+      variant="contained"
+    >
+      {text}
+      <ArrowForwardIcon />
+    </Button>
+  );
 };
 
 export default withStyles(styles)(MoButton);
