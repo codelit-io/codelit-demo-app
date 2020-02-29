4822974bff5e23db71abe4e5c779be0078d85c2f
Mo Sharif <mo@moskool.com>
2020-02-28 19:09:15 -0800

    Create styles.js

---
+++ src/components/shared/MoLink/styles.js
@@ -0,0 +1,13 @@
+const styles = theme => ({
+  link: {
+    backgroundColor: "#daff81",
+    textDecoration: "none",
+    padding: theme.spacing(2),
+    color: "black",
+    fontSize: "20px",
+    boxShadow: "0 24px 24px -18px rgba(69,104,129,.33), 0 9px 45px 0 rgba(114,119,160,.12)",
+    borderRadius: "3px"
+  }
+});
+
+export default styles;
