b95d78aa4d05939bb04c668635b2fc0860613559
Mo Sharif <mo@moskool.com>
2020-02-28 22:08:31 -0800

    Update styles.js

--- src/components/shared/MoButton/styles.js
+++ src/components/shared/MoButton/styles.js
@@ -1,13 +1,13 @@
 const styles = theme => ({
 	button: {
 		padding: theme.spacing(2),
-		color: "black",
+		color: "#545452",
 		fontSize: "20px",
 		boxShadow: "0 24px 24px -18px rgba(69,104,129,.33), 0 9px 45px 0 rgba(114,119,160,.12)",
-		backgroundColor: "#daff81",
+		backgroundColor: "#dbf3a1",
 		borderRadius: "3px",
 		"&:hover": {
-			backgroundColor: "#daff81",
+			backgroundColor: "#dbf3a1",
 		}
 	}
 });
