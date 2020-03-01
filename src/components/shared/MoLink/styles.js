af35f54bce052495c029dc2987a6fff64a5785c2
Mo Sharif <mo@mosh-media.com>
2020-03-01 15:01:33 -0800

    Update styles.js

--- src/components/shared/MoLink/styles.js
+++ src/components/shared/MoLink/styles.js
@@ -1,9 +1,9 @@
 const styles = theme => ({
   link: {
-    backgroundColor: "#dbf3a1",
+    backgroundColor: "#41D3BD",
     textDecoration: "none",
     padding: theme.spacing(2),
-    color: "#545452",
+    color: "#FFFFF2",
     fontSize: "20px",
     boxShadow: "0 24px 24px -18px rgba(69,104,129,.33), 0 9px 45px 0 rgba(114,119,160,.12)",
     borderRadius: "3px"
