5738fefaa464dcf111ffb32048f7d4e627ba46e2
Mo Sharif <mo@mosh-media.com>
2020-03-01 15:01:31 -0800

    Update styles.js

--- src/components/shared/MoButton/styles.js
+++ src/components/shared/MoButton/styles.js
@@ -1,13 +1,13 @@
 const styles = theme => ({
 	button: {
 		padding: theme.spacing(2),
-		color: "#545452",
+		color: "#FFFFF2",
 		fontSize: "20px",
 		boxShadow: "0 24px 24px -18px rgba(69,104,129,.33), 0 9px 45px 0 rgba(114,119,160,.12)",
-		backgroundColor: "#dbf3a1",
+		backgroundColor: "#41D3BD",
 		borderRadius: "3px",
 		"&:hover": {
-			backgroundColor: "#dbf3a1",
+			backgroundColor: "#41D3BD",
 		}
 	}
 });
