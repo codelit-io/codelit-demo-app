20d89e3abedbca96cf47f6aa4da14a81d9db03fd
Mo Sharif <mo@mosh-media.com>
2020-03-01 19:05:27 -0800

    Update index.js

--- src/components/CodeEditor/LiveProviderBase/index.js
+++ src/components/CodeEditor/LiveProviderBase/index.js
@@ -11,51 +11,57 @@ import { reactLiveTheme } from "../../../utils/reactLiveTheme";
 
 const LiveProviderBase = ({ classes, question, handleOnChange }) => {
 	return (
-			<LiveProvider code={question.question} language="jsx" noInline={false}>
-				<Grid item md={6} sm={12} xs={12} style={{ width: "100%" }}>
-					<Title text={question.label} fade={question.label && true} />
-					<Slide
-						direction="right"
-						in={question.question && true}
-						mountOnEnter
-						timeout={{ enter: 800, exit: 400 }}
-						unmountOnExit
+		<LiveProvider code={question.question} language="jsx" noInline={false}>
+			<Grid item md={6} sm={12} xs={12} style={{ width: "100%" }}>
+				<Title text={question.label} fade={question.label && true} />
+				<Slide
+					direction="right"
+					in={question.question && true}
+					mountOnEnter
+					timeout={{ enter: 800, exit: 400 }}
+					unmountOnExit
+				>
+					<div
+						className={`${classes.browserMockup} ${question.isCorrect &&
+							classes.correct}`}
 					>
-						<div
-							className={`${classes.editor} ${classes.card} ${question.isCorrect &&
-								classes.correct}`}
-						>
+						<div className={classes.browserButtons}></div>
+						<div className={classes.browserFilename}>
+							<span>{`index.${question.language}`}</span>
+						</div>
+						<div className={classes.editor}>
 							<LiveEditor onChange={handleOnChange} theme={reactLiveTheme} />
 						</div>
-					</Slide>
-				</Grid>
-				<Grid item md={6} sm={12} xs={12}>
-					<Title text="Code Preview" fade={true} />
-					<Slide
-						direction="left"
-						in={(question.isPlayground && true) || (question.question && true)}
-						mountOnEnter
-						timeout={{ enter: 800, exit: 800 }}
-						unmountOnExit
-					>
-						<div className={classes.browserMockup}>
-							<div className={classes.browserButtons}></div>
-							<div className={classes.browserButtons2}></div>
-							<div className={classes.preview} >
+					</div>
+				</Slide>
+			</Grid>
+			<Grid item md={6} sm={12} xs={12}>
+				<Title text="Live Preview" fade={true} />
+				<Slide
+					direction="left"
+					in={(question.isPlayground && true) || (question.question && true)}
+					mountOnEnter
+					timeout={{ enter: 800, exit: 800 }}
+					unmountOnExit
+				>
+					<div className={classes.browserMockup}>
+						<div className={classes.browserButtons}></div>
+						<div className={classes.browserButtons2}><span>localhost:3000</span></div>
+						<div className={classes.preview}>
 							<LivePreview />
-							</div>
 						</div>
-					</Slide>
-				</Grid>
-				{question.isPlayground && (
-					<Grid item md={12} sm={12} xs={12}>
-						<LiveError />
-					</Grid>
-				)}
+					</div>
+				</Slide>
+			</Grid>
+			{question.isPlayground && (
 				<Grid item md={12} sm={12} xs={12}>
-					<Headline isCorrect={question && question.isCorrect} />
+					<LiveError />
 				</Grid>
-			</LiveProvider>
+			)}
+			<Grid item md={12} sm={12} xs={12}>
+				<Headline isCorrect={question && question.isCorrect} />
+			</Grid>
+		</LiveProvider>
 	);
 };
 
