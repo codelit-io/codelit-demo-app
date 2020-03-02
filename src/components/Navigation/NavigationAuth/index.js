a059609b35f4dc3b848f372513c21d8e03854b26
Mo Sharif <mo@mosh-media.com>
2020-03-01 21:42:09 -0800

    Update index.js

--- src/components/Navigation/NavigationAuth/index.js
+++ src/components/Navigation/NavigationAuth/index.js
@@ -1,47 +1,22 @@
 import React from "react";
 import * as ROUTES from "../../../constants/routes";
 import { Link } from "react-router-dom";
-import SignOutButton from "../../SignOut";
-import { List, ListItem, Button } from "@material-ui/core";
-import * as ROLES from '../../../constants/roles';
+import { List, ListItem } from "@material-ui/core";
+import * as ROLES from "../../../constants/roles";
 
 const NavigationAuth = ({ authUser }) => (
 	<List>
-		<ListItem>
-			<Button>
-				<Link
-					to={ROUTES.HOME.path}
-					style={{ color: "black", textDecoration: "none" }}
-				>
-					Home
-				</Link>
-			</Button>
+		<ListItem button component={Link} to={ROUTES.HOME.path}>
+			Home
 		</ListItem>
-		<ListItem>
-			<Button>
-				<Link
-					to={ROUTES.ACCOUNT.path}
-					style={{ color: "black", textDecoration: "none" }}
-				>
-					Account
-				</Link>
-			</Button>
+		<ListItem button component={Link} to={ROUTES.ACCOUNT.path}>
+			My Account
 		</ListItem>
 		{authUser.roles && !!authUser.roles[ROLES.ADMIN] && (
-			<ListItem>
-				<Button>
-					<Link
-						to={ROUTES.ADMIN.path}
-						style={{ color: "black", textDecoration: "none" }}
-					>
-						Admin
-					</Link>
-				</Button>
+			<ListItem button component={Link} to={ROUTES.ACCOUNT.path}>
+				Admin
 			</ListItem>
 		)}
-		<ListItem>
-			<SignOutButton />
-		</ListItem>
 	</List>
 );
 
