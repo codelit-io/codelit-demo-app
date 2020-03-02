dbb68e99c642eb6579aaa846174223734df89361
Mo Sharif <mo@mosh-media.com>
2020-03-01 23:33:25 -0800

    Create index.js

---
+++ src/containers/Questions/Question/Content/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import Box from "@material-ui/core/Box";
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import styles from "./styles";
+import withStyles from "@material-ui/core/styles/withStyles";
+
+const Content = ({ classes, content }) => (
+	<Grid container spacing={4}>
+		<Grid item sm={12}>
+			<Card className={classes.card}>
+				<Typography variant="h4" gutterBottom className={classes.content}>
+					<Box fontWeight="fontWeightLight">{content}</Box>
+				</Typography>
+			</Card>
+		</Grid>
+	</Grid>
+);
+
+export default withStyles(styles)(Content);
