e1c2af6cb6c730d022dd75a2c100a240a5b3adb7
Mo Sharif <mo@moskool.com>
2020-03-05 18:22:09 -0800

    Added Beautifer functionality to code

--- src/containers/Admin/Questions/index.js
+++ src/containers/Admin/Questions/index.js
@@ -7,70 +7,81 @@ import { withFirebase } from "../../../components/Firebase";
 import QuestionsTable from "./QuestionsTable";
 
 const Questions = ({ firebase, history }) => {
-	const [loading, setLoading] = useState(false);
-	const [questions, setQuestions] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState(null);
 
-	useEffect(() => {
-		setLoading(true);
-		const unsubscribe = firebase.questions().onSnapshot(snapshot => {
-			if (snapshot.size) {
-				let questions = [];
-				snapshot.forEach(doc => questions.push({ ...doc.data(), uid: doc.id }));
-				setQuestions(questions);
-			} else {
-				setQuestions(null);
-			}
-			setLoading(false);
-		});
+  useEffect(() => {
+    setLoading(true);
+    const unsubscribe = firebase.questions().onSnapshot(snapshot => {
+      if (snapshot.size) {
+        let questions = [];
+        snapshot.forEach(doc => questions.push({ ...doc.data(), uid: doc.id }));
+        setQuestions(questions);
+      } else {
+        setQuestions(null);
+      }
+      setLoading(false);
+    });
 
-		return () => unsubscribe();
-	}, [firebase]);
+    return () => unsubscribe();
+  }, [firebase]);
 
-	const onCreateQuestion = (event, authUser) => {
-		if (event.label) {
-			firebase.createQuestionById({
-				...event,
-				id: Number(event.id),
-				userId: authUser.uid,
-				createdAt: firebase.fieldValue.serverTimestamp()
-			});
-		}
-	};
-
-	const onEditQuestion = event => {
-		firebase.question(event.uid).update({
-			...event,
-			editedAt: firebase.fieldValue.serverTimestamp()
-		});
-	};
-
-	const onRemoveQuestion = uid => {
-		firebase.question(uid).delete();
+  const stringifyQuestionFromEvent = event => {
+    return {
+      ...event,
+      question: JSON.stringify(event.question, null, 2)
     };
-    
-    const handleRowClick = (id) => {
-        history.push(ROUTES.QUESTIONS.path + "/" + id)
+  };
+
+  const onCreateQuestion = (event, authUser) => {
+    if (event.label) {
+      const question = stringifyQuestionFromEvent(event);
+
+      firebase.createQuestionById({
+        ...question,
+        id: Number(event.id),
+        userId: authUser.uid,
+        createdAt: firebase.fieldValue.serverTimestamp()
+      });
     }
+  };
 
-	return (
-		<AuthUserContext.Consumer>
-			{authUser => (
-				<>
-					{loading && <Spinner loading={loading} />}
+  const onEditQuestion = event => {
+	  debugger
+	const question = stringifyQuestionFromEvent(event);
+    firebase.question(event.uid).update({
+      ...question,
+      editedAt: firebase.fieldValue.serverTimestamp()
+    });
+  };
 
-					{questions && (
-						<QuestionsTable
-							questions={questions}
-							onEditQuestion={onEditQuestion}
-							onRemoveQuestion={onRemoveQuestion}
-                            onCreateQuestion={event => onCreateQuestion(event, authUser)}
-                            handleRowClick={handleRowClick}
-						/>
-					)}
-				</>
-			)}
-		</AuthUserContext.Consumer>
-	);
+  const onRemoveQuestion = uid => {
+    firebase.question(uid).delete();
+  };
+
+  const handleRowClick = id => {
+    history.push(ROUTES.QUESTIONS.path + "/" + id);
+  };
+
+  return (
+    <AuthUserContext.Consumer>
+      {authUser => (
+        <>
+          {loading && <Spinner loading={loading} />}
+
+          {questions && (
+            <QuestionsTable
+              questions={questions}
+              onEditQuestion={onEditQuestion}
+              onRemoveQuestion={onRemoveQuestion}
+              onCreateQuestion={event => onCreateQuestion(event, authUser)}
+              handleRowClick={handleRowClick}
+            />
+          )}
+        </>
+      )}
+    </AuthUserContext.Consumer>
+  );
 };
 
 export default withFirebase(Questions);
