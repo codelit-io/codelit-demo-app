236e3b1e2f77defd8c35f60d1a3ea84d96f17a05
Mo Sharif <mo@mosh-media.com>
2020-03-08 16:57:53 -0700

    Update routes.js

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -3,6 +3,9 @@ export const ACCOUNT = { path: "/account", title: "My account" };
 export const ADMIN = { path: "/admin", title: "Admin Page" };
 export const ADMIN_DETAILS = { path: "/admin/:id", title: "Admin Details" };
 export const ADMIN_QUESTIONS = { path: "/admin/questions", title: "Questions" };
+export const ADMIN_QUESTIONS_EASY = { path: "/admin/questions/questions", title: "Questions Easy" };
+export const ADMIN_QUESTIONS_MID = { path: "/admin/questions/questions-mid", title: "Questions Mid" };
+export const ADMIN_QUESTIONS_PRO = { path: "/admin/questions/questions-pro", title: "Questions Pro" };
 export const ADMIN_USERS = { path: "/admin/users", title: "Users" };
 
 export const HOME = { path: "/home", title: "Home Page" };
@@ -10,7 +13,7 @@ export const LANDING = {
 	path: "/",
 	title: "New way to Mastering Front-end Development"
 };
-export const QUESTIONS = { path: "/topics", title: "Topics" };
+export const QUESTIONS = { path: "/topics", title: "Easy" };
 export const SIGN_IN = { path: "/signin", title: "Sign in" };
 export const SIGN_UP = { path: "/signup", title: "Sign up" };
 export const PASSWORD_FORGET = { path: "/pw-forget", title: "Forgot Password" };
