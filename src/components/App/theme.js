4d0493170b2780e4bd7816460296a65079e668ef
Mo Sharif <mo@mosh-media.com>
2020-03-10 00:02:47 -0700

    Update theme.js

--- src/components/App/theme.js
+++ src/components/App/theme.js
@@ -3,6 +3,10 @@ import { createMuiTheme } from "@material-ui/core/styles";
 const bigShadow =
 	"0 24px 24px -18px rgba(69,104,129,.33), 0 9px 45px 0 rgba(114,119,160,.12)";
 
+const colors = {
+	grey: "#a6a6a6",
+	green: "#41D3BD",
+};
 const theme = createMuiTheme({
 	bigShadow: bigShadow,
 	card: {
@@ -17,7 +21,9 @@ const theme = createMuiTheme({
 		transition:
 			"transform .35s cubic-bezier(.4,0,.2,1),box-shadow .35s cubic-bezier(.4,0,.2,1)",
 		transform: "translateY(0)"
-	}
+	},
+	grey: colors.grey,
+	green: colors.green,
 });
 
 export default theme;
