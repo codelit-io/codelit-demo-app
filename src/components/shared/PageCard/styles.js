3f5be542b95a44200600ba6a85c2b18ee615a0f3
Mo Sharif <mo@mosh-media.com>
2020-03-09 23:27:20 -0700

    Update styles.js

--- src/components/shared/PageCard/styles.js
+++ src/components/shared/PageCard/styles.js
@@ -1,17 +1,5 @@
 const styles = theme => ({
-	card: {
-		boxShadow: "0 24px 24px -18px rgba(69,104,129,.33), 0 9px 45px 0 rgba(114,119,160,.12)",
-		"&:hover": {
-			transform: "translateY(-5px)",
-			backgroundColor: "#FFF"
-		},
-		"&:hover .desc": {
-			color: "#1890ff"
-		},
-		transition:
-			"transform .35s cubic-bezier(.4,0,.2,1),box-shadow .35s cubic-bezier(.4,0,.2,1)",
-		transform: "translateY(0)"
-	},
+	card: theme.card,
 	content: {
 		padding: 20,
 		display: "flex",
