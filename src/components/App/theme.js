723a38f4432b8a9d121cf15e79118e3ee772e168
Mo Sharif <mo@mosh-media.com>
2020-03-09 23:27:29 -0700

    Create theme.js

---
+++ src/components/App/theme.js
@@ -0,0 +1,23 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const bigShadow =
+	"0 24px 24px -18px rgba(69,104,129,.33), 0 9px 45px 0 rgba(114,119,160,.12)";
+
+const theme = createMuiTheme({
+	bigShadow: bigShadow,
+	card: {
+		boxShadow: bigShadow,
+		"&:hover": {
+			transform: "translateY(-5px)",
+			backgroundColor: "#FFF"
+		},
+		"&:hover .desc": {
+			color: "white"
+		},
+		transition:
+			"transform .35s cubic-bezier(.4,0,.2,1),box-shadow .35s cubic-bezier(.4,0,.2,1)",
+		transform: "translateY(0)"
+	}
+});
+
+export default theme;
