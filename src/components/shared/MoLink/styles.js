b738e2012cc3ca2b10e91c76e62b1d33efa4ac35
Mo Sharif <mo@mosh-media.com>
2020-03-09 23:27:23 -0700

    Update styles.js

--- src/components/shared/MoLink/styles.js
+++ src/components/shared/MoLink/styles.js
@@ -5,7 +5,7 @@ const styles = theme => ({
     padding: theme.spacing(2),
     color: "#FFFFF2",
     fontSize: "20px",
-    boxShadow: "0 24px 24px -18px rgba(69,104,129,.33), 0 9px 45px 0 rgba(114,119,160,.12)",
+    boxShadow: theme.bigShadow,
     borderRadius: "3px"
   }
 });
