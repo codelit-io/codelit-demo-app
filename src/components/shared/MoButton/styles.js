5aee9fef90bc7806570d71aade8fc25f57289337
Mo Sharif <mo@mosh-media.com>
2020-03-09 23:27:26 -0700

    Update styles.js

--- src/components/shared/MoButton/styles.js
+++ src/components/shared/MoButton/styles.js
@@ -3,7 +3,7 @@ const styles = theme => ({
 		padding: theme.spacing(2),
 		color: "#FFFFF2",
 		fontSize: "20px",
-		boxShadow: "0 24px 24px -18px rgba(69,104,129,.33), 0 9px 45px 0 rgba(114,119,160,.12)",
+		boxShadow: bigShadow,
 		backgroundColor: "#41D3BD",
 		borderRadius: "3px",
 		"&:hover": {
