63335adadf25f7bc7da363565b054ebeb4279606
Mo Sharif <mo@mosh-media.com>
2020-03-09 22:28:56 -0700

    Create styles.js

---
+++ src/components/shared/MoSnackBar/MoSnackbarCore/styles.js
@@ -0,0 +1,23 @@
+const styles = theme => ({
+	container: {
+		textAlign: "center"
+	},
+	snackbarContent: {
+		backgroundColor: "white",
+		color: "black",
+		padding: "2em",
+		boxShadow:
+			"0 24px 24px -18px rgba(69,104,129,.33), 0 9px 45px 0 rgba(114,119,160,.12)"
+	},
+	message: {
+		display: "flex",
+		alignItems: "center"
+	},
+	checkIcon: {
+		color: " #99bb33",
+		fontSize: "40px",
+		marginRight: "20px"
+	}
+});
+
+export default styles;
