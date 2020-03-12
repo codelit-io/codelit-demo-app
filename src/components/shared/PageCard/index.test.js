973ed711ba3d35b6802185039ed42a6bf8d48df8
Mo Sharif <mo@mosh-media.com>
2020-03-11 22:35:04 -0700

    Delete index.test.js

--- src/components/shared/PageCard/index.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import PageCard from "./index.js";
-import renderer from "react-test-renderer";
-
-it("Should render PageCard", () => {
-	const tree = renderer.create(<PageCard />).toJSON();
-	expect(tree).toMatchSnapshot();
-});
