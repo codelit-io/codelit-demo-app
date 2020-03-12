1353f2a2696b83830895bcddd656db8c532d3be7
Mo Sharif <mo@mosh-media.com>
2020-03-11 21:24:20 -0700

    Update index.js

--- src/components/shared/PageHeader/index.js
+++ src/components/shared/PageHeader/index.js
@@ -1,33 +1,19 @@
 import React from "react";
 
-import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import Box from "@material-ui/core/Box";
 import Fade from "@material-ui/core/Fade";
-import IconButton from "@material-ui/core/IconButton";
-import Grid from "@material-ui/core/Grid";
 import styles from "./styles";
 import Typography from "@material-ui/core/Typography";
 import withStyles from "@material-ui/core/styles/withStyles";
 
-const PageHeader = ({ classes, history, title }) => {
+const PageHeader = ({ classes, title }) => {
 	return (
-		<Fade in={true} timeout={{ enter: 1000 }}>
-			<Grid container spacing={4}>
-				<Grid item xs={12} sm={12} md={4} className={classes.arrowSection}>
-					{history && history.location.pathname !== "/" && (
-						<IconButton aria-label="delete" onClick={() => history.goBack()}>
-							<ArrowBackIcon className={classes.arrow} />
-						</IconButton>
-					)}
-				</Grid>
-				<Grid item xs={12} sm={12} md={4}>
-					<Typography variant="h4" gutterBottom className={classes.header}>
-						<Box fontWeight="fontWeightLight" className={classes.linkText}>
-							{title}
-						</Box>
-					</Typography>
-				</Grid>
-			</Grid>
+		<Fade in={title && true} timeout={{ enter: 1000 }}>
+			<Typography variant="h2" bottomGutter>
+				<Box fontWeight="fontWeightLight" className={classes.linkText}>
+					{title}
+				</Box>
+			</Typography>
 		</Fade>
 	);
 };
