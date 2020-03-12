e3f87b8da713300cc2b7d0d367d1a40d0b291c8f
Mo Sharif <mo@mosh-media.com>
2020-03-11 22:35:17 -0700

    Update index.js

--- src/containers/SignUp/index.js
+++ src/containers/SignUp/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 
 import Grid from "@material-ui/core/Grid";
-import PageCard from "../../components/shared/PageCard";
+import MoPage from "../../components/shared/MoPage";
 import SocialSignIn from "../../components/SocialSignIn/";
 import SignUpForm from "./SignUpForm";
 
 const SignUpPage = () => (
 	<>
-		<PageCard img="" title="Get Started today!">
+		<MoPage img="" title="Get Started today!" loading={true} isCard={true}>
 			<Grid container spacing={3}>
 				<SignUpForm />
 				<SocialSignIn />
 			</Grid>
-		</PageCard>
+		</MoPage>
 	</>
 );
 
