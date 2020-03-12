e6000a5855b264b88a64bd8a518087febf5b7c48
Mo Sharif <mo@mosh-media.com>
2020-03-11 22:34:56 -0700

    Create index.test.js

---
+++ src/components/shared/MoPage/index.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import MoPage from "./index.js";
+import renderer from "react-test-renderer";
+
+it("Should render MoPage", () => {
+	const tree = renderer.create(<MoPage />).toJSON();
+	expect(tree).toMatchSnapshot();
+});
