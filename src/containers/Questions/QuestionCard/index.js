98b731c093dda4cbd2df2a67213649b169b1b89c
Mo Sharif <mo@mosh-media.com>
2020-03-11 21:24:08 -0700

    Update index.js

--- src/containers/Questions/QuestionCard/index.js
+++ src/containers/Questions/QuestionCard/index.js
@@ -2,18 +2,18 @@ import React from "react";
 
 import Grid from "@material-ui/core/Grid";
 import MoCard from "../../../components/shared/MoCard";
-import Title from "../../../components/shared/Title";
+import MoTitle from "../../../components/shared/MoTitle";
 
-const QuestionCard = ({ question, userPoints, key }) => (
+const QuestionCard = ({ index, question, userPoints }) => (
 	<>
 		{question.category && (
 			<Grid item xs={12} sm={12} md={12}>
-				<Title
+				<MoTitle
 					text={question.category}
 					fade={true}
-					margin="40px 0 0 0"
+					margin={index === 0 ? "36px 0 26px" : "40px 0 36px" }
 					width="100%"
-				></Title>
+				></MoTitle>
 			</Grid>
 		)}
 		<Grid item xs={12} sm={6} md={6}>
