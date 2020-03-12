f93b1a7bd151acc3d6daf24d5d0b6df97a8e86df
Mo Sharif <mo@mosh-media.com>
2020-03-11 22:34:55 -0700

    Create index.js

---
+++ src/components/shared/MoPage/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./styles";
+import { withRouter } from "react-router-dom";
+import withStyles from "@material-ui/core/styles/withStyles";
+import { compose } from "recompose";
+import PageHeader from "../PageHeader";
+import Spinner from "../Spinner";
+
+const MoPage = ({ classes, children, img, loading, isCard, title }) => {
+	return (
+		<>
+			<PageHeader img="" title={title} />
+			<Spinner loading={loading} color="primary" />
+			<section className={`${classes.content} ${isCard && classes.card}`}>{children}</section>
+		</>
+	);
+};
+
+export default compose(withStyles(styles), withRouter)(MoPage);
