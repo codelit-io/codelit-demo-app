cf70117151dad7efdab587dcff7aaf8c764d1b71
Mo Sharif <mo@mosh-media.com>
2020-03-11 21:24:22 -0700

    Update index.js

--- src/components/shared/PageCard/index.js
+++ src/components/shared/PageCard/index.js
@@ -2,16 +2,17 @@ import React from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import PageHeader from "../PageHeader";
 import styles from "./styles";
 import { withRouter } from "react-router-dom";
 import withStyles from "@material-ui/core/styles/withStyles";
 import { compose } from "recompose";
+import MoTitle from "../MoTitle";
 
-const PageCard = ({ img, title, children, history, classes }) => {
+const PageCard = ({ img, title, children, classes }) => {
+	console.log(title)
 	return (
 		<>
-			<PageHeader img="" title={title} history={history}/>
+			<MoTitle fade={title} text={title} margin="0 0 26px 0"/>
 			<Card className={classes.card}>
 				{img && <CardMedia className={classes.img} image={img} />}
 				<CardContent>{children}</CardContent>
