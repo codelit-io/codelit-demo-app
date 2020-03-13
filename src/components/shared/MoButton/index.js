aed24ed0223b6046a1a9a4522741adec7273c750
Mo Sharif <mo@mosh-media.com>
2020-03-12 23:11:43 -0700

    Update index.js

--- src/components/shared/MoButton/index.js
+++ src/components/shared/MoButton/index.js
@@ -5,18 +5,16 @@ import Button from "@material-ui/core/Button";
 import styles from "./styles";
 import withStyles from "@material-ui/core/styles/withStyles";
 
-const MoButton = ({ classes, handleClick, text }) => {
-  return (
-    <Button
-      color="primary"
-      className={classes.button}
-      onClick={() => handleClick()}
-      variant="contained"
-    >
-      {text}
-      <ArrowForwardIcon />
-    </Button>
-  );
+const MoButton = ({ classes, handleButtonClick, text }) => {
+	return (
+		<Button className={classes.link} onClick={() => handleButtonClick()}>
+			{text}
+			<ArrowForwardIcon
+				className="arrow"
+				style={{ fontSize: "1em", paddingLeft: "4px" }}
+			/>
+		</Button>
+	);
 };
 
 export default withStyles(styles)(MoButton);
