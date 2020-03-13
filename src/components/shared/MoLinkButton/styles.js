4fdfae59105d4f222861abffac75d275e32f8d02
Mo Sharif <mo@mosh-media.com>
2020-03-12 23:11:58 -0700

    Create styles.js

---
+++ src/components/shared/MoLinkButton/styles.js
@@ -0,0 +1,23 @@
+const styles = theme => ({
+	link: {
+		backgroundColor: theme.blue.medium,
+		textDecoration: "none",
+		padding: "12px 22px",
+    color: theme.white.medium,
+		fontSize: "1.1rem",
+    borderRadius: "3px",
+    textTransform: "initial",
+    fontWeight: "600",
+		"&:hover": {
+      background:theme.grey.dark
+		},
+		"&:hover .arrow": {
+      transform: "translateX(5px)",
+    },
+    ".arrow": {
+      fontSize: "1.0rem",
+    }
+	}
+});
+
+export default styles;
