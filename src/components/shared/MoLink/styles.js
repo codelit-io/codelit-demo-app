d12c498a7ab03e72833cfbfe25db02114e15afee
Mo Sharif <mo@mosh-media.com>
2020-03-12 23:11:52 -0700

    Update styles.js

--- src/components/shared/MoLink/styles.js
+++ src/components/shared/MoLink/styles.js
@@ -1,13 +1,23 @@
 const styles = theme => ({
-  link: {
-    backgroundColor: "#41D3BD",
-    textDecoration: "none",
-    padding: theme.spacing(2),
-    color: "#FFFFF2",
-    fontSize: "20px",
-    boxShadow: theme.bigShadow,
-    borderRadius: "3px"
-  }
+	link: {
+		backgroundColor: "none",
+		textDecoration: "none",
+		padding: "0px",
+    color: theme.blue.medium,
+		fontSize: "1.1rem",
+    borderRadius: "3px",
+    textTransform: "initial",
+    fontWeight: "600",
+		"&:hover": {
+      background:"none"
+		},
+		"&:hover .arrow": {
+      transform: "translateX(5px)",
+    },
+    ".arrow": {
+      fontSize: "1.0rem",
+    }
+	}
 });
 
 export default styles;
