46e2a1748a5f8192d6ee88caf89fae28fb9bf309
Mo Sharif <mo@mosh-media.com>
2020-03-12 23:11:56 -0700

    Create index.test.js

---
+++ src/components/shared/MoLinkButton/index.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import MoLink from "./index.js.js";
+import renderer from "react-test-renderer";
+
+it("Should render MoLink", () => {
+	const tree = renderer.create(<MoLink />).toJSON();
+	expect(tree).toMatchSnapshot();
+});
