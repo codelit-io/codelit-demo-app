57c039b6ae6ce30e246b69dcfe89319923f8e070
Mo Sharif <mo@mosh-media.com>
2020-03-12 23:11:47 -0700

    Update index.js

--- src/components/shared/MoConfetti/index.js
+++ src/components/shared/MoConfetti/index.js
@@ -3,42 +3,22 @@ import React from "react";
 import Confetti from "react-dom-confetti";
 
 const config = {
-	angle: -30,
-	// spread: 45,
-	// startVelocity: 50,
-	// elementCount: "42",
-	// dragFriction: "0.04",
-	// duration: "3000",
-	// stagger: "12",
+	angle: 260,
+	spread: 285,
+	startVelocity: 30,
+	elementCount: "102",
+	duration: "3000",
 	width: "12px",
 	height: "12px",
 	colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"]
 };
-const config2 = {
-	angle: 200,
-	// spread: 65,
-	// startVelocity: 50,
-	// elementCount: "42",
-	// dragFriction: "0.04",
-	// duration: "3000",
-	// stagger: "12",
-	width: "12px",
-	height: "12px",
-	colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"],
-};
-const MoConfettiBase = ({ isActive }) => {
+
+const MoConfetti = ({ isActive }) => {
 	return (
-		<>
-			<div style={{ position: "absolute", top: "0", left: "0" }}>
-				<Confetti active={isActive} config={config} />
-			</div>
-			<div style={{ position: "absolute", top: "0", right: "0" }}>
-				<Confetti active={isActive} config={config2} />
-			</div>
-		</>
+		<div style={{ position: "absolute", top: "0", left: "50%" }}>
+			<Confetti active={isActive} config={config} />
+		</div>
 	);
 };
-
-const MoConfetti = React.memo(MoConfettiBase);
 
 export default MoConfetti;
