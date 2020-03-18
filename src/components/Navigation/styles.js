80a7dbd5b45049f1be2eee3031ffd4b811bc1301
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:31:21 -0700

    Update styles.js

--- src/components/Navigation/styles.js
+++ src/components/Navigation/styles.js
@@ -1,34 +1,34 @@
 const styles = theme => ({
-	root: {
-		flexGrow: 1
-	},
-	alignCenter: theme.flexAlignCenter,
-	title: {
-		textAlign: "center",
-		alignSelf: "center",
-		[theme.breakpoints.up("sm")]: {
-			display: "block"
-		}
-	},
-	toolbar: {
-		padding: "0"
-	},
-	avatar: {
-		width: theme.spacing(4),
-		height: theme.spacing(4)
-	},
-	adminAvatar: {
-		border: `2px solid ${theme.yellow.light}`,
-		overflow: "visible",
-	},
-	appBar: {
-		boxShadow: "none",
-		backgroundColor: "transparent",
-		marginBottom: "96px",
-	},
-	MoSkool: {
-		textAlign: "left"
-	}
+  root: {
+    flexGrow: 1
+  },
+  alignCenter: theme.flexAlignCenter,
+  title: {
+    textAlign: "center",
+    alignSelf: "center",
+    [theme.breakpoints.up("sm")]: {
+      display: "block"
+    }
+  },
+  toolbar: {
+    padding: "0"
+  },
+  avatar: {
+    width: theme.spacing(4),
+    height: theme.spacing(4)
+  },
+  adminAvatar: {
+    border: `2px solid ${theme.yellow.light}`,
+    overflow: "visible"
+  },
+  appBar: {
+    boxShadow: "none",
+    backgroundColor: "transparent",
+    marginBottom: "96px"
+  },
+  MoSkool: {
+    textAlign: "left"
+  }
 });
 
 export default styles;
