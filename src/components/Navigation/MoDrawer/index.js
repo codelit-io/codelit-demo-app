7073cf4f877e8bf2aed8ac2ea064809024a3f595
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:31:27 -0700

    Update index.js

--- src/components/Navigation/MoDrawer/index.js
+++ src/components/Navigation/MoDrawer/index.js
@@ -8,58 +8,58 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
 
 const styles = theme => ({
-	menuButton: {
-		marginRight: theme.spacing(2)
-	}
+  menuButton: {
+    marginRight: theme.spacing(2)
+  }
 });
 
 const MoDrawer = classes => {
-	const [state, setState] = useState({
-		isDrawerOpen: false
-	});
+  const [state, setState] = useState({
+    isDrawerOpen: false
+  });
 
-	const toggleDrawer = isDrawerOpen => {
-		setState({ ...state, isDrawerOpen });
-	};
+  const toggleDrawer = isDrawerOpen => {
+    setState({ ...state, isDrawerOpen });
+  };
 
-	return (
-		<>
-			<IconButton
-				edge="start"
-				color="inherit"
-				aria-label="open drawer"
-				onClick={() => toggleDrawer(true)}
-			>
-				<MenuIcon />
-			</IconButton>
-			<SwipeableDrawer
-				anchor="right"
-				open={state.isDrawerOpen}
-				onClose={() => toggleDrawer(false)}
-				onOpen={() => toggleDrawer(true)}
-			>
-				<AuthUserContext.Consumer>
-					{authUser =>
-						authUser ? (
-							<div
-								onClick={() => toggleDrawer(false)}
-								onKeyDown={() => toggleDrawer(false)}
-							>
-								<NavigationAuth authUser={authUser} />
-							</div>
-						) : (
-							<div
-								onClick={() => toggleDrawer(false)}
-								onKeyDown={() => toggleDrawer(false)}
-							>
-								<NavigationNonAuth />
-							</div>
-						)
-					}
-				</AuthUserContext.Consumer>
-			</SwipeableDrawer>
-		</>
-	);
+  return (
+    <>
+      <IconButton
+        edge="start"
+        color="inherit"
+        aria-label="open drawer"
+        onClick={() => toggleDrawer(true)}
+      >
+        <MenuIcon />
+      </IconButton>
+      <SwipeableDrawer
+        anchor="right"
+        open={state.isDrawerOpen}
+        onClose={() => toggleDrawer(false)}
+        onOpen={() => toggleDrawer(true)}
+      >
+        <AuthUserContext.Consumer>
+          {authUser =>
+            authUser ? (
+              <div
+                onClick={() => toggleDrawer(false)}
+                onKeyDown={() => toggleDrawer(false)}
+              >
+                <NavigationAuth authUser={authUser} />
+              </div>
+            ) : (
+              <div
+                onClick={() => toggleDrawer(false)}
+                onKeyDown={() => toggleDrawer(false)}
+              >
+                <NavigationNonAuth />
+              </div>
+            )
+          }
+        </AuthUserContext.Consumer>
+      </SwipeableDrawer>
+    </>
+  );
 };
 
 export default withStyles(styles)(MoDrawer);
