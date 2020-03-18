4ff753a76b17ac20f9cb965101049e8c6d74b9fe
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:29:15 -0700

    Update index.test.js

--- src/containers/Account/index.test.js
+++ src/containers/Account/index.test.js
@@ -3,6 +3,6 @@ import Account from "./index.js";
 import renderer from "react-test-renderer";
 
 it("renders Account Page", () => {
-	const tree = renderer.create(<Account />).toJSON();
-	expect(tree).toMatchSnapshot();
+  const tree = renderer.create(<Account />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
