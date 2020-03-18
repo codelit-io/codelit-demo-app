dc1c4afb1015837fdf62483c590016acc39dd888
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:30:11 -0700

    Update index.js

--- src/components/shared/MoSnackBar/MoSnackbarCore/index.js
+++ src/components/shared/MoSnackBar/MoSnackbarCore/index.js
@@ -10,59 +10,59 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import MoButton from "../../MoButton";
 
 const MoSnackbarCore = ({
-	isActive,
-	handleClick,
-	snackbarProps,
-	authUser,
-	classes
+  isActive,
+  handleClick,
+  snackbarProps,
+  authUser,
+  classes
 }) => {
-	const [state, setState] = React.useState({
-		isActive: isActive,
-		Transition: Slide
-	});
+  const [state, setState] = React.useState({
+    isActive: isActive,
+    Transition: Slide
+  });
 
-	const handleButtonClick = () => {
-		setState({
-			isActive: false,
-			Transition: Slide
-		});
-		handleClick();
-	};
+  const handleButtonClick = () => {
+    setState({
+      isActive: false,
+      Transition: Slide
+    });
+    handleClick();
+  };
 
-	const handleClose = () => {
-		setState({
-			...state,
-			isActive: false
-		});
-	};
+  const handleClose = () => {
+    setState({
+      ...state,
+      isActive: false
+    });
+  };
 
-	return (
-		<Snackbar
-			open={isActive}
-			onClose={handleClose}
-			TransitionComponent={state.Transition}
-		>
-			<SnackbarContent
-				message={
-					<div className={classes.message}>
-						<CheckCircleIcon className={classes.checkIcon} />
-						<MoTitle
-							text={snackbarProps && snackbarProps.title}
-							fade={true}
-							margin="0 36px 0 0"
-						/>
-					</div>
-				}
-				className={classes.snackbarContent}
-				action={
-					<MoButton handleButtonClick={() => handleButtonClick()} text="Next">
-						{snackbarProps && snackbarProps.buttonText}
-						{snackbarProps && snackbarProps.buttonIcon}
-					</MoButton>
-				}
-			></SnackbarContent>
-		</Snackbar>
-	);
+  return (
+    <Snackbar
+      open={isActive}
+      onClose={handleClose}
+      TransitionComponent={state.Transition}
+    >
+      <SnackbarContent
+        message={
+          <div className={classes.message}>
+            <CheckCircleIcon className={classes.checkIcon} />
+            <MoTitle
+              text={snackbarProps && snackbarProps.title}
+              fade={true}
+              margin="0 36px 0 0"
+            />
+          </div>
+        }
+        className={classes.snackbarContent}
+        action={
+          <MoButton handleButtonClick={() => handleButtonClick()} text="Next">
+            {snackbarProps && snackbarProps.buttonText}
+            {snackbarProps && snackbarProps.buttonIcon}
+          </MoButton>
+        }
+      ></SnackbarContent>
+    </Snackbar>
+  );
 };
 
 export default withStyles(styles)(MoSnackbarCore);
