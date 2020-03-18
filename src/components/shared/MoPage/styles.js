e54c600601c8b170d3329b11d65cb9b8a908c83e
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:30:16 -0700

    Update styles.js

--- src/components/shared/MoPage/styles.js
+++ src/components/shared/MoPage/styles.js
@@ -1,13 +1,13 @@
 const styles = theme => ({
-	card: theme.card,
-	content: {
-		display: "flex",
-		flexDirection: "column"
-	},
-	img: {
-		height: 100,
-		width: 100
-	}
+  card: theme.card,
+  content: {
+    display: "flex",
+    flexDirection: "column"
+  },
+  img: {
+    height: 100,
+    width: 100
+  }
 });
 
 export default styles;
