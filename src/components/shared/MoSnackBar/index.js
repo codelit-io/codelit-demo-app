e2dfadac85da86ac2cfdd2df03bc076ccb97d306
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:30:14 -0700

    Update index.js

--- src/components/shared/MoSnackBar/index.js
+++ src/components/shared/MoSnackBar/index.js
@@ -3,19 +3,19 @@ import React from "react";
 import MoSnackbarCore from "./MoSnackbarCore";
 
 const MoSnackbarContainer = ({
-	isActive,
-	authUser,
-	snackbarProps,
-	triggerNextQuestion
+  isActive,
+  authUser,
+  snackbarProps,
+  triggerNextQuestion
 }) => {
-	return (
-		<MoSnackbarCore
-			isActive={isActive}
-			authUser={authUser}
-			handleClick={() => triggerNextQuestion()}
-			snackbarProps={snackbarProps}
-		></MoSnackbarCore>
-	);
+  return (
+    <MoSnackbarCore
+      isActive={isActive}
+      authUser={authUser}
+      handleClick={() => triggerNextQuestion()}
+      snackbarProps={snackbarProps}
+    ></MoSnackbarCore>
+  );
 };
 
 const MoSnackbar = React.memo(MoSnackbarContainer);
