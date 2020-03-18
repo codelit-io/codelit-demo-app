de28085218f0a99d4b174a9d1098c5d0a0195426
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:31:10 -0700

    Update withAuthorization.js

--- src/components/Session/withAuthorization.js
+++ src/components/Session/withAuthorization.js
@@ -7,33 +7,33 @@ import { withFirebase } from "../Firebase";
 import * as ROUTES from "../../constants/routes";
 
 const withAuthorization = condition => Component => {
-	class WithAuthorization extends React.Component {
-		componentDidMount() {
-			this.listener = this.props.firebase.onAuthUserListener(
-				authUser => {
-					if (!condition(authUser)) {
-						this.props.history.push(ROUTES.NOT_FOUND.path);
-					}
-				},
-				() => this.props.history.push(ROUTES.SIGN_IN.path),
-			);
-		}
+  class WithAuthorization extends React.Component {
+    componentDidMount() {
+      this.listener = this.props.firebase.onAuthUserListener(
+        authUser => {
+          if (!condition(authUser)) {
+            this.props.history.push(ROUTES.NOT_FOUND.path);
+          }
+        },
+        () => this.props.history.push(ROUTES.SIGN_IN.path)
+      );
+    }
 
-		componentWillUnmount() {
-			this.listener();
-		}
+    componentWillUnmount() {
+      this.listener();
+    }
 
-		render() {
-			return (
-				<AuthUserContext.Consumer>
-					{authUser =>
-						condition(authUser) ? <Component {...this.props} /> : null
-					}
-				</AuthUserContext.Consumer>
-			);
-		}
-	}
-	return compose(withRouter, withFirebase)(WithAuthorization);
+    render() {
+      return (
+        <AuthUserContext.Consumer>
+          {authUser =>
+            condition(authUser) ? <Component {...this.props} /> : null
+          }
+        </AuthUserContext.Consumer>
+      );
+    }
+  }
+  return compose(withRouter, withFirebase)(WithAuthorization);
 };
 
 export default withAuthorization;
