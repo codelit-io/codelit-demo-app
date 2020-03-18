f472030121e86007f86d24fd124bf468cb8abfad
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:26:47 -0700

    Update index.js

--- src/containers/Questions/index.js
+++ src/containers/Questions/index.js
@@ -6,66 +6,66 @@ import QuestionCard from "./QuestionCard";
 import MoPage from "../../components/shared/MoPage";
 
 const Questions = ({ firebase }) => {
-	const [loading, setLoading] = useState(false);
-	const [questions, setQuestions] = useState([]);
-	const [userPoints, setUserPoints] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [userPoints, setUserPoints] = useState(0);
 
-	const QuestionsList = ({ authUser, questions }) => {
-		if (authUser) {
-			setUserPoints(authUser.points);
-		}
+  const QuestionsList = ({ authUser, questions }) => {
+    if (authUser) {
+      setUserPoints(authUser.points);
+    }
 
-		return questions.map((question, index) => (
-			<React.Fragment key={index}>
-				<QuestionCard
-					userPoints={userPoints}
-					question={question}
-					index={index}
-				/>
-			</React.Fragment>
-		));
-	};
+    return questions.map((question, index) => (
+      <React.Fragment key={index}>
+        <QuestionCard
+          userPoints={userPoints}
+          question={question}
+          index={index}
+        />
+      </React.Fragment>
+    ));
+  };
 
-	useEffect(() => {
-		setLoading(true);
-		const getQuestions = firebase
-			.questions()
-			.orderBy("id")
-			.onSnapshot(snapshot => {
-				if (snapshot.size) {
-					let questions = [];
-					snapshot.forEach(doc => {
-						questions.push({
-							...doc.data(),
-							uid: doc.id
-						});
-					});
-					setQuestions(questions);
-					setLoading(false);
-				} else {
-					setQuestions([]);
-					setLoading(false);
-				}
-			});
-		/* Unsubscribe from firebase on unmount */
-		return () => {
-			setQuestions([]);
-			getQuestions();
-		};
-	}, [firebase]);
+  useEffect(() => {
+    setLoading(true);
+    const getQuestions = firebase
+      .questions()
+      .orderBy("id")
+      .onSnapshot(snapshot => {
+        if (snapshot.size) {
+          let questions = [];
+          snapshot.forEach(doc => {
+            questions.push({
+              ...doc.data(),
+              uid: doc.id
+            });
+          });
+          setQuestions(questions);
+          setLoading(false);
+        } else {
+          setQuestions([]);
+          setLoading(false);
+        }
+      });
+    /* Unsubscribe from firebase on unmount */
+    return () => {
+      setQuestions([]);
+      getQuestions();
+    };
+  }, [firebase]);
 
-	return (
-		<MoPage title="Topics" loading={loading} isCard={false}>
-			<Grid container spacing={4}>
-				{questions && (
-					<AuthUserContext.Consumer>
-						{authUser => (
-							<QuestionsList authUser={authUser} questions={questions} />
-						)}
-					</AuthUserContext.Consumer>
-				)}
-			</Grid>
-		</MoPage>
-	);
+  return (
+    <MoPage title="Topics" loading={loading} isCard={false}>
+      <Grid container spacing={4}>
+        {questions && (
+          <AuthUserContext.Consumer>
+            {authUser => (
+              <QuestionsList authUser={authUser} questions={questions} />
+            )}
+          </AuthUserContext.Consumer>
+        )}
+      </Grid>
+    </MoPage>
+  );
 };
 export default withAuthentication(Questions);
