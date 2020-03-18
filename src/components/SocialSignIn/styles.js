b127d1786531882e6cd6f3412bfa9e6679c32f83
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:29:37 -0700

    Update styles.js

--- src/components/SocialSignIn/styles.js
+++ src/components/SocialSignIn/styles.js
@@ -1,22 +1,22 @@
 const styles = theme => ({
-	main: {
-		width: "auto",
-		display: "block", // Fix IE 11 issue.
-		marginLeft: theme.spacing(3),
-		marginRight: theme.spacing(3),
-		[theme.breakpoints.up(400 + theme.spacing(6))]: {
-			width: 400,
-			marginLeft: "auto",
-			marginRight: "auto"
-		}
-	},
-	paper: {
-		marginTop: theme.spacing(8),
-		display: "flex",
-		flexDirection: "column",
-		alignItems: "center",
-		padding: `${theme.spacing(2)}px ${theme.spacing(3)}px ${theme.spacing(3)}px`
-	},
+  main: {
+    width: "auto",
+    display: "block", // Fix IE 11 issue.
+    marginLeft: theme.spacing(3),
+    marginRight: theme.spacing(3),
+    [theme.breakpoints.up(400 + theme.spacing(6))]: {
+      width: 400,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: `${theme.spacing(2)}px ${theme.spacing(3)}px ${theme.spacing(3)}px`
+  }
 });
 
 export default styles;
