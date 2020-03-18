64a0c551313996ac2388e548c5b3910b283da01b
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:26:41 -0700

    Update util.js

--- src/components/CodeEditor/LiveProviderCore/util.js
+++ src/components/CodeEditor/LiveProviderCore/util.js
@@ -2,15 +2,14 @@
  *  Figure out a new way to do this                    *
  * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-
 export const addFocusOnEditor = () => {
-	setTimeout(() => {
-		try {
-			document
-				.querySelector(".npm__react-simple-code-editor__textarea")
-				.focus();
-		} catch {
-			console.log("Focus on editor failed");
-		}
-	}, 1000);
+  setTimeout(() => {
+    try {
+      document
+        .querySelector(".npm__react-simple-code-editor__textarea")
+        .focus();
+    } catch {
+      console.log("Focus on editor failed");
+    }
+  }, 1000);
 };
