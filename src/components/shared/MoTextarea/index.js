6d372354b2955719ff9d45abbe2575cf130149a1
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:30:08 -0700

    Update index.js

--- src/components/shared/MoTextarea/index.js
+++ src/components/shared/MoTextarea/index.js
@@ -1,27 +1,27 @@
 import React from "react";
 
 const style = {
-	textarea: {
-		boxSizing: "border-box",
-		border: "none",
-		borderRadius: "3px",
-		lineHeight: "24px",
-		overflow: "auto",
-		height: "auto",
-		padding: "8px",
-		boxShadow: "0px 4px 10px -8px black"
-	}
+  textarea: {
+    boxSizing: "border-box",
+    border: "none",
+    borderRadius: "3px",
+    lineHeight: "24px",
+    overflow: "auto",
+    height: "auto",
+    padding: "8px",
+    boxShadow: "0px 4px 10px -8px black"
+  }
 };
 const MoTextarea = props => {
-	return (
-		<textarea
-			aria-label={props.value}
-			placeholder=""
-			value={props.value}
-			onChange={e => props.onChange(e.target.value)}
-			style={style.textarea}
-		/>
-	);
+  return (
+    <textarea
+      aria-label={props.value}
+      placeholder=""
+      value={props.value}
+      onChange={e => props.onChange(e.target.value)}
+      style={style.textarea}
+    />
+  );
 };
 
 export default MoTextarea;
