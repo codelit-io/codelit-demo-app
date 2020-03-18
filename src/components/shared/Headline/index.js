d2e3f723b5c0c6342f2085cac2e68bf722f59e30
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:31:05 -0700

    Update index.js

--- src/components/shared/Headline/index.js
+++ src/components/shared/Headline/index.js
@@ -3,31 +3,31 @@ import Grow from "@material-ui/core/Grow";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
-	container: {
-		display: "flex"
-	}
+  container: {
+    display: "flex"
+  }
 }));
 
 const Headline = ({ isCorrect }) => {
-	const classes = useStyles();
-	const [isVisible, setIsVisible] = useState(isCorrect);
+  const classes = useStyles();
+  const [isVisible, setIsVisible] = useState(isCorrect);
 
-	const handleChange = () => {
-		setIsVisible(prev => !prev);
-	};
+  const handleChange = () => {
+    setIsVisible(prev => !prev);
+  };
 
-	return (
-		<div className={classes.container}>
-			<Grow
-				onChange={handleChange}
-				in={isVisible}
-				style={{ transformOrigin: "0 0 0" }}
-				{...(isVisible ? { timeout: 1000 } : {})}
-			>
-				<h1>Level 1</h1>
-			</Grow>
-		</div>
-	);
+  return (
+    <div className={classes.container}>
+      <Grow
+        onChange={handleChange}
+        in={isVisible}
+        style={{ transformOrigin: "0 0 0" }}
+        {...(isVisible ? { timeout: 1000 } : {})}
+      >
+        <h1>Level 1</h1>
+      </Grow>
+    </div>
+  );
 };
 
 export default Headline;
