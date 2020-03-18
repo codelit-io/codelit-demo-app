5b2cd19a536733da9f4a7351e81585d00a5284f7
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:29:49 -0700

    Update index.js

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -5,15 +5,15 @@ import { withFirebase } from "../Firebase";
 import MenuItem from "@material-ui/core/MenuItem";
 
 const SingOutButton = ({ firebase, handleClose }) => (
-	<MenuItem
-		button
-		onClick={() => {
-			firebase.signOut();
-			handleClose();
-		}}
-	>
-		Sign Out
-	</MenuItem>
+  <MenuItem
+    button
+    onClick={() => {
+      firebase.signOut();
+      handleClose();
+    }}
+  >
+    Sign Out
+  </MenuItem>
 );
 
 export default withFirebase(SingOutButton);
