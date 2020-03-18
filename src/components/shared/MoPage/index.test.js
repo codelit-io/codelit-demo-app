2491d0d7a7b0c6f334a50393cc618b8b94575502
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:30:18 -0700

    Update index.test.js

--- src/components/shared/MoPage/index.test.js
+++ src/components/shared/MoPage/index.test.js
@@ -3,6 +3,6 @@ import MoPage from "./index.js";
 import renderer from "react-test-renderer";
 
 it("Should render MoPage", () => {
-	const tree = renderer.create(<MoPage />).toJSON();
-	expect(tree).toMatchSnapshot();
+  const tree = renderer.create(<MoPage />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
