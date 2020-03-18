28a0e5c648c241d5e89f3208775d0f76c15ba58e
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:29:06 -0700

    Update index.js

--- src/containers/Admin/AdminNav/index.js
+++ src/containers/Admin/AdminNav/index.js
@@ -7,39 +7,39 @@ import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 import PeopleIcon from "@material-ui/icons/People";
 
 const AdminNav = ({ history }) => {
-	const [value, setValue] = useState(history.location.pathname);
-	
-	return (
-		<BottomNavigation
-			value={value}
-			onChange={(event, newValue) => {
-				setValue(newValue);
-				history.push(newValue);
-			}}
-			showLabels
-		>
-			<BottomNavigationAction
-				label="Users"
-				value={ROUTES.ADMIN_USERS.path}
-				icon={<PeopleIcon />}
-			/>
-			<BottomNavigationAction
-				label={ROUTES.ADMIN_QUESTIONS_EASY.title}
-				value={ROUTES.ADMIN_QUESTIONS_EASY.path}
-				icon={<AddIcon />}
-			/>
-			<BottomNavigationAction
-				label={ROUTES.ADMIN_QUESTIONS_MID.title}
-				value={ROUTES.ADMIN_QUESTIONS_MID.path}
-				icon={<AddIcon />}
-			/>
-			<BottomNavigationAction
-				label={ROUTES.ADMIN_QUESTIONS_PRO.title}
-				value={ROUTES.ADMIN_QUESTIONS_PRO.path}
-				icon={<AddIcon />}
-			/>
-		</BottomNavigation>
-	);
+  const [value, setValue] = useState(history.location.pathname);
+
+  return (
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+        history.push(newValue);
+      }}
+      showLabels
+    >
+      <BottomNavigationAction
+        label="Users"
+        value={ROUTES.ADMIN_USERS.path}
+        icon={<PeopleIcon />}
+      />
+      <BottomNavigationAction
+        label={ROUTES.ADMIN_QUESTIONS_EASY.title}
+        value={ROUTES.ADMIN_QUESTIONS_EASY.path}
+        icon={<AddIcon />}
+      />
+      <BottomNavigationAction
+        label={ROUTES.ADMIN_QUESTIONS_MID.title}
+        value={ROUTES.ADMIN_QUESTIONS_MID.path}
+        icon={<AddIcon />}
+      />
+      <BottomNavigationAction
+        label={ROUTES.ADMIN_QUESTIONS_PRO.title}
+        value={ROUTES.ADMIN_QUESTIONS_PRO.path}
+        icon={<AddIcon />}
+      />
+    </BottomNavigation>
+  );
 };
 
 export default AdminNav;
