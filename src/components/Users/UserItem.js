361ff426c36171966eba6cc26a16a2fbd08b02c6
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:29:35 -0700

    Update UserItem.js

--- src/components/Users/UserItem.js
+++ src/components/Users/UserItem.js
@@ -5,72 +5,72 @@ import Button from "@material-ui/core/Button";
 import { withFirebase } from "../Firebase";
 
 class UserItem extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			loading: false,
-			user: null,
-			...props.location.state
-		};
-	}
+    this.state = {
+      loading: false,
+      user: null,
+      ...props.location.state
+    };
+  }
 
-	componentDidMount() {
-		if (this.state.user) {
-			return;
-		}
+  componentDidMount() {
+    if (this.state.user) {
+      return;
+    }
 
-		this.setState({ loading: true });
-		this.unsubscribe = this.props.firebase
-			.user(this.props.match.params.id)
-			.onSnapshot(snapshot => {
-				this.setState({
-					user: snapshot.data(),
-					loading: false
-				});
-			});
-	}
+    this.setState({ loading: true });
+    this.unsubscribe = this.props.firebase
+      .user(this.props.match.params.id)
+      .onSnapshot(snapshot => {
+        this.setState({
+          user: snapshot.data(),
+          loading: false
+        });
+      });
+  }
 
-	componentWillUnmount() {
-		this.unsubscribe && this.unsubscribe();
-	}
+  componentWillUnmount() {
+    this.unsubscribe && this.unsubscribe();
+  }
 
-	onSendPasswordResetEmail = () => {
-		this.props.firebase.passwordReset(this.state.user.email);
-	};
+  onSendPasswordResetEmail = () => {
+    this.props.firebase.passwordReset(this.state.user.email);
+  };
 
-	render() {
-		const { user, loading } = this.state;
+  render() {
+    const { user, loading } = this.state;
 
-		return (
-			<div>
-				<h2>{this.props.match.params.id}</h2>
-				{loading && <div>Loading ...</div>}
+    return (
+      <div>
+        <h2>{this.props.match.params.id}</h2>
+        {loading && <div>Loading ...</div>}
 
-				{user && (
-					<div>
-						<span>
-							<strong>ID:</strong> {user.uid}
-						</span>
-						<br></br>
-						<span>
-							<strong>Email:</strong> {user.email}
-						</span>
-						<br></br>
-						<span>
-							<strong>Username:</strong> {user.username}
-						</span>
-						<br></br>
-						<span>
-							<Button type="button" onClick={this.onSendPasswordResetEmail}>
-								Send Password Reset
-							</Button>
-						</span>
-					</div>
-				)}
-			</div>
-		);
-	}
+        {user && (
+          <div>
+            <span>
+              <strong>ID:</strong> {user.uid}
+            </span>
+            <br></br>
+            <span>
+              <strong>Email:</strong> {user.email}
+            </span>
+            <br></br>
+            <span>
+              <strong>Username:</strong> {user.username}
+            </span>
+            <br></br>
+            <span>
+              <Button type="button" onClick={this.onSendPasswordResetEmail}>
+                Send Password Reset
+              </Button>
+            </span>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default withFirebase(UserItem);
