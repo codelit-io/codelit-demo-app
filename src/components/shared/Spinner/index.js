7b172210d70bca3ef573931628a38cbd18c41940
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:29:52 -0700

    Update index.js

--- src/components/shared/Spinner/index.js
+++ src/components/shared/Spinner/index.js
@@ -4,8 +4,9 @@ import styles from "./styles";
 import withStyles from "@material-ui/core/styles/withStyles";
 
 const Spinner = ({ loading, color, classes }) => {
-
-	return loading && <CircularProgress className={classes.spinner} color={color} />;
+  return (
+    loading && <CircularProgress className={classes.spinner} color={color} />
+  );
 };
 
 export default withStyles(styles)(Spinner);
