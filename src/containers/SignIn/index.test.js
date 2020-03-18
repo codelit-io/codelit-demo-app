1f363e2a3fcd16902d5578292226c7d3c67ecea8
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:26:10 -0700

    Update index.test.js

--- src/containers/SignIn/index.test.js
+++ src/containers/SignIn/index.test.js
@@ -3,6 +3,6 @@ import SignInPage from "./index.js";
 import renderer from "react-test-renderer";
 
 it("Sign In Page", () => {
-	const tree = renderer.create(<SignInPage />).toJSON();
-	expect(tree).toMatchSnapshot();
+  const tree = renderer.create(<SignInPage />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
