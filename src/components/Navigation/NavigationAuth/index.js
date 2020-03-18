3d03624b6219b5cfe128b9547e98ad237ce2cc28
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:31:25 -0700

    Update index.js

--- src/components/Navigation/NavigationAuth/index.js
+++ src/components/Navigation/NavigationAuth/index.js
@@ -5,19 +5,19 @@ import { List, ListItem } from "@material-ui/core";
 import * as ROLES from "../../../constants/roles";
 
 const NavigationAuth = ({ authUser }) => (
-	<List>
-		<ListItem button component={Link} to={ROUTES.HOME.path}>
-			Home
-		</ListItem>
-		<ListItem button component={Link} to={ROUTES.ACCOUNT.path}>
-			My Account
-		</ListItem>
-		{authUser.roles && !!authUser.roles[ROLES.ADMIN] && (
-			<ListItem button component={Link} to={ROUTES.ADMIN.path}>
-				Admin
-			</ListItem>
-		)}
-	</List>
+  <List>
+    <ListItem button component={Link} to={ROUTES.HOME.path}>
+      Home
+    </ListItem>
+    <ListItem button component={Link} to={ROUTES.ACCOUNT.path}>
+      My Account
+    </ListItem>
+    {authUser.roles && !!authUser.roles[ROLES.ADMIN] && (
+      <ListItem button component={Link} to={ROUTES.ADMIN.path}>
+        Admin
+      </ListItem>
+    )}
+  </List>
 );
 
 export default NavigationAuth;
