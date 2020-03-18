fd9f8e0538c5537fbee415ad78562a626fe5aa5f
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:26:13 -0700

    Update styles.js

--- src/containers/Questions/styles.js
+++ src/containers/Questions/styles.js
@@ -1,18 +1,17 @@
 const styles = theme => ({
-	editor: {
-		background: "#fff",
-		borderRadius: "8px",
-		boxShadow:
-			bigShadow,
-		width: "100%"
-	},
-	preview: {
-		// background: "#fff",
-		borderRadius: "8px",
-		// padding: "10px",
-		// boxShadow:bigShadow,
-		// width: "100%"
-	}
+  editor: {
+    background: "#fff",
+    borderRadius: "8px",
+    boxShadow: theme.bigShadow,
+    width: "100%"
+  },
+  preview: {
+    // background: "#fff",
+    borderRadius: "8px"
+    // padding: "10px",
+    // boxShadow:bigShadow,
+    // width: "100%"
+  }
 });
 
 export default styles;
