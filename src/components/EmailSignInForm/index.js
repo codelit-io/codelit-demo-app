2451705f67adf5f23e9ee276f0f90129cc3b948d
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:31:48 -0700

    Update index.js

--- src/components/EmailSignInForm/index.js
+++ src/components/EmailSignInForm/index.js
@@ -16,70 +16,70 @@ import PropTypes from "prop-types";
 import MoTitle from "../shared/MoTitle";
 
 const EmailSignInForm = ({ classes, onSubmit, isInvalid, onChange, error }) => {
-	return (
-		<Card>
-			<main className={classes.main}>
-				<CssBaseline />
-				<div className={classes.paper}>
-				<MoTitle text="Sign in" fade={true} margin="36px 0 36px" />
-					<form className={classes.form} onSubmit={onSubmit}>
-						<FormControl margin="normal" required fullWidth>
-							<InputLabel htmlFor="email">Email Address</InputLabel>
-							<Input
-								id="email"
-								name="email"
-								autoComplete="email"
-								type="email"
-								autoFocus
-								onChange={onChange}
-							/>
-						</FormControl>
-						<FormControl margin="normal" required fullWidth>
-							<InputLabel htmlFor="password">Password</InputLabel>
-							<Input
-								name="password"
-								type="password"
-								id="password"
-								autoComplete="current-password"
-								onChange={onChange}
-							/>
-						</FormControl>
-						<FormControlLabel
-							control={<Checkbox value="remember" color="primary" />}
-							label="Remember me"
-						/>
-						<Button
-							type="submit"
-							fullWidth
-							variant="contained"
-							color="primary"
-							className={classes.submit}
-							disabled={isInvalid}
-						>
-							Sign in
-						</Button>
-						{error && (
-							<FormHelperText
-								className={classes.error}
-								id="component-error-text"
-								margin="dense"
-								error={true}
-								focused={true}
-							>
-								{error.message}
-							</FormHelperText>
-						)}
-					</form>
-					<PasswordForgetLink />
-					<SignUpLink />
-				</div>
-			</main>
-		</Card>
-	);
+  return (
+    <Card>
+      <main className={classes.main}>
+        <CssBaseline />
+        <div className={classes.paper}>
+          <MoTitle text="Sign in" fade={true} margin="36px 0 36px" />
+          <form className={classes.form} onSubmit={onSubmit}>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="email">Email Address</InputLabel>
+              <Input
+                id="email"
+                name="email"
+                autoComplete="email"
+                type="email"
+                autoFocus
+                onChange={onChange}
+              />
+            </FormControl>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <Input
+                name="password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={onChange}
+              />
+            </FormControl>
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              disabled={isInvalid}
+            >
+              Sign in
+            </Button>
+            {error && (
+              <FormHelperText
+                className={classes.error}
+                id="component-error-text"
+                margin="dense"
+                error={true}
+                focused={true}
+              >
+                {error.message}
+              </FormHelperText>
+            )}
+          </form>
+          <PasswordForgetLink />
+          <SignUpLink />
+        </div>
+      </main>
+    </Card>
+  );
 };
 
 EmailSignInForm.propTypes = {
-	classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(EmailSignInForm);
