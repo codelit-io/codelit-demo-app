b0089a1170002868b168470518ca78a34ab86f47
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:29:40 -0700

    Update index.js

--- src/components/SocialSignIn/SignInWithGoogle/index.js
+++ src/components/SocialSignIn/SignInWithGoogle/index.js
@@ -8,59 +8,64 @@ import { withRouter } from "react-router-dom";
 import withStyles from "@material-ui/core/styles/withStyles";
 
 const styles = theme => ({
-	button: {
-		margin: theme.spacing(1)
-	},
-	input: {
-		margin: theme.spacing(1)
-	}
+  button: {
+    margin: theme.spacing(1)
+  },
+  input: {
+    margin: theme.spacing(1)
+  }
 });
 
 const SignInWithGoogleBase = ({ firebase, classes, history }) => {
-	const [error, setError] = useState({ error: null });
-	const onSubmit = event => {
-		firebase
-			.signInWithGoogle()
-			.then(socialAuthUser => {
-				// Create a user in Firebase Realtime Database
-				return firebase.user(socialAuthUser.user.uid).set(
-					{
-						username: socialAuthUser.user.displayName,
-						email: socialAuthUser.user.email,
-						roles: {}
-					},
-					{ merge: true }
-				);
-			})
-			.then(() => {
-				setError(null);
-				// Investigate if this this the correct approach
-				history.goBack();
-			})
-			.catch(error => setError(error));
-		event.preventDefault();
-	};
+  const [error, setError] = useState({ error: null });
+  const onSubmit = event => {
+    firebase
+      .signInWithGoogle()
+      .then(socialAuthUser => {
+        // Create a user in Firebase Realtime Database
+        return firebase.user(socialAuthUser.user.uid).set(
+          {
+            username: socialAuthUser.user.displayName,
+            email: socialAuthUser.user.email,
+            roles: {}
+          },
+          { merge: true }
+        );
+      })
+      .then(() => {
+        setError(null);
+        // Investigate if this this the correct approach
+        history.goBack();
+      })
+      .catch(error => setError(error));
+    event.preventDefault();
+  };
 
-	return (
-		<form onSubmit={onSubmit}>
-			<Button color="primary" variant="contained" className={classes.button} type="submit">
-				{" "}
-				SIgn In with Google
-			</Button>
+  return (
+    <form onSubmit={onSubmit}>
+      <Button
+        color="primary"
+        variant="contained"
+        className={classes.button}
+        type="submit"
+      >
+        {" "}
+        SIgn In with Google
+      </Button>
 
-			{error && <p>{error.message}</p>}
-		</form>
-	);
+      {error && <p>{error.message}</p>}
+    </form>
+  );
 };
 
 SignInWithGoogleBase.propTypes = {
-	classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired
 };
 
 const SignInWithGoogle = compose(
-	withStyles(styles),
-	withRouter,
-	withFirebase
+  withStyles(styles),
+  withRouter,
+  withFirebase
 )(SignInWithGoogleBase);
 
 export default SignInWithGoogle;
