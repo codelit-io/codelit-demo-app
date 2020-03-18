5b3218735f20d9514963a6e93db83fd25e51b760
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:26:15 -0700

    Update index.js

--- src/containers/Questions/QuestionCard/index.js
+++ src/containers/Questions/QuestionCard/index.js
@@ -5,21 +5,21 @@ import MoCard from "../../../components/shared/MoCard";
 import MoTitle from "../../../components/shared/MoTitle";
 
 const QuestionCard = ({ index, question, userPoints }) => (
-	<>
-		{question.category && (
-			<Grid item xs={12} sm={12} md={12}>
-				<MoTitle
-					text={question.category}
-					fade={true}
-					margin={index === 0 ? "36px 0 26px" : "40px 0 36px" }
-					width="100%"
-				></MoTitle>
-			</Grid>
-		)}
-		<Grid item xs={12} sm={6} md={6}>
-			<MoCard userPoints={userPoints} item={question}></MoCard>
-		</Grid>
-	</>
+  <>
+    {question.category && (
+      <Grid item xs={12} sm={12} md={12}>
+        <MoTitle
+          text={question.category}
+          fade={true}
+          margin={index === 0 ? "36px 0 26px" : "40px 0 36px"}
+          width="100%"
+        ></MoTitle>
+      </Grid>
+    )}
+    <Grid item xs={12} sm={6} md={6}>
+      <MoCard userPoints={userPoints} item={question}></MoCard>
+    </Grid>
+  </>
 );
 
 export default QuestionCard;
