c757eee464f7bffa04bdb4c91dced38ab215fa2f
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:30:01 -0700

    Update index.js

--- src/components/shared/PageHeader/index.js
+++ src/components/shared/PageHeader/index.js
@@ -7,15 +7,15 @@ import Typography from "@material-ui/core/Typography";
 import withStyles from "@material-ui/core/styles/withStyles";
 
 const PageHeader = ({ classes, title }) => {
-	return (
-		<Fade in={title && true} timeout={{ enter: 1000 }}>
-			<Typography variant="h2" gutterBottom>
-				<Box fontWeight="fontWeightLight" className={classes.linkText}>
-					{title}
-				</Box>
-			</Typography>
-		</Fade>
-	);
+  return (
+    <Fade in={title && true} timeout={{ enter: 1000 }}>
+      <Typography variant="h2" gutterBottom>
+        <Box fontWeight="fontWeightLight" className={classes.linkText}>
+          {title}
+        </Box>
+      </Typography>
+    </Fade>
+  );
 };
 
 export default withStyles(styles)(PageHeader);
