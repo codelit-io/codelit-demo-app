ecf8c3d577db29205dd19725d0ef9e396136077d
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:28:57 -0700

    Update index.test.js

--- src/containers/Home/index.test.js
+++ src/containers/Home/index.test.js
@@ -3,6 +3,6 @@ import Home from "./index.js";
 import renderer from "react-test-renderer";
 
 it("Home Page", () => {
-	const tree = renderer.create(<Home />).toJSON();
-	expect(tree).toMatchSnapshot();
+  const tree = renderer.create(<Home />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
