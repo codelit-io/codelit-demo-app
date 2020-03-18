72be076bc12409ad1e369f9d7a2013731481bdee
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:29:59 -0700

    Update styles.js

--- src/components/shared/PageHeader/styles.js
+++ src/components/shared/PageHeader/styles.js
@@ -1,12 +1,12 @@
 const styles = theme => ({
-	linkText: {
-		color: theme.grey.dark,
-		textDecoration: "none",
-		verticalAlign: "middle"
-	},
-	container: {
-		margin: "26px 0px 26px"
-	}
+  linkText: {
+    color: theme.grey.dark,
+    textDecoration: "none",
+    verticalAlign: "middle"
+  },
+  container: {
+    margin: "26px 0px 26px"
+  }
 });
 
 export default styles;
