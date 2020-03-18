4dc0b6b422d9aae96301cc4aba790f7821597e81
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:28:06 -0700

    Update styles.js

--- src/containers/Landing/styles.js
+++ src/containers/Landing/styles.js
@@ -1,30 +1,30 @@
 const styles = theme => ({
-	container: {
-		marginTop: theme.spacing(12),
-		marginBottom: theme.spacing(12),
-	},
-	heroText: {
-		color: "rgb(43, 43, 43)",
-		fontWeight: "500",
-		lineHeight: "1.2",
-		letterSpacing: "-0.01em",
-	},
-	heroSubtitle: {
-		color: "rgb(94, 94, 94)",
-		fontSize: "24px",
-		lineHeight: "1.5",
-		letterSpacing: "-0.01em",
-		marginBottom: "24px"
-	},
-	img: {
-		width: "100%",
-		[theme.breakpoints.down("sm")]: {
-			display: "none"
-		}
-	},
-	checkMark: {
-		color: theme.green.light
-	}
+  container: {
+    marginTop: theme.spacing(12),
+    marginBottom: theme.spacing(12)
+  },
+  heroText: {
+    color: "rgb(43, 43, 43)",
+    fontWeight: "500",
+    lineHeight: "1.2",
+    letterSpacing: "-0.01em"
+  },
+  heroSubtitle: {
+    color: "rgb(94, 94, 94)",
+    fontSize: "24px",
+    lineHeight: "1.5",
+    letterSpacing: "-0.01em",
+    marginBottom: "24px"
+  },
+  img: {
+    width: "100%",
+    [theme.breakpoints.down("sm")]: {
+      display: "none"
+    }
+  },
+  checkMark: {
+    color: theme.green.light
+  }
 });
 
 export default styles;
