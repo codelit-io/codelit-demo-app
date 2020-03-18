cfb3a6cdfd21c90b9614aed4e5dbcf47cc8539aa
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:29:28 -0700

    Update routes.js

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -3,15 +3,24 @@ export const ACCOUNT = { path: "/account", title: "My account" };
 export const ADMIN = { path: "/admin", title: "Admin Page" };
 export const ADMIN_DETAILS = { path: "/admin/:id", title: "Admin Details" };
 export const ADMIN_QUESTIONS = { path: "/admin/questions", title: "Questions" };
-export const ADMIN_QUESTIONS_EASY = { path: "/admin/questions/questions", title: "Questions Easy" };
-export const ADMIN_QUESTIONS_MID = { path: "/admin/questions/questions-mid", title: "Questions Mid" };
-export const ADMIN_QUESTIONS_PRO = { path: "/admin/questions/questions-pro", title: "Questions Pro" };
+export const ADMIN_QUESTIONS_EASY = {
+  path: "/admin/questions/questions",
+  title: "Questions Easy"
+};
+export const ADMIN_QUESTIONS_MID = {
+  path: "/admin/questions/questions-mid",
+  title: "Questions Mid"
+};
+export const ADMIN_QUESTIONS_PRO = {
+  path: "/admin/questions/questions-pro",
+  title: "Questions Pro"
+};
 export const ADMIN_USERS = { path: "/admin/users", title: "Users" };
 
 export const HOME = { path: "/home", title: "Home Page" };
 export const LANDING = {
-	path: "/",
-	title: "New way to Mastering Front-end Development"
+  path: "/",
+  title: "New way to Mastering Front-end Development"
 };
 export const QUESTIONS = { path: "/topics", title: "Easy" };
 export const SIGN_IN = { path: "/signin", title: "Sign in" };
