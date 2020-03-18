c1d2e60db6b27f19c2fe12a8d7e9edbb88c2d351
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:31:14 -0700

    Update index.js

--- src/components/Session/index.js
+++ src/components/Session/index.js
@@ -3,4 +3,9 @@ import withAuthentication from "./withAuthentication";
 import withAuthorization from "./withAuthorization";
 import withEmailVerification from "./withEmailVerification";
 
-export { AuthUserContext, withAuthentication, withAuthorization, withEmailVerification };
+export {
+  AuthUserContext,
+  withAuthentication,
+  withAuthorization,
+  withEmailVerification
+};
