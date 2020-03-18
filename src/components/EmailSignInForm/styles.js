28a21eb9b30c020f23afa0c8c8cc69722fbb544a
Mo Sharif <mo@mosh-media.com>
2020-03-17 17:25:09 -0700

    Update styles.js

--- src/components/EmailSignInForm/styles.js
+++ src/components/EmailSignInForm/styles.js
@@ -17,12 +17,6 @@ const styles = theme => ({
 		alignItems: "center",
 		padding: `${theme.spacing(2)}px ${theme.spacing(3)}px ${theme.spacing(3)}px`
 	},
-	avatar: {
-		margin: theme.spacing(),
-		backgroundColor: theme.palette.primary.main,
-		width: theme.spacing(3),
-		height: theme.spacing(3)
-	},
 	form: {
 		width: "100%", // Fix IE 11 issue.
 		marginTop: theme.spacing()
