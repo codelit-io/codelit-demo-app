8a1ace16013c4bf683e7a1e1ab59966da41f6435
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:31:28 -0700

    Update index.js

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -19,104 +19,108 @@ import MenuItem from "@material-ui/core/MenuItem";
 import SignOutButton from "../SignOut";
 
 const Navigation = ({ classes }) => {
-	const [anchorEl, setAnchorEl] = useState(null);
+  const [anchorEl, setAnchorEl] = useState(null);
 
-	const handleClick = event => {
-		setAnchorEl(event.currentTarget);
-	};
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
 
-	const handleClose = () => {
-		setAnchorEl(null);
-	};
-	return (
-		<div className={classes.root}>
-			<AppBar position="static" color="default" className={classes.appBar}>
-				<Toolbar className={classes.toolbar}>
-					<Grid container className={classes.alignCenter}>
-						<Grid item xs={4} sm={4} md={4} lg={4}>
-							<Typography className={classes.title} variant="h6" noWrap>
-								<Link
-									to={ROUTES.LANDING.path}
-									style={{ color: "#383c40", textDecoration: "none" }}
-								>
-									<Box
-										fontWeight="fontWeightLight"
-										className={classes.MoSkool}
-									>
-										Mo Skool
-									</Box>
-								</Link>
-							</Typography>
-						</Grid>
-						<Grid item xs={4} sm={4} md={4} lg={4}></Grid>
-						<Grid item xs={4} sm={4} md={4} lg={4} style={{textAlign: "right"}}>
-							<MoDrawer />
-							<AuthUserContext.Consumer>
-								{authUser => (
-									<>
-										<Button
-											aria-controls="avatar-menu"
-											aria-haspopup="true"
-											className={classes.avatarButton}
-											onClick={handleClick}
-										>
-											<Avatar
-												alt="Me"
-												src={authUser && authUser.photoURL}
-												className={`${classes.avatar} ${authUser &&
-													authUser.roles &&
-													authUser.roles.ADMIN &&
-													classes.adminAvatar}`}
-											/>
-										</Button>
-										<Menu
-											id="avatar-menu"
-											anchorEl={anchorEl}
-											keepMounted
-											open={Boolean(anchorEl)}
-											onClose={handleClose}
-										>
-											{authUser ? (
-												<div>
-													<MenuItem
-														to={ROUTES.ACCOUNT.path}
-														component={Link}
-														onClick={handleClose}
-													>
-														My account
-													</MenuItem>
-													<SignOutButton
-														handleClose={handleClose}
-													></SignOutButton>
-												</div>
-											) : (
-												<div>
-													<MenuItem
-														to={ROUTES.SIGN_IN.path}
-														component={Link}
-														onClick={handleClose}
-													>
-														Sign in
-													</MenuItem>
-													<MenuItem
-														to={ROUTES.SIGN_UP.path}
-														component={Link}
-														onClick={handleClose}
-													>
-														Sign up
-													</MenuItem>
-												</div>
-											)}
-										</Menu>
-									</>
-								)}
-							</AuthUserContext.Consumer>
-						</Grid>
-					</Grid>
-				</Toolbar>
-			</AppBar>
-		</div>
-	);
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="default" className={classes.appBar}>
+        <Toolbar className={classes.toolbar}>
+          <Grid container className={classes.alignCenter}>
+            <Grid item xs={4} sm={4} md={4} lg={4}>
+              <Typography className={classes.title} variant="h6" noWrap>
+                <Link
+                  to={ROUTES.LANDING.path}
+                  style={{ color: "#383c40", textDecoration: "none" }}
+                >
+                  <Box fontWeight="fontWeightLight" className={classes.MoSkool}>
+                    Mo Skool
+                  </Box>
+                </Link>
+              </Typography>
+            </Grid>
+            <Grid item xs={4} sm={4} md={4} lg={4}></Grid>
+            <Grid
+              item
+              xs={4}
+              sm={4}
+              md={4}
+              lg={4}
+              style={{ textAlign: "right" }}
+            >
+              <MoDrawer />
+              <AuthUserContext.Consumer>
+                {authUser => (
+                  <>
+                    <Button
+                      aria-controls="avatar-menu"
+                      aria-haspopup="true"
+                      className={classes.avatarButton}
+                      onClick={handleClick}
+                    >
+                      <Avatar
+                        alt="Me"
+                        src={authUser && authUser.photoURL}
+                        className={`${classes.avatar} ${authUser &&
+                          authUser.roles &&
+                          authUser.roles.ADMIN &&
+                          classes.adminAvatar}`}
+                      />
+                    </Button>
+                    <Menu
+                      id="avatar-menu"
+                      anchorEl={anchorEl}
+                      keepMounted
+                      open={Boolean(anchorEl)}
+                      onClose={handleClose}
+                    >
+                      {authUser ? (
+                        <div>
+                          <MenuItem
+                            to={ROUTES.ACCOUNT.path}
+                            component={Link}
+                            onClick={handleClose}
+                          >
+                            My account
+                          </MenuItem>
+                          <SignOutButton
+                            handleClose={handleClose}
+                          ></SignOutButton>
+                        </div>
+                      ) : (
+                        <div>
+                          <MenuItem
+                            to={ROUTES.SIGN_IN.path}
+                            component={Link}
+                            onClick={handleClose}
+                          >
+                            Sign in
+                          </MenuItem>
+                          <MenuItem
+                            to={ROUTES.SIGN_UP.path}
+                            component={Link}
+                            onClick={handleClose}
+                          >
+                            Sign up
+                          </MenuItem>
+                        </div>
+                      )}
+                    </Menu>
+                  </>
+                )}
+              </AuthUserContext.Consumer>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 };
 
 export default withStyles(styles)(Navigation);
