ec4d387435262bb8680b1b332e0a5f2ae5f26977
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:31:12 -0700

    Update withAuthentication.js

--- src/components/Session/withAuthentication.js
+++ src/components/Session/withAuthentication.js
@@ -3,41 +3,43 @@ import AuthUserContext from "./context";
 import { withFirebase } from "../Firebase";
 
 const withAuthentication = Component => {
-	class withAuthentication extends React.Component {
-		constructor(props) {
-			super(props);
+  class withAuthentication extends React.Component {
+    constructor(props) {
+      super(props);
 
-			this.state = {
-				authUser: JSON.parse(localStorage.getItem("authUser"))
-			};
-		}
+      this.state = {
+        authUser: JSON.parse(localStorage.getItem("authUser"))
+      };
+    }
 
-		componentDidMount() {
-			this.listener = this.props.firebase && this.props.firebase.onAuthUserListener(
-				authUser => {
-					localStorage.setItem("authUser", JSON.stringify(authUser));
-					this.setState({ authUser });
-				},
-				() => {
-					localStorage.removeItem("authUser");
-					this.setState({ authUser: null });
-				}
-			);
-		}
+    componentDidMount() {
+      this.listener =
+        this.props.firebase &&
+        this.props.firebase.onAuthUserListener(
+          authUser => {
+            localStorage.setItem("authUser", JSON.stringify(authUser));
+            this.setState({ authUser });
+          },
+          () => {
+            localStorage.removeItem("authUser");
+            this.setState({ authUser: null });
+          }
+        );
+    }
 
-		componentWillUnmount() {
-			this.listener();
-		}
+    componentWillUnmount() {
+      this.listener();
+    }
 
-		render() {
-			return (
-				<AuthUserContext.Provider value={this.state.authUser}>
-					<Component {...this.props} />
-				</AuthUserContext.Provider>
-			);
-		}
-	}
-	return withFirebase(withAuthentication);
+    render() {
+      return (
+        <AuthUserContext.Provider value={this.state.authUser}>
+          <Component {...this.props} />
+        </AuthUserContext.Provider>
+      );
+    }
+  }
+  return withFirebase(withAuthentication);
 };
 
 export default withAuthentication;
