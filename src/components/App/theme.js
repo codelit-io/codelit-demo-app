450494545730ed88b3c68b746839a56226186db4
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:26:33 -0700

    Update theme.js

--- src/components/App/theme.js
+++ src/components/App/theme.js
@@ -2,66 +2,64 @@ import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 import responsiveFontSizes from "@material-ui/core/styles/responsiveFontSizes";
 
 const bigShadow =
-	"0 24px 24px -18px rgba(69,104,129,.33), 0 9px 45px 0 rgba(114,119,160,.12)";
+  "0 24px 24px -18px rgba(69,104,129,.33), 0 9px 45px 0 rgba(114,119,160,.12)";
 
 const colors = {
-	grey: {
-		light: "#a6a6a6",
-		medium: "#424B54",
-		dark: "#383c40",
-		darkest: "#2b2b2b",
-	},
-	green: {
-		light: "#41D3BD",
-		dark: "#157A6E",
-		darkest: ""
-	},
-	yellow: {
-		light: "#F5D547",
-		dark: "",
-		darkest: ""
-	},
-	white: { 
-		medium: "#FFFFF2",
-	},
-	blue: {
-		medium: "#276ef1",
-	}
-		
-
+  grey: {
+    light: "#a6a6a6",
+    medium: "#424B54",
+    dark: "#383c40",
+    darkest: "#2b2b2b"
+  },
+  green: {
+    light: "#41D3BD",
+    dark: "#157A6E",
+    darkest: ""
+  },
+  yellow: {
+    light: "#F5D547",
+    dark: "",
+    darkest: ""
+  },
+  white: {
+    medium: "#FFFFF2"
+  },
+  blue: {
+    medium: "#276ef1"
+  }
 };
 
 let theme = createMuiTheme({
-	bigShadow: bigShadow,
-	card: {
-		boxShadow: bigShadow,
-		"&:hover": {
-			transform: "translateY(-5px)",
-			backgroundColor: "#FFF"
-		},
-		"&:hover .desc": {
-			color: "white"
-		},
-		transition:
-			"transform .35s cubic-bezier(.4,0,.2,1),box-shadow .35s cubic-bezier(.4,0,.2,1)",
-		transform: "translateY(0)"
-	},
-	container: {
-		width: "calc(100vw - 28.799999999999997rem)"
-	},
-	flexAlignCenter: {
-		display: "flex",
-		alignItems: "center"
-	},
-	grey: colors.grey,
-	green: colors.green,
-	yellow: colors.yellow,
-	white: colors.white,
-	blue: colors.blue
+  bigShadow: bigShadow,
+  card: {
+    boxShadow: bigShadow,
+    "&:hover": {
+      transform: "translateY(-5px)",
+      backgroundColor: "#FFF"
+    },
+    "&:hover .desc": {
+      color: "white"
+    },
+    transition:
+      "transform .35s cubic-bezier(.4,0,.2,1),box-shadow .35s cubic-bezier(.4,0,.2,1)",
+    transform: "translateY(0)"
+  },
+  container: {
+    width: "calc(100vw - 28.799999999999997rem)"
+  },
+  flexAlignCenter: {
+    display: "flex",
+    alignItems: "center"
+  },
+  grey: colors.grey,
+  green: colors.green,
+  yellow: colors.yellow,
+  white: colors.white,
+  blue: colors.blue
 });
 
 const fontOptions = {
-	breakpoints: ["sm", "md", "lg", "xl"]
+  breakpoints: ["sm", "md", "lg", "xl"]
 };
 
 theme = responsiveFontSizes(theme, fontOptions);
