717925b50ab0f61a098a75784b009c3dcbf84670
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:30:09 -0700

    Update styles.js

--- src/components/shared/MoSnackBar/MoSnackbarCore/styles.js
+++ src/components/shared/MoSnackBar/MoSnackbarCore/styles.js
@@ -1,22 +1,22 @@
 const styles = theme => ({
-	container: {
-		textAlign: "center"
-	},
-	snackbarContent: {
-		backgroundColor: "white",
-		color: "black",
-		padding: "2em",
-		boxShadow: theme.bigShadow
-	},
-	message: {
-		display: "flex",
-		alignItems: "center"
-	},
-	checkIcon: {
-		color: " #99bb33",
-		fontSize: "40px",
-		marginRight: "20px"
-	}
+  container: {
+    textAlign: "center"
+  },
+  snackbarContent: {
+    backgroundColor: "white",
+    color: "black",
+    padding: "2em",
+    boxShadow: theme.bigShadow
+  },
+  message: {
+    display: "flex",
+    alignItems: "center"
+  },
+  checkIcon: {
+    color: " #99bb33",
+    fontSize: "40px",
+    marginRight: "20px"
+  }
 });
 
 export default styles;
