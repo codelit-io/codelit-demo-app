83a7f170b488f0ce063cb031f9a77727ce2fc700
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:28:34 -0700

    Update index.js

--- src/components/ComponentMaker/Components/index.js
+++ src/components/ComponentMaker/Components/index.js
@@ -1,24 +1,24 @@
 import React from "react";
 
 const inputHtml = () => {
-	return <input type="text" />;
+  return <input type="text" />;
 };
 
 const buttonHtml = () => {
-	return <button type="submit" >I am a boring button :)</button>;
+  return <button type="submit">I am a boring button :)</button>;
 };
 
 const Components = {
-	inputHtml: inputHtml,
-	buttonHtml: buttonHtml,
-	button: React.lazy(() => import("@material-ui/core/Button")),
-	textField: React.lazy(() => import("@material-ui/core/TextField")),
-	input: React.lazy(() => import("@material-ui/core/Input")),
-	switch: React.lazy(() => import("@material-ui/core/Switch")),
-	checkbox: React.lazy(() => import("@material-ui/core/Checkbox")),
-	slider: React.lazy(() => import("@material-ui/core/Slider")),
-	link: React.lazy(() => import("@material-ui/core/Link")),
-	typography: React.lazy(() => import("@material-ui/core/Typography"))
+  inputHtml: inputHtml,
+  buttonHtml: buttonHtml,
+  button: React.lazy(() => import("@material-ui/core/Button")),
+  textField: React.lazy(() => import("@material-ui/core/TextField")),
+  input: React.lazy(() => import("@material-ui/core/Input")),
+  switch: React.lazy(() => import("@material-ui/core/Switch")),
+  checkbox: React.lazy(() => import("@material-ui/core/Checkbox")),
+  slider: React.lazy(() => import("@material-ui/core/Slider")),
+  link: React.lazy(() => import("@material-ui/core/Link")),
+  typography: React.lazy(() => import("@material-ui/core/Typography"))
 };
 
 export default Components;
