7f38607b9a71f6df678e3f6f865d019cb6e942ee
Mo Sharif <mo@mosh-media.com>
2020-03-17 20:29:04 -0700

    Update index.js

--- src/containers/Admin/index.js
+++ src/containers/Admin/index.js
@@ -9,32 +9,36 @@ import MoPage from "../../components/shared/MoPage";
 import Spinner from "../../components/shared/Spinner";
 import { UserList, UserItem } from "../../components/Users";
 import {
-	withAuthorization,
-	withEmailVerification
+  withAuthorization,
+  withEmailVerification
 } from "../../components/Session";
 
 const Questions = lazy(() => import("./Questions"));
 
 const AdminPage = ({ history }) => (
-	<MoPage title="Admin" isCard={true} loading={false}>
-		<AdminNav history={history} />
-		<Suspense fallback={<Spinner loading={true} color="primary" />}></Suspense>
-		<Switch>
-			<Route exact path={ROUTES.ADMIN_QUESTIONS.path + "/:level"} component={Questions} />
-			<Route exact path={ROUTES.ADMIN_DETAILS.path} component={UserList} />
-			<Route exact path={ROUTES.ADMIN_USERS.path} component={UserList} />
-			<Route
-				exact
-				path={ROUTES.ADMIN_USERS.path + "/:id"}
-				component={UserItem}
-			/>
-		</Switch>
-	</MoPage>
+  <MoPage title="Admin" isCard={true} loading={false}>
+    <AdminNav history={history} />
+    <Suspense fallback={<Spinner loading={true} color="primary" />}></Suspense>
+    <Switch>
+      <Route
+        exact
+        path={ROUTES.ADMIN_QUESTIONS.path + "/:level"}
+        component={Questions}
+      />
+      <Route exact path={ROUTES.ADMIN_DETAILS.path} component={UserList} />
+      <Route exact path={ROUTES.ADMIN_USERS.path} component={UserList} />
+      <Route
+        exact
+        path={ROUTES.ADMIN_USERS.path + "/:id"}
+        component={UserItem}
+      />
+    </Switch>
+  </MoPage>
 );
 
 const condition = authUser => authUser && !!authUser.roles[ROLES.ADMIN];
 
 export default compose(
-	withEmailVerification,
-	withAuthorization(condition)
+  withEmailVerification,
+  withAuthorization(condition)
 )(AdminPage);
