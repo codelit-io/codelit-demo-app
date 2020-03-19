97a5152e7ff8343d8a18415dd98b4e8dc72f1ecd
Mo Sharif <mo@mosh-media.com>
2020-03-18 20:21:24 -0700

    Delete index.test.js

--- src/components/App/index.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import App from "./index.js";
-import renderer from "react-test-renderer";
-
-it("Should render App", () => {
-  const tree = renderer.create(<App />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
