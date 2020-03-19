82332af73438a26c55cc4b01a54b0aaebfa31c9c
Mo Sharif <mo@mosh-media.com>
2020-03-18 20:21:16 -0700

    Remove check mark from answered questions

--- src/components/shared/MoCard/index.js
+++ src/components/shared/MoCard/index.js
@@ -4,7 +4,6 @@ import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import Typography from "@material-ui/core/Typography";
 import { Link } from "react-router-dom";
 import styles from "./styles";
