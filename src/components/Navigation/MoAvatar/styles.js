4b46f8c0dd01f5c5789a826bc76af8c48b284842
Mo Sharif <mo@mosh-media.com>
2020-03-19 22:16:26 -0700

    Create styles.js

---
+++ src/components/Navigation/MoAvatar/styles.js
@@ -0,0 +1,12 @@
+const styles = theme => ({
+  avatar: {
+    width: theme.spacing(4),
+    height: theme.spacing(4)
+  },
+  adminAvatar: {
+    border: `2px solid ${theme.yellow.light}`,
+    overflow: "visible"
+  }
+});
+
+export default styles;
