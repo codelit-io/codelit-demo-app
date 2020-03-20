d6a554651fa1000393f86d96c281c66f38063e6d
Mo Sharif <mo@mosh-media.com>
2020-03-19 22:16:14 -0700

    Update index.js

--- src/components/Navigation/NavigationNonAuth/index.js
+++ src/components/Navigation/NavigationNonAuth/index.js
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 import MenuItem from "@material-ui/core/MenuItem";
 
 const NavigationNonAuth = ({ handleClose }) => (
-  <>
+  <div>
     <MenuItem to={ROUTES.SIGN_IN.path} component={Link} onClick={handleClose}>
       Sign in
     </MenuItem>
     <MenuItem to={ROUTES.SIGN_UP.path} component={Link} onClick={handleClose}>
       Sign up
     </MenuItem>
-  </>
+  </div>
 );
 
 export default NavigationNonAuth;
