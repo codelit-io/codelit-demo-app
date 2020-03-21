9ac404bf75aaa128218b252e62be403fd4b93384
Mo Sharif <mo@mosh-media.com>
2020-03-21 00:01:13 -0700

    Update styles.js

--- src/components/shared/MoPage/styles.js
+++ src/components/shared/MoPage/styles.js
@@ -1,11 +1,22 @@
 const styles = theme => ({
   content: {
     display: "flex",
-    flexDirection: "column"
+    flexDirection: "column",
+    width: "100%"
   },
   img: {
     height: 100,
     width: 100
+  },
+  section: {
+    width: "100%",
+	  paddingTop: theme.padding.lg,
+	  paddingBottom: theme.padding.lg
+  },
+  scoreBoard: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
   }
 });
 
