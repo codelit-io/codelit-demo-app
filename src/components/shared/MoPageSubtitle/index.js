d7bf0ffc4ad86778ab302fed5e9f9342e8ffdd0c
Mo Sharif <mo@mosh-media.com>
2020-03-21 00:03:44 -0700

    Update index.js

--- src/components/shared/MoPageSubtitle/index.js
+++ src/components/shared/MoPageSubtitle/index.js
@@ -4,14 +4,14 @@ import Typography from "@material-ui/core/Typography";
 import Fade from "@material-ui/core/Fade";
 
 const MoPageSubtitle = ({ subtitle }) => (
-	<Fade timeout={{ enter: 2000 }} in={true}>
-		<Typography
-			variant="body1"
-			style={{ paddingLeft: "4px", color: "#484848" }}
-		>
-			{subtitle}
-		</Typography>
-	</Fade>
+  <Fade timeout={{ enter: 2000 }} in={true}>
+    <Typography
+      variant="body1"
+      style={{ paddingLeft: "4px", color: "#484848" }}
+    >
+      {subtitle}
+    </Typography>
+  </Fade>
 );
 
 export default MoPageSubtitle;
