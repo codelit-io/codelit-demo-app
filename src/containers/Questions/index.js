3413772cc3c6bd9a2062d0f495d3cfe90a76012a
Mo Sharif <mo@mosh-media.com>
2020-03-21 00:01:32 -0700

    Update index.js

--- src/containers/Questions/index.js
+++ src/containers/Questions/index.js
@@ -4,83 +4,79 @@ import { AuthUserContext, withAuthentication } from "../../components/Session";
 import Grid from "@material-ui/core/Grid";
 import QuestionCard from "./QuestionCard";
 import MoPage from "../../components/shared/MoPage";
-import MoLink from "../../components/shared/MoLink";
-import * as ROUTES from "../../constants/routes";
+import QuestionCategory from "./QuestionCategory";
+
 const Questions = ({ firebase }) => {
-  const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
-  const [userPoints, setUserPoints] = useState(0);
+	const [loading, setLoading] = useState(false);
+	const [questions, setQuestions] = useState([]);
+	const [userPoints, setUserPoints] = useState(0);
 
-  const QuestionsList = ({ authUser, questions }) => {
-    if (authUser) {
-      setUserPoints(authUser.points);
-    }
+	const QuestionsList = ({ authUser, questions }) => {
+		if (authUser) {
+			setUserPoints(authUser.points);
+		}
 
-    return questions.map((question, index) => (
-      <React.Fragment key={index}>
-        <QuestionCard
-          userPoints={userPoints}
-          question={question}
-          index={index}
-        />
-      </React.Fragment>
-    ));
-  };
+		return questions.map((question, index) => (
+			<React.Fragment key={index}>
+				{question.category && <QuestionCategory category={question.category} index={index} />}
+				<QuestionCard
+					userPoints={userPoints}
+					question={question}
+					index={index}
+				/>
+			</React.Fragment>
+		));
+	};
 
-  useEffect(() => {
-    setLoading(true);
-    const getQuestions = firebase
-      .questions()
-      .orderBy("id")
-      .onSnapshot(snapshot => {
-        if (snapshot.size) {
-          let questions = [];
-          snapshot.forEach(doc => {
-            questions.push({
-              ...doc.data(),
-              uid: doc.id
-            });
-          });
-          setQuestions(questions);
-          setLoading(false);
-        } else {
-          setQuestions([]);
-          setLoading(false);
-        }
-      });
-    /* Unsubscribe from firebase on unmount */
-    return () => {
-      setQuestions([]);
-      getQuestions();
-    };
-  }, [firebase]);
+	useEffect(() => {
+		setLoading(true);
+		const getQuestions = firebase
+			.questions()
+			.orderBy("id")
+			.onSnapshot(snapshot => {
+				if (snapshot.size) {
+					let questions = [];
+					snapshot.forEach(doc => {
+						questions.push({
+							...doc.data(),
+							uid: doc.id
+						});
+					});
+					setQuestions(questions);
+					setLoading(false);
+				} else {
+					setQuestions([]);
+					setLoading(false);
+				}
+			});
+		/* Unsubscribe from firebase on unmount */
+		return () => {
+			setQuestions([]);
+			getQuestions();
+		};
+	}, [firebase]);
 
-  return (
-    <MoPage title="Easy" loading={loading} isCard={false}>
-      {questions && (
-        <AuthUserContext.Consumer>
-          {authUser => (
-            <>
-              <div>
-                {authUser && authUser.points ? (
-                  <p>
-                    <strong>{authUser.points}</strong> Points
-                  </p>
-                ) : (
-                  <MoLink
-                    text="Login to earn points and save progress"
-                    href={ROUTES.SIGN_UP.path}
-                  />
-                )}
-              </div>
-              <Grid container spacing={4}>
-                <QuestionsList authUser={authUser} questions={questions} />
-              </Grid>
-            </>
-          )}
-        </AuthUserContext.Consumer>
-      )}
-    </MoPage>
-  );
+	return (
+		<AuthUserContext.Consumer>
+			{authUser => (
+				<MoPage
+					title="Super Easy"
+					points={authUser && authUser.points}
+					loading={loading}
+					numberOfQuestions={questions.length}
+					
+					isScoreBoard={true}
+				>
+					<>
+						{questions && (
+							<Grid container spacing={4}>
+								<QuestionsList authUser={authUser} questions={questions} />
+							</Grid>
+						)}
+					</>
+				</MoPage>
+			)}
+		</AuthUserContext.Consumer>
+	);
 };
 export default withAuthentication(Questions);
