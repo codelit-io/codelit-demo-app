b6cfa8c48698f1539d866cb848af0e4a60c3643d
Mo Sharif <mo@mosh-media.com>
2020-03-21 00:04:12 -0700

    Update index.js

--- src/containers/Questions/QuestionCard/index.js
+++ src/containers/Questions/QuestionCard/index.js
@@ -4,9 +4,9 @@ import Grid from "@material-ui/core/Grid";
 import MoCard from "../../../components/shared/MoCard";
 
 const QuestionCard = ({ index, question, userPoints }) => (
-	<Grid item xs={12} sm={6} md={3}>
-		<MoCard userPoints={userPoints} item={question}></MoCard>
-	</Grid>
+  <Grid item xs={12} sm={6} md={3}>
+    <MoCard userPoints={userPoints} item={question}></MoCard>
+  </Grid>
 );
 
 export default QuestionCard;
