7d914db88d59b259bfd740ebd13f15f1ea76a9e9
Mo Sharif <mo@mosh-media.com>
2020-03-21 00:01:18 -0700

    Create index.js

---
+++ src/components/shared/MoPageSubtitle/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Typography from "@material-ui/core/Typography";
+import Fade from "@material-ui/core/Fade";
+
+const MoPageSubtitle = ({ subtitle }) => (
+	<Fade timeout={{ enter: 2000 }} in={true}>
+		<Typography
+			variant="body1"
+			style={{ paddingLeft: "4px", color: "#484848" }}
+		>
+			{subtitle}
+		</Typography>
+	</Fade>
+);
+
+export default MoPageSubtitle;
