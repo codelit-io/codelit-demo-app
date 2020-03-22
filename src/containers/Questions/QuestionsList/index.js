6c125e084b06fec2079196341d023891ebf29b04
Mo Sharif <mo@mosh-media.com>
2020-03-22 11:46:02 -0700

    Update index.js

--- src/containers/Questions/QuestionsList/index.js
+++ src/containers/Questions/QuestionsList/index.js
@@ -4,15 +4,19 @@ import QuestionCard from "./QuestionCard";
 import QuestionCategory from "./QuestionCategory";
 
 const QuestionsList = ({ authUser, questions, configs }) => {
-	let url = configs.slug
-	return questions.map((question, index) => (
-		<React.Fragment key={index}>
-			{question.category && (
-				<QuestionCategory category={question.category} index={index} />
-			)}
-			<QuestionCard userPoints={authUser.points} question={question} url={url} />
-		</React.Fragment>
-	));
+  let url = configs.slug;
+  return questions.map((question, index) => (
+    <React.Fragment key={index}>
+      {question.category && (
+        <QuestionCategory category={question.category} index={index} />
+      )}
+      <QuestionCard
+        userPoints={authUser && authUser.points}
+        question={question}
+        url={url}
+      />
+    </React.Fragment>
+  ));
 };
 
 export default QuestionsList;
