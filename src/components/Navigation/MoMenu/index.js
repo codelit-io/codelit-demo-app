8292225e360a44dd98dd08f4c4edbca221f799a6
Mo Sharif <mo@mosh-media.com>
2020-03-22 11:46:22 -0700

    Update index.js

--- src/components/Navigation/MoMenu/index.js
+++ src/components/Navigation/MoMenu/index.js
@@ -11,58 +11,56 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import { IconButton } from "@material-ui/core";
 
 const MoMenu = ({ authUser, classes }) => {
-	const [anchorEl, setAnchorEl] = useState(null);
+  const [anchorEl, setAnchorEl] = useState(null);
 
-	const handleClick = event => {
-		setAnchorEl(event.currentTarget);
-	};
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
 
-	const handleClose = () => {
-		setAnchorEl(null);
-	};
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-	return (
-		<>
-			<IconButton
-				aria-label="View Topics Menu"
-				aria-controls="topics-menu"
-				aria-haspopup="true"
-				onClick={handleClick}
-			>
-				<AppsIcon />
-			</IconButton>
-			<Menu
-				anchorEl={anchorEl}
-				anchorOrigin={{
-					vertical: "bottom",
-					horizontal: "center"
-				}}
-				elevation={2}
-				getContentAnchorEl={null}
-				id="topics-menu"
-				keepMounted
-				open={Boolean(anchorEl)}
-				onClose={handleClose}
-				transformOrigin={{
-					vertical: "top",
-					horizontal: "center"
-				}}
-				TransitionComponent={Slide}
-			>
-				{authUser && (
-					<div>
-						<MenuItem
-							to={ROUTES.SUPER_EASY_QUESTIONS.path}
-							component={Link}
-							onClick={handleClose}
-						>
-							{ROUTES.SUPER_EASY_QUESTIONS.title}
-						</MenuItem>
-					</div>
-				)}
-			</Menu>
-		</>
-	);
+  return (
+    <>
+      <IconButton
+        aria-label="View Topics Menu"
+        aria-controls="topics-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <AppsIcon />
+      </IconButton>
+      <Menu
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center"
+        }}
+        elevation={2}
+        getContentAnchorEl={null}
+        id="topics-menu"
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center"
+        }}
+        TransitionComponent={Slide}
+      >
+        <div>
+          <MenuItem
+            to={ROUTES.SUPER_EASY_QUESTIONS.path}
+            component={Link}
+            onClick={handleClose}
+          >
+            {ROUTES.SUPER_EASY_QUESTIONS.title}
+          </MenuItem>
+        </div>
+      </Menu>
+    </>
+  );
 };
 
 export default withStyles(styles)(MoMenu);
