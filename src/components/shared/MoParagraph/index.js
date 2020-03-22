929bc3dcf4d1e102f93d7c287ac14ebaac41b714
Mo Sharif <mo@mosh-media.com>
2020-03-22 11:46:17 -0700

    Update index.js

--- src/components/shared/MoParagraph/index.js
+++ src/components/shared/MoParagraph/index.js
@@ -5,29 +5,29 @@ import Fade from "@material-ui/core/Fade";
 import Typography from "@material-ui/core/Typography";
 
 const MoParagraph = ({ text, fade, textAlign, margin, width }) => {
-	const styles = {
-		text: {
-			color: "#383c40",
-			verticalAlign: "middle",
-			textDecoration: "none",
-			textAlign: textAlign ? textAlign : "",
-			margin: margin ? margin : "",
-			lineHeight: "3rem",
-			fontSize: "2rem"
-		},
-		container: {
-			width: width ? width : ""
-		}
-	};
-	return (
-		<Fade in={fade} timeout={{ enter: 1000 }}>
-			<Typography variant="subtitle1">
-				<Box fontWeight="fontWeightLight" style={styles.text}>
-					{text}
-				</Box>
-			</Typography>
-		</Fade>
-	);
+  const styles = {
+    text: {
+      color: "#383c40",
+      verticalAlign: "middle",
+      textDecoration: "none",
+      textAlign: textAlign ? textAlign : "",
+      margin: margin ? margin : "",
+      lineHeight: "3rem",
+      fontSize: "2rem"
+    },
+    container: {
+      width: width ? width : ""
+    }
+  };
+  return (
+    <Fade in={fade} timeout={{ enter: 1000 }}>
+      <Typography variant="subtitle1">
+        <Box fontWeight="fontWeightLight" style={styles.text}>
+          {text}
+        </Box>
+      </Typography>
+    </Fade>
+  );
 };
 
 export default MoParagraph;
