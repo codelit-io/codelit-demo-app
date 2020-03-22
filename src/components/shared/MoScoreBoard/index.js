6d42fc60fe72ac097fc3593935d9452b9e8f1aec
Mo Sharif <mo@mosh-media.com>
2020-03-22 11:46:15 -0700

    Update index.js

--- src/components/shared/MoScoreBoard/index.js
+++ src/components/shared/MoScoreBoard/index.js
@@ -4,26 +4,26 @@ import Button from "@material-ui/core/Button";
 import Slide from "@material-ui/core/Slide";
 
 const MoScoreBoard = ({ numberOfQuestions, points }) => (
-	<Slide
-		direction="left"
-		in={numberOfQuestions ? true : false}
-		mountOnEnter
-		timeout={{ enter: 800, exit: 400 }}
-		unmountOnExit
-	>
-		<ButtonGroup
-			variant="text"
-			color="default"
-			aria-label="Points and progress"
-		>
-			<Button> {points ? `${points} Points` : "0 Points"}</Button>
-			<Button>
-				{numberOfQuestions
-					? Math.round((points / numberOfQuestions) * 100) + "% Complete"
-					: "..."}
-			</Button>
-		</ButtonGroup>
-	</Slide>
+  <Slide
+    direction="left"
+    in={numberOfQuestions ? true : false}
+    mountOnEnter
+    timeout={{ enter: 800, exit: 400 }}
+    unmountOnExit
+  >
+    <ButtonGroup
+      variant="text"
+      color="default"
+      aria-label="Points and progress"
+    >
+      <Button> {points ? `${points} Points` : "0 Points"}</Button>
+      <Button>
+        {numberOfQuestions
+          ? Math.round((points / numberOfQuestions) * 100) + "% Complete"
+          : "..."}
+      </Button>
+    </ButtonGroup>
+  </Slide>
 );
 
 export default MoScoreBoard;
