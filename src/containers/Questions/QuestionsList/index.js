b1a363d46ca4ec0512dd70890ef8d1fe87f55acc
Mo Sharif <mo@mosh-media.com>
2020-03-21 19:55:59 -0700

    Create index.js

---
+++ src/containers/Questions/QuestionsList/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import QuestionCard from "./QuestionCard";
+import QuestionCategory from "./QuestionCategory";
+
+const QuestionsList = ({ authUser, questions, configs }) => {
+	let url = configs.slug
+	return questions.map((question, index) => (
+		<React.Fragment key={index}>
+			{question.category && (
+				<QuestionCategory category={question.category} index={index} />
+			)}
+			<QuestionCard userPoints={authUser.points} question={question} url={url} />
+		</React.Fragment>
+	));
+};
+
+export default QuestionsList;
