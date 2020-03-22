77519b1284a87c6395a00f98389153a53acb3a78
Mo Sharif <mo@mosh-media.com>
2020-03-22 11:46:11 -0700

    Update index.js

--- src/containers/Landing/index.js
+++ src/containers/Landing/index.js
@@ -11,6 +11,7 @@ import Slide from "@material-ui/core/Slide";
 import Typography from "@material-ui/core/Typography";
 import withStyles from "@material-ui/core/styles/withStyles";
 import MoLinkButton from "../../components/shared/MoLinkButton";
+import MoBrowserMockup from "../../components/shared/MoBrowserMockup";
 
 const LandingPage = ({ classes }) => {
   return (
@@ -24,21 +25,60 @@ const LandingPage = ({ classes }) => {
             Experience a modern interactive approach to mastering React frontend
             development.
           </Typography>
-          <MoLinkButton text="Get started" href={ROUTES.SUPER_EASY_QUESTIONS.path} />
+          <MoLinkButton
+            text="Get started"
+            href={ROUTES.SUPER_EASY_QUESTIONS.path}
+          />
         </Grid>
 
         <Grid item sm={12} md={6} xs={12}>
-          <Slide
-            direction="up"
-            in={classes && true}
-            timeout={{ enter: 400, exit: 400 }}
+          <Grid
+            item
+            md={12}
+            sm={12}
+            xs={12}
+            style={{ width: "100%", marginBottom: "50px" }}
           >
-            <img
-              alt="Congrats"
-              className={classes.img}
-              src="https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/landing%2Fcongrats.png?alt=media&token=6e591903-a43c-413a-b861-52982750e530"
-            />
-          </Slide>
+            <Slide
+              direction="up"
+              in={true}
+              mountOnEnter
+              timeout={{ enter: 800, exit: 400 }}
+              unmountOnExit
+            >
+              <div>
+                <MoBrowserMockup fileType="jsx" isEditor={true}>
+                  <pre>
+                    () => &lt;h1&gt;Hello React
+                    <span aria-label="img" role="img">
+                      🤩
+                    </span>
+                    &lt;/h1&gt;
+                  </pre>
+                </MoBrowserMockup>
+              </div>
+            </Slide>
+          </Grid>
+          <Grid item md={12} sm={12} xs={12}>
+            <Slide
+              direction="left"
+              in={true}
+              mountOnEnter
+              timeout={{ enter: 800, exit: 800 }}
+              unmountOnExit
+            >
+              <div>
+                <MoBrowserMockup isBrowser={true}>
+                  <h1>
+                    Hello React{" "}
+                    <span aria-label="img" role="img">
+                      🤩
+                    </span>
+                  </h1>
+                </MoBrowserMockup>
+              </div>
+            </Slide>
+          </Grid>
         </Grid>
       </Grid>
       <Grid container spacing={4} className={classes.container}>
@@ -95,7 +135,7 @@ const LandingPage = ({ classes }) => {
             <img
               alt="Congrats"
               className={classes.img}
-              src="https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/landing%2Frocket-learn.png?alt=media&token=2a3a0cc9-21bd-4119-865f-f5bb920ce939"
+              src="https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/landing%2Fcongrats.png?alt=media&token=6e591903-a43c-413a-b861-52982750e530"
             />
           </Slide>
         </Grid>
