99c0b8c1e85d2f16f8c69ec24f3135722c3d09f4
Mo Sharif <mo@mosh-media.com>
2020-03-22 11:46:04 -0700

    Update index.js

--- src/containers/Questions/index.js
+++ src/containers/Questions/index.js
@@ -1,41 +1,44 @@
 import React, { useEffect, useState } from "react";
 
+import * as ROUTES from "../../constants/routes";
 import { AuthUserContext, withAuthentication } from "../../components/Session";
 import QuestionsPage from "./QuestionsPage";
 
-const Questions = ({ firebase, match }) => {
-	const [configs, setConfigs] = useState([]);
+const Questions = ({ firebase, match, history }) => {
+  const [configs, setConfigs] = useState([]);
 
-	useEffect(() => {
-		firebase
-			.moskool()
-			.doc(match.params.collection)
-			.get()
-			.then(doc => {
-				if (doc.exists) {
-					// Convert to configs object
-					let configs = doc.data();
-					// Use a configs instance method
-					setConfigs(configs);
-				} else {
-					console.log("No such document!");
-				}
-			})
-			.catch(error => {
-				console.log("Error getting document:", error);
-			});
-	}, [firebase, match]);
+  useEffect(() => {
+    firebase
+      .moskool()
+      .doc(match.params.collection)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          // Convert to configs object
+          let configs = doc.data();
+          // Use a configs instance method
+          setConfigs(configs);
+        } else {
+          history.push(ROUTES.NOT_FOUND.path);
+        }
+      })
+      .catch(error => {
+        console.log("Error getting document:", error);
+      });
 
-	return (
-		<AuthUserContext.Consumer>
-			{authUser => (
-				<QuestionsPage
-					authUser={authUser}
-					configs={configs}
-					firebase={firebase}
-				/>
-			)}
-		</AuthUserContext.Consumer>
-	);
+    return () => {};
+  }, [firebase, match, history]);
+
+  return (
+    <AuthUserContext.Consumer>
+      {authUser => (
+        <QuestionsPage
+          authUser={authUser}
+          configs={configs}
+          firebase={firebase}
+        />
+      )}
+    </AuthUserContext.Consumer>
+  );
 };
 export default withAuthentication(Questions);
