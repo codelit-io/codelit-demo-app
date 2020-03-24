04994ac31ba829ed7c8b0b22ed2eb76b72a7110f
Mo Sharif <mo@mosh-media.com>
2020-03-23 22:15:25 -0700

    Update index.js

--- src/containers/Questions/QuestionsPage/index.js
+++ src/containers/Questions/QuestionsPage/index.js
@@ -4,58 +4,57 @@ import Grid from "@material-ui/core/Grid";
 import QuestionsList from "./QuestionsList";
 import MoPage from "../../../components/shared/MoPage";
 
-
 const QuestionsPage = ({ authUser, configs, firebase }) => {
-	const [loading, setLoading] = useState(false);
-  	const [questions, setQuestions] = useState([]);
-  
-	useEffect(() => {
-		setLoading(true);
-		const getQuestions = firebase
-			.collection(configs.slug === "super-easy" ? "questions" : configs.slug)
-			.orderBy("id")
-			.onSnapshot(snapshot => {
-				if (snapshot.size) {
-					let questions = [];
-					snapshot.forEach(doc => {
-						questions.push({
-							...doc.data(),
-							uid: doc.id
-						});
-					});
-					setQuestions(questions);
-					setLoading(false);
-				} else {
-					setQuestions([]);
-					setLoading(false);
-				}
-			});
-		/* Unsubscribe from firebase on unmount */
-		return () => {
-			setQuestions([]);
-			getQuestions();
-		};
-	}, [firebase, configs]);
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
 
-	return (
-		<MoPage
-			title={configs.label}
-			points={authUser && authUser.points}
-			loading={loading}
-			numberOfQuestions={questions.length}
-			isScoreBoard={true}
-		>
-			{questions && (
-				<Grid container spacing={4}>
-					<QuestionsList
-						authUser={authUser}
-						questions={questions}
-						configs={configs}
-					/>
-				</Grid>
-			)}
-		</MoPage>
-	);
+  useEffect(() => {
+    setLoading(true);
+    const getQuestions = firebase
+      .collection(configs.slug === "super-easy" ? "questions" : configs.slug)
+      .orderBy("id")
+      .onSnapshot(snapshot => {
+        if (snapshot.size) {
+          let questions = [];
+          snapshot.forEach(doc => {
+            questions.push({
+              ...doc.data(),
+              uid: doc.id
+            });
+          });
+          setQuestions(questions);
+          setLoading(false);
+        } else {
+          setQuestions([]);
+          setLoading(false);
+        }
+      });
+    /* Unsubscribe from firebase on unmount */
+    return () => {
+      setQuestions([]);
+      getQuestions();
+    };
+  }, [firebase, configs]);
+
+  return (
+    <MoPage
+      title={configs.label}
+      points={authUser && authUser.points}
+      loading={loading}
+      numberOfQuestions={questions.length}
+      isScoreBoard={true}
+    >
+      {questions && (
+        <Grid container spacing={4}>
+          <QuestionsList
+            authUser={authUser}
+            questions={questions}
+            configs={configs}
+          />
+        </Grid>
+      )}
+    </MoPage>
+  );
 };
 
 export default QuestionsPage;
