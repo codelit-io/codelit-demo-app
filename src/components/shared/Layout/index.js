d9cf6e60a9f58fc254ad85b9f3a45b6a4d9a9bad
Mo Sharif <mo@mosh-media.com>
2020-03-24 12:21:26 -0700

    Revert "Delete index.js"
    This reverts commit 88f3345316549eafc77f9bdd8b7a7eec6c92bf3a.

---
+++ src/components/shared/Layout/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+
+const withLayout = Component => ({ rows, cols }) => {
+  return (
+    <Grid item sm={12} md={6}>
+      <Component />
+    </Grid>
+  );
+};
+
+export default withLayout;
