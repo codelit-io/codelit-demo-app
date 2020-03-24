66b97d934627c0fce0f7170cc4a259d66b90c826
Mo Sharif <mo@mosh-media.com>
2020-03-23 22:15:23 -0700

    Update index.js

--- src/containers/Questions/index.js
+++ src/containers/Questions/index.js
@@ -4,41 +4,41 @@ import { AuthUserContext, withAuthentication } from "../../components/Session";
 import QuestionsPage from "./QuestionsPage";
 
 const Questions = ({ firebase, match, history }) => {
-	const [configs, setConfigs] = useState({});
-	useEffect(() => {
-		firebase
-			.moskool()
-			.doc(match.params.collection)
-			.get()
-			.then(doc => {
-				if (doc.exists) {
-					setConfigs(doc.data());
-				} else {
-					setConfigs({});
-				}
-			})
-			.catch(error => {
-				console.log("Error getting document:", error);
-			});
+  const [configs, setConfigs] = useState({});
+  useEffect(() => {
+    firebase
+      .moskool()
+      .doc(match.params.collection)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          setConfigs(doc.data());
+        } else {
+          setConfigs({});
+        }
+      })
+      .catch(error => {
+        console.log("Error getting document:", error);
+      });
 
-		return () => {
-			setConfigs({});
-		};
-	}, [firebase, match, history]);
+    return () => {
+      setConfigs({});
+    };
+  }, [firebase, match, history]);
 
-	return (
-		<AuthUserContext.Consumer>
-			{authUser =>
-				configs &&
-				configs.slug && (
-					<QuestionsPage
-						authUser={authUser}
-						configs={configs}
-						firebase={firebase}
-					/>
-				)
-			}
-		</AuthUserContext.Consumer>
-	);
+  return (
+    <AuthUserContext.Consumer>
+      {authUser =>
+        configs &&
+        configs.slug && (
+          <QuestionsPage
+            authUser={authUser}
+            configs={configs}
+            firebase={firebase}
+          />
+        )
+      }
+    </AuthUserContext.Consumer>
+  );
 };
 export default withAuthentication(Questions);
