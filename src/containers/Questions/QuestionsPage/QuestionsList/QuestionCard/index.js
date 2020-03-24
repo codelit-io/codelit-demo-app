d4b2204b09d0300b518bee4875d4da281476c259
Mo Sharif <mo@mosh-media.com>
2020-03-23 21:07:06 -0700

    Create index.js

---
+++ src/containers/Questions/QuestionsPage/QuestionsList/QuestionCard/index.js
@@ -0,0 +1,12 @@
+import React, { lazy } from "react";
+
+const Grid = lazy(() => import("@material-ui/core/Grid"));
+const MoCard = lazy(() => import("../../../../../components/shared/MoCard"));
+
+const QuestionCard = ({ question, userPoints, url }) => (
+  <Grid item xs={12} sm={6} md={3}>
+    <MoCard userPoints={userPoints} item={question} url={url}></MoCard>
+  </Grid>
+);
+
+export default QuestionCard;
