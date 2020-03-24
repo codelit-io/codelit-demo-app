6bf1a199c8dfbb8d17770ea038ce584cfba22c14
Mo Sharif <mo@mosh-media.com>
2020-03-23 21:07:04 -0700

    Create index.js

---
+++ src/containers/Questions/QuestionsPage/QuestionsList/QuestionCategory/index.js
@@ -0,0 +1,17 @@
+import React, { lazy } from "react";
+
+const Grid = lazy(() => import("@material-ui/core/Grid"));
+const MoTitle = lazy(() => import("../../../../../components/shared/MoTitle"));
+
+const QuestionCategory = ({ category, index }) => (
+  <Grid item xs={12} sm={12} md={12}>
+    <MoTitle
+      text={category}
+      fade={true}
+      margin={index === 0 ? "36px 0 26px" : "40px 0 36px"}
+      width="100%"
+    ></MoTitle>
+  </Grid>
+);
+
+export default QuestionCategory;
