b9c8ffcea58b75215ce9f3584608f3d8efba21f3
Mo Sharif <mo@mosh-media.com>
2020-03-23 22:15:28 -0700

    Update index.js

--- src/containers/Questions/QuestionsPage/QuestionsList/QuestionCategory/index.js
+++ src/containers/Questions/QuestionsPage/QuestionsList/QuestionCategory/index.js
@@ -4,14 +4,14 @@ import MoTitle from "../../../../../components/shared/MoTitle";
 import Grid from "@material-ui/core/Grid";
 
 const QuestionCategory = ({ category, index }) => (
-	<Grid item xs={12} sm={12} md={12}>
-		<MoTitle
-			text={category}
-			fade={true}
-			margin={index === 0 ? "0px" : "40px 0 36px"}
-			width="100%"
-		></MoTitle>
-	</Grid>
+  <Grid item xs={12} sm={12} md={12}>
+    <MoTitle
+      text={category}
+      fade={true}
+      margin={index === 0 ? "0px" : "40px 0 36px"}
+      width="100%"
+    ></MoTitle>
+  </Grid>
 );
 
 export default QuestionCategory;
