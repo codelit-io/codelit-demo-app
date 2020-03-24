daebc8630eb5cca50d39ef30fb39e1c6076aa904
Mo Sharif <mo@mosh-media.com>
2020-03-23 21:07:02 -0700

    Update index.js

--- src/containers/Questions/index.js
+++ src/containers/Questions/index.js
@@ -6,20 +6,15 @@ import { AuthUserContext, withAuthentication } from "../../components/Session";
 const QuestionsPage = lazy(() => import("./QuestionsPage"));
 
 const Questions = ({ firebase, match, history }) => {
-	const [configs, setConfigs] = useState([]);
-
+	const [configs, setConfigs] = useState({});
 	useEffect(() => {
-    console.log('REndered')
 		firebase
 			.moskool()
 			.doc(match.params.collection)
 			.get()
 			.then(doc => {
 				if (doc.exists) {
-					// Convert to configs object
-					let configs = doc.data();
-					// Use a configs instance method
-					setConfigs(configs);
+					setConfigs(doc.data());
 				} else {
 					history.push(ROUTES.NOT_FOUND.path);
 				}
@@ -29,24 +24,23 @@ const Questions = ({ firebase, match, history }) => {
 			});
 
 		return () => {
-			setConfigs([]);
+			setConfigs({});
 		};
 	}, [firebase, match, history]);
 
 	return (
-		configs && (
-			<AuthUserContext.Consumer>
-				{authUser =>
-					configs && (
-						<QuestionsPage
-							authUser={authUser}
-							configs={configs}
-							firebase={firebase}
-						/>
-					)
-				}
-			</AuthUserContext.Consumer>
-		)
+		<AuthUserContext.Consumer>
+			{authUser =>
+				configs &&
+				configs.slug && (
+					<QuestionsPage
+						authUser={authUser}
+						configs={configs}
+						firebase={firebase}
+					/>
+				)
+			}
+		</AuthUserContext.Consumer>
 	);
 };
 export default withAuthentication(Questions);
