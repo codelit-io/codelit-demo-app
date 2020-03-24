dacaccc303fb2fe8fc76de290ec87df98b8415ff
Mo Sharif <mo@mosh-media.com>
2020-03-23 21:07:08 -0700

    Update index.js

--- src/containers/Questions/QuestionsPage/index.js
+++ src/containers/Questions/QuestionsPage/index.js
@@ -2,40 +2,38 @@ import React, { lazy, useEffect, useState } from "react";
 
 const Grid = lazy(() => import("@material-ui/core/Grid"));
 const MoPage = lazy(() => import("../../../components/shared/MoPage"));
-const QuestionsList = lazy(() => import("../QuestionsList"));
+const QuestionsList = lazy(() => import("./QuestionsList"));
 
 const QuestionsPage = ({ authUser, configs, firebase }) => {
 	const [loading, setLoading] = useState(false);
-	const [questions, setQuestions] = useState([]);
-
+  const [questions, setQuestions] = useState([]);
+  
 	useEffect(() => {
 		setLoading(true);
-		if (configs) {
-			const getQuestions = firebase
-				.questions()
-				.orderBy("id")
-				.onSnapshot(snapshot => {
-					if (snapshot.size) {
-						let questions = [];
-						snapshot.forEach(doc => {
-							questions.push({
-								...doc.data(),
-								uid: doc.id
-							});
+		const getQuestions = firebase
+			.collection(configs.slug === "super-easy" ? "questions" : configs.slug)
+			.orderBy("id")
+			.onSnapshot(snapshot => {
+				if (snapshot.size) {
+					let questions = [];
+					snapshot.forEach(doc => {
+						questions.push({
+							...doc.data(),
+							uid: doc.id
 						});
-						setQuestions(questions);
-						setLoading(false);
-					} else {
-						setQuestions([]);
-						setLoading(false);
-					}
-				});
-			/* Unsubscribe from firebase on unmount */
-			return () => {
-				setQuestions([]);
-				getQuestions();
-			};
-		}
+					});
+					setQuestions(questions);
+					setLoading(false);
+				} else {
+					setQuestions([]);
+					setLoading(false);
+				}
+			});
+		/* Unsubscribe from firebase on unmount */
+		return () => {
+			setQuestions([]);
+			getQuestions();
+		};
 	}, [firebase, configs]);
 
 	return (
