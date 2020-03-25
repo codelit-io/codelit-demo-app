2bc80133457b3f16c7c40fd381e3db151564ff0b
Mo Sharif <mo@mosh-media.com>
2020-03-25 00:00:33 -0700

    Create index.js

---
+++ src/components/Footer/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import styles from "./styles";
+import withStyles from "@material-ui/core/styles/withStyles";
+import Fade from "@material-ui/core/Fade";
+import Grid from "@material-ui/core/Grid";
+
+const Footer = ({ classes, fade }) => (
+	<Fade
+		in={fade && true}
+		timeout={{ enter: 1200, exit: 1200 }}
+		mountOnEnter
+		unmountOnExit
+	>
+		<Grid container spacing={4}>
+			<Grid item md={6}>
+				test
+			</Grid>
+			<Grid item md={6}>
+				test
+			</Grid>
+		</Grid>
+	</Fade>
+);
+
+export default withStyles(styles)(Footer);
