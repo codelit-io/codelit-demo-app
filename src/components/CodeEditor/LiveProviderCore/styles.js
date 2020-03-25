82db68783dbaaed56af6c3598c0d21428d60c8be
Mo Sharif <mo@mosh-media.com>
2020-03-25 00:00:52 -0700

    Create styles.js

---
+++ src/components/CodeEditor/LiveProviderCore/styles.js
@@ -0,0 +1,17 @@
+const styles = theme => ({
+  hint: {
+    ...theme.editorFont,
+    color: "#8e8e8e",
+    top: "20px",
+    position: "absolute",
+    zIndex: "-1",
+    padding: "10px"
+  },
+  brush: {
+    position: "absolute",
+    top: "1px",
+    font: "30px"
+  }
+});
+
+export default styles;
