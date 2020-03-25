c0d002f37e8b770245012e493ce675d57d9dc349
Mo Sharif <mo@mosh-media.com>
2020-03-25 00:02:49 -0700

    Update index.js

--- src/components/shared/MoPage/index.js
+++ src/components/shared/MoPage/index.js
@@ -11,33 +11,33 @@ import { Grid } from "@material-ui/core";
 import MoScoreBoard from "../MoScoreBoard";
 
 const MoPage = ({
-	classes,
-	children,
-	img,
-	loading,
-	isCard,
-	isScoreBoard,
-	subtitle,
-	points,
-	numberOfQuestions,
-	title
+  classes,
+  children,
+  img,
+  loading,
+  isCard,
+  isScoreBoard,
+  subtitle,
+  points,
+  numberOfQuestions,
+  title
 }) => {
-	return (
-		<Grid container className={classes.section}>
-			<Grid item md={6}>
-				<MoPageHeader img="" title={title} />
-			</Grid>
-			<Grid item md={6} className={classes.scoreBoard}>
-				{isScoreBoard ? (
-					<MoScoreBoard points={points} numberOfQuestions={numberOfQuestions} />
-				) : (
-					<MoPageSubtitle subtitle={subtitle} />
-				)}
-			</Grid>
-			<Spinner loading={loading} color="primary" />
-			<section className={classes.content}>{children}</section>
-		</Grid>
-	);
+  return (
+    <Grid container className={classes.section}>
+      <Grid item md={6}>
+        <MoPageHeader img="" title={title} />
+      </Grid>
+      <Grid item md={6} className={classes.scoreBoard}>
+        {isScoreBoard ? (
+          <MoScoreBoard points={points} numberOfQuestions={numberOfQuestions} />
+        ) : (
+          <MoPageSubtitle subtitle={subtitle} />
+        )}
+      </Grid>
+      <Spinner loading={loading} color="primary" />
+      <section className={classes.content}>{children}</section>
+    </Grid>
+  );
 };
 
 export default compose(withStyles(styles), withRouter)(MoPage);
