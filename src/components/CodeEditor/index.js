243ae10c2e9753ba66d1f8a29541e8ca91798b89
Mo Sharif <mo@mosh-media.com>
2020-03-24 19:22:57 -0700

    Update index.js

--- src/components/CodeEditor/index.js
+++ src/components/CodeEditor/index.js
@@ -1,32 +1,35 @@
-import React, { lazy, useState, useEffect } from "react";
+import React, { lazy, useState, useEffect, Suspense } from "react";
 
-const Grid = lazy(() => import("@material-ui/core/Grid"));
+import Grid from "@material-ui/core/Grid";
+
 const LiveProviderCore = lazy(() => import("./LiveProviderCore"));
 
 const CodeEditor = ({ handleOnChange, md, sm, question }) => {
-  const [state, setState] = useState(question);
+	const [state, setState] = useState(question);
 
-  useEffect(() => {
-    if (question.question) {
-      try {
-        const prettyQuestion = JSON.parse(question.question);
-        setState({ ...question, question: prettyQuestion });
-      } catch {
-        setState(question);
-      }
-    }
-  }, [question]);
+	useEffect(() => {
+		if (question.question) {
+			try {
+				const prettyQuestion = JSON.parse(question.question);
+				setState({ ...question, question: prettyQuestion });
+			} catch {
+				setState(question);
+			}
+		}
+	}, [question]);
 
-  return (
-    <Grid container spacing={4}>
-      <LiveProviderCore
-        question={state}
-        handleOnChange={handleOnChange}
-        md={md}
-        sm={sm}
-      />
-    </Grid>
-  );
+	return (
+		<Grid container spacing={4}>
+			<Suspense>
+				<LiveProviderCore
+					question={state}
+					handleOnChange={handleOnChange}
+					md={md}
+					sm={sm}
+				/>
+			</Suspense>
+		</Grid>
+	);
 };
 
 export default CodeEditor;
