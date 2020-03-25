bbdbef213c006c6013c61d4df8290cb1c986c135
Mo Sharif <mo@mosh-media.com>
2020-03-25 00:02:44 -0700

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,21 +6,21 @@ import Fade from "@material-ui/core/Fade";
 import Grid from "@material-ui/core/Grid";
 
 const Footer = ({ classes, fade }) => (
-	<Fade
-		in={fade && true}
-		timeout={{ enter: 1200, exit: 1200 }}
-		mountOnEnter
-		unmountOnExit
-	>
-		<Grid container spacing={4}>
-			<Grid item md={6}>
-				test
-			</Grid>
-			<Grid item md={6}>
-				test
-			</Grid>
-		</Grid>
-	</Fade>
+  <Fade
+    in={fade && true}
+    timeout={{ enter: 1200, exit: 1200 }}
+    mountOnEnter
+    unmountOnExit
+  >
+    <Grid container spacing={4}>
+      <Grid item md={6}>
+        test
+      </Grid>
+      <Grid item md={6}>
+        test
+      </Grid>
+    </Grid>
+  </Fade>
 );
 
 export default withStyles(styles)(Footer);
