3afea678d8c99cd3770ff51f63ebcc9df38fc3ae
Mo Sharif <mo@mosh-media.com>
2020-03-28 00:04:07 -0700

    Refactor point calculations

--- src/components/shared/MoScoreBoard/index.js
+++ src/components/shared/MoScoreBoard/index.js
@@ -4,27 +4,27 @@ import Button from "@material-ui/core/Button";
 import Fade from "@material-ui/core/Fade";
 
 const MoScoreBoard = ({ numberOfQuestions, points }) => (
-  <Fade
-    direction="left"
-    in={points && true}
-    mountOnEnter
-    timeout={{ enter: 800, exit: 400 }}
-    unmountOnExit
-  >
-    <ButtonGroup
-      aria-label="Points and progress"
-      variant="text"
-      color="default"
-      style={{ marginBottom: "48px" }}
-    >
-      <Button> {points ? `${points} Points` : "0 Points"}</Button>
-      <Button>
-        {numberOfQuestions
-          ? Math.round((points / numberOfQuestions) * 100) + "% Complete"
-          : "..."}
-      </Button>
-    </ButtonGroup>
-  </Fade>
+	<Fade
+		direction="left"
+		in={points ? true : false}
+		mountOnEnter
+		timeout={{ enter: 800, exit: 400 }}
+		unmountOnExit
+	>
+		<ButtonGroup
+			aria-label="Points and progress"
+			variant="text"
+			color="default"
+			style={{ marginBottom: "48px" }}
+		>
+			<Button> {points ? `${points} Points` : "0 Points"}</Button>
+			<Button>
+				{numberOfQuestions
+					? Math.round((points / numberOfQuestions) * 100) + "% Complete"
+					: "..."}
+			</Button>
+		</ButtonGroup>
+	</Fade>
 );
 
 export default MoScoreBoard;
