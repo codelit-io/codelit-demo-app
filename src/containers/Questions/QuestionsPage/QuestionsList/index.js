e3a2f8ded57c41c348c3c3065f68d1cc1802d0b5
Mo Sharif <mo@mosh-media.com>
2020-03-28 00:03:00 -0700

    Change user points

--- src/containers/Questions/QuestionsPage/QuestionsList/index.js
+++ src/containers/Questions/QuestionsPage/QuestionsList/index.js
@@ -3,14 +3,14 @@ import React from "react";
 import QuestionCategory from "./QuestionCategory";
 import QuestionCard from "./QuestionCard";
 
-const QuestionsList = ({ authUser, questions, configs }) =>
+const QuestionsList = ({ configs, questions, points }) =>
   questions.map((question, index) => (
     <React.Fragment key={index}>
       {question.category && (
         <QuestionCategory category={question.category} index={index} />
       )}
       <QuestionCard
-        userPoints={authUser && authUser.points}
+        points={points}
         question={question}
         url={configs.collection}
       />
