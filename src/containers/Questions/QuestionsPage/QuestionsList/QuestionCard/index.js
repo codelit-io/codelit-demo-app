f8786ed9e0f335a82cc3a00fedd515db42301f48
Mo Sharif <mo@mosh-media.com>
2020-03-28 00:02:34 -0700

    Change ve

--- src/containers/Questions/QuestionsPage/QuestionsList/QuestionCard/index.js
+++ src/containers/Questions/QuestionsPage/QuestionsList/QuestionCard/index.js
@@ -3,9 +3,9 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import MoCard from "../../../../../components/shared/MoCard";
 
-const QuestionCard = ({ question, userPoints, url }) => (
+const QuestionCard = ({ question, points, url }) => (
   <Grid item xs={12} sm={6} md={3}>
-    <MoCard userPoints={userPoints} item={question} url={url}></MoCard>
+    <MoCard points={points} item={question} url={url}></MoCard>
   </Grid>
 );
 
