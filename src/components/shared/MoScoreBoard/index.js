5f48b74ac33cf08f8b6da40b5a62943970d212e2
Mo Sharif <mo@mosh-media.com>
2020-03-31 15:26:49 -0700

    Add sign up link for non logged in users

--- src/components/shared/MoScoreBoard/index.js
+++ src/components/shared/MoScoreBoard/index.js
@@ -2,39 +2,49 @@ import React from "react";
 
 import * as ROLES from "../../../constants/roles";
 import * as ROUTES from "../../../constants/routes";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Button from "@material-ui/core/Button";
-import PostAddIcon from "@material-ui/icons/PostAdd";
 import Fade from "@material-ui/core/Fade";
 import { Link } from "react-router-dom";
+import PostAddIcon from "@material-ui/icons/PostAdd";
 
 const MoScoreBoard = ({ authUser, numberOfQuestions, points }) => (
-  <Fade
-    direction="left"
-    in={authUser && true}
-    mountOnEnter
-    timeout={{ enter: 800, exit: 400 }}
-    unmountOnExit
-  >
-    <ButtonGroup
-      aria-label="Points and progress"
-      variant="text"
-      color="default"
-      style={{ marginBottom: "48px" }}
-    >
-      {authUser.roles && !!authUser.roles[ROLES.AUTHOR] && (
-        <Button to={ROUTES.ADMIN.path} component={Link}>
-          Add <PostAddIcon />
-        </Button>
-      )}
-      <Button> {points ? `${points} Points` : "0 Points"}</Button>
-      <Button>
-        {numberOfQuestions
-          ? Math.round((points / numberOfQuestions) * 100) + "% Complete"
-          : "..."}
-      </Button>
-    </ButtonGroup>
-  </Fade>
+	<Fade
+		direction="left"
+		in={true}
+		mountOnEnter
+		timeout={{ enter: 800, exit: 400 }}
+		unmountOnExit
+	>
+		<ButtonGroup
+			aria-label="Points and progress"
+			variant="text"
+			color="default"
+			style={{ marginBottom: "48px" }}
+		>
+			{authUser?.roles && !!authUser?.roles[ROLES.AUTHOR] && (
+				<Button to={ROUTES.ADMIN.path} component={Link}>
+					Add <PostAddIcon />
+				</Button>
+			)}
+			{authUser ? (
+				<>
+					<Button> {points ? `${points} Points` : "0 Points"}</Button>
+					<Button>
+						{numberOfQuestions && points
+							? Math.round((points / numberOfQuestions) * 100) + "% Complete"
+							: "0% Complete"}
+					</Button>
+				</>
+			) : (
+				<Button to={ROUTES.SIGN_UP.path} component={Link}>
+					Sign up to earn points{" "}
+					<CheckCircleIcon style={{ marginLeft: "10px" }} />
+				</Button>
+			)}
+		</ButtonGroup>
+	</Fade>
 );
 
 export default MoScoreBoard;
