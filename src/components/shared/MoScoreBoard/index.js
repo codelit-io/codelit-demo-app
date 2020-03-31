ec19043fcbf3532c8b0292ab55c303ed2094b355
Mo Sharif <mo@mosh-media.com>
2020-03-31 15:48:34 -0700

    Update MoScoreBoard component

--- src/components/shared/MoScoreBoard/index.js
+++ src/components/shared/MoScoreBoard/index.js
@@ -10,41 +10,41 @@ import { Link } from "react-router-dom";
 import PostAddIcon from "@material-ui/icons/PostAdd";
 
 const MoScoreBoard = ({ authUser, numberOfQuestions, points }) => (
-	<Fade
-		direction="left"
-		in={true}
-		mountOnEnter
-		timeout={{ enter: 800, exit: 400 }}
-		unmountOnExit
-	>
-		<ButtonGroup
-			aria-label="Points and progress"
-			variant="text"
-			color="default"
-			style={{ marginBottom: "48px" }}
-		>
-			{authUser?.roles && !!authUser?.roles[ROLES.AUTHOR] && (
-				<Button to={ROUTES.ADMIN.path} component={Link}>
-					Add <PostAddIcon />
-				</Button>
-			)}
-			{authUser ? (
-				<>
-					<Button> {points ? `${points} Points` : "0 Points"}</Button>
-					<Button>
-						{numberOfQuestions && points
-							? Math.round((points / numberOfQuestions) * 100) + "% Complete"
-							: "0% Complete"}
-					</Button>
-				</>
-			) : (
-				<Button to={ROUTES.SIGN_UP.path} component={Link}>
-					Sign up to earn points{" "}
-					<CheckCircleIcon style={{ marginLeft: "10px" }} />
-				</Button>
-			)}
-		</ButtonGroup>
-	</Fade>
+  <Fade
+    direction="left"
+    in={true}
+    mountOnEnter
+    timeout={{ enter: 800, exit: 400 }}
+    unmountOnExit
+  >
+    <ButtonGroup
+      aria-label="Points and progress"
+      variant="text"
+      color="default"
+      style={{ marginBottom: "48px" }}
+    >
+      {authUser?.roles && !!authUser?.roles[ROLES.AUTHOR] && (
+        <Button to={ROUTES.ADMIN.path} component={Link}>
+          Add <PostAddIcon />
+        </Button>
+      )}
+      {authUser ? (
+        <>
+          <Button> {points ? `${points} Points` : "0 Points"}</Button>
+          <Button>
+            {numberOfQuestions && points
+              ? Math.round((points / numberOfQuestions) * 100) + "% Complete"
+              : "0% Complete"}
+          </Button>
+        </>
+      ) : (
+        <Button to={ROUTES.SIGN_UP.path} component={Link}>
+          Sign up to earn points{" "}
+          <CheckCircleIcon style={{ marginLeft: "10px" }} />
+        </Button>
+      )}
+    </ButtonGroup>
+  </Fade>
 );
 
 export default MoScoreBoard;
