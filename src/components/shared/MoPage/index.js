eaf86518d40ef90bcf8958d41dc249856675c4fa
Mo Sharif <mo@mosh-media.com>
2020-03-30 21:25:00 -0700

    Pretty code

--- src/components/shared/MoPage/index.js
+++ src/components/shared/MoPage/index.js
@@ -9,27 +9,20 @@ import Spinner from "../Spinner";
 import MoPageSubtitle from "../MoPageSubtitle";
 import { Grid } from "@material-ui/core";
 
-const MoPage = ({
-	classes,
-	children,
-	loading,
-	subtitle,
-	title,
-	Component
-}) => {
-	return (
-		<Grid container className={classes.section}>
-			<Grid item md={6}>
-				<MoPageHeader title={title} />
-			</Grid>
-			<Grid item md={6} className={classes.component}>
-				{<Component />}
-				{subtitle && <MoPageSubtitle subtitle={subtitle} />}
-			</Grid>
-			<Spinner loading={loading} color="primary" />
-			<section className={classes.content}>{children}</section>
-		</Grid>
-	);
+const MoPage = ({ classes, children, loading, subtitle, title, Component }) => {
+  return (
+    <Grid container className={classes.section}>
+      <Grid item md={6}>
+        <MoPageHeader title={title} />
+      </Grid>
+      <Grid item md={6} className={classes.component}>
+        {Component && <Component />}
+        {subtitle && <MoPageSubtitle subtitle={subtitle} />}
+      </Grid>
+      <Spinner loading={loading} color="primary" />
+      <section className={classes.content}>{children}</section>
+    </Grid>
+  );
 };
 
 export default compose(withStyles(styles), withRouter)(MoPage);
