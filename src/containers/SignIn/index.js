027696ffeffff0f2d4183682492ce3b69eda2879
Mo Sharif <mo@mosh-media.com>
2020-03-31 21:38:52 -0700

    Update header text in MoPage in SignIn page

--- src/containers/SignIn/index.js
+++ src/containers/SignIn/index.js
@@ -6,7 +6,7 @@ import SocialSignIn from "../../components/SocialSignIn/";
 import SignInForm from "./SignInForm";
 
 const SignInPage = () => (
-  <MoPage img="" title="Welcome Back!" loading={false}>
+  <MoPage img="" title="Welcome Back 🤩" loading={false}>
     <Grid container spacing={3}>
       <SignInForm />
       <SocialSignIn />
