ed9055407c01ea22a54be64d5cbcbb834364b5c3
Mo Sharif <mo@mosh-media.com>
2020-04-01 18:33:19 -0700

    Display signed up message if user is logged in

--- src/containers/SignIn/index.js
+++ src/containers/SignIn/index.js
@@ -1,17 +1,32 @@
 import React from "react";
 
+import { AuthUserContext } from "../../components/Session";
 import Grid from "@material-ui/core/Grid";
 import MoPage from "../../components/shared/MoPage";
 import SocialSignIn from "../../components/SocialSignIn/";
 import SignInForm from "./SignInForm";
 
 const SignInPage = () => (
-  <MoPage img="" title="Welcome Back 🤩" loading={false}>
-    <Grid container spacing={3}>
-      <SignInForm />
-      <SocialSignIn />
-    </Grid>
-  </MoPage>
+	<AuthUserContext.Consumer>
+		{authUser => (
+			<MoPage
+				img=""
+				title={authUser ? "You are logged in 👍" : "Welcome Back 🤩"}
+				loading={false}
+			>
+				<Grid container spacing={3}>
+					{authUser ? (
+						""
+					) : (
+						<>
+							<SignInForm />
+							<SocialSignIn />
+						</>
+					)}
+				</Grid>
+			</MoPage>
+		)}
+	</AuthUserContext.Consumer>
 );
 
 export default SignInPage;
