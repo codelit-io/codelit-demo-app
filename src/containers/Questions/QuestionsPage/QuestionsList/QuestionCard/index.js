0b7f3b5433eee35c39ddd08521c135ab1e3d8275
Mo Sharif <mo@mosh-media.com>
2020-04-02 19:50:37 -0700

    Update props passed to MoCard

--- src/containers/Questions/QuestionsPage/QuestionsList/QuestionCard/index.js
+++ src/containers/Questions/QuestionsPage/QuestionsList/QuestionCard/index.js
@@ -3,9 +3,14 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import MoCard from "../../../../../components/shared/MoCard";
 
-const QuestionCard = ({ question, points, url }) => (
+const QuestionCard = ({ isDisabled, question, points, url }) => (
   <Grid item xs={12} sm={6} md={3}>
-    <MoCard points={points} item={question} url={url}></MoCard>
+    <MoCard
+      isDisabled={isDisabled}
+      points={points}
+      item={question}
+      url={url}
+    ></MoCard>
   </Grid>
 );
 
