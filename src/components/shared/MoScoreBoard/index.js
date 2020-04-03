a233a5cb6c06a2c2fd30a4525bc7f6e24299f1f3
Mo Sharif <mo@mosh-media.com>
2020-04-02 19:28:47 -0700

    Refactor MoScoreBoard to remove progress logic

--- src/components/shared/MoScoreBoard/index.js
+++ src/components/shared/MoScoreBoard/index.js
@@ -10,34 +10,34 @@ import { Link } from "react-router-dom";
 import PostAddIcon from "@material-ui/icons/PostAdd";
 
 const MoScoreBoard = ({ authUser, points, progress }) => (
-	<Fade
-		direction="left"
-		in={true}
-		mountOnEnter
-		timeout={{ enter: 800, exit: 400 }}
-		unmountOnExit
-	>
-		<ButtonGroup
-			aria-label="Points and progress"
-			variant="text"
-			color="default"
-			style={{ marginBottom: "48px" }}
-		>
-			{authUser?.roles && !!authUser?.roles[ROLES.AUTHOR] && (
-				<Button to={ROUTES.ADMIN.path} component={Link}>
-					Add <PostAddIcon />
-				</Button>
-			)}
-			{authUser && <Button> {points ? `${points} Points` : "0 Points"}</Button>}
-			<Button>{progress}</Button>
-			{!authUser && (
-				<Button to={ROUTES.SIGN_UP.path} component={Link}>
-					Sign up to earn points
-					<CheckCircleIcon style={{ marginLeft: "10px" }} />
-				</Button>
-			)}
-		</ButtonGroup>
-	</Fade>
+  <Fade
+    direction="left"
+    in={true}
+    mountOnEnter
+    timeout={{ enter: 800, exit: 400 }}
+    unmountOnExit
+  >
+    <ButtonGroup
+      aria-label="Points and progress"
+      variant="text"
+      color="default"
+      style={{ marginBottom: "48px" }}
+    >
+      {authUser?.roles && !!authUser?.roles[ROLES.AUTHOR] && (
+        <Button to={ROUTES.ADMIN.path} component={Link}>
+          Add <PostAddIcon />
+        </Button>
+      )}
+      {authUser && <Button> {points ? `${points} Points` : "0 Points"}</Button>}
+      <Button>{progress}</Button>
+      {!authUser && (
+        <Button to={ROUTES.SIGN_UP.path} component={Link}>
+          Sign up to earn points
+          <CheckCircleIcon style={{ marginLeft: "10px" }} />
+        </Button>
+      )}
+    </ButtonGroup>
+  </Fade>
 );
 
 export default MoScoreBoard;
