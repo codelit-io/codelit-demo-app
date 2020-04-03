59b0821314e53a21fca7a05c64c40dbc6cb3c970
Mo Sharif <mo@mosh-media.com>
2020-04-02 18:14:41 -0700

    Update footer with author data

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,23 +1,31 @@
 import React from "react";
 
-import styles from "./styles";
-import withStyles from "@material-ui/core/styles/withStyles";
+import Button from "@material-ui/core/Button";
 import Fade from "@material-ui/core/Fade";
 import Grid from "@material-ui/core/Grid";
+import styles from "./styles";
+import withStyles from "@material-ui/core/styles/withStyles";
 
-const Footer = ({ classes, fade }) => (
+const Footer = ({ classes }) => (
   <Fade
-    in={fade && true}
-    timeout={{ enter: 1200, exit: 1200 }}
+    in={true}
+    timeout={{ enter: 2200, exit: 1200 }}
     mountOnEnter
     unmountOnExit
   >
-    <Grid container spacing={4}>
-      <Grid item md={6}>
-        test
-      </Grid>
-      <Grid item md={6}>
-        test
+    <Grid container spacing={4} className={classes.container}>
+      <Grid item md={12} className={classes.footerText}>
+        <Button
+          href="https://github.com/mo-sharif"
+          aria-label="Mo Sharif Github"
+          className={classes.footerButton}
+        >
+          Developed with{" "}
+          <span role="img" aria-label="heart emoji">
+            &nbsp;💙
+          </span>{" "}
+          by Mo Sharif
+        </Button>
       </Grid>
     </Grid>
   </Fade>
