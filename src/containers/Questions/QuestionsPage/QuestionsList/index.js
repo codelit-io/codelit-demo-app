8ecb87b8381becf9f2258b6234e4c2ca138b1304
Mo Sharif <mo@mosh-media.com>
2020-04-02 19:50:59 -0700

    Add card disabled logic to Questions List

--- src/containers/Questions/QuestionsPage/QuestionsList/index.js
+++ src/containers/Questions/QuestionsPage/QuestionsList/index.js
@@ -4,17 +4,24 @@ import QuestionCategory from "./QuestionCategory";
 import QuestionCard from "./QuestionCard";
 
 const QuestionsList = ({ configs, questions, points }) =>
-  questions.map((question, index) => (
-    <React.Fragment key={index}>
-      {question.category && (
-        <QuestionCategory category={question.category} index={index} />
-      )}
-      <QuestionCard
-        points={points}
-        question={question}
-        url={configs.collection}
-      />
-    </React.Fragment>
-  ));
+  questions.map((question, index) => {
+    const isDisabled = points
+      ? points < Number(question.id) - 1 && Number(question.id) !== 1
+      : Number(question.id) !== 1;
+
+    return (
+      <React.Fragment key={index}>
+        {question.category && (
+          <QuestionCategory category={question.category} index={index} />
+        )}
+        <QuestionCard
+          isDisabled={isDisabled}
+          points={points}
+          question={question}
+          url={configs.collection}
+        />
+      </React.Fragment>
+    );
+  });
 
 export default QuestionsList;
