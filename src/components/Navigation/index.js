133c057becf360276161163da0c22daa7333db26
Mo Sharif <mo@mosh-media.com>
2020-04-02 18:15:56 -0700

    Abstract Mo Skool Logo to it's own component

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,16 +1,13 @@
 import React from "react";
 
-import * as ROUTES from "../../constants/routes";
 import { AuthUserContext } from "../Session";
 import AppBar from "@material-ui/core/AppBar";
-import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
-import { Link } from "react-router-dom";
 import MoAvatar from "./MoAvatar";
 import MoMenu from "./MoMenu";
+import MoSkoolLogo from "../MoSkoolLogo";
 import styles from "./styles";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import withStyles from "@material-ui/core/styles/withStyles";
 
 const Navigation = ({ classes }) => {
@@ -20,16 +17,7 @@ const Navigation = ({ classes }) => {
         <Toolbar className={classes.toolbar}>
           <Grid container className={classes.alignCenter}>
             <Grid item xs={4} sm={4} md={4} lg={4}>
-              <Typography className={classes.title} variant="h6" noWrap>
-                <Link
-                  to={ROUTES.LANDING.path}
-                  style={{ color: "#383c40", textDecoration: "none" }}
-                >
-                  <Box fontWeight="fontWeightLight" className={classes.MoSkool}>
-                    Mo Skool
-                  </Box>
-                </Link>
-              </Typography>
+              <MoSkoolLogo />
             </Grid>
             <Grid item xs={4} sm={4} md={4} lg={4}></Grid>
             <Grid
