701fca95808dbf0e17c1356af03a51eec4f2e41f
Mo Sharif <mo@mosh-media.com>
2020-04-05 22:40:48 -0700

    Remove firebase query

--- src/containers/Questions/QuestionsPage/QuestionsList/index.js
+++ src/containers/Questions/QuestionsPage/QuestionsList/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import QuestionCategory from "./QuestionCategory";
 import QuestionCard from "./QuestionCard";
 
-const QuestionsList = ({ configs, questions, points }) =>
+const QuestionsList = ({ url, questions, points }) =>
   questions.map((question, index) => {
     const isDisabled = points
       ? points < Number(question.id) - 1 && Number(question.id) !== 1
@@ -18,7 +18,7 @@ const QuestionsList = ({ configs, questions, points }) =>
           isDisabled={isDisabled}
           points={points}
           question={question}
-          url={configs.collection}
+          url={url}
         />
       </React.Fragment>
     );
