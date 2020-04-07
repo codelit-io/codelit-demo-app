0d2ac650d61cbf11956b18375fc98f61ae6ba219
Mo Sharif <mo@mosh-media.com>
2020-04-06 22:35:28 -0700

    Update fade in animation speed

--- src/components/shared/MoPageSubtitle/index.js
+++ src/components/shared/MoPageSubtitle/index.js
@@ -4,7 +4,7 @@ import Typography from "@material-ui/core/Typography";
 import Fade from "@material-ui/core/Fade";
 
 const MoPageSubtitle = ({ subtitle }) => (
-  <Fade timeout={{ enter: 2000 }} in={true}>
+  <Fade timeout={{ enter: 800 }} in={true}>
     <Typography
       variant="body1"
       style={{ paddingLeft: "4px", color: "#484848" }}
