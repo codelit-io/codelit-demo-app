97776ce88090546d87aee02c8e54610dac672d7d
Mo Sharif <mo@mosh-media.com>
2020-04-06 22:33:41 -0700

    Update index.js

--- src/containers/Questions/QuestionsPage/QuestionsList/index.js
+++ src/containers/Questions/QuestionsPage/QuestionsList/index.js
@@ -4,24 +4,24 @@ import QuestionCategory from "./QuestionCategory";
 import QuestionCard from "./QuestionCard";
 
 const QuestionsList = ({ url, questions, points }) =>
-  questions.map((question, index) => {
-    const isDisabled = points
-      ? points < Number(question.id) - 1 && Number(question.id) !== 1
-      : Number(question.id) !== 1;
+	questions.map((question, index) => {
+		const isDisabled = points
+			? points < Number(question.id) - 1 && Number(question.id) !== 1
+			: Number(question.id) !== 1;
 
-    return (
-      <React.Fragment key={index}>
-        {question.category && (
-          <QuestionCategory category={question.category} index={index} />
-        )}
-        <QuestionCard
-          isDisabled={isDisabled}
-          points={points}
-          question={question}
-          url={url}
-        />
-      </React.Fragment>
-    );
-  });
+		return (
+			<React.Fragment key={index}>
+				{question.category && (
+					<QuestionCategory category={question.category} index={index} />
+				)}
+				<QuestionCard
+					isDisabled={isDisabled}
+					points={points}
+					question={question}
+					url={url}
+				/>
+			</React.Fragment>
+		);
+	});
 
 export default QuestionsList;
