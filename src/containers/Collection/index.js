684f1c9835165e7db89de616b023661aac80b3ad
Mo Sharif <mo@mosh-media.com>
2020-04-07 20:22:48 -0700

    Update index.js

--- src/containers/Collection/index.js
+++ src/containers/Collection/index.js
@@ -1,26 +1,28 @@
 /**
- * Collections is a container that fetches firebase data using hooks and renders cards of questions 
+ * Collections is a container that fetches firebase data using hooks and renders cards of questions
  * @prop {Object} firebase - Firebase class provides access to authUser and db - comes from withAuthentication hoc
  * @prop {Object} match - Contains information about how a <Route path> matched the URL - comes from withRouter and passed to withAuthentication hoc
- * @returns {<QuestionsPage/>} - returns QuestionsPage component which renders the rest of the components 
+ * @returns {<QuestionsPage/>} - returns QuestionsPage component which renders the rest of the components
  * @withAuthentication - HOC provides firebase and match props
  */
-
 
 import React from "react";
 
 import { AuthUserContext, withAuthentication } from "../../components/Session";
 import QuestionsPage from "./QuestionsPage";
-import useQuestions from "./useQuestions";
 import useTopicDetails from "./useTopicDetails";
+import useCollections from "../../Hooks/useCollections";
 
 const Collection = ({ firebase, match }) => {
-	const questions = useQuestions(firebase, match);
-  const topicDetails = useTopicDetails(firebase, match);
-  
+	const collectionQuery = firebase
+		.collection("collections/" + match.params.collection + "/questions")
+		.orderBy("id");
+	const questions = useCollections(collectionQuery);
+	const topicDetails = useTopicDetails(firebase, match);
+
 	return (
 		<AuthUserContext.Consumer>
-			{authUser => (
+			{(authUser) => (
 				<QuestionsPage
 					authUser={authUser}
 					isLoading={topicDetails.isLoading || questions.isLoading}
