36dbb41d3733b3c00f359a3b73f6abd1cca0929c
Mo Sharif <mo@mosh-media.com>
2020-04-07 20:21:36 -0700

    Create Collections page

---
+++ src/containers/Collections/index.js
@@ -0,0 +1,33 @@
+/**
+ * Collections is a container that fetches firebase data using hooks and renders a list of all collections
+ * @prop {Object} firebase - Firebase class provides access to authUser and db - comes from withAuthentication hoc
+ * @prop {Object} match - Contains information about how a <Route path> matched the URL - comes from withRouter and passed to withAuthentication hoc
+ * @returns {<QuestionsPage/>} - returns QuestionsPage component which renders the rest of the components
+ * @withAuthentication - HOC provides firebase and match props
+ */
+
+import React from "react";
+
+import { AuthUserContext, withAuthentication } from "../../components/Session";
+import QuestionsPage from "../Collection/QuestionsPage";
+import useCollections from "../../Hooks/useCollections";
+
+const Collections = ({ firebase, match }) => {
+	const collectionQuery = firebase.collection("collections");
+	const collections = useCollections(collectionQuery);
+
+	return (
+		<AuthUserContext.Consumer>
+			{(authUser) => (
+				<QuestionsPage
+					authUser={authUser}
+					isLoading={collections.isLoading}
+					match={match}
+					questions={collections.data}
+					topicDetails={{ label: "Collections" }}
+				/>
+			)}
+		</AuthUserContext.Consumer>
+	);
+};
+export default withAuthentication(Collections);
