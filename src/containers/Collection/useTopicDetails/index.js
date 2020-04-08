8c12640ce673f488e6076fe1e5d4d37aeca62f64
Mo Sharif <mo@mosh-media.com>
2020-04-07 20:21:46 -0700

    Small refactor

--- src/containers/Collection/useTopicDetails/index.js
+++ src/containers/Collection/useTopicDetails/index.js
@@ -1,5 +1,5 @@
 /**
- * Custom Hook to get more details about a topic or collection of questions 
+ * Custom Hook to get more details about a topic or collection of questions
  * @param {Object} firebase - Firebase class provides access to authUser and db
  * @param {Object} match - Contains information about how a <Route path> matched the URL
  * @returns {isLoading: boolean, isError: Object, topicDetails: Object} - returns loading boolean, error Object and topicDetails
@@ -19,26 +19,30 @@ const useTopicDetails = (firebase, match) => {
             topic and description */
 			const getTopicDetails = firebase
 				.collection("collections")
-				.doc(match.params.collection)
-				.get()
-				.then(doc => {
-					if (doc.exists) {
-						setData(doc.data());
-						setIsLoading(false);
-					} else {
-						setData({});
-						setIsLoading(false);
-					}
-				})
-				.catch(error => {
-					setIsError(error.message);
-                });
-                
-                return () => getTopicDetails()
-		})();
-    }, [firebase, match]);
+				.where("slug", "==", match.params.collection)
+				.onSnapshot(
+					(snapshot) => {
+						if (snapshot.size) {
+							let data = [];
+							snapshot.forEach((doc) =>
+								data.push({ ...doc.data(), uid: doc.id })
+							);
+							setData(data[0]);
+							setIsLoading(false);
+						} else {
+							setData([]);
+							setIsLoading(false);
+						}
+						/* Unsubscribe from firebase on unmount */
+					},
+					(error) => setIsError(error.message)
+				);
 
-    return { isLoading, isError, data}
+			return () => getTopicDetails();
+		})();
+	}, [firebase, match]);
+
+	return { isLoading, isError, data };
 };
 
 export default useTopicDetails;
