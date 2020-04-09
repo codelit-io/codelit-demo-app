45028eac0c171e4383775461990ff91b27d64b2a
Mo Sharif <mo@mosh-media.com>
2020-04-08 21:16:13 -0700

    Abstract url configuration from MoCard

--- src/components/shared/MoCard/index.js
+++ src/components/shared/MoCard/index.js
@@ -12,41 +12,30 @@ import styles from "./styles";
 import withStyles from "@material-ui/core/styles/withStyles";
 
 const MoCard = ({ item, isDisabled, classes, url }) => (
-  <Link
-    to={
-      (item && item.url) || (item && !isDisabled && `${url}/${item.id}`) || ""
-    }
-    className={item && isDisabled ? classes.disableLink : classes.link}
-  >
-    <Fade timeout={{ enter: 800 }} in={true}>
-      <Card
-        className={`${classes.card} ${item &&
-          isDisabled &&
-          classes.disableCard}`}
-      >
-        <CardActionArea className={classes.content}>
-          {item && isDisabled && <LockIcon className={classes.lockIcon} />}
-          {item && item.img && (
-            <CardMedia
-              className={classes.img}
-              image={item.img}
-              title={item.label}
-            />
-          )}
-          <CardContent className={classes.cardContent}>
-            <Typography gutterBottom variant="h5" component="h2">
-              {item && item.label
-                ? item.item || item.topic || item.label
-                : "No Name"}
-            </Typography>
-            <Typography variant="overline" gutterBottom>
-              {item && (item.desc || item.language)}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    </Fade>
-  </Link>
+	<Link to={url} className={isDisabled ? classes.disableLink : classes.link}>
+		<Fade timeout={{ enter: 800 }} in={true}>
+			<Card className={`${classes.card} ${isDisabled && classes.disableCard}`}>
+				<CardActionArea className={classes.content}>
+					{isDisabled && <LockIcon className={classes.lockIcon} />}
+					{item.img && (
+						<CardMedia
+							className={classes.img}
+							image={item.img}
+							title={item.label}
+						/>
+					)}
+					<CardContent className={classes.cardContent}>
+						<Typography gutterBottom variant="h5" component="h2">
+							{item.label ? item.item || item.topic || item.label : "No Name"}
+						</Typography>
+						<Typography variant="overline" gutterBottom>
+							{item.desc || item.language}
+						</Typography>
+					</CardContent>
+				</CardActionArea>
+			</Card>
+		</Fade>
+	</Link>
 );
 
 export default withStyles(styles)(MoCard);
