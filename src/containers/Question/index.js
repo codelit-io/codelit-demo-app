d6661efc7604baf3213b8d0b83e95da4f9959c3e
Mo Sharif <mo@mosh-media.com>
2020-04-08 20:10:43 -0700

    Update index.js

--- src/containers/Question/index.js
+++ src/containers/Question/index.js
@@ -7,7 +7,7 @@
  * @withAuthentication - HOC provides firebase and match props
  */
 
-import React, { lazy, useEffect, useState, Suspense } from "react";
+import React, { lazy, useCallback, useEffect, useState, Suspense } from "react";
 
 import * as ROUTES from "../../constants/routes";
 
@@ -25,130 +25,132 @@ const CodeEditor = lazy(() => import("../../components/CodeEditor"));
 const MoConfetti = lazy(() => import("../../components/shared/MoConfetti"));
 
 const Question = ({ firebase, history, match }) => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [question, setQuestion] = useState({});
-  const [isCorrect, setIsCorrect] = useState(false);
-  const [snackbarProps, setSnackbarProps] = useState(null);
+	const [isLoading, setIsLoading] = useState(true);
+	const [question, setQuestion] = useState({});
+	const [isCorrect, setIsCorrect] = useState(false);
+	const [snackbarProps, setSnackbarProps] = useState(null);
 
-  const triggerNextQuestion = () => {
-    const nextLevelReqPoints = Number(question.id) + 1;
-    navigateToNextLevel(nextLevelReqPoints);
-  };
+	const triggerNextQuestion = useCallback(() => {
+		const id = Number(question.id) + 1;
+		/* Clear questions */
+		setQuestion({});
 
-  const navigateToNextLevel = id => {
-    /* Clear questions */
-    setQuestion({});
+		setIsCorrect(false);
+		/* A delay before navigating to a new page */
+		setTimeout(() => {
+			history.push(
+				ROUTES.COLLECTIONS.path + "/" + match.params.collection + "/" + id
+			);
+		}, 600);
+	}, [history, match.params.collection, question.id]);
 
-    setIsCorrect(false);
-    /* A delay before navigating to a new page */
-    setTimeout(() => {
-      history.push(
-        ROUTES.COLLECTIONS.path + "/" + match.params.collection + "/" + id
-      );
-    }, 600);
-  };
+	/* Checks if user code matches Pre made answer */
+	const handleOnChange = useCallback(
+		(authUser, userAnswer) => {
+			if (userAnswer === "{}" || userAnswer === "") {
+				return;
+			}
+			if (
+				userAnswer.replace(/\s/g, "") === question.answer.replace(/\s/g, "")
+			) {
+				setQuestion({ ...question, isCorrect: true, question: userAnswer });
+				/* Awards users a point based on level completion */
 
-  /* Awards users a point based on level completion */
-  const handleAwardPlayerPoints = authUser =>
-    awardPlayerPoints(authUser, firebase, question.id, match.params.collection);
+				awardPlayerPoints(
+					authUser,
+					firebase,
+					question.id,
+					match.params.collection
+				);
+				setIsCorrect(true);
+				setSnackbarProps({
+					title: "Hooray!",
+					buttonText: "Keep Going",
+					buttonIcon: <ArrowForwardIcon />,
+				});
+			} else {
+				setQuestion({ ...question, question: userAnswer });
+			}
+		},
+		[firebase, match.params.collection, question]
+	);
 
-  /* Checks if user code matches Pre made answer */
-  const handleOnChange = (authUser, userAnswer) => {
-    if (userAnswer === "{}" || userAnswer === "") {
-      return;
-    }
-    if (userAnswer.replace(/\s/g, "") === question.answer.replace(/\s/g, "")) {
-      setQuestion({ ...question, isCorrect: true, question: userAnswer });
-      handleAwardPlayerPoints(authUser);
-      setIsCorrect(true);
-      setSnackbarProps({
-        title: "Hooray!",
-        buttonText: "Keep Going",
-        buttonIcon: <ArrowForwardIcon />
-      });
-    } else {
-      setQuestion({ ...question, question: userAnswer });
-    }
-  };
+	useEffect(() => {
+		const id = match.params.question;
+		setIsLoading(true);
+		const unsubscribe = firebase
+			.getCollectionById(
+				"collections/" + match.params.collection + "/questions",
+				id
+			)
+			.onSnapshot((snapshot) => {
+				if (snapshot.size) {
+					let question = [];
+					snapshot.forEach((doc) =>
+						question.push({ ...doc.data(), uid: doc.id })
+					);
+					setQuestion(question[0]);
+				} else {
+					setQuestion({
+						label: "You have finished all questions ✅",
+						question: "<h1>Nice Job 🎉</h1>",
+						language: "html",
+					});
+				}
+				setIsLoading(false);
+			});
 
-  useEffect(() => {
-    const id = match.params.question;
-    setIsLoading(true);
-    const unsubscribe = firebase
-      .getCollectionById(
-        "collections/" + match.params.collection + "/questions",
-        id
-      )
-      .onSnapshot(snapshot => {
-        if (snapshot.size) {
-          let question = [];
-          snapshot.forEach(doc =>
-            question.push({ ...doc.data(), uid: doc.id })
-          );
-          setQuestion(question[0]);
-        } else {
-          setQuestion({
-            label: "You have finished all questions ✅",
-            question: "<h1>Nice Job 🎉</h1>",
-            language: "html"
-          });
-        }
-        setIsLoading(false);
-      });
+		return () => {
+			unsubscribe();
+			setSnackbarProps(null);
+		};
+	}, [firebase, match]);
 
-    return () => {
-      unsubscribe();
-      setSnackbarProps(null);
-    };
-  }, [firebase, match]);
-
-  return (
-    question && (
-      <Suspense>
-        <MoConfetti isActive={isCorrect} />
-        <MoPage
-          img=""
-          title={question.topic}
-          isLoading={isLoading}
-          isCard={false}
-        >
-          {question.content && <Content content={question.content} />}
-          {!isLoading && (
-            <AuthUserContext.Consumer>
-              {authUser => (
-                <>
-                  <Grid item md={6} sm={12}>
-                    <MoParagraph
-                      text={question.label}
-                      fade={question.label && true}
-                      margin="36px 0 36px"
-                    />
-                  </Grid>
-                  {question && (
-                    <CodeEditor
-                      handleOnChange={userAnswer =>
-                        handleOnChange(authUser, userAnswer)
-                      }
-                      sm={6}
-                      md={6}
-                      question={question}
-                    />
-                  )}
-                  {snackbarProps && (
-                    <MoSnackbar
-                      isActive={isCorrect}
-                      authUser={authUser}
-                      snackbarProps={snackbarProps}
-                      triggerNextQuestion={() => triggerNextQuestion()}
-                    />
-                  )}
-                </>
-              )}
-            </AuthUserContext.Consumer>
-          )}
-        </MoPage>
-      </Suspense>
-    )
-  );
+	return (
+		<Suspense>
+			<MoConfetti isActive={isCorrect} />
+			<MoPage
+				img=""
+				title={question.topic}
+				isLoading={isLoading}
+				isCard={false}
+			>
+				{question.content && <Content content={question.content} />}
+				{!isLoading && (
+					<AuthUserContext.Consumer>
+						{(authUser) => (
+							<>
+								<Grid item md={6} sm={12}>
+									<MoParagraph
+										text={question.label}
+										fade={question.label && true}
+										margin="36px 0 36px"
+									/>
+								</Grid>
+								{question && (
+									<CodeEditor
+										handleOnChange={(userAnswer) =>
+											handleOnChange(authUser, userAnswer)
+										}
+										sm={6}
+										md={6}
+										question={question}
+									/>
+								)}
+								{snackbarProps && (
+									<MoSnackbar
+										isActive={isCorrect}
+										authUser={authUser}
+										snackbarProps={snackbarProps}
+										triggerNextQuestion={() => triggerNextQuestion()}
+									/>
+								)}
+							</>
+						)}
+					</AuthUserContext.Consumer>
+				)}
+			</MoPage>
+		</Suspense>
+	);
 };
 export default withAuthentication(Question);
