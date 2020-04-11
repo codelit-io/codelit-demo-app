ed6ac3eee504765427c33d80c130f750811b0dec
Mo Sharif <mo@mosh-media.com>
2020-04-11 14:27:53 -0700

    Update formating for QuestionsPage component

--- src/containers/Collection/QuestionsPage/index.js
+++ src/containers/Collection/QuestionsPage/index.js
@@ -7,56 +7,56 @@ import MoScoreBoard from "../../../components/shared/MoScoreBoard";
 import QuestionCard from "./QuestionsList/QuestionCard";
 
 const QuestionsPage = ({
-	authUser,
-	isLoading,
-	match,
-	questions,
-	topicDetails,
+  authUser,
+  isLoading,
+  match,
+  questions,
+  topicDetails
 }) => {
-	const [points, setPoints] = useState(0);
-	const calculateProgress = useCallback(() => {
-		if (!authUser) {
-			return false;
-		}
-		let numberOfQuestions = questions && questions.length;
-		return numberOfQuestions && points
-			? Math.round((points / numberOfQuestions) * 100) + "% Complete"
-			: "0% Complete";
-	}, [authUser, points, questions]);
+  const [points, setPoints] = useState(0);
+  const calculateProgress = useCallback(() => {
+    if (!authUser) {
+      return false;
+    }
+    let numberOfQuestions = questions && questions.length;
+    return numberOfQuestions && points
+      ? Math.round((points / numberOfQuestions) * 100) + "% Complete"
+      : "0% Complete";
+  }, [authUser, points, questions]);
 
-	useEffect(() => {
-		setPoints(authUser?.reports?.[match.params.collection]?.points);
-	}, [authUser, match]);
+  useEffect(() => {
+    setPoints(authUser?.reports?.[match.params.collection]?.points);
+  }, [authUser, match]);
 
-	return (
-		<MoPage
-			title={topicDetails.label}
-			isLoading={isLoading}
-			Component={() => (
-				<MoScoreBoard
-					authUser={authUser}
-					points={points}
-					progress={calculateProgress()}
-				/>
-			)}
-		>
-			{questions && (
-				<Grid container spacing={4}>
-					<QuestionsList
-						points={points}
-						questions={questions}
-						url={match.params.collection}
-					/>
-					<QuestionCard
-						isDisabled={false}
-						points={points}
-						question={{ label: "Add New Course", desc: "Get Started" }}
-						url={`collections/add`}
-					/>
-				</Grid>
-			)}
-		</MoPage>
-	);
+  return (
+    <MoPage
+      title={topicDetails.label}
+      isLoading={isLoading}
+      Component={() => (
+        <MoScoreBoard
+          authUser={authUser}
+          points={points}
+          progress={calculateProgress()}
+        />
+      )}
+    >
+      {questions && (
+        <Grid container spacing={4}>
+          <QuestionsList
+            points={points}
+            questions={questions}
+            url={match.params.collection}
+          />
+          <QuestionCard
+            isDisabled={false}
+            points={points}
+            question={{ label: "Add New Course", desc: "Get Started" }}
+            url={`collections/add`}
+          />
+        </Grid>
+      )}
+    </MoPage>
+  );
 };
 
 export default QuestionsPage;
