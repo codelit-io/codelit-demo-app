723a9a9561441e497a4bbf398ebd3b964fa20b63
Mo Sharif <mo@mosh-media.com>
2020-04-10 22:01:43 -0700

    Update index.js

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,45 +3,45 @@ import React from "react";
 import { AuthUserContext } from "../Session";
 import AppBar from "@material-ui/core/AppBar";
 import Grid from "@material-ui/core/Grid";
-import MoAvatar from "./MoAvatar";
-import MoMenu from "./MoMenu";
+import MoAvatar from "../shared/MoAvatar";
+import MoMenu from "../shared/MoMenu";
 import MoSkoolLogo from "../MoSkoolLogo";
 import styles from "./styles";
 import Toolbar from "@material-ui/core/Toolbar";
 import withStyles from "@material-ui/core/styles/withStyles";
 
 const Navigation = ({ classes }) => {
-  return (
-    <header className={classes.root}>
-      <AppBar position="static" color="default" className={classes.appBar}>
-        <Toolbar className={classes.toolbar}>
-          <Grid container className={classes.alignCenter}>
-            <Grid item xs={4} sm={4} md={4} lg={4}>
-              <MoSkoolLogo />
-            </Grid>
-            <Grid item xs={4} sm={4} md={4} lg={4}></Grid>
-            <Grid
-              item
-              xs={4}
-              sm={4}
-              md={4}
-              lg={4}
-              style={{ textAlign: "right" }}
-            >
-              <AuthUserContext.Consumer>
-                {authUser => (
-                  <>
-                    <MoMenu authUser={authUser} />
-                    <MoAvatar authUser={authUser} />
-                  </>
-                )}
-              </AuthUserContext.Consumer>
-            </Grid>
-          </Grid>
-        </Toolbar>
-      </AppBar>
-    </header>
-  );
+	return (
+		<header className={classes.root}>
+			<AppBar position="static" color="default" className={classes.appBar}>
+				<Toolbar className={classes.toolbar}>
+					<Grid container className={classes.alignCenter}>
+						<Grid item xs={4} sm={4} md={4} lg={4}>
+							<MoSkoolLogo />
+						</Grid>
+						<Grid item xs={4} sm={4} md={4} lg={4}></Grid>
+						<Grid
+							item
+							xs={4}
+							sm={4}
+							md={4}
+							lg={4}
+							style={{ textAlign: "right" }}
+						>
+							<AuthUserContext.Consumer>
+								{(authUser) => (
+									<>
+										<MoMenu authUser={authUser} />
+										<MoAvatar authUser={authUser} />
+									</>
+								)}
+							</AuthUserContext.Consumer>
+						</Grid>
+					</Grid>
+				</Toolbar>
+			</AppBar>
+		</header>
+	);
 };
 
 export default withStyles(styles)(Navigation);
