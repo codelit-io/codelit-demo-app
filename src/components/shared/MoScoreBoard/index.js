c34745ac887e2b6a695a967b805db041803a75a7
Mo Sharif <mo@mosh-media.com>
2020-04-11 22:19:43 -0700

    Remove add collection from scoreboard

--- src/components/shared/MoScoreBoard/index.js
+++ src/components/shared/MoScoreBoard/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 
-import * as ROLES from "../../../constants/roles";
 import * as ROUTES from "../../../constants/routes";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Button from "@material-ui/core/Button";
 import Fade from "@material-ui/core/Fade";
 import { Link } from "react-router-dom";
-import PostAddIcon from "@material-ui/icons/PostAdd";
 
 const MoScoreBoard = ({ authUser, points, progress }) => (
   <Fade
@@ -23,11 +21,6 @@ const MoScoreBoard = ({ authUser, points, progress }) => (
       color="default"
       style={{ marginBottom: "48px" }}
     >
-      {authUser?.roles && !!authUser?.roles[ROLES.AUTHOR] && (
-        <Button to={ROUTES.ADMIN.path} component={Link}>
-          Add <PostAddIcon />
-        </Button>
-      )}
       {authUser && <Button> {points ? `${points} Points` : "0 Points"}</Button>}
       {authUser && <Button>{progress}</Button>}
       {!authUser && (
