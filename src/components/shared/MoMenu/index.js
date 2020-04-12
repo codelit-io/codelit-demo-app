3e5cb033642e8912edf3169c1b4651874b31d5d8
Mo Sharif <mo@mosh-media.com>
2020-04-12 12:40:47 -0700

    Update index.js

--- src/components/shared/MoMenu/index.js
+++ src/components/shared/MoMenu/index.js
@@ -13,80 +13,80 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import Tooltip from "@material-ui/core/Tooltip";
 
 const MoMenu = ({ authUser, classes }) => {
-	const [anchorEl, setAnchorEl] = useState(null);
+  const [anchorEl, setAnchorEl] = useState(null);
 
-	const handleClick = (event) => {
-		setAnchorEl(event.currentTarget);
-	};
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
 
-	const handleClose = () => {
-		setAnchorEl(null);
-	};
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-	return (
-		<>
-			<Tooltip title="My Courses" arrow>
-				<IconButton
-					aria-label="View Collections Menu"
-					aria-controls="collections-menu"
-					aria-haspopup="true"
-					onClick={handleClick}
-				>
-					<AppsIcon />
-				</IconButton>
-			</Tooltip>
-			<Menu
-				anchorEl={anchorEl}
-				anchorOrigin={{
-					vertical: "bottom",
-					horizontal: "center",
-				}}
-				elevation={2}
-				getContentAnchorEl={null}
-				id="collections-menu"
-				keepMounted
-				open={Boolean(anchorEl)}
-				onClose={handleClose}
-				transformOrigin={{
-					vertical: "top",
-					horizontal: "center",
-				}}
-				TransitionComponent={Slide}
-			>
-				<div>
-					<MenuItem
-						to={ROUTES.EASY_COLLECTIONS.path}
-						component={Link}
-						onClick={handleClose}
-					>
-						{ROUTES.EASY_COLLECTIONS.title}
-					</MenuItem>
-					<MenuItem
-						to={ROUTES.MEDIUM_COLLECTIONS.path}
-						component={Link}
-						onClick={handleClose}
-					>
-						{ROUTES.MEDIUM_COLLECTIONS.title}
-					</MenuItem>
-					<MenuItem
-						to={ROUTES.PRO_COLLECTIONS.path}
-						component={Link}
-						onClick={handleClose}
-					>
-						{ROUTES.PRO_COLLECTIONS.title}
-					</MenuItem>
-					<Divider variant="middle" />
-					<MenuItem
-						to={ROUTES.COLLECTIONS.path}
-						component={Link}
-						onClick={handleClose}
-					>
-						My Courses
-					</MenuItem>
-				</div>
-			</Menu>
-		</>
-	);
+  return (
+    <>
+      <Tooltip title="My Courses" arrow>
+        <IconButton
+          aria-label="View Collections Menu"
+          aria-controls="collections-menu"
+          aria-haspopup="true"
+          onClick={handleClick}
+        >
+          <AppsIcon />
+        </IconButton>
+      </Tooltip>
+      <Menu
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center"
+        }}
+        elevation={2}
+        getContentAnchorEl={null}
+        id="collections-menu"
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center"
+        }}
+        TransitionComponent={Slide}
+      >
+        <div>
+          <MenuItem
+            to={ROUTES.EASY_COLLECTIONS.path}
+            component={Link}
+            onClick={handleClose}
+          >
+            {ROUTES.EASY_COLLECTIONS.title}
+          </MenuItem>
+          <MenuItem
+            to={ROUTES.MEDIUM_COLLECTIONS.path}
+            component={Link}
+            onClick={handleClose}
+          >
+            {ROUTES.MEDIUM_COLLECTIONS.title}
+          </MenuItem>
+          <MenuItem
+            to={ROUTES.PRO_COLLECTIONS.path}
+            component={Link}
+            onClick={handleClose}
+          >
+            {ROUTES.PRO_COLLECTIONS.title}
+          </MenuItem>
+          <Divider variant="middle" />
+          <MenuItem
+            to={ROUTES.COLLECTIONS.path}
+            component={Link}
+            onClick={handleClose}
+          >
+            My Courses
+          </MenuItem>
+        </div>
+      </Menu>
+    </>
+  );
 };
 
 export default withStyles(styles)(MoMenu);
