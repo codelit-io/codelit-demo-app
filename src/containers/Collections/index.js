fc170ca69a412f2bcf74dc93fff128abb6124f72
Mo Sharif <mo@mosh-media.com>
2020-04-11 22:53:28 -0700

    Update index.js

--- src/containers/Collections/index.js
+++ src/containers/Collections/index.js
@@ -13,21 +13,21 @@ import QuestionsPage from "../Collection/QuestionsPage";
 import useCollections from "../../Hooks/useCollections";
 
 const Collections = ({ firebase, match }) => {
-  const collections = useCollections("collections", firebase);
-  const courses = useCollections("courses", firebase);
-  
-  return (
-    <AuthUserContext.Consumer>
-      {authUser => (
-        <QuestionsPage
-          authUser={authUser}
-          isLoading={collections.isLoading}
-          match={match}
-          questions={[...collections.data, ...courses.data]}
-          collectionDetails={{ label: "My Courses" }}
-        />
-      )}
-    </AuthUserContext.Consumer>
-  );
+	const collections = useCollections("collections", firebase);
+	const courses = useCollections("courses", firebase);
+
+	return (
+		<AuthUserContext.Consumer>
+			{(authUser) => (
+				<QuestionsPage
+					authUser={authUser}
+					isLoading={collections.isLoading}
+					match={match}
+					questions={[...collections.data, ...courses.data]}
+					collectionDetails={{ label: "My Courses" }}
+				/>
+			)}
+		</AuthUserContext.Consumer>
+	);
 };
 export default withAuthentication(Collections);
