bd8a406d866cc43e2a48af282b9e2e39905a495a
Mo Sharif <mo@mosh-media.com>
2020-04-11 23:24:07 -0700

    Update index.js

--- src/components/shared/MoMenu/index.js
+++ src/components/shared/MoMenu/index.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 
 import * as ROUTES from "../../../constants/routes";
 import AppsIcon from "@material-ui/icons/Apps";
-import { IconButton } from "@material-ui/core";
+import IconButton from "@material-ui/core/IconButton";
+import Divider from "@material-ui/core/Divider";
 import { Link } from "react-router-dom";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
@@ -12,72 +13,80 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import Tooltip from "@material-ui/core/Tooltip";
 
 const MoMenu = ({ authUser, classes }) => {
-  const [anchorEl, setAnchorEl] = useState(null);
+	const [anchorEl, setAnchorEl] = useState(null);
 
-  const handleClick = event => {
-    setAnchorEl(event.currentTarget);
-  };
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
 
-  return (
-    <>
-      <Tooltip title="My Courses" arrow>
-        <IconButton
-          aria-label="View Collections Menu"
-          aria-controls="collections-menu"
-          aria-haspopup="true"
-          onClick={handleClick}
-        >
-          <AppsIcon />
-        </IconButton>
-      </Tooltip>
-      <Menu
-        anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "center"
-        }}
-        elevation={2}
-        getContentAnchorEl={null}
-        id="collections-menu"
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "center"
-        }}
-        TransitionComponent={Slide}
-      >
-        <div>
-          <MenuItem
-            to={ROUTES.EASY_COLLECTIONS.path}
-            component={Link}
-            onClick={handleClose}
-          >
-            {ROUTES.EASY_COLLECTIONS.title}
-          </MenuItem>
-          <MenuItem
-            to={ROUTES.MEDIUM_COLLECTIONS.path}
-            component={Link}
-            onClick={handleClose}
-          >
-            Medium
-          </MenuItem>
-          <MenuItem
-            to={ROUTES.PRO_COLLECTIONS.path}
-            component={Link}
-            onClick={handleClose}
-          >
-            Pro
-          </MenuItem>
-        </div>
-      </Menu>
-    </>
-  );
+	return (
+		<>
+			<Tooltip title="My Courses" arrow>
+				<IconButton
+					aria-label="View Collections Menu"
+					aria-controls="collections-menu"
+					aria-haspopup="true"
+					onClick={handleClick}
+				>
+					<AppsIcon />
+				</IconButton>
+			</Tooltip>
+			<Menu
+				anchorEl={anchorEl}
+				anchorOrigin={{
+					vertical: "bottom",
+					horizontal: "center",
+				}}
+				elevation={2}
+				getContentAnchorEl={null}
+				id="collections-menu"
+				keepMounted
+				open={Boolean(anchorEl)}
+				onClose={handleClose}
+				transformOrigin={{
+					vertical: "top",
+					horizontal: "center",
+				}}
+				TransitionComponent={Slide}
+			>
+				<div>
+					<MenuItem
+						to={ROUTES.EASY_COLLECTIONS.path}
+						component={Link}
+						onClick={handleClose}
+					>
+						{ROUTES.EASY_COLLECTIONS.title}
+					</MenuItem>
+					<MenuItem
+						to={ROUTES.MEDIUM_COLLECTIONS.path}
+						component={Link}
+						onClick={handleClose}
+					>
+						{ROUTES.MEDIUM_COLLECTIONS.title}
+					</MenuItem>
+					<MenuItem
+						to={ROUTES.PRO_COLLECTIONS.path}
+						component={Link}
+						onClick={handleClose}
+					>
+						{ROUTES.PRO_COLLECTIONS.title}
+					</MenuItem>
+					<Divider variant="middle" />
+					<MenuItem
+						to={ROUTES.COLLECTIONS.path}
+						component={Link}
+						onClick={handleClose}
+					>
+						My Courses
+					</MenuItem>
+				</div>
+			</Menu>
+		</>
+	);
 };
 
 export default withStyles(styles)(MoMenu);
