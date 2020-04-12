c325e8d1bcdde25d9e3834e5d3d34ce105a1e2c2
Mo Sharif <mo@mosh-media.com>
2020-04-11 22:18:43 -0700

    Create useCollection 🚧under dev

---
+++ src/Hooks/useCollection/index.js
@@ -0,0 +1,33 @@
+/**
+ * Custom Hook to get list of all Collections
+ * @param {Object} firebase.collection - Collection function in firebase provides access to collection in db
+ * @param {Object} collectionPath - Path to collection or doc in firebase ex: COLLECTION_NAME/DOC_ID
+ * @returns {isLoading: boolean, isError: Object, data: Strings[]} - returns loading boolean, error Object and an Array of questions
+ */
+
+import { useEffect, useState } from "react";
+
+const useCollection = (firebase, payload) => {
+	const [isLoading, setIsLoading] = useState(false);
+	const [isError, setIsError] = useState(false);
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			setIsLoading(true);
+			/* Make a firebase query to get details about 
+            the collection or questions Such as name and description
+            */
+			const createCollection = firebase.createCollection("Test", payload)
+				// (data) => {setData(data)},
+				// (error) => setIsError(error.message)
+			return () => {
+				setData({});
+				createCollection();
+			};
+		})();
+	}, [firebase, payload]);
+
+	return { isLoading, isError, data };
+};
+export default useCollection;
