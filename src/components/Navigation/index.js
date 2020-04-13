3c691f3f91ae6e4d034f169a61ab19490dd5d8d1
Mo Sharif <mo@mosh-media.com>
2020-04-12 18:43:06 -0700

    Update index.js

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -6,7 +6,6 @@ import AppBar from "@material-ui/core/AppBar";
 import { compose } from "recompose";
 import Grid from "@material-ui/core/Grid";
 import MoAvatar from "../shared/MoAvatar";
-import MoMenu from "../shared/MoMenu";
 import MoSkoolLogo from "../MoSkoolLogo";
 import styles from "./styles";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -14,44 +13,43 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import { withRouter } from "react-router-dom";
 
 const Navigation = ({ classes, firebase, history, match }) => {
-  return (
-    <header className={classes.root}>
-      <AppBar position="static" color="default" className={classes.appBar}>
-        <Toolbar className={classes.toolbar}>
-          <Grid container className={classes.alignCenter}>
-            <Grid item xs={6} sm={6} md={6} lg={6}>
-              <MoSkoolLogo />
-            </Grid>
-            <Grid
-              item
-              xs={6}
-              sm={6}
-              md={6}
-              lg={6}
-              style={{ textAlign: "right" }}
-            >
-              <AuthUserContext.Consumer>
-                {authUser => (
-                  <>
-                    {authUser && (
-                      <AddCourse
-                        authUser={authUser}
-                        firebase={firebase}
-                        history={history}
-                        match={match}
-                      />
-                    )}
-                    <MoMenu authUser={authUser} />
-                    <MoAvatar authUser={authUser} />
-                  </>
-                )}
-              </AuthUserContext.Consumer>
-            </Grid>
-          </Grid>
-        </Toolbar>
-      </AppBar>
-    </header>
-  );
+	return (
+		<header className={classes.root}>
+			<AppBar position="static" color="default" className={classes.appBar}>
+				<Toolbar className={classes.toolbar}>
+					<Grid container className={classes.alignCenter}>
+						<Grid item xs={6} sm={6} md={6} lg={6}>
+							<MoSkoolLogo />
+						</Grid>
+						<Grid
+							item
+							xs={6}
+							sm={6}
+							md={6}
+							lg={6}
+							style={{ textAlign: "right" }}
+						>
+							<AuthUserContext.Consumer>
+								{(authUser) => (
+									<>
+										{authUser && (
+											<AddCourse
+												authUser={authUser}
+												firebase={firebase}
+												history={history}
+												match={match}
+											/>
+										)}
+										<MoAvatar authUser={authUser} firebase={firebase} />
+									</>
+								)}
+							</AuthUserContext.Consumer>
+						</Grid>
+					</Grid>
+				</Toolbar>
+			</AppBar>
+		</header>
+	);
 };
 
 export default compose(withStyles(styles), withRouter)(Navigation);
