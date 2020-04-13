531c9f3f581b650ddaa01374072cca7798742a81
Mo Sharif <mo@mosh-media.com>
2020-04-12 19:17:58 -0700

    Update styles.js

--- src/components/shared/MoPage/styles.js
+++ src/components/shared/MoPage/styles.js
@@ -1,27 +1,31 @@
-const styles = theme => ({
-  content: {
-    display: "flex",
-    flexDirection: "column",
-    width: "100%"
-  },
-  img: {
-    height: 100,
-    width: 100
-  },
-  section: {
-    maxHeight: "100%",
-    height: "100%",
-    width: "100%",
-    paddingTop: theme.padding?.lg,
-    paddingBottom: theme.padding?.lg
-  },
-  component: {
-    display: "flex",
-    alignItems: "baseline",
-    [theme.breakpoints.up(400)]: {
-      justifyContent: "flex-end"
-    }
-  }
+const styles = (theme) => ({
+	content: {
+		display: "flex",
+		flexDirection: "column",
+		width: "100%",
+	},
+	img: {
+		height: 100,
+		width: 100,
+	},
+	section: {
+		maxHeight: "100%",
+		height: "100%",
+		width: "100%",
+		paddingTop: theme.padding?.md,
+		paddingBottom: theme.padding?.md,
+		[theme.breakpoints.up(400)]: {
+			paddingTop: theme.padding?.lg,
+			paddingBottom: theme.padding?.xl,
+		},
+	},
+	component: {
+		display: "flex",
+		alignItems: "baseline",
+		[theme.breakpoints.up(400)]: {
+			justifyContent: "flex-end",
+		},
+	},
 });
 
 export default styles;
