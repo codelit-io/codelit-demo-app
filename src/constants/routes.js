e44b3d512ccac37ff72234f771016f9f1020afef
Mo Sharif <mo@mosh-media.com>
2020-04-12 18:42:54 -0700

    Update routes.js

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,5 +1,5 @@
 /*  Pages */
-export const ACCOUNT = { path: "/account", title: "My account" };
+export const ACCOUNT = { path: "/account", title: "Your account" };
 export const ADMIN = { path: "/admin", title: "Admin Page" };
 export const ADMIN_DETAILS = { path: "/admin/:id", title: "Admin Details" };
 export const ADMIN_COLLECTIONS = {
@@ -25,7 +25,7 @@ export const LANDING = {
   path: "/",
   title: "New way to Mastering Front-end Development"
 };
-export const COLLECTIONS = { path: "/courses", title: "My Courses" };
+export const COLLECTIONS = { path: "/courses", title: "Your Courses" };
 export const COLLECTIONS_ADD = {
   path: "/courses/add",
   title: "Add New Course"
@@ -42,8 +42,8 @@ export const PRO_COLLECTIONS = {
   path: "/courses/mo-pro",
   title: "React for Pros"
 };
-export const SIGN_IN = { path: "/signin", title: "Sign in" };
-export const SIGN_UP = { path: "/signup", title: "Sign up" };
+export const SIGN_IN = { path: "/signin", title: "Email Sign in" };
+export const SIGN_UP = { path: "/signup", title: "Email Sign up" };
 export const PASSWORD_FORGET = { path: "/pw-forget", title: "Forgot Password" };
 export const PLAYGROUND = { path: "/playgound", title: "React Playground" };
 export const NOT_FOUND = { path: "/not_found", title: "Page Not Found" };
