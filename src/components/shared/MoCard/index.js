9f3c26d1f6002f0896715a845b9798d2eb9ee146
Mo Sharif <mo@mosh-media.com>
2020-04-14 21:11:48 -0700

    Update MoCard component layout

--- src/components/shared/MoCard/index.js
+++ src/components/shared/MoCard/index.js
@@ -1,38 +1,40 @@
 import React from "react";
 
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import { Link } from "react-router-dom";
 import LockIcon from "@material-ui/icons/Lock";
 import styles from "./styles";
 import withStyles from "@material-ui/core/styles/withStyles";
+import { Grid } from "@material-ui/core";
+
+import SportsEsportsIcon from '@material-ui/icons/SportsEsports';
 
 const MoCard = ({ item, isDisabled, classes, url }) => (
-  <Link to={url} className={isDisabled ? classes.disableLink : classes.link}>
-    <Card className={`${classes.card} ${isDisabled && classes.disableCard}`}>
-      <CardActionArea className={classes.content}>
-        {isDisabled && <LockIcon className={classes.lockIcon} />}
-        {item.img && (
-          <CardMedia
-            className={classes.img}
-            image={item.img}
-            title={item.label}
-          />
-        )}
-        <CardContent className={classes.cardContent}>
-          <Typography gutterBottom variant="h5" component="h2">
-            {item.label ? item.item || item.topic || item.label : "No Name"}
-          </Typography>
-          <Typography variant="overline" gutterBottom>
-            {item.desc || item.language}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-  </Link>
+	<Link to={url} className={isDisabled ? classes.disableLink : classes.link}>
+		<div className={`${classes.card} ${isDisabled && classes.disableCard}`}>
+			{isDisabled && <LockIcon className={classes.lockIcon} />}
+			<Grid container spacing={4} className={classes.content}>
+				<Grid item className={classes.cardContent} xm={2} sm={2} md={2} lg={2}>
+					<SportsEsportsIcon className={classes.heroIcon}/>
+				</Grid>
+				<Grid
+					item
+					className={classes.cardContent}
+					xm={10}
+					sm={10}
+					md={10}
+					lg={10}
+				>
+					<Typography gutterBottom variant="h5" component="h2">
+						{item.label ? item.item || item.topic || item.label : "No Name"}
+					</Typography>
+					<Typography variant="overline" gutterBottom>
+						{item.desc || item.language}
+					</Typography>
+				</Grid>
+			</Grid>
+		</div>
+	</Link>
 );
 
 export default withStyles(styles)(MoCard);
