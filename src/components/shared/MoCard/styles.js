0d04c3005cba436b1c0e97c37c743ed3f39e1586
Mo Sharif <mo@mosh-media.com>
2020-04-14 21:11:40 -0700

    Update styling for MoCard

--- src/components/shared/MoCard/styles.js
+++ src/components/shared/MoCard/styles.js
@@ -1,44 +1,50 @@
-const styles = theme => ({
-  card: theme.card,
-  disableCard: {
-    backgroundColor: "#f2f2f2"
-  },
-  content: {
-    padding: 20,
-    display: "flex",
-    flexDirection: "column"
-  },
-  cardContent: {
-    textAlign: "center"
-  },
-  img: {
-    backgroundSize: "contain",
-    height: 280,
-    width: 200
-  },
-  link: {
-    textDecorationLine: "none",
-    "&:hover": {
-      textDecorationLine: "none"
-    }
-  },
-  disableLink: {
-    pointerEvents: "none",
-    textDecoration: "none",
-    cursor: "default"
-  },
-  lockIcon: {
-    color: theme.grey?.dark,
-    position: "absolute",
-    top: "10px",
-    right: "10px"
-  },
-  checkIcon: {
-    color: " #99bb33",
-    position: "absolute",
-    top: "10px",
-    right: "10px"
-  }
+const styles = (theme) => ({
+	card: {
+		...theme.card,
+		marginTop: theme.padding.md,
+		marginBottom: theme.padding.md,
+	},
+	disableCard: {
+		backgroundColor: "#f2f2f2",
+	},
+	content: {
+		padding: 20,
+	},
+	cardContent: {
+		color: theme.grey.medium,
+	},
+	img: {
+		backgroundSize: "contain",
+		height: 280,
+		width: 200,
+	},
+	link: {
+		textDecorationLine: "none",
+		"&:hover": {
+			textDecorationLine: "none",
+		},
+	},
+	disableLink: {
+		pointerEvents: "none",
+		textDecoration: "none",
+		cursor: "default",
+	},
+	lockIcon: {
+		color: theme.grey?.dark,
+		position: "absolute",
+		top: "10px",
+		right: "10px",
+	},
+	checkIcon: {
+		color: " #99bb33",
+		position: "absolute",
+		top: "10px",
+		right: "10px",
+	},
+	heroIcon: {
+		fontSize: theme.fontSize.xl,
+		color: theme.grey.superLight,
+	},
 });
 
 export default styles;
