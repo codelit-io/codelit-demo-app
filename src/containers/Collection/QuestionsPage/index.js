53ff866b0ac212e9fa79a1a15ea19aa3294d0c3e
Mo Sharif <mo@mosh-media.com>
2020-04-14 21:11:02 -0700

    Update index.js

--- src/containers/Collection/QuestionsPage/index.js
+++ src/containers/Collection/QuestionsPage/index.js
@@ -6,51 +6,54 @@ import MoPage from "../../../components/shared/MoPage";
 import MoProgressBar from "../../../components/shared/MoProgressBar";
 
 const QuestionsPage = ({
-  authUser,
-  isLoading,
-  match,
-  questions,
-  collectionDetails
+	authUser,
+	isLoading,
+	match,
+	questions,
+	collectionDetails,
 }) => {
-  const [points, setPoints] = useState(0);
-  const calculateProgress = useCallback(() => {
-    if (!authUser) {
-      return false;
-    }
-    let numberOfQuestions = questions && questions.length;
-    return numberOfQuestions && points
-      ? Math.round((points / numberOfQuestions) * 100) + "% Complete"
-      : "0% Complete";
-  }, [authUser, points, questions]);
-  useEffect(() => {
-    setPoints(authUser?.reports?.[match.params.collection]?.points);
-  }, [authUser, match]);
+	const [points, setPoints] = useState(0);
+	const calculateProgress = useCallback(() => {
+		if (!authUser) {
+			return false;
+		}
+		let numberOfQuestions = questions && questions.length;
+		return numberOfQuestions && points
+			? Math.round((points / numberOfQuestions) * 100) + "% Complete"
+			: "0% Complete";
+	}, [authUser, points, questions]);
+	useEffect(() => {
+		setPoints(authUser?.reports?.[match.params.collection]?.points);
+	}, [authUser, match]);
 
-  return (
-    <MoPage
-      title={collectionDetails?.label}
-      isLoading={isLoading}
-      Component={() =>
-        collectionDetails.isProgressBar && (
-          <MoProgressBar
-            authUser={authUser}
-            points={points}
-            progress={calculateProgress()}
-          />
-        )
-      }
-    >
-      {questions && (
-        <Grid container spacing={4}>
-          <QuestionsList
-            points={points}
-            questions={questions}
-            url={match?.params?.collection}
-          />
-        </Grid>
-      )}
-    </MoPage>
-  );
+	return (
+		<MoPage
+			title={collectionDetails?.label}
+			isLoading={isLoading}
+			Component={() =>
+				collectionDetails.isProgressBar && (
+					<MoProgressBar
+						authUser={authUser}
+						points={points}
+						progress={calculateProgress()}
+					/>
+				)
+			}
+		>
+			{questions && (
+				<Grid container spacing={4}>
+					<Grid item xs={12} sm={12} md={12} lg={12}>
+						<QuestionsList
+							points={points}
+							questions={questions}
+							url={match?.params?.collection}
+						/>
+					</Grid>
+					<Grid item xs={12} sm={12} md={12} lg={12}></Grid>
+				</Grid>
+			)}
+		</MoPage>
+	);
 };
 
 export default QuestionsPage;
