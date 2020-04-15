cd79e878bf22b991bb10a784617d7323a51d80f8
Mo Sharif <mo@mosh-media.com>
2020-04-14 21:13:03 -0700

    Lint ✅

--- src/components/shared/MoCard/index.js
+++ src/components/shared/MoCard/index.js
@@ -7,34 +7,34 @@ import styles from "./styles";
 import withStyles from "@material-ui/core/styles/withStyles";
 import { Grid } from "@material-ui/core";
 
-import SportsEsportsIcon from '@material-ui/icons/SportsEsports';
+import SportsEsportsIcon from "@material-ui/icons/SportsEsports";
 
 const MoCard = ({ item, isDisabled, classes, url }) => (
-	<Link to={url} className={isDisabled ? classes.disableLink : classes.link}>
-		<div className={`${classes.card} ${isDisabled && classes.disableCard}`}>
-			{isDisabled && <LockIcon className={classes.lockIcon} />}
-			<Grid container spacing={4} className={classes.content}>
-				<Grid item className={classes.cardContent} xm={2} sm={2} md={2} lg={2}>
-					<SportsEsportsIcon className={classes.heroIcon}/>
-				</Grid>
-				<Grid
-					item
-					className={classes.cardContent}
-					xm={10}
-					sm={10}
-					md={10}
-					lg={10}
-				>
-					<Typography gutterBottom variant="h5" component="h2">
-						{item.label ? item.item || item.topic || item.label : "No Name"}
-					</Typography>
-					<Typography variant="overline" gutterBottom>
-						{item.desc || item.language}
-					</Typography>
-				</Grid>
-			</Grid>
-		</div>
-	</Link>
+  <Link to={url} className={isDisabled ? classes.disableLink : classes.link}>
+    <div className={`${classes.card} ${isDisabled && classes.disableCard}`}>
+      {isDisabled && <LockIcon className={classes.lockIcon} />}
+      <Grid container spacing={4} className={classes.content}>
+        <Grid item className={classes.cardContent} xm={2} sm={2} md={2} lg={2}>
+          <SportsEsportsIcon className={classes.heroIcon} />
+        </Grid>
+        <Grid
+          item
+          className={classes.cardContent}
+          xm={10}
+          sm={10}
+          md={10}
+          lg={10}
+        >
+          <Typography gutterBottom variant="h5" component="h2">
+            {item.label ? item.item || item.topic || item.label : "No Name"}
+          </Typography>
+          <Typography variant="overline" gutterBottom>
+            {item.desc || item.language}
+          </Typography>
+        </Grid>
+      </Grid>
+    </div>
+  </Link>
 );
 
 export default withStyles(styles)(MoCard);
--- src/components/shared/MoCard/styles.js
+++ src/components/shared/MoCard/styles.js
@@ -1,50 +1,50 @@
-const styles = (theme) => ({
-	card: {
-		...theme.card,
-		marginTop: theme.padding.md,
-		marginBottom: theme.padding.md,
-	},
-	disableCard: {
-		backgroundColor: "#f2f2f2",
-	},
-	content: {
-		padding: 20,
-	},
-	cardContent: {
-		color: theme.grey.medium,
-	},
-	img: {
-		backgroundSize: "contain",
-		height: 280,
-		width: 200,
-	},
-	link: {
-		textDecorationLine: "none",
-		"&:hover": {
-			textDecorationLine: "none",
-		},
-	},
-	disableLink: {
-		pointerEvents: "none",
-		textDecoration: "none",
-		cursor: "default",
-	},
-	lockIcon: {
-		color: theme.grey?.dark,
-		position: "absolute",
-		top: "10px",
-		right: "10px",
-	},
-	checkIcon: {
-		color: " #99bb33",
-		position: "absolute",
-		top: "10px",
-		right: "10px",
-	},
-	heroIcon: {
-		fontSize: theme.fontSize.xl,
-		color: theme.grey.superLight,
-	},
+const styles = theme => ({
+  card: {
+    ...theme.card,
+    marginTop: theme.padding.md,
+    marginBottom: theme.padding.md
+  },
+  disableCard: {
+    backgroundColor: "#f2f2f2"
+  },
+  content: {
+    padding: 20
+  },
+  cardContent: {
+    color: theme.grey.medium
+  },
+  img: {
+    backgroundSize: "contain",
+    height: 280,
+    width: 200
+  },
+  link: {
+    textDecorationLine: "none",
+    "&:hover": {
+      textDecorationLine: "none"
+    }
+  },
+  disableLink: {
+    pointerEvents: "none",
+    textDecoration: "none",
+    cursor: "default"
+  },
+  lockIcon: {
+    color: theme.grey?.dark,
+    position: "absolute",
+    top: "10px",
+    right: "10px"
+  },
+  checkIcon: {
+    color: " #99bb33",
+    position: "absolute",
+    top: "10px",
+    right: "10px"
+  },
+  heroIcon: {
+    fontSize: theme.fontSize.xl,
+    color: theme.grey.superLight
+  }
 });
 
 export default styles;
