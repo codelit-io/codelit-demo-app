ad35355ce9ee289965ef6879984d4fe4f9de85f9
Mo Sharif <mo@mosh-media.com>
2020-04-14 21:10:48 -0700

    Update Question Card grid layout

--- src/containers/Collection/QuestionsPage/QuestionsList/QuestionCard/index.js
+++ src/containers/Collection/QuestionsPage/QuestionsList/QuestionCard/index.js
@@ -4,7 +4,7 @@ import Grid from "@material-ui/core/Grid";
 import MoCard from "../../../../../components/shared/MoCard";
 
 const QuestionCard = ({ isDisabled, question, points, url }) => (
-  <Grid item xs={12} sm={6} md={3}>
+  <Grid item xs={12} sm={6} md={6}>
     <MoCard
       isDisabled={isDisabled}
       points={points}
