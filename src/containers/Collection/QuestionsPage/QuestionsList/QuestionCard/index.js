c98c5c729f6cbf88629d45bf53c44465562f70a5
Mo Sharif <mo@mosh-media.com>
2020-04-15 23:35:19 -0700

    Remvoe Grid from QuestionCard

--- src/containers/Collection/QuestionsPage/QuestionsList/QuestionCard/index.js
+++ src/containers/Collection/QuestionsPage/QuestionsList/QuestionCard/index.js
@@ -1,19 +1,18 @@
 import React from "react";
 
-import Grid from "@material-ui/core/Grid";
 import MoCard from "../../../../../components/shared/MoCard";
 import { withStyles } from "@material-ui/core";
 import styles from "./styles";
 
 const QuestionCard = ({ classes, isDisabled, question, points, url }) => (
-  <Grid item xs={12} sm={6} md={6} className={classes.padding}>
-    <MoCard
-      isDisabled={isDisabled}
-      points={points}
-      item={question}
-      url={url}
-    ></MoCard>
-  </Grid>
+	<div className={classes.padding}>
+		<MoCard
+			isDisabled={isDisabled}
+			points={points}
+			item={question}
+			url={url}
+		></MoCard>
+	</div>
 );
 
 export default withStyles(styles)(QuestionCard);
