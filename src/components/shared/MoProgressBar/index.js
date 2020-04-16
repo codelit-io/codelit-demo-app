0663e5c0fc6fa5c4bc1f67028f9dbf7d40ee9933
Mo Sharif <mo@mosh-media.com>
2020-04-15 23:35:51 -0700

    Update index.js

--- src/components/shared/MoProgressBar/index.js
+++ src/components/shared/MoProgressBar/index.js
@@ -8,29 +8,29 @@ import Fade from "@material-ui/core/Fade";
 import { Link } from "react-router-dom";
 
 const MoProgressBar = ({ authUser, points, progress }) => (
-  <Fade
-    direction="left"
-    in={authUser && true}
-    mountOnEnter
-    timeout={{ enter: 800, exit: 400 }}
-    unmountOnExit
-  >
-    <ButtonGroup
-      aria-label="Points and progress"
-      variant="text"
-      color="default"
-      style={{ marginBottom: "48px" }}
-    >
-      {authUser && <Button> {points ? `${points} Points` : "0 Points"}</Button>}
-      {authUser && <Button>{progress}</Button>}
-      {!authUser && (
-        <Button to={ROUTES.SIGN_UP.path} component={Link}>
-          Sign up to earn points
-          <CheckCircleIcon style={{ marginLeft: "10px" }} />
-        </Button>
-      )}
-    </ButtonGroup>
-  </Fade>
+	<Fade
+		direction="left"
+		in={true}
+		mountOnEnter
+		timeout={{ enter: 800, exit: 400 }}
+		unmountOnExit
+	>
+		<ButtonGroup
+			aria-label="Points and progress"
+			variant="text"
+			color="default"
+			style={{ marginBottom: "48px" }}
+		>
+			{authUser && <Button> {points ? `${points} Points` : "0 Points"}</Button>}
+			{authUser && <Button>{progress}</Button>}
+			{!authUser && (
+				<Button to={ROUTES.SIGN_UP.path} component={Link}>
+					Sign up to earn points
+					<CheckCircleIcon style={{ marginLeft: "10px" }} />
+				</Button>
+			)}
+		</ButtonGroup>
+	</Fade>
 );
 
 export default MoProgressBar;
