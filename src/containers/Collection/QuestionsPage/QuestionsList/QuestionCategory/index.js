b8bba3792c21f01006902b4056b02c93e6067952
Mo Sharif <mo@mosh-media.com>
2020-04-15 23:32:21 -0700

    Udpate sttyling for QuestionsCategory

--- src/containers/Collection/QuestionsPage/QuestionsList/QuestionCategory/index.js
+++ src/containers/Collection/QuestionsPage/QuestionsList/QuestionCategory/index.js
@@ -1,17 +1,14 @@
 import React from "react";
 
 import MoTitle from "../../../../../components/shared/MoTitle";
-import Grid from "@material-ui/core/Grid";
 
 const QuestionCategory = ({ category, index }) => (
-  <Grid item xs={12} sm={12} md={12}>
-    <MoTitle
-      text={category}
-      fade={true}
-      margin={index === 0 ? "0px 0 36px" : "66px 0 62px"}
-      width="100%"
-    ></MoTitle>
-  </Grid>
+	<MoTitle
+		text={category}
+		fade={true}
+		margin={index === 0 ? "0px 0 12px" : "62px 0 12px"}
+		width="100%"
+	></MoTitle>
 );
 
 export default QuestionCategory;
