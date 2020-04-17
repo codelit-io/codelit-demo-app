521746f995eb2cfa74d3e71fc7448a108287ba3a
Mo Sharif <mo@mosh-media.com>
2020-04-16 23:28:47 -0700

    Update index.js

--- src/components/shared/MoPageHeader/index.js
+++ src/components/shared/MoPageHeader/index.js
@@ -6,16 +6,16 @@ import styles from "./styles";
 import Typography from "@material-ui/core/Typography";
 import withStyles from "@material-ui/core/styles/withStyles";
 
-const MoPageHeader = ({ classes, title }) => {
-  return (
-    <Fade in={title && true} timeout={{ enter: 800 }}>
-      <Typography variant="h2">
-        <Box fontWeight="fontWeightLight" className={classes.linkText}>
-          {title}
-        </Box>
-      </Typography>
-    </Fade>
-  );
+const MoPageHeader = ({ classes, title, children }) => {
+	return (
+		<Fade in={title || (children && true)} timeout={{ enter: 800 }}>
+			<Typography variant="h2">
+				<Box fontWeight="fontWeightLight" className={classes.title}>
+					{title} {children}
+				</Box>
+			</Typography>
+		</Fade>
+	);
 };
 
 export default withStyles(styles)(MoPageHeader);
