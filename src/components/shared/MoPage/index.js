c6b122b178e623354208ba01cb8c7b55be625dea
Mo Sharif <mo@mosh-media.com>
2020-04-16 23:28:49 -0700

    Update index.js

--- src/components/shared/MoPage/index.js
+++ src/components/shared/MoPage/index.js
@@ -6,7 +6,7 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import { compose } from "recompose";
 import MoPageHeader from "../MoPageHeader";
 import MoSpinner from "../MoSpinner";
-import MoPageSubtitle from "../MoPageSubtitle";
+import MoPageSubtitle from "../MoBodyText";
 
 const MoPage = ({ classes, children, isLoading, subtitle, title }) => {
   return isLoading ? (
