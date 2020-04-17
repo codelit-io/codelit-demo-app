e19cd1b13a492d5828e8f7e8cb6dd75627de1975
Mo Sharif <mo@mosh-media.com>
2020-04-16 21:02:45 -0700

    Code Clean up

--- src/components/PasswordChange/index.js
+++ src/components/PasswordChange/index.js
@@ -6,68 +6,68 @@ import Input from "@material-ui/core/Input";
 import withStyles from "@material-ui/core/styles/withStyles";
 import { withFirebase } from "../Firebase";
 
-const styles = theme => ({
-  button: {
-    margin: theme.spacing(1)
-  },
-  input: {
-    margin: theme.spacing(1)
-  }
+const styles = (theme) => ({
+	button: {
+		margin: theme.spacing(1),
+	},
+	input: {
+		margin: theme.spacing(1),
+	},
 });
 
 const INITIAL_STATE = {
-  passwordOne: "",
-  passwordTwo: "",
-  error: null
+	passwordOne: "",
+	passwordTwo: "",
+	error: null,
 };
 
 const PasswordChangeForm = ({ firebase, classes }) => {
-  const [state, setState] = useState({ ...INITIAL_STATE });
-  const { passwordOne, passwordTwo, error } = state;
-  const isInvalid = passwordOne !== passwordTwo || passwordOne === "";
+	const [state, setState] = useState({ ...INITIAL_STATE });
+	const { passwordOne, passwordTwo, error } = state;
+	const isInvalid = passwordOne !== passwordTwo || passwordOne === "";
 
-  const onSubmit = event => {
-    firebase
-      .passwordUpdate(passwordOne)
-      .then(() => {
-        setState({ ...INITIAL_STATE });
-      })
-      .catch(error => {
-        setState({ error });
-      });
-    event.preventDefault();
-  };
+	const onSubmit = (event) => {
+		firebase
+			.passwordUpdate(passwordOne)
+			.then(() => {
+				setState({ ...INITIAL_STATE });
+			})
+			.catch((error) => {
+				setState({ error });
+			});
+		event.preventDefault();
+	};
 
-  const onChange = event => {
-    setState({ ...state, [event.target.name]: event.target.value });
-  };
+	const onChange = (event) => {
+		setState({ ...state, [event.target.name]: event.target.value });
+	};
 
-  return (
-    <form onSubmit={onSubmit}>
-      <label> Password Change: </label>
-      <Input
-        className={classes.Input}
-        name="passwordOne"
-        value={passwordOne}
-        onChange={onChange}
-        type="password"
-        placeholder="New Password"
-      />
-      <Input
-        className={classes.Input}
-        name="passwordTwo"
-        value={passwordTwo}
-        onChange={onChange}
-        type="password"
-        placeholder="Confirm New Password"
-      />
-      <Button className={classes.button} disabled={isInvalid} type="submit">
-        Reset Your Password
-      </Button>
+	return (
+		<form onSubmit={onSubmit}>
+			<label> Password Change: </label>
+			<Input
+				className={classes.Input}
+				name="passwordOne"
+				value={passwordOne}
+				onChange={onChange}
+				type="password"
+				placeholder="New Password"
+			/>
+			<Input
+				className={classes.Input}
+				name="passwordTwo"
+				value={passwordTwo}
+				onChange={onChange}
+				type="password"
+				placeholder="Confirm New Password"
+			/>
+			<Button className={classes.button} disabled={isInvalid} type="submit">
+				Reset Your Password
+			</Button>
 
-      {error && <p> {error.message} </p>}
-    </form>
-  );
+			{error && <p> {error.message} </p>}
+		</form>
+	);
 };
 
 export default compose(withStyles(styles), withFirebase)(PasswordChangeForm);
