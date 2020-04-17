94ad57a56bfe0b7e55367ef4659276846f409d09
Mo Sharif <mo@mosh-media.com>
2020-04-16 21:02:35 -0700

    Remove unused components

--- src/components/shared/MoLink/index.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import MoLink from "./index.js";
-import renderer from "react-test-renderer";
-
-it("Should render MoLink", () => {
-  const tree = renderer.create(<MoLink />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
