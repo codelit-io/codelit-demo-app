46f6cefc5db6c065ba0de0cced64aa06c30fbae4
Mo Sharif <mo@mosh-media.com>
2020-04-17 21:11:08 -0700

    Lint ✅

--- src/components/shared/MoBodyText/index.js
+++ src/components/shared/MoBodyText/index.js
@@ -4,15 +4,15 @@ import Typography from "@material-ui/core/Typography";
 import Fade from "@material-ui/core/Fade";
 
 const MoBodyText = ({ subtitle, children }) => (
-	<Fade timeout={{ enter: 800 }} in={(subtitle || children) && true}>
-		<Typography
-			variant="body1"
-			style={{ paddingLeft: "4px", color: "#484848" }}
-		>
-			{subtitle}
-			{children}
-		</Typography>
-	</Fade>
+  <Fade timeout={{ enter: 800 }} in={(subtitle || children) && true}>
+    <Typography
+      variant="body1"
+      style={{ paddingLeft: "4px", color: "#484848" }}
+    >
+      {subtitle}
+      {children}
+    </Typography>
+  </Fade>
 );
 
 export default MoBodyText;
