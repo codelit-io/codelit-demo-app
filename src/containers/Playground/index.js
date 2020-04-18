eb1cb058c2ff1fbac6a7df11785969b2c969c12b
Mo Sharif <mo@mosh-media.com>
2020-04-18 14:11:47 -0700

    Lint

--- src/containers/Playground/index.js
+++ src/containers/Playground/index.js
@@ -3,39 +3,39 @@ import React, { useState } from "react";
 import CodeEditor from "../../components/CodeEditor";
 
 const Playground = () => {
-	const initialState = {
-		answer: "<button> I am a Button </button>",
-		language: "jsx",
-		element: "button",
-		id: "0",
-		isCorrect: false,
-		question: `() => {
+  const initialState = {
+    answer: "<button> I am a Button </button>",
+    language: "jsx",
+    element: "button",
+    id: "0",
+    isCorrect: false,
+    question: `() => {
       return (
         <section>
           <h1>I love React 💙</h1>
         </section>
       );
     };`,
-		isPlayground: true,
-	};
+    isPlayground: true,
+  };
 
-	const [question, setQuestion] = useState(initialState);
+  const [question, setQuestion] = useState(initialState);
 
-	const handleOnChange = (userAnswer) => {
-		if (userAnswer === "{}" || userAnswer === "") {
-			return;
-		}
-		setQuestion({ ...question, question: userAnswer });
-	};
+  const handleOnChange = (userAnswer) => {
+    if (userAnswer === "{}" || userAnswer === "") {
+      return;
+    }
+    setQuestion({ ...question, question: userAnswer });
+  };
 
-	return (
-		<CodeEditor
-			handleOnChange={(userAnswer) => handleOnChange(userAnswer)}
-			md={12}
-			sm={12}
-			question={question}
-		/>
-	);
+  return (
+    <CodeEditor
+      handleOnChange={(userAnswer) => handleOnChange(userAnswer)}
+      md={12}
+      sm={12}
+      question={question}
+    />
+  );
 };
 
 export default Playground;
