3b7794d676bc937d0f07749e3bfa6542ffba2c68
Mo Sharif <mo@mosh-media.com>
2020-04-20 20:50:59 -0700

    Lint ✅

--- src/containers/Collection/QuestionsPage/QuestionsList/index.js
+++ src/containers/Collection/QuestionsPage/QuestionsList/index.js
@@ -4,35 +4,35 @@ import CourseCategory from "./CourseCategory";
 import QuestionCard from "./QuestionCard";
 
 const QuestionsList = ({ url, questions, points }) => {
-	return questions.map((question, index) => {
-		/* isDisabled is configured based on points and question's id */
-		const isDisabled = url
-			? points
-				? points < Number(question.id) - 1 && Number(question.id) !== 1
-				: Number(question.id) !== 1
-			: false;
+  return questions.map((question, index) => {
+    /* isDisabled is configured based on points and question's id */
+    const isDisabled = url
+      ? points
+        ? points < Number(question.id) - 1 && Number(question.id) !== 1
+        : Number(question.id) !== 1
+      : false;
 
-		/* Configure url route for each item */
-		const configureUrl = isDisabled
-			? ""
-			: url
-			? `${url}/${question.id}`
-			: `courses/${question.doc}`;
+    /* Configure url route for each item */
+    const configureUrl = isDisabled
+      ? ""
+      : url
+      ? `${url}/${question.id}`
+      : `courses/${question.doc}`;
 
-		return (
-			<React.Fragment key={index}>
-				{question.category && (
-					<CourseCategory category={question.category} index={index} />
-				)}
-				<QuestionCard
-					isDisabled={isDisabled}
-					points={points}
-					question={question}
-					url={configureUrl}
-				/>
-			</React.Fragment>
-		);
-	});
+    return (
+      <React.Fragment key={index}>
+        {question.category && (
+          <CourseCategory category={question.category} index={index} />
+        )}
+        <QuestionCard
+          isDisabled={isDisabled}
+          points={points}
+          question={question}
+          url={configureUrl}
+        />
+      </React.Fragment>
+    );
+  });
 };
 
 export default QuestionsList;
