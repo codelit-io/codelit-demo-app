d45b27547f7df4873559b6f288be0ef0f6935f55
Mo Sharif <mo@mosh-media.com>
2020-04-20 23:42:29 -0700

    Update index.js

--- src/containers/Courses/index.js
+++ src/containers/Courses/index.js
@@ -1,17 +1,17 @@
 /**
- * Courses container contains a list of courses created by MoSkool and a list of courses created by the community
+ * A Container contains a list of courses created by MoSkool and a list of courses created by the community
  * @prop {Object} authUser - Passed from parent container and has everything about the logged in user
  * @prop {Object} firebase - Firebase class provides access to authUser and db - comes from withAuthentication hoc
  * @prop {Object} match - Contains information about how a <Route path> matched the URL - comes from withRouter and passed to withAuthentication hoc
  * @withAuthentication - HOC provides firebase and match props
- * @returns {<Collections/>} - returns Collections component which then the children fetch the correct data
+ * @returns {<CoursesContainer/>} - returns component which then the children fetch the correct data
  */
 
 import React, { lazy, Suspense } from "react";
 
 import { AuthUserContext, withAuthentication } from "../../components/Session";
 
-const Collections = lazy(() => import("./Collections"));
+const CoursesCollection = lazy(() => import("./CoursesCollection"));
 
 const collections = [
   { path: "courses", title: "Your Courses", isProgressBar: false },
@@ -22,7 +22,7 @@ const Courses = ({ firebase, match }) => (
     <AuthUserContext.Consumer>
       {(authUser) =>
         collections.map((collection, index) => (
-          <Collections
+          <CoursesCollection
             key={index}
             authUser={authUser}
             collection={collection}
