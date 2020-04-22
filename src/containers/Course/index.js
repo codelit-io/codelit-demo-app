54ccb9b2340ea062b05cd74be77c0b6a72be1f83
Mo Sharif <mo@mosh-media.com>
2020-04-21 19:02:01 -0700

    Update Course component

--- src/containers/Course/index.js
+++ src/containers/Course/index.js
@@ -3,45 +3,79 @@
  * @prop {Object} firebase - Firebase class provides access to authUser and db - comes from withAuthentication hoc
  * @prop {Object} match - Contains information about how a <Route path> matched the URL - comes from withRouter and passed to withAuthentication hoc
  * @withAuthentication - HOC provides firebase and match props
- * @returns {<Lessons/>} - returns Lessons component which renders the course with it's lessons
+ * @returns - returns a lesson list on the left column and course tracking info on the right column
  */
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { AuthUserContext, withAuthentication } from "../../components/Session";
-import Lessons from "../../components/Lessons";
+import Grid from "@material-ui/core/Grid";
+import LessonsList from "../../components/Lessons/LessonsList";
 import calculateProgress from "./calculateProgress";
+import MoPage from "../../components/shared/MoPage";
 import useCollectionDetails from "../../Hooks/useCollectionDetails";
 import useCollections from "../../Hooks/useCollections";
+import MoPageSubtitle from "../../components/shared/MoPageSubtitle";
+import MoProgressBar from "../../components/shared/MoProgressBar";
 
-const Course = ({ firebase, match }) => {
-  const courseDetails = useCollectionDetails(
-    "courses",
-    match.params.collection,
-    firebase
-  );
+const Course = ({ authUser, firebase, match }) => {
+	const courseDetails = useCollectionDetails(
+		"courses",
+		match.params.collection,
+		firebase
+	);
 
   const courses = useCollections(
-    "courses/" + match.params.collection + "/questions",
-    firebase
-  );
+		"courses/" + match.params.collection + "/questions",
+		firebase
+	);
 
-  return (
-    <AuthUserContext.Consumer>
-      {(authUser) => (
-        <Lessons
-          authUser={authUser}
-          isLoading={courseDetails.isLoading || courses.isLoading}
-          match={match}
-          questions={[...courses.data]}
-          collectionDetails={{
-            ...courseDetails.data,
-            isProgressBar: true,
-          }}
-          calculateProgress={calculateProgress}
-        />
-      )}
-    </AuthUserContext.Consumer>
-  );
+  const [points, setPoints] = useState(0);
+
+	useEffect(() => {
+		setPoints(authUser?.reports?.[match.params.collection]?.points);
+	}, [authUser, match]);
+
+	return (
+		<MoPage
+			title={courseDetails?.data?.title}
+			isLoading={courseDetails.isLoading || courses.isLoading}
+		>
+			{courses && (
+				<Grid container spacing={4} style={{ flexFlow: "wrap-reverse" }}>
+					<Grid item xs={12} sm={12} md={6} lg={6}>
+						<LessonsList
+							points={points}
+							questions={courses.data}
+							url={match?.params?.collection}
+						/>
+					</Grid>
+					<Grid item xs={12} sm={12} md={6} lg={6}>
+						<AuthUserContext.Consumer>
+							{(authUser) => (
+								<>
+									<MoPageSubtitle
+										text={"Your Progress"}
+										fade={true}
+										margin="0px 0 36px"
+										width="100%"
+									></MoPageSubtitle>
+									<MoProgressBar
+										authUser={authUser}
+										points={points}
+										progress={calculateProgress(
+											authUser,
+											points,
+											courses.data?.length
+										)}
+									/>
+								</>
+							)}
+						</AuthUserContext.Consumer>
+					</Grid>
+				</Grid>
+			)}
+		</MoPage>
+	);
 };
 export default withAuthentication(Course);
