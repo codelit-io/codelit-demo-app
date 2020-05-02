3e8b779f14684dc9a79d63c45e6088833482562b
Mo Sharif <mo@mosh-media.com>
2020-05-02 09:57:59 -0700

    Create snapshot test for Courses Component

---
+++ src/containers/Courses/index.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Courses from "./index.js";
+import ShallowRenderer from "react-test-renderer/shallow";
+
+const renderer = new ShallowRenderer();
+
+describe("Courses Component", () => {
+	test("should match the snapshot", () => {
+		renderer.render(<Courses />);
+		expect(renderer.getRenderOutput()).toMatchSnapshot();
+	});
+});
