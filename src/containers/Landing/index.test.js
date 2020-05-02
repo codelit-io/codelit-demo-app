5d19c3cdfdd5b0fb419bdb424956aafc6dd9ad95
Mo Sharif <mo@mosh-media.com>
2020-05-02 09:57:43 -0700

    Create snapshot test for LandingPage

--- src/containers/Landing/index.test.js
+++ src/containers/Landing/index.test.js
@@ -1,8 +1,13 @@
 import React from "react";
-import LandingPage from "./index.js";
-import renderer from "react-test-renderer";
 
-it("Landing Page", () => {
-  const tree = renderer.create(<LandingPage />).toJSON();
-  expect(tree).toMatchSnapshot();
+import LandingPage from "./index.js";
+import ShallowRenderer from "react-test-renderer/shallow";
+
+const renderer = new ShallowRenderer();
+
+describe("LandingPage Component", () => {
+	test("should match the snapshot", () => {
+		renderer.render(<LandingPage />);
+		expect(renderer.getRenderOutput()).toMatchSnapshot();
+	});
 });
