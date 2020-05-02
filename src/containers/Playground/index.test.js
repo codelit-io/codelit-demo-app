2cb493a2beacd10c77c8243a8f30ff9286df8722
Mo Sharif <mo@mosh-media.com>
2020-05-02 09:56:38 -0700

    Create snapshot test for Playground

---
+++ src/containers/Playground/index.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Playground from "./index.js";
+import ShallowRenderer from "react-test-renderer/shallow";
+
+const renderer = new ShallowRenderer();
+
+describe("Playground Component", () => {
+	test("should match the snapshot", () => {
+		renderer.render(<Playground />);
+		expect(renderer.getRenderOutput()).toMatchSnapshot();
+	});
+});
