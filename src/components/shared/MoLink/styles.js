cf7de05907d636d0c566fd4853f3ef0b2d04ca34
Mo Sharif <mo@mosh-media.com>
2020-05-01 23:32:59 -0700

    Add styling for MoLink

---
+++ src/components/shared/MoLink/styles.js
@@ -0,0 +1,21 @@
+const styles = (theme) => ({
+	link: {
+        fontFamily: theme.fontFamily,
+		textDecoration: "none",
+		padding: "0",
+		color: theme.blue?.medium,
+		fontSize: "1.1rem",
+		borderRadius: "3px",
+		textTransform: "initial",
+		fontWeight: "600",
+		"&:hover arrow": {
+			transform: "translateX(5px)",
+		},
+		"&.arrow": {
+			fontSize: "1rem",
+			paddingLeft: "4px",
+		},
+	},
+});
+
+export default styles;
