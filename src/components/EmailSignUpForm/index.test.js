82889459a10f0f810c7ed8626d3c423df04fc4dd
Mo Sharif <mo@mosh-media.com>
2020-05-02 09:58:26 -0700

    Create snapshot test for EmailSignUp Component

---
+++ src/components/EmailSignUpForm/index.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import EmailSignUpForm from "./index.js";
+import ShallowRenderer from "react-test-renderer/shallow";
+
+const renderer = new ShallowRenderer();
+
+describe("EmailSignUpForm Component", () => {
+	test("should match the snapshot", () => {
+		renderer.render(<EmailSignUpForm />);
+		expect(renderer.getRenderOutput()).toMatchSnapshot();
+	});
+});
