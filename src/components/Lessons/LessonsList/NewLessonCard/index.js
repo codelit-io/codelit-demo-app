462190f2683808944b7963d601877459fec8065f
Mo Sharif <mo@mosh-media.com>
2020-05-01 23:34:24 -0700

    Create Add New Lesson component

---
+++ src/components/Lessons/LessonsList/NewLessonCard/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import MoLink from "components/shared/MoLink";
+import styles from "./styles";
+import { withStyles } from "@material-ui/core";
+
+const NewLessonCard = ({ classes, url }) => (
+	<div className={classes.padding}>
+		<MoLink text="Add New Lesson" href={url}/>
+	</div>
+);
+export default withStyles(styles)(NewLessonCard);
