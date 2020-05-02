9a239f268f36dd6fca0397c702310af8822d7a5c
Mo Sharif <mo@mosh-media.com>
2020-05-02 09:58:42 -0700

    Create snapshot test for CodeEditor Component

---
+++ src/components/CodeEditor/index.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import CodeEditor from "./index.js";
+import ShallowRenderer from "react-test-renderer/shallow";
+
+const renderer = new ShallowRenderer();
+
+describe("CodeEditor Component", () => {
+	test("should match the snapshot", () => {
+		renderer.render(<CodeEditor />);
+		expect(renderer.getRenderOutput()).toMatchSnapshot();
+	});
+});
