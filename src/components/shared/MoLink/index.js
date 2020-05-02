abc29fa7b8d4b9e38d8469918cefdff0d50eb112
Mo Sharif <mo@mosh-media.com>
2020-05-01 23:33:32 -0700

    Create MoLink component

---
+++ src/components/shared/MoLink/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
+import { Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import styles from "./styles";
+import withStyles from "@material-ui/core/styles/withStyles";
+
+const MoLink = ({ classes, href, text }) => {
+	return (
+		<Button component={Link} to={href ? href : "#"} className={classes.link}>
+			{text}
+			<ArrowForwardIcon className="arrow" />
+		</Button>
+	);
+};
+
+export default withStyles(styles)(MoLink);
