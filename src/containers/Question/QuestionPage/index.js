84a9c6ef0b5ba7304568f2d2aeccfcfcbeb434c0
Mo Sharif <mo@mosh-media.com>
2020-05-02 23:36:23 -0700

    Update index.js

--- src/containers/Question/QuestionPage/index.js
+++ src/containers/Question/QuestionPage/index.js
@@ -4,24 +4,20 @@ import * as ROLES from "constants/roles";
 import MoPage from "components/shared/MoPage";
 import MoPageEdit from "components/shared/MoPageEdit";
 
-const QuestionPage = ({ authUser, children, isLoading, subtitle, title }) =>
-  authUser?.roles[ROLES.ADMIN] ? (
-    <MoPageEdit
-      subtitle={subtitle}
-      title={title}
-      isLoading={isLoading}
-      isCard={false}
-    >
-      {children}
-    </MoPageEdit>
-  ) : (
-    <MoPage
-      subtitle={subtitle}
-      title={title}
-      isLoading={isLoading}
-      isCard={false}
-    >
-      {children}
-    </MoPage>
-  );
+const QuestionPage = ({ authUser, isLoading, subtitle, title }) =>
+	authUser?.roles[ROLES.ADMIN] ? (
+		<MoPageEdit
+			subtitle={subtitle}
+			title={title}
+			isLoading={isLoading}
+			isCard={false}
+		/>
+	) : (
+		<MoPage
+			subtitle={subtitle}
+			title={title}
+			isLoading={isLoading}
+			isCard={false}
+		/>
+	);
 export default QuestionPage;
