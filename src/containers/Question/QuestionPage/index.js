a59c94afd4661b41cce5d07a4c80a46d142d705a
Mo Sharif <mo@mosh-media.com>
2020-05-02 23:24:03 -0700

    Create index.js

---
+++ src/containers/Question/QuestionPage/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import MoPage from "components/shared/MoPage";
+import MoPageEdit from "components/shared/MoPageEdit";
+
+const QuestionPage = ({ authUser, children, isLoading, subtitle, title }) =>
+	authUser ? (
+		<MoPageEdit
+			subtitle={subtitle}
+			title={title}
+			isLoading={isLoading}
+			isCard={false}
+		>
+			{children}
+		</MoPageEdit>
+	) : (
+		<MoPage
+			subtitle={subtitle}
+			title={title}
+			isLoading={isLoading}
+			isCard={false}
+		>
+			{children}
+		</MoPage>
+	);
+export default QuestionPage;
