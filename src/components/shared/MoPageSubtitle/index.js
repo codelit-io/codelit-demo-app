e90716d4e0f6f0580615d8a48c0371e3301a60d8
Mo Sharif <mo@mosh-media.com>
2020-05-03 19:26:27 -0700

    Lint ✨

--- src/components/shared/MoPageSubtitle/index.js
+++ src/components/shared/MoPageSubtitle/index.js
@@ -5,28 +5,28 @@ import Fade from "@material-ui/core/Fade";
 import Typography from "@material-ui/core/Typography";
 
 const MoPageSubtitle = ({ children, subtitle, margin, textAlign, width }) => {
-	const styles = {
-		text: {
-			color: "#383c40",
-			verticalAlign: "middle",
-			textDecoration: "none",
-			textAlign: textAlign ? textAlign : "",
-			margin: margin ? margin : "",
-		},
-		container: {
-			width: width ? width : "",
-		},
-	};
+  const styles = {
+    text: {
+      color: "#383c40",
+      verticalAlign: "middle",
+      textDecoration: "none",
+      textAlign: textAlign ? textAlign : "",
+      margin: margin ? margin : "",
+    },
+    container: {
+      width: width ? width : "",
+    },
+  };
 
-	return (
-			<Fade in={true} timeout={{ enter: 800 }}>
-				<Typography variant="h4" style={styles.container}>
-					<Box fontWeight="fontWeightLight" style={styles.text}>
-						{subtitle} {children}
-					</Box>
-				</Typography>
-			</Fade>
-	);
+  return (
+    <Fade in={true} timeout={{ enter: 800 }}>
+      <Typography variant="h4" style={styles.container}>
+        <Box fontWeight="fontWeightLight" style={styles.text}>
+          {subtitle} {children}
+        </Box>
+      </Typography>
+    </Fade>
+  );
 };
 
 export default MoPageSubtitle;
