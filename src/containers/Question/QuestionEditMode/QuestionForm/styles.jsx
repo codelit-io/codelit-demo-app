653c2208d4649f9056743cc529b94cab45c72d61
Mo Sharif <mo@mosh-media.com>
2020-05-09 15:52:41 -0700

    refactor: clean up old code

---
+++ src/containers/Question/QuestionEditMode/QuestionForm/styles.jsx
@@ -0,0 +1,15 @@
+const styles = (theme) => ({
+  section: {
+    maxHeight: "100%",
+    height: "100%",
+    width: "100%",
+    paddingTop: theme.padding?.lg,
+    paddingBottom: theme.padding?.lg,
+    [theme.breakpoints.up(400)]: {
+      paddingTop: theme.padding?.lg,
+      paddingBottom: theme.padding?.lg,
+    },
+  },
+});
+
+export default styles;
