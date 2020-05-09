eaf0f852a1efb8e169f49fa109201c5beb592606
Mo Sharif <mo@mosh-media.com>
2020-05-09 15:51:05 -0700

    feat: set default value for form input

--- src/components/shared/MoPageHeaderEdit/index.js
+++ src/components/shared/MoPageHeaderEdit/index.js
@@ -4,12 +4,15 @@ import Fade from "@material-ui/core/Fade";
 import styles from "./styles";
 import withStyles from "@material-ui/core/styles/withStyles";
 
-const MoPageHeaderEdit = ({ children, classes, title }) => {
+const MoPageHeaderEdit = ({ children, classes, title, register, name }) => {
   return (
     <Fade in={(title || children) && true} timeout={{ enter: 800 }}>
       <input
+        ref={register}
         className={`${classes.title} MuiTypography-h2`}
         placeholder={title || children}
+        defaultValue={title || children}
+        name={name}
       />
     </Fade>
   );
