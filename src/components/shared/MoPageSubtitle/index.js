2c250a76cf52bc9a67aec41e5f4fe58439d11973
Mo Sharif <mo@mosh-media.com>
2020-05-09 15:51:23 -0700

    refactor: clean up old code

--- src/components/shared/MoPageSubtitle/index.js
+++ src/components/shared/MoPageSubtitle/index.js
@@ -4,23 +4,40 @@ import Box from "@material-ui/core/Box";
 import Fade from "@material-ui/core/Fade";
 import Typography from "@material-ui/core/Typography";
 
-const MoPageSubtitle = ({ children, subtitle, margin, textAlign, width }) => {
+const MoPageSubtitle = ({
+  children,
+  handleOnClick,
+  isAdmin,
+  subtitle,
+  margin,
+  textAlign,
+  width,
+}) => {
   const styles = {
     text: {
       color: "#383c40",
       verticalAlign: "middle",
-      textDecoration: "none",
       textAlign: textAlign ? textAlign : "",
       margin: margin ? margin : "",
+      "&:hover": {
+        textDecoration: isAdmin ? "underline" : "none",
+      },
     },
     container: {
       width: width ? width : "",
+      "&:hover": {
+        textDecoration: isAdmin ? "underline" : "none",
+      },
     },
   };
 
   return (
     <Fade in={true} timeout={{ enter: 800 }}>
-      <Typography variant="h4" style={styles.container}>
+      <Typography
+        variant="h4"
+        style={styles.container}
+        onClick={() => handleOnClick()}
+      >
         <Box fontWeight="fontWeightLight" style={styles.text}>
           {subtitle} {children}
         </Box>
