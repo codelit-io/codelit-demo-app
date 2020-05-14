82af3f5eca3beecebc6559c9802b6cbeaafd013c
Mo Sharif <mo@mosh-media.com>
2020-05-14 10:18:34 -0700

    chore: create courses mock data

---
+++ src/mocks/course.js
@@ -0,0 +1,13 @@
+export const course = () => {
+	return {
+		id: "123",
+		question: [],
+		title: "Test Course",
+	};
+};
+
+export const courses = {
+	id: "123",
+	question: [],
+	title: "Test Course",
+};
