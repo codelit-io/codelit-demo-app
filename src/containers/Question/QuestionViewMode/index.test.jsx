6ee4d64fb8ec03873c51570d5058e1344e05ef67
Mo Sharif <mo@mosh-media.com>
2020-05-14 08:46:36 -0700

    chore: update file name to jsx

--- src/containers/Question/QuestionViewMode/index.test.jsx
+++ src/containers/Question/QuestionViewMode/index.test.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Question from "./index.js";
+import Question from "./index.jsxx";
 import ShallowRenderer from "react-test-renderer/shallow";
 
 const renderer = new ShallowRenderer();
