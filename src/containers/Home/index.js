398b49869f562fa92c0711494aebaba05dfedcba
Mo Sharif <mo@mosh-media.com>
2020-05-14 10:33:18 -0700

    chore: update library and shared components path

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { compose } from "recompose";
 
-import { withAuthorization, withEmailVerification } from "components/Session";
-import Messages from "components/Messages";
+import { withAuthorization, withEmailVerification } from "components/shared/Session";
+import Messages from "components/shared/Messages";
 
 const HomePage = () => <Messages />;
 
