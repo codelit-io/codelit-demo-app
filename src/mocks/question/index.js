1c1a4f129a167e1a30a8ce453e1e0e19f8758d0b
Mo Sharif <mo@mosh-media.com>
2020-05-14 08:46:00 -0700

    feat: add mock data for a question

---
+++ src/mocks/question/index.js
@@ -0,0 +1,17 @@
+const question = () => {
+	return {
+		answer: "<button> I am a Button </button>",
+		language: "jsx",
+		element: "button",
+		id: "0",
+		isCorrect: false,
+		question: `() => {
+          return (
+            <section>
+              <h1>I love React 💙</h1>
+            </section>
+          );
+        };`,
+		isPlayground: true,
+	};
+};
