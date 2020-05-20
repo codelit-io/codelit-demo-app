a6aa63f8542e0aab813749d37f20e189fbbf72b6
Mo Sharif <mo@mosh-media.com>
2020-05-19 19:51:16 -0700

    fix: clean up code

--- src/components/library/MoBrowserMockup/index.js
+++ src/components/library/MoBrowserMockup/index.js
@@ -1,3 +1,13 @@
+/**
+ * MoBrowserMockup
+ * Browser and Editor mocks are styled to look like a browser to frame the code editor and preview panels
+ *
+ * @param {String} fileType - html, jsx and so on are valid types. They indicate the file type in the browser mockup
+ * @param {Boolean} isEditor - decides whether to use the editor or browser mocks
+ * @param {Number} matchPercent - percentage of string matching between user answer and expected answer.
+ * @return {<Dialog></Dialog>}
+ */
+
 import React from "react";
 
 import MoLinearProgress from "components/library/MoLinearProgress";
@@ -9,30 +19,26 @@ const MoBrowserMockup = ({
   children,
   fileType,
   isEditor,
-  isBrowser,
   matchPercent,
-}) => (
-  <>
-    {isEditor && (
-      <div className={classes.browserMockup}>
-        <div className={classes.browserButtons}></div>
-        <div className={classes.browserFilename}>
-          <span className={classes.font}>{`index.${fileType}`}</span>
-        </div>
-        <div className={classes.editor}>{children}</div>
-        {matchPercent && <MoLinearProgress percent={matchPercent} />}
+}) => {
+  return isEditor ? (
+    <div className={classes.browserMockup}>
+      <div className={classes.browserButtons}></div>
+      <div className={classes.browserFilename}>
+        <span className={classes.font}>{`index.${fileType}`}</span>
       </div>
-    )}
-    {isBrowser && (
-      <div className={classes.browserMockup}>
-        <div className={classes.browserButtons}></div>
-        <div className={classes.browserButtons2}>
-          <span className={classes.font}>localhost:3000</span>
-        </div>
-        <div className={classes.preview}>{children}</div>
+      <div className={classes.editor}>{children}</div>
+      {matchPercent && <MoLinearProgress percent={matchPercent} />}
+    </div>
+  ) : (
+    <div className={classes.browserMockup}>
+      <div className={classes.browserButtons}></div>
+      <div className={classes.browserButtons2}>
+        <span className={classes.font}>localhost:3000</span>
       </div>
-    )}
-  </>
-);
+      <div className={classes.preview}>{children}</div>
+    </div>
+  );
+};
 
 export default withStyles(styles)(MoBrowserMockup);
