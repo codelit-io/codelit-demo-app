6339f353689ac994a6867391fccc8f1d7772cb97
Mo Sharif <mo@mosh-media.com>
2020-05-19 19:52:31 -0700

    fix: refactor styles for MoSkoolLogo

--- src/components/library/MoSkoolLogo/index.js
+++ src/components/library/MoSkoolLogo/index.js
@@ -4,15 +4,16 @@ import * as ROUTES from "constants/routes";
 import Box from "@material-ui/core/Box";
 import { Link } from "react-router-dom";
 import Typography from "@material-ui/core/Typography";
+import withStyles from "@material-ui/core/styles/withStyles";
+import { styles } from "./styles";
 
-const MoSkoolLogo = () => (
-  <Typography variant="h6" noWrap>
-    <Link
-      to={ROUTES.LANDING.path}
-      style={{ color: "#383c40", textDecoration: "none" }}
-    >
-      <Box fontWeight="fontWeightLight">Mo Skool</Box>
-    </Link>
-  </Typography>
-);
-export default MoSkoolLogo;
+const MoSkoolLogo = ({ classes }) => {
+  return (
+    <Typography variant="h6" noWrap>
+      <Link to={ROUTES.LANDING.path} className={classes.logo}>
+        <Box fontWeight="fontWeightLight">Mo Skool</Box>
+      </Link>
+    </Typography>
+  );
+};
+export default withStyles(styles)(MoSkoolLogo);
