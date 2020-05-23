7452e3290cf1b8061cfc4e4c5ba7579039abad40
Mo Sharif <mo@mosh-media.com>
2020-05-23 09:20:30 -0700

    feat: update admin page to use dynamic routes

--- src/containers/AdminPage/AdminNav/index.js
+++ src/containers/AdminPage/AdminNav/index.js
@@ -5,41 +5,37 @@ import AddIcon from "@material-ui/icons/Add";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 import PeopleIcon from "@material-ui/icons/People";
+import { Fade } from "@material-ui/core";
 
-const AdminNav = ({ history }) => {
+const AdminNav = ({ courses, history }) => {
   const [value, setValue] = useState(history.location.pathname);
 
   return (
-    <BottomNavigation
-      value={value}
-      style={{ height: "72px" }}
-      onChange={(event, newValue) => {
-        setValue(newValue);
-        history.push(newValue);
-      }}
-      showLabels
-    >
-      <BottomNavigationAction
-        label="Users"
-        value={ROUTES.ADMIN_USERS.path}
-        icon={<PeopleIcon />}
-      />
-      <BottomNavigationAction
-        label={ROUTES.ADMIN_COLLECTIONS_EASY.title}
-        value={ROUTES.ADMIN_COLLECTIONS_EASY.path}
-        icon={<AddIcon />}
-      />
-      <BottomNavigationAction
-        label={ROUTES.ADMIN_COLLECTIONS_MID.title}
-        value={ROUTES.ADMIN_COLLECTIONS_MID.path}
-        icon={<AddIcon />}
-      />
-      <BottomNavigationAction
-        label={ROUTES.ADMIN_COLLECTIONS_PRO.title}
-        value={ROUTES.ADMIN_COLLECTIONS_PRO.path}
-        icon={<AddIcon />}
-      />
-    </BottomNavigation>
+    <Fade in={courses.data.length > 1} timeout={{ enter: 800 }}>
+      <BottomNavigation
+        value={value}
+        style={{ height: "72px" }}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+          history.push(newValue);
+        }}
+        showLabels
+      >
+        <BottomNavigationAction
+          label="Users"
+          value={ROUTES.ADMIN_USERS.path}
+          icon={<PeopleIcon />}
+        />
+        {courses.data.map((course, i) => (
+          <BottomNavigationAction
+            key={i}
+            label={course.title}
+            value={ROUTES.ADMIN_COLLECTIONS.path + "/" + course.doc}
+            icon={<AddIcon />}
+          />
+        ))}
+      </BottomNavigation>
+    </Fade>
   );
 };
 
