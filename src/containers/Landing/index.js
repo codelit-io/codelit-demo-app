d0ae4497ed05c0c2992764548333dd0df27d7a0b
Mo Sharif <mo@mosh-media.com>
2020-06-04 11:12:16 -0700

    refactor: clean up landing page

--- src/containers/Landing/index.js
+++ src/containers/Landing/index.js
@@ -1,4 +1,4 @@
-import React, { useState, lazy, Suspense } from "react";
+import React from "react";
 
 import * as ROUTES from "constants/routes";
 
@@ -8,19 +8,14 @@ import Grid from "@material-ui/core/Grid";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import MoBrowserMockup from "components/library/MoBrowserMockup";
 import MoLinkButton from "components/library/MoLinkButton";
 import MoPageHeader from "components/library/MoPageHeader";
 import MoPageSubtitle from "components/library/MoPageSubtitle";
 import styles from "./styles";
-import Typography from "@material-ui/core/Typography";
 import withStyles from "@material-ui/core/styles/withStyles";
-
-const Footer = lazy(() => import("components/shared/Footer"));
-const Typist = lazy(() => import("react-typist"));
+import Footer from "components/shared/Footer";
 
 const LandingPage = ({ classes }) => {
-  const [isPreview, setIsPreview] = useState(false);
   return (
     <>
       <Grid container spacing={4} className={classes.container}>
@@ -40,81 +35,16 @@ const LandingPage = ({ classes }) => {
         </Grid>
 
         <Grid item sm={12} md={6} xs={12}>
-          <Grid
-            item
-            md={12}
-            sm={12}
-            xs={12}
-            style={{ width: "100%", marginBottom: "50px" }}
-          >
-            <Fade in={true} mountOnEnter timeout={{ enter: 800 }} unmountOnExit>
-              <div>
-                <Typography variant="body1" gutterBottom>
-                  Write your code
-                </Typography>
-                <MoBrowserMockup fileType="jsx" isEditor={true}>
-                  <pre className={classes.editorFont}>
-                    <Suspense>
-                      <Typist
-                        avgTypingDelay={60}
-                        stdTypingDelay={30}
-                        startDelay={100}
-                        cursor={{
-                          show: true,
-                          blink: true,
-                          element: "|",
-                          hideWhenDone: true,
-                        }}
-                        onTypingDone={() => setIsPreview(true)}
-                      >
-                        <span style={{ color: "#4d4d4c" }}>() =></span>{" "}
-                        <span style={{ color: "#c82829" }}>&lt;h1&gt;</span>
-                        <span style={{ color: "#4d4d4c" }}>Hello React</span>
-                        <span
-                          aria-label="img"
-                          role="img"
-                          className={classes.emoji}
-                        >
-                          🤩
-                        </span>
-                        <span style={{ color: "#c82829" }}>&lt;/h1&gt;</span>
-                      </Typist>
-                    </Suspense>
-                  </pre>
-                </MoBrowserMockup>
-              </div>
-            </Fade>
-          </Grid>
-          <Grid item md={12} sm={12} xs={12}>
-            <Fade in={true} mountOnEnter timeout={{ enter: 800 }} unmountOnExit>
-              <div>
-                <Typography variant="body1" gutterBottom>
-                  Preview in real-time
-                </Typography>
-                <MoBrowserMockup isEditor={false}>
-                  <Fade
-                    in={isPreview}
-                    mountOnEnter
-                    timeout={{ enter: 600 }}
-                    unmountOnExit
-                  >
-                    <h1 style={{ height: "46px" }}>
-                      Hello React
-                      <span
-                        aria-label="img"
-                        role="img"
-                        className={classes.emoji}
-                      >
-                        🤩
-                      </span>
-                    </h1>
-                  </Fade>
-                </MoBrowserMockup>
-              </div>
-            </Fade>
-          </Grid>
+          <video autoPlay loop style={{ width: "100%" }}>
+            <source
+              src="https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/landing%2Fmoskool_landing.mp4?alt=media&token=c36b719c-99ee-4db2-9fce-da212153b4a5"
+              type="video/mp4"
+            />
+            Your browser does not support the video tag.
+          </video>
         </Grid>
       </Grid>
+
       <Grid container spacing={4} className={classes.container}>
         <Grid item sm={12} md={6} xs={12}>
           <MoPageHeader>Learn all about React</MoPageHeader>
