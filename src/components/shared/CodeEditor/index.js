97c402d3bbcc29af0a06766a2764b88a70d5a054
Mo Sharif <mo@mosh-media.com>
2020-06-04 11:10:36 -0700

    refactor: update grid spacing

--- src/components/shared/CodeEditor/index.js
+++ src/components/shared/CodeEditor/index.js
@@ -24,7 +24,7 @@ const CodeEditor = ({ handleOnChange, md, sm, matchPercent, question }) => {
   }, [question]);
 
   return (
-    <Grid container>
+    <Grid container spacing={2}>
       <Suspense>
         <LiveProviderCore
           question={state}
