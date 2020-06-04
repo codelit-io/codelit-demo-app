eef6ca614cb0745f87e998938213c418dc3094d2
Mo Sharif <mo@mosh-media.com>
2020-06-04 10:03:30 -0700

    refactor: update userAnswer prop

--- src/containers/Question/QuestionViewMode/index.jsx
+++ src/containers/Question/QuestionViewMode/index.jsx
@@ -67,7 +67,7 @@ const QuestionViewMode = ({ authUser, firebase, history, match }) => {
 
 	/* Checks if user code matches Pre made answer */
 	const handleOnChange = useCallback(
-		(userAnswer) => {
+		({ userAnswer }) => {
 			if (userAnswer === "{}" || userAnswer === "") {
 				return;
 			}
@@ -164,8 +164,6 @@ const QuestionViewMode = ({ authUser, firebase, history, match }) => {
 			{question && (
 				<CodeEditor
 					handleOnChange={(userAnswer) => handleOnChange(userAnswer)}
-					sm={6}
-					md={6}
 					question={question}
 					matchPercent={matchPercent}
 				/>
