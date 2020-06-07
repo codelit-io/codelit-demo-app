222a14b486332b1f172d50581aba54cc75309e4e
Mo Sharif <mo@mosh-media.com>
2020-06-06 22:22:34 -0700

    feat: update component classes

--- src/components/library/MoButton/styles.js
+++ src/components/library/MoButton/styles.js
@@ -1,23 +1,26 @@
 const styles = (theme) => ({
-  link: {
-    backgroundColor: theme.blue?.medium,
-    textDecoration: "none",
-    padding: "12px 22px",
-    color: theme.white?.medium,
-    fontSize: "1.1rem",
-    borderRadius: "3px",
-    textTransform: "initial",
-    fontWeight: "600",
-    "&:hover": {
-      background: theme.grey?.dark,
-    },
-    "&:hover arrow": {
-      transform: "translateX(5px)",
-    },
-    arrow: {
-      fontSize: "1.0rem",
-    },
-  },
+	link: {
+		transition: "transform .35s",
+		backgroundColor: theme.blue?.medium,
+		textDecoration: "none",
+		padding: "12px 22px",
+		color: theme.white?.medium,
+		fontSize: "1.1rem",
+		borderRadius: "3px",
+		textTransform: "initial",
+		fontWeight: "600",
+		"&:hover": {
+			background: theme.grey?.dark,
+		},
+		"&:hover .arrow": {
+			transform: "translateX(5px)",
+		},
+		icon: {
+			transition: "transform .35s",
+			paddingLeft: "4px",
+			paddingTop: "2px",
+		},
+	},
 });
 
 export default styles;
