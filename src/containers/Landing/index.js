f8ab1e225e0c069cee0bf94ebc6bbb51e536b57b
Mo Sharif <mo@mosh-media.com>
2020-06-06 22:23:19 -0700

    feat: add transition and transform effects to buttons

--- src/containers/Landing/index.js
+++ src/containers/Landing/index.js
@@ -9,6 +9,7 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import MoLinkButton from "components/library/MoLinkButton";
+import MoLink from "components/library/MoLink";
 import MoPageHeader from "components/library/MoPageHeader";
 import MoPageSubtitle from "components/library/MoPageSubtitle";
 import styles from "./styles";
@@ -16,93 +17,103 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import Footer from "components/shared/Footer";
 
 const LandingPage = ({ classes }) => {
-  return (
-    <>
-      <Grid container spacing={4} className={classes.container}>
-        <Grid item sm={12} md={6} xs={12}>
-          <Fade in={true} mountOnEnter timeout={{ enter: 800 }} unmountOnExit>
-            <div className={classes.responsiveGrid}>
-              <MoPageHeader>Master React without the fees</MoPageHeader>
-              <MoPageSubtitle margin="36px 0 36px">
-                Introducing a Modern learning experience for everyone
-              </MoPageSubtitle>
-              <MoLinkButton
-                text="Get started"
-                href={ROUTES.EASY_COLLECTIONS.path}
-              />
-            </div>
-          </Fade>
-        </Grid>
+	return (
+		<>
+			<Grid container spacing={4} className={classes.container}>
+				<Grid item sm={12} md={6} xs={12}>
+					<Fade in={true} mountOnEnter timeout={{ enter: 800 }} unmountOnExit>
+						<div className={classes.responsiveGrid}>
+							<MoPageHeader>Master React without the fees</MoPageHeader>
+							<MoPageSubtitle margin="36px 0 36px">
+								Introducing a Modern learning experience for everyone
+							</MoPageSubtitle>
+							<MoLinkButton
+								text="Get started"
+								href={ROUTES.EASY_COLLECTIONS.path}
+							/>
+						</div>
+					</Fade>
+				</Grid>
 
-        <Grid item sm={12} md={6} xs={12}>
-          <video autoPlay loop style={{ width: "100%" }}>
-            <source
-              src="https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/landing%2Fmoskool_landing.mp4?alt=media&token=c36b719c-99ee-4db2-9fce-da212153b4a5"
-              type="video/mp4"
-            />
-            Your browser does not support the video tag.
-          </video>
-        </Grid>
-      </Grid>
+				<Grid item sm={12} md={6} xs={12}>
+					<video autoPlay loop style={{ width: "100%" }}>
+						<source
+							src="https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/landing%2Fmoskool_landing.mp4?alt=media&token=c36b719c-99ee-4db2-9fce-da212153b4a5"
+							type="video/mp4"
+						/>
+						Your browser does not support the video tag.
+					</video>
+				</Grid>
+			</Grid>
 
-      <Grid container spacing={4} className={classes.container}>
-        <Grid item sm={12} md={6} xs={12}>
-          <MoPageHeader>Learn all about React</MoPageHeader>
-          <ListItem>
-            <ListItemAvatar className={classes.checkMark}>
-              <CheckIcon />
-            </ListItemAvatar>
-            <ListItemText primary="Basic HTML and usage in React" />
-          </ListItem>
-          <ListItem>
-            <ListItemAvatar className={classes.checkMark}>
-              <CheckIcon />
-            </ListItemAvatar>
-            <ListItemText primary="Styling components and elements in React" />
-          </ListItem>
-          <ListItem>
-            <ListItemAvatar className={classes.checkMark}>
-              <CheckIcon />
-            </ListItemAvatar>
-            <ListItemText primary="JavaScript functionality in React" />
-          </ListItem>
-          <ListItem>
-            <ListItemAvatar className={classes.checkMark}>
-              <CheckIcon />
-            </ListItemAvatar>
-            <ListItemText primary="React Hooks, Context API, and Redux" />
-          </ListItem>
-          <ListItem>
-            <ListItemAvatar className={classes.checkMark}>
-              <CheckIcon />
-            </ListItemAvatar>
-            <ListItemText primary="Best UI/UX practices" />
-          </ListItem>
-        </Grid>
-
-        <Grid item sm={12} md={6} xs={12}>
-          <img
-            alt="Congrats"
-            className={classes.img}
-            src="https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/landing%2Fcongrats.png?alt=media&token=6e591903-a43c-413a-b861-52982750e530"
-          />
-        </Grid>
-      </Grid>
-      <Grid container spacing={4} className={classes.container}>
-        <Grid item sm={12} md={6} xs={12}>
-          <MoPageHeader>Try the playground</MoPageHeader>
-          <MoPageSubtitle margin="36px 0 36px">
-            Write code in JSX and watch it render your code magically.
-          </MoPageSubtitle>
-          <MoLinkButton
-            text="Try the playground"
-            href={ROUTES.PLAYGROUND.path}
-          />
-        </Grid>
-      </Grid>
-      <Footer />
-    </>
-  );
+			<Grid container spacing={4} className={classes.container}>
+				<Grid item sm={12} md={6} xs={12}>
+					<MoPageHeader>Learn all about React</MoPageHeader>
+					<ListItem>
+						<ListItemAvatar className={classes.checkMark}>
+							<CheckIcon />
+						</ListItemAvatar>
+						<ListItemText primary="Basic HTML and usage in React" />
+					</ListItem>
+					<ListItem>
+						<ListItemAvatar className={classes.checkMark}>
+							<CheckIcon />
+						</ListItemAvatar>
+						<ListItemText primary="Styling components and elements in React" />
+					</ListItem>
+					<ListItem>
+						<ListItemAvatar className={classes.checkMark}>
+							<CheckIcon />
+						</ListItemAvatar>
+						<ListItemText primary="JavaScript functionality in React" />
+					</ListItem>
+					<ListItem>
+						<ListItemAvatar className={classes.checkMark}>
+							<CheckIcon />
+						</ListItemAvatar>
+						<ListItemText primary="React Hooks, Context API, and Redux" />
+					</ListItem>
+					<ListItem>
+						<ListItemAvatar className={classes.checkMark}>
+							<CheckIcon />
+						</ListItemAvatar>
+						<ListItemText primary="Best UI/UX practices" />
+					</ListItem>
+					<ListItem>
+						<ListItemAvatar className={classes.checkMark}>
+							<CheckIcon />
+						</ListItemAvatar>
+						<MoLink
+							text="View All Courses"
+							href={ROUTES.COLLECTIONS.path}
+						/>
+					</ListItem>
+				</Grid>
+				<Grid item sm={12} md={6} xs={12}>
+					<video autoPlay loop style={{ width: "100%" }}>
+						<source
+							src="https://firebasestorage.googleapis.com/v0/b/tool-builder.appspot.com/o/landing%2Fmoskool_courses.mp4?alt=media&token=36c246b3-8004-4de5-8005-35e3474472bc"
+							type="video/mp4"
+						/>
+						Your browser does not support the video tag.
+					</video>
+				</Grid>
+			</Grid>
+			<Grid container spacing={4} className={classes.container}>
+				<Grid item sm={12} md={6} xs={12}>
+					<MoPageHeader>Try the playground</MoPageHeader>
+					<MoPageSubtitle margin="36px 0 36px">
+						Write code in JSX and watch it render your code magically.
+					</MoPageSubtitle>
+					<MoLinkButton
+						text="Try the playground"
+						href={ROUTES.PLAYGROUND.path}
+					/>
+				</Grid>
+			</Grid>
+			<Footer />
+		</>
+	);
 };
 
 export default withStyles(styles)(LandingPage);
