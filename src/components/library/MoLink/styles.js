bcd25bab107aa0b8518958c403d9f260afe4ba0f
Mo Sharif <mo@mosh-media.com>
2020-06-06 22:22:52 -0700

    feat: update component styling

--- src/components/library/MoLink/styles.js
+++ src/components/library/MoLink/styles.js
@@ -1,21 +1,22 @@
 const styles = (theme) => ({
-  link: {
-    fontFamily: theme.fontFamily,
-    textDecoration: "none",
-    padding: "0",
-    color: theme.blue?.medium,
-    fontSize: "1.1rem",
-    borderRadius: "3px",
-    textTransform: "initial",
-    fontWeight: "600",
-    "&:hover arrow": {
-      transform: "translateX(5px)",
-    },
-    "&.arrow": {
-      fontSize: "1rem",
-      paddingLeft: "4px",
-    },
-  },
+	link: {
+		fontFamily: theme.fontFamily,
+		textDecoration: "none",
+		padding: "0",
+		color: theme.blue?.medium,
+		fontSize: "1.1rem",
+		borderRadius: "3px",
+		textTransform: "initial",
+		fontWeight: "600",
+		"&:hover .arrow": {
+			transform: "translateX(5px)",
+		},
+	},
+	icon: {
+		transition: "transform .35s",
+		paddingLeft: "4px",
+		paddingTop: "2px",
+	},
 });
 
 export default styles;
