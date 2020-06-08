6f3891cf4005e01c8cf451f53d9be9a9b2a33634
Mo Sharif <mo@mosh-media.com>
2020-06-07 20:13:07 -0700

    feat: remove unused code

--- src/components/library/MoBodyText/index.js
+++ src/components/library/MoBodyText/index.js
@@ -2,17 +2,28 @@ import React from "react";
 
 import Typography from "@material-ui/core/Typography";
 import Fade from "@material-ui/core/Fade";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import createStyles from "@material-ui/core/styles/createStyles";
 
-const MoBodyText = ({ subtitle, children }) => (
-  <Fade timeout={{ enter: 800 }} in={(subtitle || children) && true}>
-    <Typography
-      variant="body1"
-      style={{ paddingLeft: "4px", color: "#484848" }}
-    >
-      {subtitle}
-      {children}
-    </Typography>
-  </Fade>
-);
+const MoBodyText = ({ subtitle, children }) => {
+	const useStyles = makeStyles((theme) =>
+		createStyles({
+			text: {
+				paddingLeft: theme.space.sm,
+				color: theme.grey.light,
+			},
+		})
+	);
+
+	const classes = useStyles();
+	return (
+		<Fade timeout={{ enter: 800 }} in={(subtitle || children) && true}>
+			<Typography variant="body1" className={classes.text}>
+				{subtitle}
+				{children}
+			</Typography>
+		</Fade>
+	);
+};
 
 export default MoBodyText;
