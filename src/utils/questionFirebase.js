888de54a8e42e1540e917da6a1f2de3f2fef556e
Mo Sharif <mo@mosh-media.com>
2020-06-13 00:40:57 -0700

    refactor: add checks before adding questions

--- src/utils/questionFirebase.js
+++ src/utils/questionFirebase.js
@@ -4,56 +4,56 @@ import * as ROUTES from "constants/routes";
  * createQuestion(authUser, event, firebase, match)
  */
 export const createQuestion = (authUser, event, firebase, match) => {
-  if (event?.label) {
-    firebase.createQuestionById(match.params.collection, {
-      ...event,
-      id: Number(event.id),
-      userId: authUser.uid,
-      createdAt: firebase.fieldValue.serverTimestamp(),
-      question: escapeCode(event.question),
-    });
-  }
+	if (!event?.label) {
+		return;
+	}
+	firebase.createQuestionById(match.params.collection, {
+		...event,
+		id: Number(event.id),
+		userId: authUser.uid,
+		createdAt: firebase.fieldValue.serverTimestamp(),
+		question: event?.question ? escapeCode(event.question) : "",
+	});
 };
 
 /* Edit Question
  * editQuestion(event, firebase, match)
  */
 export const editQuestion = (event, firebase, match) => {
-  console.log(event.question);
-  if (!match.params.collection) {
-    return;
-  }
-  firebase
-    .doc("courses/" + match.params.collection + "/questions", event.uid)
-    .update({
-      ...event,
-      id: Number(event.id),
-      editedAt: firebase.fieldValue.serverTimestamp(),
-      question: event.question ? escapeCode(event.question) : "",
-    });
+	if (!match.params.collection) {
+		return;
+	}
+	firebase
+		.doc("courses/" + match.params.collection + "/questions", event.uid)
+		.update({
+			...event,
+			id: Number(event.id),
+			editedAt: firebase.fieldValue.serverTimestamp(),
+			question: event?.question ? escapeCode(event.question) : "",
+		});
 };
 
 /* Remove Question
  * removeQuestion(id, firebase, match)
  */
 export const removeQuestion = (id, firebase, match) => {
-  firebase
-    .doc("courses/" + match.params.collection + "/questions", id)
-    .delete();
+	firebase
+		.doc("courses/" + match.params.collection + "/questions", id)
+		.delete();
 };
 
 /* Remove Question
  * rowClick(id, history, match)
  */
 export const rowClick = (id, history, match) => {
-  history.push(
-    ROUTES.COLLECTIONS.path + "/" + match.params.collection + "/" + id
-  );
+	history.push(
+		ROUTES.COLLECTIONS.path + "/" + match.params.collection + "/" + id
+	);
 };
 
 export const escapeCode = (code) => {
-  if (!code) {
-    return;
-  }
-  return JSON.stringify(code, null, 2);
+	if (!code) {
+		return;
+	}
+	return JSON.stringify(code, null, 2);
 };
