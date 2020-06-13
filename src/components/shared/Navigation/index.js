993474dc04b696aa2c712eaed9230379044eebab
Mo Sharif <mo@mosh-media.com>
2020-06-12 19:52:35 -0700

    feat: update new course dialog

--- src/components/shared/Navigation/index.js
+++ src/components/shared/Navigation/index.js
@@ -4,7 +4,7 @@ import * as ROLES from "constants/roles";
 import * as ROUTES from "constants/routes";
 import AppsIcon from "@material-ui/icons/Apps";
 import { AuthUserContext } from "../Session";
-import AddCourse from "./AddCourse";
+import NewCourseDialog from "./NewCourseDialog";
 import AppBar from "@material-ui/core/AppBar";
 import Button from "@material-ui/core/Button";
 import { compose } from "recompose";
@@ -16,57 +16,55 @@ import styles from "./styles";
 import Toolbar from "@material-ui/core/Toolbar";
 import withStyles from "@material-ui/core/styles/withStyles";
 import { withRouter } from "react-router-dom";
-const Navigation = ({ classes, firebase, history, match }) => {
-  return (
-    <header className={classes.root}>
-      <AppBar position="static" color="default" className={classes.appBar}>
-        <Toolbar className={classes.toolbar}>
-          <Grid container className={classes.alignCenter}>
-            <Grid item xs={6} sm={6} md={6} lg={6}>
-              <MoSkoolLogo />
-            </Grid>
-            <Grid
-              item
-              xs={6}
-              sm={6}
-              md={6}
-              lg={6}
-              style={{ textAlign: "right" }}
-            >
-              <Button
-                color="primary"
-                className={classes.button}
-                aria-label="Courses"
-                aria-haspopup="true"
-                startIcon={<AppsIcon />}
-                component={Link}
-                to={ROUTES.COLLECTIONS.path}
-              >
-                Courses
-              </Button>
-              <AuthUserContext.Consumer>
-                {(authUser) => (
-                  <>
-                    {authUser &&
-                      authUser.roles &&
-                      !!authUser.roles[ROLES.ADMIN] && (
-                        <AddCourse
-                          authUser={authUser}
-                          firebase={firebase}
-                          history={history}
-                          match={match}
-                        />
-                      )}
-                    <MoAvatar authUser={authUser} firebase={firebase} />
-                  </>
-                )}
-              </AuthUserContext.Consumer>
-            </Grid>
-          </Grid>
-        </Toolbar>
-      </AppBar>
-    </header>
-  );
+const Navigation = ({ classes, firebase, history }) => {
+	return (
+		<header className={classes.root}>
+			<AppBar position="static" color="default" className={classes.appBar}>
+				<Toolbar className={classes.toolbar}>
+					<Grid container className={classes.alignCenter}>
+						<Grid item xs={6} sm={6} md={6} lg={6}>
+							<MoSkoolLogo />
+						</Grid>
+						<Grid
+							item
+							xs={6}
+							sm={6}
+							md={6}
+							lg={6}
+							style={{ textAlign: "right" }}
+						>
+							<Button
+								color="primary"
+								className={classes.button}
+								aria-label="Courses"
+								aria-haspopup="true"
+								startIcon={<AppsIcon />}
+								component={Link}
+								to={ROUTES.COLLECTIONS.path}
+							>
+								Courses
+							</Button>
+							<AuthUserContext.Consumer>
+								{(authUser) => (
+									<>
+										{history.location.pathname === ROUTES.COLLECTIONS.path &&
+											!!authUser?.roles[ROLES?.ADMIN] && (
+												<NewCourseDialog
+													authUser={authUser}
+													firebase={firebase}
+													history={history}
+												/>
+											)}
+										<MoAvatar authUser={authUser} firebase={firebase} />
+									</>
+								)}
+							</AuthUserContext.Consumer>
+						</Grid>
+					</Grid>
+				</Toolbar>
+			</AppBar>
+		</header>
+	);
 };
 
 export default compose(withStyles(styles), withRouter)(Navigation);
