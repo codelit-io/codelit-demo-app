429155db0337fbbf1849470b2d1c9676e677c317
Mo Sharif <mo@mosh-media.com>
2020-06-14 23:09:40 -0700

    chore: add comments

--- src/utils/retryLazyImports.js
+++ src/utils/retryLazyImports.js
@@ -1,4 +1,22 @@
-/*  */
+/**
+ *
+ *
+ * @author MoSkool
+ * @version 1.0.0
+ * @visibleName retry
+ *
+ *
+ * retry is used with React lazy imports to retry imports if an error
+ * happen with an import
+ *
+ * deploying a new code to prod causes cashing issues, and viewers
+ * won't be served new code. Instead crashing their browser for not
+ * finding the old code.
+ *
+ * @param {CallableFunction} fn - function that  needs to be recalled after an error
+ * @param {Number} retriesLeft - number of attempts to call the function
+ * @param {Number} interval - interval between function calls
+ */
 
 export const retry = (fn, retriesLeft = 5, interval = 1000) => {
 	return new Promise((resolve, reject) => {
