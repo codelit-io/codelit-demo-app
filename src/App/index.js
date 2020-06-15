d2f4422ee9b986ffb5a5021523f1935acd60fbbd
Mo Sharif <mo@mosh-media.com>
2020-06-14 23:01:54 -0700

    chore: add retry and comments

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,10 @@
 /**
- * App
+ *
+ *
+ * @author MoSkool
+ * @version 1.0.0
+ * @visibleName App Component 🥇
+ *
  *
  * Entry component to the app and does the following
  *
@@ -19,64 +24,71 @@ import MoSpinner from "components/library/MoSpinner";
 import theme from "./theme";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { withAuthentication } from "components/shared/Session";
+import { retry } from "utils/retryLazyImports";
 
-const Navigation = lazy(() => import("components/shared/Navigation"));
-const Account = lazy(() => import("containers/Account"));
-const AdminPage = lazy(() => import("containers/AdminPage"));
-const Container = lazy(() => import("@material-ui/core/Container"));
-const Home = lazy(() => import("containers/Home"));
-const LandingPage = lazy(() => import("containers/Landing"));
-const NotFound = lazy(() => import("components/shared/NotFound"));
-const PasswordForgot = lazy(() => import("components/shared/PasswordForgot"));
-const Playground = lazy(() => import("containers/Playground"));
-const Course = lazy(() => import("containers/Course"));
-const Courses = lazy(() => import("containers/Courses"));
-const Question = lazy(() => import("containers/Question"));
-const SignUp = lazy(() => import("containers/SignUp"));
-const SignIn = lazy(() => import("containers/SignIn"));
+const Navigation = lazy(() =>
+	retry(() => import("components/shared/Navigation"))
+);
+const Account = lazy(() => retry(() => import("containers/Account")));
+const AdminPage = lazy(() => retry(() => import("containers/AdminPage")));
+const Container = lazy(() =>
+	retry(() => import("@material-ui/core/Container"))
+);
+const Home = lazy(() => retry(() => import("containers/Home")));
+const LandingPage = lazy(() => retry(() => import("containers/Landing")));
+const NotFound = lazy(() => retry(() => import("components/shared/NotFound")));
+const PasswordForgot = lazy(() =>
+	retry(() => import("components/shared/PasswordForgot"))
+);
+const Playground = lazy(() => retry(() => import("containers/Playground")));
+const Course = lazy(() => retry(() => import("containers/Course")));
+const Courses = lazy(() => retry(() => import("containers/Courses")));
+const Question = lazy(() => retry(() => import("containers/Question")));
+const SignUp = lazy(() => retry(() => import("containers/SignUp")));
+const SignIn = lazy(() => retry(() => import("containers/SignIn")));
 
 const App = (props) => (
-  <ThemeProvider theme={theme}>
-    <Router>
-      <Suspense fallback={<MoSpinner isLoading={true} color="primary" />}>
-        <Container maxWidth="lg">
-          <Navigation {...props} />
-          <Switch>
-            <Route path={ROUTES.ADMIN.path} component={AdminPage} />
-            <Route path={ROUTES.ACCOUNT.path} component={Account} />
-            <Route path={ROUTES.HOME.path} component={Home} />
-            <Route exact path={ROUTES.LANDING.path} component={LandingPage} />
-            <Route exact path={ROUTES.NOT_FOUND.path} component={NotFound} />
+	<ThemeProvider theme={theme}>
+		<Router>
+			<Suspense fallback={<MoSpinner isLoading={true} color="primary" />}>
+				<Container maxWidth="lg">
+					<Navigation {...props} />
+					<Switch>
+						<Route path={ROUTES.ADMIN.path} component={AdminPage} />
+						<Route path={ROUTES.ACCOUNT.path} component={Account} />
+						<Route path={ROUTES.HOME.path} component={Home} />
+						<Route exact path={ROUTES.LANDING.path} component={LandingPage} />
+						<Route exact path={ROUTES.NOT_FOUND.path} component={NotFound} />
 
-            <Route path={ROUTES.SIGN_IN.path} component={SignIn} />
-            <Route path={ROUTES.SIGN_UP.path} component={SignUp} />
-            <Route
-              path={ROUTES.PASSWORD_FORGET.path}
-              component={PasswordForgot}
-            />
-            <Route path={ROUTES.PLAYGROUND.path} component={Playground} />
-            <Route exact path={ROUTES.COLLECTIONS.path} component={Courses} />
-            <Route
-              exact
-              path={ROUTES.COLLECTIONS.path + "/:collection"}
-              component={Course}
-            />
-            <Route
-              path={ROUTES.COLLECTIONS.path + "/:collection/:questionId"}
-              component={Question}
-            />
-            <Route component={NotFound} />
-          </Switch>
-        </Container>
-      </Suspense>
-    </Router>
-  </ThemeProvider>
+						<Route path={ROUTES.SIGN_IN.path} component={SignIn} />
+						<Route path={ROUTES.SIGN_UP.path} component={SignUp} />
+						<Route
+							path={ROUTES.PASSWORD_FORGET.path}
+							component={PasswordForgot}
+						/>
+						<Route path={ROUTES.PLAYGROUND.path} component={Playground} />
+						<Route exact path={ROUTES.COLLECTIONS.path} component={Courses} />
+						<Route
+							exact
+							path={ROUTES.COLLECTIONS.path + "/:collection"}
+							component={Course}
+						/>
+						<Route
+							path={ROUTES.COLLECTIONS.path + "/:collection/:questionId"}
+							component={Question}
+						/>
+						<Route component={NotFound} />
+					</Switch>
+				</Container>
+			</Suspense>
+		</Router>
+	</ThemeProvider>
 );
 
 if (process.env.NODE_ENV === "production") {
-  Sentry.init({
-    dsn: "https://2cb4b0fa634941a69b5bdd868a07a024@sentry.io/1878459",
-  });
+	Sentry.init({
+		dsn: "https://2cb4b0fa634941a69b5bdd868a07a024@sentry.io/1878459",
+	});
 }
 
 export default withAuthentication(App);
