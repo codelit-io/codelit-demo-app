5db6ce170017f55c2e4a8a963bc6f282c84e4ac8
Mo Sharif <mo@mosh-media.com>
2020-06-16 14:26:11 -0700

    fix: lint

--- src/components/shared/SignInLink/index.js
+++ src/components/shared/SignInLink/index.js
@@ -4,9 +4,9 @@ import * as ROUTES from "constants/routes";
 import { Link } from "react-router-dom";
 
 const SignInLink = () => (
-	<p>
-		Don't have an account? <Link to={ROUTES.SIGN_IN.path}>Sign In</Link>
-	</p>
+  <p>
+    Don't have an account? <Link to={ROUTES.SIGN_IN.path}>Sign In</Link>
+  </p>
 );
 
 export default SignInLink;
