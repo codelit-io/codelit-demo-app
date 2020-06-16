fd50fc7153f4ba40bb92aa5517a4fce450c7f9b5
Mo Sharif <mo@mosh-media.com>
2020-06-15 22:51:40 -0700

    chore: upgrade to functional component

--- src/components/shared/Session/withAuthentication.js
+++ src/components/shared/Session/withAuthentication.js
@@ -1,45 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AuthUserContext from "./context";
 import { withFirebase } from "../Firebase";
 
 const withAuthentication = (Component) => {
-  class withAuthentication extends React.Component {
-    constructor(props) {
-      super(props);
+  const WithAuthentication = (props) => {
+    const [state, setState] = useState({
+      authUser: JSON.parse(localStorage.getItem("authUser")),
+    });
 
-      this.state = {
-        authUser: JSON.parse(localStorage.getItem("authUser")),
-      };
-    }
+    const { firebase } = props;
 
-    componentDidMount() {
-      this.listener =
-        this.props.firebase &&
-        this.props.firebase.onAuthUserListener(
-          (authUser) => {
-            localStorage.setItem("authUser", JSON.stringify(authUser));
-            this.setState({ authUser });
-          },
-          () => {
-            localStorage.removeItem("authUser");
-            this.setState({ authUser: null });
-          }
-        );
-    }
-
-    componentWillUnmount() {
-      this.listener();
-    }
-
-    render() {
-      return (
-        <AuthUserContext.Provider value={this.state.authUser}>
-          <Component {...this.props} authUser={this.state.authUser} />
-        </AuthUserContext.Provider>
+    useEffect(() => {
+      const listener = firebase.onAuthUserListener(
+        (authUser) => {
+          localStorage.setItem("authUser", JSON.stringify(authUser));
+          setState({ authUser });
+        },
+        () => {
+          localStorage.removeItem("authUser");
+          setState({ authUser: null });
+        }
       );
-    }
-  }
-  return withFirebase(withAuthentication);
+
+      return () => listener();
+    }, [firebase]);
+
+    return (
+      <AuthUserContext.Provider value={state.authUser}>
+        <Component {...props} authUser={state.authUser} />
+      </AuthUserContext.Provider>
+    );
+  };
+  return withFirebase(WithAuthentication);
 };
 
 export default withAuthentication;
