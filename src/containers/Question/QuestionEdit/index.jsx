7100a0a6d0dacfefe408a8467a13541ecc30dece
Mo Sharif <mo@mosh-media.com>
2020-06-15 22:52:39 -0700

    feat: add new features

--- src/containers/Question/QuestionEdit/index.jsx
+++ src/containers/Question/QuestionEdit/index.jsx
@@ -14,10 +14,10 @@ import * as ROLES from "constants/roles";
 
 import MoSnackbar from "components/library/MoSnackBar";
 import MoSpinner from "components/library/MoSpinner";
-import QuestionForm from "containers/Question/QuestionEditMode/QuestionForm";
+import QuestionForm from "containers/Question/QuestionEdit/QuestionForm";
 import withAuthorization from "components/shared/Session/withAuthorization";
 
-const QuestionEditMode = ({ authUser, firebase, history, match }) => {
+const QuestionEdit = ({ authUser, firebase, history, match }) => {
 	const [isLoading, setIsLoading] = useState(true);
 	const [question, setQuestion] = useState({});
 	const [isCorrect, setIsCorrect] = useState(false);
@@ -109,4 +109,4 @@ const QuestionEditMode = ({ authUser, firebase, history, match }) => {
 
 const condition = (authUser) => authUser && !!authUser.roles[ROLES.ADMIN];
 
-export default withAuthorization(condition)(QuestionEditMode);
+export default withAuthorization(condition)(QuestionEdit);
