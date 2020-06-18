8be69a72e3966fb03b59e8aad6f06f7b5fb13601
Mo Sharif <mo@mosh-media.com>
2020-06-18 00:09:47 -0700

    chore: lint

--- src/components/library/MoButton/styles.js
+++ src/components/library/MoButton/styles.js
@@ -1,26 +1,26 @@
 const styles = (theme) => ({
-	link: {
-		transition: "transform .35s",
-		backgroundColor: theme.palette.primary.main,
-		textDecoration: "none",
-		padding: "12px 22px",
-		color: theme.white?.medium,
-		fontSize: "1.1rem",
-		borderRadius: "3px",
-		textTransform: "initial",
-		fontWeight: "600",
-		"&:hover": {
-			background: theme.grey?.dark,
-		},
-		"&:hover .arrow": {
-			transform: "translateX(5px)",
-		},
-		icon: {
-			transition: "transform .35s",
-			paddingLeft: "4px",
-			paddingTop: "2px",
-		},
-	},
+  link: {
+    transition: "transform .35s",
+    backgroundColor: theme.palette.primary.main,
+    textDecoration: "none",
+    padding: "12px 22px",
+    color: theme.white?.medium,
+    fontSize: "1.1rem",
+    borderRadius: "3px",
+    textTransform: "initial",
+    fontWeight: "600",
+    "&:hover": {
+      background: theme.grey?.dark,
+    },
+    "&:hover .arrow": {
+      transform: "translateX(5px)",
+    },
+    icon: {
+      transition: "transform .35s",
+      paddingLeft: "4px",
+      paddingTop: "2px",
+    },
+  },
 });
 
 export default styles;
--- src/containers/Landing/index.js
+++ src/containers/Landing/index.js
@@ -4,12 +4,11 @@ import React from "react";
 
 import * as ROUTES from "constants/routes";
 
-
 import CheckIcon from "@material-ui/icons/Check";
 import Fade from "@material-ui/core/Fade";
 import Grid from "@material-ui/core/Grid";
-import Landing1 from "assets/landing1.png"
-import Landing2 from "assets/landing2.png"
+import Landing1 from "assets/landing1.png";
+import Landing2 from "assets/landing2.png";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
@@ -22,88 +21,88 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import Footer from "components/shared/Footer";
 
 const LandingPage = ({ classes }) => {
-	return (
-		<>
-			<Grid container spacing={4} className={classes.container}>
-				<Grid item sm={12} md={6} xs={12}>
-					<Fade in={true} mountOnEnter timeout={{ enter: 800 }} unmountOnExit>
-						<div className={classes.responsiveGrid}>
-							<MoPageHeader>Master React without the fees</MoPageHeader>
-							<MoPageSubtitle margin="36px 0 36px">
-								Introducing a Modern learning experience for everyone
-							</MoPageSubtitle>
-							<MoLinkButton
-								text="Get started"
-								href={ROUTES.EASY_COLLECTIONS.path}
-							/>
-						</div>
-					</Fade>
-				</Grid>
+  return (
+    <>
+      <Grid container spacing={4} className={classes.container}>
+        <Grid item sm={12} md={6} xs={12}>
+          <Fade in={true} mountOnEnter timeout={{ enter: 800 }} unmountOnExit>
+            <div className={classes.responsiveGrid}>
+              <MoPageHeader>Master React without the fees</MoPageHeader>
+              <MoPageSubtitle margin="36px 0 36px">
+                Introducing a Modern learning experience for everyone
+              </MoPageSubtitle>
+              <MoLinkButton
+                text="Get started"
+                href={ROUTES.EASY_COLLECTIONS.path}
+              />
+            </div>
+          </Fade>
+        </Grid>
 
-				<Grid item sm={12} md={6} xs={12}>
-					<img src={Landing1} className={classes.img} />
-				</Grid>
-			</Grid>
+        <Grid item sm={12} md={6} xs={12}>
+          <img src={Landing1} className={classes.img} />
+        </Grid>
+      </Grid>
 
-			<Grid container spacing={4} className={classes.container}>
-				<Grid item sm={12} md={6} xs={12}>
-					<MoPageHeader>Learn all about React</MoPageHeader>
-					<ListItem>
-						<ListItemAvatar className={classes.checkMark}>
-							<CheckIcon />
-						</ListItemAvatar>
-						<ListItemText primary="Basic HTML and usage in React" />
-					</ListItem>
-					<ListItem>
-						<ListItemAvatar className={classes.checkMark}>
-							<CheckIcon />
-						</ListItemAvatar>
-						<ListItemText primary="Styling components and elements in React" />
-					</ListItem>
-					<ListItem>
-						<ListItemAvatar className={classes.checkMark}>
-							<CheckIcon />
-						</ListItemAvatar>
-						<ListItemText primary="JavaScript functionality in React" />
-					</ListItem>
-					<ListItem>
-						<ListItemAvatar className={classes.checkMark}>
-							<CheckIcon />
-						</ListItemAvatar>
-						<ListItemText primary="React Hooks, Context API, and Redux" />
-					</ListItem>
-					<ListItem>
-						<ListItemAvatar className={classes.checkMark}>
-							<CheckIcon />
-						</ListItemAvatar>
-						<ListItemText primary="Best UI/UX practices" />
-					</ListItem>
-					<ListItem>
-						<ListItemAvatar className={classes.checkMark}>
-							<CheckIcon />
-						</ListItemAvatar>
-						<MoLink text="View All Courses" href={ROUTES.COLLECTIONS.path} />
-					</ListItem>
-				</Grid>
-				<Grid item sm={12} md={6} xs={12}>
-					<img src={Landing2} className={classes.img} />
-				</Grid>
-			</Grid>
-			<Grid container spacing={4} className={classes.container}>
-				<Grid item sm={12} md={6} xs={12}>
-					<MoPageHeader>Try the playground</MoPageHeader>
-					<MoPageSubtitle margin="36px 0 36px">
-						Write code in JSX and watch it render your code magically.
-					</MoPageSubtitle>
-					<MoLinkButton
-						text="Try the playground"
-						href={ROUTES.PLAYGROUND.path}
-					/>
-				</Grid>
-			</Grid>
-			<Footer />
-		</>
-	);
+      <Grid container spacing={4} className={classes.container}>
+        <Grid item sm={12} md={6} xs={12}>
+          <MoPageHeader>Learn all about React</MoPageHeader>
+          <ListItem>
+            <ListItemAvatar className={classes.checkMark}>
+              <CheckIcon />
+            </ListItemAvatar>
+            <ListItemText primary="Basic HTML and usage in React" />
+          </ListItem>
+          <ListItem>
+            <ListItemAvatar className={classes.checkMark}>
+              <CheckIcon />
+            </ListItemAvatar>
+            <ListItemText primary="Styling components and elements in React" />
+          </ListItem>
+          <ListItem>
+            <ListItemAvatar className={classes.checkMark}>
+              <CheckIcon />
+            </ListItemAvatar>
+            <ListItemText primary="JavaScript functionality in React" />
+          </ListItem>
+          <ListItem>
+            <ListItemAvatar className={classes.checkMark}>
+              <CheckIcon />
+            </ListItemAvatar>
+            <ListItemText primary="React Hooks, Context API, and Redux" />
+          </ListItem>
+          <ListItem>
+            <ListItemAvatar className={classes.checkMark}>
+              <CheckIcon />
+            </ListItemAvatar>
+            <ListItemText primary="Best UI/UX practices" />
+          </ListItem>
+          <ListItem>
+            <ListItemAvatar className={classes.checkMark}>
+              <CheckIcon />
+            </ListItemAvatar>
+            <MoLink text="View All Courses" href={ROUTES.COLLECTIONS.path} />
+          </ListItem>
+        </Grid>
+        <Grid item sm={12} md={6} xs={12}>
+          <img src={Landing2} className={classes.img} />
+        </Grid>
+      </Grid>
+      <Grid container spacing={4} className={classes.container}>
+        <Grid item sm={12} md={6} xs={12}>
+          <MoPageHeader>Try the playground</MoPageHeader>
+          <MoPageSubtitle margin="36px 0 36px">
+            Write code in JSX and watch it render your code magically.
+          </MoPageSubtitle>
+          <MoLinkButton
+            text="Try the playground"
+            href={ROUTES.PLAYGROUND.path}
+          />
+        </Grid>
+      </Grid>
+      <Footer />
+    </>
+  );
 };
 
 export default withStyles(styles)(LandingPage);
