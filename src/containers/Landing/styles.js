a8803f9428dccc90e01975be550dfcea97545d95
Mo Sharif <mo@mosh-media.com>
2020-06-18 01:22:18 -0700

    chore: lint

--- src/containers/Landing/styles.js
+++ src/containers/Landing/styles.js
@@ -1,22 +1,22 @@
 const styles = (theme) => ({
-	container: {
-		alignItems: "center",
-		paddingBottom: theme.space?.xl,
-		[theme.breakpoints.down("sm")]: {
-			paddingTop: theme.space?.lg,
-		},
-	},
-	responsiveGrid: {
-		[theme.breakpoints.down("sm")]: {
-			paddingBottom: theme.space?.md,
-		},
-	},
-	img: {
-		width: "100%",
-	},
-	checkMark: {
-		color: theme.green?.light,
-	},
+  container: {
+    alignItems: "center",
+    paddingBottom: theme.space?.xl,
+    [theme.breakpoints.down("sm")]: {
+      paddingTop: theme.space?.lg,
+    },
+  },
+  responsiveGrid: {
+    [theme.breakpoints.down("sm")]: {
+      paddingBottom: theme.space?.md,
+    },
+  },
+  img: {
+    width: "100%",
+  },
+  checkMark: {
+    color: theme.green?.light,
+  },
 });
 
 export default styles;
