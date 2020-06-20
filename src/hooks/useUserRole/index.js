745ff020e1c19c25d079587773bea4326b9a1ebb
Mo Sharif <mo@mosh-media.com>
2020-06-20 13:22:52 -0700

    refactor: move hooks to a new dir

--- src/hooks/useUserRole/index.js
+++ src/hooks/useUserRole/index.js
@@ -23,15 +23,15 @@
 import * as ROLES from "constants/roles";
 
 const useUserRole = (authUser) => {
-	if (!authUser) {
-		return { isLoggedIn: false };
-	}
+  if (!authUser) {
+    return { isLoggedIn: false };
+  }
 
-	return {
-		isAdmin: authUser.roles && authUser.roles[ROLES.ADMIN] ? true : false,
-		isAuthor: authUser.roles && authUser.roles[ROLES.AUTHOR] ? true : false,
-		isStudent: authUser.roles && authUser.roles[ROLES.STUDENT] ? true : false,
-	};
+  return {
+    isAdmin: authUser.roles && authUser.roles[ROLES.ADMIN] ? true : false,
+    isAuthor: authUser.roles && authUser.roles[ROLES.AUTHOR] ? true : false,
+    isStudent: authUser.roles && authUser.roles[ROLES.STUDENT] ? true : false,
+  };
 };
 
 export default useUserRole;
