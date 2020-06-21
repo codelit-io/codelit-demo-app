e9568c91a8246ec8f1eaf13f97e0ba766723e60d
Mo Sharif <mo@mosh-media.com>
2020-06-20 22:01:27 -0700

    chore: update styling

--- src/components/library/MoHint/styles.jsx
+++ src/components/library/MoHint/styles.jsx
@@ -1,8 +1,12 @@
-const styles = theme => ({
-  container: {
-    marginTop: theme.space?.md
-  },
-  text: { ...theme.editorFont, color: theme.palette.primary.main }
+const styles = (theme) => ({
+	container: {
+		marginTop: theme.space?.md,
+	},
+	text: {
+		...theme.editorFont,
+		color: theme.palette.primary.main,
+		marginBottom: theme.space?.md,
+	},
 });
 
 export default styles;
