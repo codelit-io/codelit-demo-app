f984b65481bb0e6f4f58f0dc15d29c1e3d3ccead
Mo Sharif <mo@mosh-media.com>
2020-06-21 09:16:40 -0700

    chore:  clean up unused code

--- src/components/shared/CodeEditor/index.js
+++ src/components/shared/CodeEditor/index.js
@@ -1,47 +1,147 @@
-import React, { lazy, useState, useEffect, Suspense } from "react";
+import React, { useEffect } from "react";
 
+import { addFocusOnEditor, getPreviewElement } from "./util";
+import { LiveEditor, LiveProvider, LivePreview, LiveError } from "react-live";
+import { lightTheme } from "utils/reactLiveTheme";
 import Grid from "@material-ui/core/Grid";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
-import useTheme from "@material-ui/core/styles/useTheme";
-import { retry } from "utils/retryLazyImports";
+import Fade from "@material-ui/core/Fade";
+// import Headline from "components/library/MoHeadline";
+import MoLinearProgress from "components/library/MoLinearProgress";
+import MoBrowserMockup from "components/library/MoBrowserMockup";
+import Typist from "react-typist";
+import createStyles from "@material-ui/core/styles/createStyles";
+import makeStyles from "@material-ui/core/styles/makeStyles";
 
-const LiveProviderCore = lazy(() => retry(() => import("./LiveProviderCore")));
-
-const CodeEditor = ({ handleOnChange, md, sm, matchPercent, question }) => {
-  const [state, setState] = useState(question);
-
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-
+const CodeEditorPage = ({
+  codeAnswer,
+  codeLanguage,
+  codeQuestion,
+  handleOnChange,
+  isMobile,
+  isPlayground,
+  md,
+  matchPercent,
+  sm
+}) => {
   useEffect(() => {
-    if (question) {
-      try {
-        /* Questions can contain special JSON characters that needs to be parsed */
-        const parseQuestion = JSON.parse(question.question);
-        setState({ ...question, question: parseQuestion });
-      } catch {
-        setState(question);
-      }
-    }
-  }, [question]);
+    addFocusOnEditor();
+  }, [codeQuestion]);
 
+  const onChange = userAnswer => {
+    const previewElement = getPreviewElement();
+    handleOnChange({ userAnswer, previewElement });
+  };
+
+  // TODO finish number of lines for editor
+  // Count newlines and pad to match actual line numbers
+  const lines = (codeQuestion?.match(/\n/g) || []).length + 2;
+  // Create content with all line numbers and newline them
+  const lineNos = [...Array(lines).keys()].slice(1).join("\\00000a");
+
+  const useStyles = makeStyles(theme =>
+    createStyles({
+      hint: {
+        ...theme.editorFont,
+        color: "#8e8e8e",
+        top: "1.25rem",
+        position: "absolute",
+        zIndex: "-1",
+        padding: "0.625rem"
+      },
+      liveEditor: {
+        overflow: "visible !important",
+        "&:before": {
+          color: theme.grey.light,
+          left: "-1.75rem",
+          fontFamily: "Inconsolata, monospace",
+          paddingTop: "0.6rem",
+          content: `"${lineNos}"`,
+          width: `1.25rem`,
+          position: "absolute",
+          whiteSpace: "pre",
+          textAlign: "right",
+          opacity: 1,
+          top: 0
+        }
+      }
+    })
+  );
+  const classes = useStyles();
   return (
     <Grid container spacing={2}>
-      <Suspense>
-        <LiveProviderCore
-          codeAnswer={state.answer}
-          codeLanguage={state.language}
-          codeQuestion={state.question}
-          handleOnChange={handleOnChange}
-          md={md}
-          sm={sm}
-          matchPercent={matchPercent}
-          isPlayground={state.isPlayground}
-          isMobile={isMobile}
-        />
-      </Suspense>
+      <LiveProvider
+        code={codeQuestion}
+        language={codeLanguage || "jsx"}
+        noInline={false}
+      >
+        <Grid item md={md} sm={sm} xs={12} style={{ width: "100%" }}>
+          <Fade
+            in={codeAnswer && true}
+            timeout={{ enter: 800 }}
+            mountOnEnter
+            unmountOnExit
+          >
+            <div>
+              <MoBrowserMockup
+                fileType={codeLanguage}
+                matchPercent={matchPercent}
+                isEditor={true}
+              >
+                <div>
+                  <LiveEditor
+                    onChange={e => onChange(e)}
+                    theme={lightTheme}
+                    className={classes.liveEditor}
+                  />
+                  {!codeQuestion && codeAnswer && (
+                    <div className={classes.hint}>
+                      <Typist
+                        avgTypingDelay={60}
+                        stdTypingDelay={30}
+                        startDelay={800}
+                        cursor={{
+                          show: true,
+                          blink: false,
+                          hideWhenDone: true
+                        }}
+                      >
+                        {codeAnswer}
+                      </Typist>
+                    </div>
+                  )}
+                </div>
+              </MoBrowserMockup>
+              {matchPercent && <MoLinearProgress percent={matchPercent} />}
+            </div>
+          </Fade>
+        </Grid>
+        <Grid item md={md} sm={sm} xs={12}>
+          <Fade
+            in={(isPlayground && true) || (codeAnswer && true)}
+            timeout={{ enter: 800 }}
+            mountOnEnter
+            unmountOnExit
+          >
+            <div>
+              <MoBrowserMockup fileType={codeLanguage} isEditor={false}>
+                {codeAnswer && <LivePreview className={classes.livePreview} />}
+              </MoBrowserMockup>
+            </div>
+          </Fade>
+        </Grid>
+        {isPlayground && (
+          <Grid item md={12} sm={12} xs={12}>
+            <LiveError />
+          </Grid>
+        )}
+        {/* 
+      TODO Add Headline effect
+      <Grid item md={12} sm={12} xs={12}>
+        <Headline isCorrect={question && question.isCorrect} />
+      </Grid> */}
+      </LiveProvider>
     </Grid>
   );
 };
 
-export default CodeEditor;
+export default CodeEditorPage;
