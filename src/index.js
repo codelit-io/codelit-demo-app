0f470d121ff04be9e0b07c74cabd8d8e0c5563cd
Mo Sharif <mo@mosh-media.com>
2020-06-24 15:40:31 -0700

    feat: lazy load App

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-import React from "react";
+/**
+ *
+ *
+ * @author MoSkool
+ * @version 1.0.0
+ * @visibleName Index.js 🥇
+ *
+ *
+ * Entry component to the app and provides the App component with Firebase class instance
+ */
+
+import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import Firebase, { FirebaseContext } from "./components/shared/Firebase";
+import { retry } from "utils/retryLazyImports";
+import MoSpinner from "components/library/MoSpinner";
+
+const App = lazy(() => retry(() => import("./App")));
 
 ReactDOM.render(
   <FirebaseContext.Provider value={new Firebase()}>
-    <App />
+    <Suspense fallback={<MoSpinner isLoading={true} color="primary" />}>
+      <App />
+    </Suspense>
   </FirebaseContext.Provider>,
   document.getElementById("root")
 );
