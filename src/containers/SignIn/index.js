eca70b6b7664f02e6e529237dddca5ac5f10b24a
Mo Sharif <mo@mosh-media.com>
2020-07-01 21:31:34 -0700

    fix: clean up message for authUser

--- src/containers/SignIn/index.js
+++ src/containers/SignIn/index.js
@@ -1,36 +1,36 @@
 import React from "react";
 
-import { AuthUserContext } from "components/shared/Session";
+import * as ROUTES from "constants/routes";
+import { withAuthentication } from "components/shared/Session";
 import Grid from "@material-ui/core/Grid";
 import MoPage from "components/library/MoPage";
 import SocialSignIn from "components/shared/SocialSignIn";
 import SignInForm from "./SignInForm";
+import MoLink from "components/library/MoLink";
 
-const SignInPage = () => (
-  <AuthUserContext.Consumer>
-    {authUser => (
-      <MoPage
-        img=""
-        title={authUser ? "You are logged in 👍" : "Welcome Back 🤩"}
-        isLoading={false}
-      >
-        <Grid container spacing={3}>
-          {authUser ? (
-            ""
-          ) : (
-            <>
-              <Grid item sm={6} md={6}>
-                <SocialSignIn />
-              </Grid>
-              <Grid item sm={6} md={6}>
-                <SignInForm />
-              </Grid>
-            </>
-          )}
+const SignInPage = ({ authUser }) => (
+  <MoPage
+    img=""
+    title={authUser ? "You are logged in 👍" : "Welcome Back 🤩"}
+    isLoading={false}
+  >
+    <Grid container spacing={3}>
+      {authUser ? (
+        <Grid item sm={12} md={12}>
+          <MoLink text="View All Courses" href={ROUTES.COLLECTIONS.path} />
         </Grid>
-      </MoPage>
-    )}
-  </AuthUserContext.Consumer>
+      ) : (
+        <>
+          <Grid item sm={6} md={6}>
+            <SocialSignIn />
+          </Grid>
+          <Grid item sm={6} md={6}>
+            <SignInForm />
+          </Grid>
+        </>
+      )}
+    </Grid>
+  </MoPage>
 );
 
-export default SignInPage;
+export default withAuthentication(SignInPage);
--- src/containers/SignUp/index.js
+++ src/containers/SignUp/index.js
@@ -1,40 +1,36 @@
 import React from "react";
 
 import * as ROUTES from "constants/routes";
-import { AuthUserContext } from "components/shared/Session";
+import { withAuthentication } from "components/shared/Session";
 import Grid from "@material-ui/core/Grid";
 import MoPage from "components/library/MoPage";
 import MoLink from "components/library/MoLink";
 import SocialSignIn from "components/shared/SocialSignIn";
 import SignUpForm from "./SignUpForm";
 
-const SignUpPage = () => (
-  <AuthUserContext.Consumer>
-    {authUser => (
-      <MoPage
-        img=""
-        title={authUser ? "You are signed up 👍" : "Sign up to get started 😎"}
-        isLoading={false}
-      >
-        <Grid container spacing={3}>
-          {authUser ? (
-            <Grid item sm={12} md={12}>
-              <MoLink text="View All Courses" href={ROUTES.COLLECTIONS.path} />
-            </Grid>
-          ) : (
-            <>
-              <Grid item sm={6} md={6}>
-                <SocialSignIn />
-              </Grid>
-              <Grid item sm={6} md={6}>
-                <SignUpForm />
-              </Grid>
-            </>
-          )}
+const SignUpPage = ({ authUser }) => (
+  <MoPage
+    img=""
+    title={authUser ? "You are signed up 👍" : "Sign up to get started 😎"}
+    isLoading={false}
+  >
+    <Grid container spacing={3}>
+      {authUser ? (
+        <Grid item sm={12} md={12}>
+          <MoLink text="View All Courses" href={ROUTES.COLLECTIONS.path} />
         </Grid>
-      </MoPage>
-    )}
-  </AuthUserContext.Consumer>
+      ) : (
+        <>
+          <Grid item sm={6} md={6}>
+            <SocialSignIn />
+          </Grid>
+          <Grid item sm={6} md={6}>
+            <SignUpForm />
+          </Grid>
+        </>
+      )}
+    </Grid>
+  </MoPage>
 );
 
-export default SignUpPage;
+export default withAuthentication(SignUpPage);
