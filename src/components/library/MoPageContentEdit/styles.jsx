cde0ae39256637bf8d4f7cadeeb5b5ee6502fc68
Mo Sharif <mo@mosh-media.com>
2020-07-03 15:55:44 -0700

    feat: update component

---
+++ src/components/library/MoPageContentEdit/styles.jsx
@@ -0,0 +1,13 @@
+const styles = (theme) => ({
+	text: {
+		color: theme.grey?.medium,
+		fontFamily: theme.openSans,
+		textDecoration: "none",
+		marginBottom: theme.space?.md,
+		width: "100%",
+		border: "none",
+		outline: "none"
+	}
+});
+
+export default styles;
