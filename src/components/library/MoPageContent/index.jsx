3eded323158c9a9f5188165d11132ab9e1085066
Mo Sharif <mo@mosh-media.com>
2020-07-03 15:54:54 -0700

    feat: update mopage content component

---
+++ src/components/library/MoPageContent/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import withStyles from "@material-ui/core/styles/withStyles";
+import Fade from "@material-ui/core/Fade";
+import styles from "./styles";
+
+const MoPageContent = ({ classes, children, text }) => (
+	<Fade
+		in={(text || children) && true}
+		mountOnEnter
+		timeout={{ enter: 200, exit: 800 }}
+		unmountOnExit
+	>
+		<Typography variant="h6">
+			<Box fontWeight="fontWeightLight" className={classes.text}>
+				{text} {children}
+			</Box>
+		</Typography>
+	</Fade>
+);
+
+export default withStyles(styles)(MoPageContent);
---
+++ src/components/library/MoPageContent/styles.jsx
@@ -0,0 +1,10 @@
+const styles = (theme) => ({
+	text: {
+		color: theme.grey?.medium,
+		fontFamily: theme.openSans,
+		textDecoration: "none",
+		marginBottom: theme.space?.md
+	}
+});
+
+export default styles;
