ceec863eed2f8afc454d21df727e7a29f737aeb5
Mo Sharif <mo@mosh-media.com>
2020-07-04 15:04:40 -0700

    chore: update snack bar component

--- src/components/library/MoSnackBar/index.jsx
+++ src/components/library/MoSnackBar/index.jsx
@@ -9,14 +9,13 @@
  * @param {Object} authUser - Passed from parent container and has everything about the logged in user
  * @param {Object} classes - Class names that has styling details for elements - used with Material-UI
  * @param {CallableFunction} handleClick - Callback function when clicking the snackbar button
- * @param {Boolean} isActive - Flag to trigger the snackbar
- * @param {Object} snackbarProps - Contains snackbar title and button text. eg {title: string, buttonText: string, buttonIcon: React Element/Component}
+ * @param {Object} snackbarProps - Contains snackbar title and button text. eg {isActive: boolean, buttonText: string, buttonIcon: React Element/Component, title: string}
  * @withStyle - HOC provides classes object to component for styling
  *
  * @see See [Material IU Snackbar](https://material-ui.com/components/snackbars/#snackbar)
  * */
 
-import React from "react";
+import React, { useEffect } from "react";
 
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import Slide from "@material-ui/core/Slide";
@@ -28,17 +27,15 @@ import MoButton from "components/library/MoButton";
 import propTypes from "prop-types";
 import styles from "./styles";
 
-const MoSnackBar = ({
-	authUser,
-	classes,
-	handleClick,
-	isActive,
-	snackbarProps
-}) => {
+const MoSnackBar = ({ authUser, classes, handleClick, snackbarProps }) => {
 	const [state, setState] = React.useState({
-		isActive,
+		isActive: snackbarProps.isActive,
 		Transition: Slide
 	});
+
+	useEffect(() => {
+		setState({ isActive: snackbarProps?.isActive });
+	}, [snackbarProps]);
 
 	const handleButtonClick = () => {
 		setState({
@@ -58,7 +55,8 @@ const MoSnackBar = ({
 
 	return (
 		<Snackbar
-			open={isActive}
+			autoHideDuration={snackbarProps.autoHideDuration}
+			open={state.isActive}
 			onClose={handleClose}
 			TransitionComponent={state.Transition}
 			anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
@@ -67,18 +65,17 @@ const MoSnackBar = ({
 				message={
 					<div className={classes.message}>
 						<CheckCircleIcon className={classes.checkIcon} />
-						<MoTitle
-							text={snackbarProps && snackbarProps.title}
-							fade
-							margin="0 36px 0 0"
-						/>
+						<MoTitle text={snackbarProps.title} fade margin="0 36px 0 0" />
 					</div>
 				}
 				className={classes.snackbarContent}
 				action={
-					<MoButton handleButtonClick={() => handleButtonClick()} text="Next">
-						{snackbarProps && snackbarProps.buttonText}
-						{snackbarProps && snackbarProps.buttonIcon}
+					<MoButton
+						handleButtonClick={() => handleButtonClick()}
+						text={snackbarProps.buttonText}
+					>
+						{snackbarProps.buttonText}
+						{snackbarProps.buttonIcon}
 					</MoButton>
 				}
 			/>
@@ -90,11 +87,12 @@ MoSnackBar.propTypes = {
 	authUser: propTypes.object,
 	classes: propTypes.object,
 	handleClick: propTypes.func.isRequired,
-	isActive: propTypes.bool.isRequired,
 	snackbarProps: propTypes.shape({
-		title: propTypes.string,
+		autoHideDuration: propTypes.oneOf([1000, 2000, 3000, 4000, 5000, null]),
 		buttonText: propTypes.string,
-		buttonIcon: propTypes.object
+		buttonIcon: propTypes.object,
+		isActive: propTypes.bool.isRequired,
+		title: propTypes.string
 	}).isRequired
 };
 export default withStyles(styles)(MoSnackBar);
