aa5eb65b6752e267a0f6246f6ddc159c7759b930
Mo Sharif <mo@mosh-media.com>
2020-07-04 22:20:32 -0700

    chore: clean up component

--- src/App/index.test.js
+++ src/App/index.test.js
@@ -1,8 +1,8 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import App from "./index";
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from './index';
 
-it("Should render App", () => {
+it('Should render App', () => {
   const tree = renderer.create(<App />).toJSON();
   expect(tree).toMatchSnapshot();
 });
--- src/App/theme.js
+++ src/App/theme.js
@@ -1,96 +1,96 @@
-import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
-import responsiveFontSizes from "@material-ui/core/styles/responsiveFontSizes";
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import responsiveFontSizes from '@material-ui/core/styles/responsiveFontSizes';
 
 const bigShadow =
-  "0 24px 24px -18px rgba(69, 104, 129, 0.19), 0 9px 45px 0 rgba(114, 119, 160, 0.06)";
+  '0 24px 24px -18px rgba(69, 104, 129, 0.19), 0 9px 45px 0 rgba(114, 119, 160, 0.06)';
 
 const colors = {
-  primary: "#3399bb",
-  secondary: "#99bb33",
-  tertiary: "#424B54",
+  primary: '#3399bb',
+  secondary: '#99bb33',
+  tertiary: '#424B54',
   grey: {
-    superLight: "#eeeeee",
-    light: "#a6a6a6",
-    medium: "#424B54",
-    mediumDark: "#747474",
-    dark: "#383c40",
-    darkest: "#2b2b2b"
+    superLight: '#eeeeee',
+    light: '#a6a6a6',
+    medium: '#424B54',
+    mediumDark: '#747474',
+    dark: '#383c40',
+    darkest: '#2b2b2b',
   },
   green: {
-    light: "#41D3BD",
-    medium: "#99bb33",
-    dark: "#157A6E",
-    darkest: ""
+    light: '#41D3BD',
+    medium: '#99bb33',
+    dark: '#157A6E',
+    darkest: '',
   },
   yellow: {
-    light: "#F5D547",
-    dark: "",
-    darkest: ""
+    light: '#F5D547',
+    dark: '',
+    darkest: '',
   },
   white: {
-    medium: "#FFFFF2"
+    medium: '#FFFFF2',
   },
   blue: {
-    superLight: "#e0e3ea",
-    medium: "#276ef1"
-  }
+    superLight: '#e0e3ea',
+    medium: '#276ef1',
+  },
 };
 
 const space = {
-  tiny: "0.1rem",
-  xs: "0.25rem",
-  sm: "0.75rem",
-  sm1: "1rem",
-  sm2: "1.25rem",
-  md: "1.5rem",
-  md1: "2rem",
-  md2: "2.25rem",
-  lg: "3rem",
-  xl: "6rem"
+  tiny: '0.1rem',
+  xs: '0.25rem',
+  sm: '0.75rem',
+  sm1: '1rem',
+  sm2: '1.25rem',
+  md: '1.5rem',
+  md1: '2rem',
+  md2: '2.25rem',
+  lg: '3rem',
+  xl: '6rem',
 };
 
 const fontSize = {
-  xs: "1rem",
-  sm: "1.5rem",
-  md: "2rem",
-  lg: "3rem",
-  xl: "4rem"
+  xs: '1rem',
+  sm: '1.5rem',
+  md: '2rem',
+  lg: '3rem',
+  xl: '4rem',
 };
 
 const fontFamily = {
-  MonoSpace: "monospace",
-  OpenSans: "Open Sans",
-  BreeSerif: "Bree Serif"
+  MonoSpace: 'monospace',
+  OpenSans: 'Open Sans',
+  BreeSerif: 'Bree Serif',
 };
 
 let theme = createMuiTheme({
   bigShadow,
   card: {
     boxShadow: bigShadow,
-    "&:hover": {
-      transform: "translateY(-5px)",
-      backgroundColor: "#FFF"
+    '&:hover': {
+      transform: 'translateY(-5px)',
+      backgroundColor: '#FFF',
     },
-    "&:hover .desc": {
-      color: "white"
+    '&:hover .desc': {
+      color: 'white',
     },
     borderRadius: space.sm,
     transition:
-      "transform .35s cubic-bezier(.4,0,.2,1),box-shadow .35s cubic-bezier(.4,0,.2,1)",
-    transform: "translateY(0)",
-    background: "white"
+      'transform .35s cubic-bezier(.4,0,.2,1),box-shadow .35s cubic-bezier(.4,0,.2,1)',
+    transform: 'translateY(0)',
+    background: 'white',
   },
   container: {
-    width: "calc(100vw - 28.799999999999997rem)"
+    width: 'calc(100vw - 28.799999999999997rem)',
   },
   editorFont: {
     fontFamily: fontFamily.MonoSpace,
-    fontSize: "16px",
-    color: "rgb(77, 77, 76)"
+    fontSize: '16px',
+    color: 'rgb(77, 77, 76)',
   },
   flexAlignCenter: {
-    display: "flex",
-    alignItems: "center"
+    display: 'flex',
+    alignItems: 'center',
   },
   fontSize,
   space,
@@ -102,28 +102,28 @@ let theme = createMuiTheme({
   typography: {
     button: {
       borderRadius: space.sm,
-      textTransform: "none"
+      textTransform: 'none',
     },
-    fontFamily: fontFamily.BreeSerif
+    fontFamily: fontFamily.BreeSerif,
   },
   breeSerif: fontFamily.BreeSerif,
   openSans: fontFamily.OpenSans,
   palette: {
     primary: {
       main: colors.primary,
-      light: colors.secondary
+      light: colors.secondary,
     },
     secondary: {
-      main: colors.secondary
+      main: colors.secondary,
     },
     tertiary: {
-      main: colors.tertiary
-    }
-  }
+      main: colors.tertiary,
+    },
+  },
 });
 
 const fontOptions = {
-  breakpoints: ["sm", "md", "lg", "xl"]
+  breakpoints: ['sm', 'md', 'lg', 'xl'],
 };
 
 theme = responsiveFontSizes(theme, fontOptions);
