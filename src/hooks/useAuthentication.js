eb3e9bfd6aecf83d07aa34f057e8fb5044e0c7e6
Mo Sharif <mo@mosh-media.com>
2020-07-08 19:16:28 -0700

    feat: create authUser hook

---
+++ src/hooks/useAuthentication.js
@@ -0,0 +1,52 @@
+/**
+ *
+ * @author MoSkool
+ * @version 1.0.0
+ * @visibleName useAuthentication custom hook 💁‍♂️
+ *
+ * Custom Hook to get authUser object from firebase
+ *
+ * @param {Object} firebase - Firebase class provides access to authUser and db - comes from withAuthentication hoc
+ * @returns {isLoading: boolean, isError: Object, authUser: Object} - returns loading boolean, error Object and authUser Object
+ *
+ * @example
+ *
+ * Example Usage
+ *
+ * ```
+ * const { authUser } = useAuthentication(
+ *   firebase
+ * );
+ * ```
+ * @see See [React custom hooks](https://reactjs.org/docs/hooks-custom.html)
+ */
+
+import { useEffect, useState } from "react";
+
+const useAuthentication = firebase => {
+  const [state, setState] = useState({
+    authUser: JSON.parse(localStorage.getItem("authUser")),
+    isLoading: true
+  });
+
+  useEffect(() => {
+    const listener = firebase.onAuthUserListener(
+      authUser => {
+        localStorage.setItem("authUser", JSON.stringify(authUser));
+        setState({ authUser, isLoading: false });
+      },
+      error => {
+        localStorage.removeItem("authUser");
+        setState({ authUser: null, isLoading: false, isError: error });
+      }
+    );
+
+    return () => {
+      listener();
+    };
+  }, [firebase]);
+
+  return { ...state };
+};
+
+export default useAuthentication;
