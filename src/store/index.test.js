a707528d79fbf5bc921b37f35d58d3b2ac7b07af
Mo Sharif <mo@mosh-media.com>
2020-07-18 11:40:48 -0700

    feat: code editor theme updates

--- src/store/index.test.js
+++ src/store/index.test.js
@@ -1,18 +1,13 @@
 import React from "react";
-import CodeEditor from "./index";
 
 // Dependencies under test
-import ShallowRenderer from "react-test-renderer/shallow";
 import useGlobal from "store";
 import useGlobalHook from "use-global-hook";
 
 import { renderHook } from "@testing-library/react-hooks";
 
 import { initialState } from "store/initialState";
-import actions from "actions";
-
-// Shallow render to test the component without it's children
-const renderer = new ShallowRenderer();
+import actions from "../actions";
 
 beforeEach(() => {
   useGlobal.mockClear();
@@ -27,12 +22,7 @@ afterEach(() => {
 jest.mock("store", () => jest.fn().mockReturnValue([]));
 jest.mock("use-global-hook", () => jest.fn());
 
-describe("CodeEditor Component", () => {
-  test("should match the snapshot", () => {
-    renderer.render(<CodeEditor />);
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
-  });
-
+describe("store", () => {
   test("Should import and invoke useGlobal", () => {
     // Call useGlobal hook
     const { result, waitForNextUpdate } = renderHook(() =>
