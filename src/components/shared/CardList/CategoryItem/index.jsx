e2b83018b6d588fc3a61e9c4d943ff3c552a8ab8
Mo Sharif <mo@mosh-media.com>
2020-07-21 11:15:58 -0700

    feat: create shared components for cards

---
+++ src/components/shared/CardList/CategoryItem/index.jsx
@@ -0,0 +1,38 @@
+/**
+ *
+ * @author MoSkool
+ * @version 1.0.0
+ * @visibleName Category Item
+ *
+ * Category for an item is displayed on top of the card representing the category of the cards below it
+ *
+ * @param {String} index - index of the item
+ * @param {String} text - category string
+ * @returns {<MoTypography/>} - returns Typography component a formatted text
+ *
+ * @see See [React hoc](https://reactjs.org/docs/higher-order-components.html)
+ * */
+import React from "react";
+
+// Mo Skool library components
+import MoTypography from "components/library/MoTypography";
+
+// MUI components
+import Grid from "@material-ui/core/Grid";
+import Grow from "@material-ui/core/Grow";
+
+const CategoryItem = ({ index, text }) =>
+  text ? (
+    <Grow in={text && true} mountOnEnter timeout={{ enter: 200 }} unmountOnExit>
+      <Grid item xs={12} sm={12} md={12} lg={12}>
+        <MoTypography
+          font="breeSerif"
+          marginTop={index > 1 ? "md" : null}
+          text={text}
+          variant="h6"
+        ></MoTypography>
+      </Grid>
+    </Grow>
+  ) : null;
+
+export default React.memo(CategoryItem);
---
+++ src/components/shared/CardList/index.jsx
@@ -0,0 +1,60 @@
+/**
+ *
+ * @author MoSkool
+ * @version 1.0.0
+ * @visibleName Card List Component
+ *
+ * Loops over an item list of questions or courses and then renders components with specific logic
+ *
+ * @param {Object} authUser - Passed from parent container and has everything about the logged in user
+ *
+ * @returns {<MoTypography/>} - returns Typography component a formatted text
+ *
+ * @see See [React hoc](https://reactjs.org/docs/higher-order-components.html)
+ * */
+
+import React, { lazy } from "react";
+
+import itemTypes from "./itemTypes";
+import SignUpCard from "./SignUpCard";
+import NewItemCard from "./NewItemCard";
+
+const CardItem = lazy(() => import("./CardItem"));
+const CategoryItem = lazy(() => import("./CategoryItem"));
+
+const CardList = ({
+  authUser,
+  itemUrl,
+  isAdmin,
+  isItemDisabled,
+  items,
+  points
+}) => {
+  return items.map((item, index) => {
+    const isDisabled = isItemDisabled(item.id);
+    //Configure url route for each item
+    const configureUrl = isDisabled ? "" : itemUrl(item.id);
+
+    const IconComponent = item.isDisabled
+      ? itemTypes.disabled
+      : itemTypes[item.type];
+    return (
+      <React.Fragment key={index}>
+        <CategoryItem index={index} text={item?.category} />
+        <NewItemCard isActive={item.id <= 1 && !!isAdmin} type="new" />
+        <SignUpCard isActive={item.id <= 1 && !authUser} type="signup" />
+        <CardItem
+          IconComponent={IconComponent}
+          index={index}
+          isDisabled={isDisabled}
+          subtitle={item.subtitle}
+          title={item.title}
+          type={item.type}
+          url={item.url || configureUrl}
+        />
+      </React.Fragment>
+    );
+  });
+};
+
+export default CardList;
