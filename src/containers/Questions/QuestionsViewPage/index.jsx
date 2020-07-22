cf3ad823cb4653855002db9b4bcaff79b0eec728
Mo Sharif <mo@mosh-media.com>
2020-07-21 19:30:07 -0700

    feat: add new routing for editing questions page

---
+++ src/containers/Questions/QuestionsViewPage/index.jsx
@@ -0,0 +1,80 @@
+/**
+ *
+ * @author MoSkool
+ * @version 1.0.0
+ * @visibleName Questions Container 🎒
+ *
+ * A Container that fetches firebase data using hooks and renders cards of questions
+ *
+ * @param {Object} firebase - Firebase class provides access to authUser and db - comes from withAuthentication hoc
+ * @param {Object} match - Contains information about how a <Route path> matched the URL - comes from withRouter and passed to withAuthentication hoc
+ * @withAuthentication - HOC provides firebase and match props
+ * @returns - returns a lesson list on the left column and course tracking info on the right column
+ *
+ * @see Link [Questions Page](https://moskool.com/courses/mo-easy)
+ *
+ */
+
+import React, { lazy, useEffect, useState } from "react";
+
+import { withAuthentication } from "components/shared/Session";
+import useCollectionDetails from "hooks/useCollectionDetails";
+import useCollections from "hooks/useCollections";
+import useUserRole from "hooks/useUserRole";
+
+const QuestionsPage = lazy(() => import("./QuestionsPage"));
+
+const Questions = ({ authUser, firebase, match }) => {
+  const userRole = useUserRole(authUser);
+  const [points, setPoints] = useState(0);
+  const doc = match.params.collection;
+  const collectionPath = "courses/" + doc + "/questions";
+  const collectionDetailsPath = "courses";
+  const newItemUrl = `${doc}/`;
+  // Configure url route for each item
+  const itemUrl = id => `${doc}/${id}`;
+  // isItemDisabled is configured based on points and question's id
+  const isItemDisabled = id =>
+    points ? points < Number(id) - 1 : Number(id) !== 1;
+
+  useEffect(() => {
+    setPoints(authUser?.reports?.[doc]?.points);
+  }, [authUser, doc]);
+
+  // Get details about a course/questions
+  const courseDetails = useCollectionDetails(
+    { collectionPath: collectionDetailsPath },
+    doc,
+    firebase
+  );
+
+  // Get list of questions
+  const questions = useCollections(
+    {
+      collectionPath,
+      data: []
+    },
+    firebase
+  );
+
+  if (!questions?.data || !courseDetails.data) {
+    return null;
+  }
+
+  return (
+    <QuestionsPage
+      authUser={authUser}
+      questions={questions.data}
+      courseDetails={courseDetails}
+      hasData={questions.data.length && true}
+      isItemDisabled={id => isItemDisabled(id)}
+      isAdmin={userRole.isAdmin}
+      isLoading={questions.data.isLoading && false}
+      itemUrl={id => itemUrl(id)}
+      doc={doc}
+      newItemUrl={newItemUrl}
+      points={points}
+    />
+  );
+};
+export default withAuthentication(Questions);
