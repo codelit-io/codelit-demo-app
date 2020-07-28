91335d6c94eafc7918cb87e0d4ae4d87154a3dbb
Mo Sharif <mo@mosh-media.com>
2020-07-27 18:24:02 -0700

    feat: update footer styling

--- src/components/shared/Footer/styles.js
+++ src/components/shared/Footer/styles.js
@@ -7,6 +7,13 @@ const styles = theme => ({
   },
   footerButton: {
     textTransform: "initial"
+  },
+  footer: {
+    zIndex: "10",
+    position: "fixed",
+    bottom: 0,
+    right: 0,
+    width: "100%"
   }
 });
 
